abf367bfb16e140d3b64df0f2eed9ce657a9f986
Trustin Lee <trustin@linecorp.com>
2016-07-15 19:04:17 +0900

    Use assertj instead of Hamcrest

--- src/test/java/com/linecorp/armeria/client/circuitbreaker/EventCountTest.java
+++ src/test/java/com/linecorp/armeria/client/circuitbreaker/EventCountTest.java
@@ -16,9 +16,8 @@
 
 package com.linecorp.armeria.client.circuitbreaker;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.Test;
 
@@ -26,59 +25,41 @@ public class EventCountTest {
 
     @Test
     public void testCounts() {
-        assertThat(new EventCount(0, 0).success(), is(0L));
-        assertThat(new EventCount(1, 0).success(), is(1L));
-        assertThat(new EventCount(1, 1).success(), is(1L));
-        assertThat(new EventCount(0, 0).failure(), is(0L));
-        assertThat(new EventCount(0, 1).failure(), is(1L));
-        assertThat(new EventCount(1, 1).failure(), is(1L));
-        assertThat(new EventCount(0, 0).total(), is(0L));
-        assertThat(new EventCount(1, 1).total(), is(2L));
+        assertThat(new EventCount(0, 0).success()).isEqualTo(0L);
+        assertThat(new EventCount(1, 0).success()).isEqualTo(1L);
+        assertThat(new EventCount(1, 1).success()).isEqualTo(1L);
+        assertThat(new EventCount(0, 0).failure()).isEqualTo(0L);
+        assertThat(new EventCount(0, 1).failure()).isEqualTo(1L);
+        assertThat(new EventCount(1, 1).failure()).isEqualTo(1L);
+        assertThat(new EventCount(0, 0).total()).isEqualTo(0L);
+        assertThat(new EventCount(1, 1).total()).isEqualTo(2L);
     }
 
     @Test
     public void testRates() {
-        try {
-            new EventCount(0, 0).successRate();
-            fail();
-        } catch (ArithmeticException e) {
-            // Expected
-        }
-        assertThat(new EventCount(1, 0).successRate(), is(1.0));
-        assertThat(new EventCount(1, 1).successRate(), is(0.5));
+        assertThatThrownBy(() -> new EventCount(0, 0).successRate()).isInstanceOf(ArithmeticException.class);
 
-        try {
-            new EventCount(0, 0).failureRate();
-            fail();
-        } catch (ArithmeticException e) {
-            // Expected
-        }
-        assertThat(new EventCount(0, 1).failureRate(), is(1.0));
-        assertThat(new EventCount(1, 1).failureRate(), is(0.5));
+        assertThat(new EventCount(1, 0).successRate()).isEqualTo(1.0);
+        assertThat(new EventCount(1, 1).successRate()).isEqualTo(0.5);
+
+        assertThatThrownBy(() -> new EventCount(0, 0).failureRate()).isInstanceOf(ArithmeticException.class);
+
+        assertThat(new EventCount(0, 1).failureRate()).isEqualTo(1.0);
+        assertThat(new EventCount(1, 1).failureRate()).isEqualTo(0.5);
     }
 
     @Test
     public void testInvalidArguments() {
-        try {
-            new EventCount(-1, 0);
-            fail();
-        } catch (AssertionError e) {
-            // Expected
-        }
-        try {
-            new EventCount(0, -1);
-            fail();
-        } catch (AssertionError e) {
-            // Expected
-        }
+        assertThatThrownBy(() -> new EventCount(-1, 0)).isInstanceOf(AssertionError.class);
+        assertThatThrownBy(() -> new EventCount(0, -1)).isInstanceOf(AssertionError.class);
     }
 
     @Test
     public void testEquals() {
         EventCount ec = new EventCount(1, 1);
-        assertThat(ec, is(ec));
-        assertThat(new EventCount(0, 0).equals(new EventCount(0, 0)), is(true));
-        assertThat(new EventCount(1, 0).equals(new EventCount(0, 0)), is(false));
-        assertThat(new EventCount(1, 0).equals(new Object()), is(false));
+        assertThat(ec).isEqualTo(ec);
+        assertThat(new EventCount(0, 0)).isEqualTo(new EventCount(0, 0));
+        assertThat(new EventCount(1, 0)).isNotEqualTo(new EventCount(0, 0));
+        assertThat(new EventCount(1, 0)).isNotEqualTo(new Object());
     }
 }
