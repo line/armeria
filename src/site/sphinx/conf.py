17e1a14eb9ef0a95cbdb1e39674773cac96f43dc
Trustin Lee <trustin@linecorp.com>
2016-08-18 18:52:36 +0900

    Update the latest version

--- src/site/sphinx/conf.py
+++ src/site/sphinx/conf.py
@@ -36,8 +36,8 @@ copyright = str(date.today().year) + ', ' + pom['organization']['name']
 # Set the project version and release.
 # Use the last known stable release if the current version ends with '-SNAPSHOT'.
 if re.match(r'^.*-SNAPSHOT$', pom['version']):
-    release = '0.21.6.Final'
-    version = '0.21'
+    release = '0.22.0.Final'
+    version = '0.22'
 else:
     release = pom['version']
     version = re.match(r'^[0-9]+\.[0-9]+', pom['version']).group(0)
