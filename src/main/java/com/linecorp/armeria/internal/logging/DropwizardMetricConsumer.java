line/armeria

commit c2dad254f19f5b3eccec987efde850b34eac9e32

Improve Dropwizard Metrics metric name generation
Related: #278 #261

Motivation:

There's currently no way to customize the metric names of the Dropwizard
Metrics integration.

Modifications:

- Add PathMapping.loggerName() and metricName() so that PathMapping
  implementation has the control over the preferred metric name
- Allow DropwizardMetricCollecting(Client|Service) accept a function
  that transforms a RequestLog into a metric name so that a user is
  given with much more freedom
- PathMapping does not extend Function anymore.
- PathMapping is not a functional interface anymore.
- Change the client-side default metric name to `<method>` to address #278
- Change the server-side default metric name to `<pathMapping.metricName>.<method>`

Result:

- Customizable metric name generation
- More sensible default metric names

src/main/java/com/linecorp/armeria/internal/logging/DropwizardMetricConsumer.java
@@ -20,14 +20,13 @@ import static java.util.Objects.requireNonNull;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.BiFunction;
 
 import com.codahale.metrics.MetricRegistry;
-import com.google.common.base.MoreObjects;
 
 import com.linecorp.armeria.client.logging.LogCollectingClient;
 import com.linecorp.armeria.common.RequestContext;
 import com.linecorp.armeria.common.SessionProtocol;
-import com.linecorp.armeria.common.http.HttpHeaders;
 import com.linecorp.armeria.common.logging.MessageLogConsumer;
 import com.linecorp.armeria.common.logging.RequestLog;
 import com.linecorp.armeria.common.logging.ResponseLog;
@@ -40,22 +39,23 @@ import com.linecorp.armeria.server.logging.LogCollectingService;
 public final class DropwizardMetricConsumer implements MessageLogConsumer {
 
     private final MetricRegistry metricRegistry;
-    private final String metricNamePrefix;
-    private final Map<String, DropwizardRequestMetrics> methodRequestMetrics;
+    private final BiFunction<RequestContext, RequestLog, String> metricNameFunc;
+    private final Map<RequestLog, DropwizardRequestMetrics> methodRequestMetrics;
 
     /**
      * Creates a new instance.
      */
-    public DropwizardMetricConsumer(MetricRegistry metricRegistry, String metricNamePrefix) {
+    public DropwizardMetricConsumer(
+            MetricRegistry metricRegistry, BiFunction<RequestContext, RequestLog, String> metricNameFunc) {
+
         this.metricRegistry = requireNonNull(metricRegistry, "metricRegistry");
-        this.metricNamePrefix = requireNonNull(metricNamePrefix, "metricNamePrefix");
+        this.metricNameFunc = requireNonNull(metricNameFunc, "metricNameFunc");
         methodRequestMetrics = new ConcurrentHashMap<>();
     }
 
     @Override
     public void onRequest(RequestContext ctx, RequestLog req) {
-        final String metricName = MetricRegistry.name(metricNamePrefix, method(req));
-        final DropwizardRequestMetrics metrics = getRequestMetrics(metricName);
+        final DropwizardRequestMetrics metrics = getRequestMetrics(ctx, req);
         if (req.cause() == null) {
             metrics.markStart();
         } else {
@@ -66,8 +66,7 @@ public final class DropwizardMetricConsumer implements MessageLogConsumer {
     @Override
     public void onResponse(RequestContext ctx, ResponseLog res) {
         final RequestLog req = res.request();
-        final String metricName = MetricRegistry.name(metricNamePrefix, method(req));
-        final DropwizardRequestMetrics metrics = getRequestMetrics(metricName);
+        final DropwizardRequestMetrics metrics = getRequestMetrics(ctx, req);
         metrics.updateTime(res.endTimeNanos() - req.startTimeNanos());
         if (isSuccess(res)) {
             metrics.markSuccess();
@@ -100,29 +99,19 @@ public final class DropwizardMetricConsumer implements MessageLogConsumer {
                res.attr(ResponseLog.RPC_RESPONSE).get().getCause() == null;
     }
 
-    private static String method(RequestLog log) {
-        if (log.hasAttr(RequestLog.RPC_REQUEST)) {
-            return log.attr(RequestLog.RPC_REQUEST).get().method();
-        }
-
-        if (log.hasAttr(RequestLog.HTTP_HEADERS)) {
-            HttpHeaders headers = log.attr(RequestLog.HTTP_HEADERS).get();
-            return headers.path() + '#' + headers.method();
-        }
-
-        return MoreObjects.firstNonNull(log.method(), "__unknown__");
-    }
-
-    private DropwizardRequestMetrics getRequestMetrics(String methodLoggedName) {
+    private DropwizardRequestMetrics getRequestMetrics(RequestContext ctx, RequestLog req) {
         return methodRequestMetrics.computeIfAbsent(
-                methodLoggedName,
-                m -> new DropwizardRequestMetrics(
-                        m,
-                        metricRegistry.timer(MetricRegistry.name(m, "requests")),
-                        metricRegistry.meter(MetricRegistry.name(m, "successes")),
-                        metricRegistry.meter(MetricRegistry.name(m, "failures")),
-                        metricRegistry.counter(MetricRegistry.name(m, "activeRequests")),
-                        metricRegistry.meter(MetricRegistry.name(m, "requestBytes")),
-                        metricRegistry.meter(MetricRegistry.name(m, "responseBytes"))));
+                req,
+                reqLog -> {
+                    final String metricName = metricNameFunc.apply(ctx, reqLog);
+                    return new DropwizardRequestMetrics(
+                            metricName,
+                            metricRegistry.timer(MetricRegistry.name(metricName, "requests")),
+                            metricRegistry.meter(MetricRegistry.name(metricName, "successes")),
+                            metricRegistry.meter(MetricRegistry.name(metricName, "failures")),
+                            metricRegistry.counter(MetricRegistry.name(metricName, "activeRequests")),
+                            metricRegistry.meter(MetricRegistry.name(metricName, "requestBytes")),
+                            metricRegistry.meter(MetricRegistry.name(metricName, "responseBytes")));
+                });
     }
 }

src/main/java/com/linecorp/armeria/internal/logging/DropwizardRequestMetrics.java
@@ -48,6 +48,10 @@ final class DropwizardRequestMetrics {
         this.responseBytes = responseBytes;
     }
 
+    String name() {
+        return name;
+    }
+
     void updateTime(long durationNanos) {
         timer.update(durationNanos, TimeUnit.NANOSECONDS);
     }

src/main/java/com/linecorp/armeria/client/http/HttpRequestSubscriber.java
@@ -133,6 +133,11 @@ final class HttpRequestSubscriber implements Subscriber<HttpObject>, ChannelFutu
         String host = firstHeaders.authority();
         if (host == null) {
             host = ((InetSocketAddress) ch.remoteAddress()).getHostString();
+        } else {
+            final int colonIdx = host.lastIndexOf(':');
+            if (colonIdx > 0) {
+                host = host.substring(0, colonIdx);
+            }
         }
 
         logBuilder.start(ch, HttpSession.get(ch).protocol(),

src/main/java/com/linecorp/armeria/client/logging/DropwizardMetricCollectingClient.java
@@ -16,17 +16,39 @@
 
 package com.linecorp.armeria.client.logging;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import com.codahale.metrics.MetricRegistry;
+import com.google.common.base.MoreObjects;
 
 import com.linecorp.armeria.client.Client;
+import com.linecorp.armeria.client.ClientRequestContext;
 import com.linecorp.armeria.common.Request;
+import com.linecorp.armeria.common.RequestContext;
 import com.linecorp.armeria.common.Response;
+import com.linecorp.armeria.common.http.HttpHeaders;
+import com.linecorp.armeria.common.logging.RequestLog;
 import com.linecorp.armeria.internal.logging.DropwizardMetricConsumer;
 
 /**
  * Decorates a {@link Client} to collect metrics into Dropwizard {@link MetricRegistry}.
+ * To use, simply prepare a {@link MetricRegistry} and add this decorator to a client.
+ *
+ * <p>Example:
+ * <pre>{@code
+ * MetricRegistry metricRegistry = new MetricRegistry();
+ * MyService.Iface client = new ClientBuilder(uri)
+ *         .decorate(DropwizardMetricCollectingClient.newDecorator(
+ *                 metricRegistry, MetricRegistry.name("clients", "myService")))
+ *         .build(MyService.Iface.class);
+ * }
+ * </pre>
+ *
+ * <p>It is generally recommended to define your own name for the service instead of using something like
+ * the Java class to make sure otherwise safe changes like renames don't break metrics.
  *
  * @param <I> the request type
  * @param <O> the response type
@@ -35,21 +57,24 @@ public final class DropwizardMetricCollectingClient<I extends Request, O extends
         extends LogCollectingClient<I, O> {
 
     /**
-     * A {@link Client} decorator that tracks request stats using the Dropwizard metrics library.
-     * To use, simply prepare a {@link MetricRegistry} and add this decorator to a client.
+     * Returns a {@link Client} decorator that tracks request stats using the Dropwizard metrics library.
      *
-     * <p>Example:
-     * <pre>{@code
-     * MetricRegistry metricRegistry = new MetricRegistry();
-     * MyService.Iface client = new ClientBuilder(uri)
-     *         .decorate(MetricCollectingClient.newDropwizardDecorator(
-     *                 metricRegistry, MetricRegistry.name("clients", "myService")))
-     *         .build(MyService.Iface.class);
-     * }
-     * </pre>
-     *
-     * <p>It is generally recommended to define your own name for the service instead of using something like
-     * the Java class to make sure otherwise safe changes like renames don't break metrics.
+     * @param metricRegistry the {@link MetricRegistry} to store metrics into.
+     * @param metricNameFunc the function that transforms a {@link RequestLog} into a metric name
+     */
+    public static <I extends Request, O extends Response>
+    Function<Client<? super I, ? extends O>, DropwizardMetricCollectingClient<I, O>> newDecorator(
+            MetricRegistry metricRegistry,
+            BiFunction<? super ClientRequestContext, ? super RequestLog, String> metricNameFunc) {
+
+        requireNonNull(metricRegistry, "metricRegistry");
+        requireNonNull(metricNameFunc, "metricNameFunc");
+
+        return client -> new DropwizardMetricCollectingClient<>(client, metricRegistry, metricNameFunc);
+    }
+
+    /**
+     * Returns a {@link Client} decorator that tracks request stats using the Dropwizard metrics library.
      *
      * @param metricRegistry the {@link MetricRegistry} to store metrics into.
      * @param metricNamePrefix the prefix of the names of the metrics created by the returned decorator.
@@ -58,12 +83,35 @@ public final class DropwizardMetricCollectingClient<I extends Request, O extends
     Function<Client<? super I, ? extends O>, DropwizardMetricCollectingClient<I, O>> newDecorator(
             MetricRegistry metricRegistry, String metricNamePrefix) {
 
-        return client -> new DropwizardMetricCollectingClient<>(
-                client, metricRegistry, metricNamePrefix);
+        requireNonNull(metricNamePrefix, "metricNamePrefix");
+        return newDecorator(metricRegistry, (ctx, req) -> defaultMetricName(req, metricNamePrefix));
     }
 
-    DropwizardMetricCollectingClient(Client<? super I, ? extends O> delegate,
-                                     MetricRegistry metricRegistry, String metricNamePrefix) {
-        super(delegate, new DropwizardMetricConsumer(metricRegistry, metricNamePrefix));
+    private static String defaultMetricName(RequestLog req, String metricNamePrefix) {
+        String methodName = null;
+
+        if (req.hasAttr(RequestLog.HTTP_HEADERS)) {
+            final HttpHeaders headers = req.attr(RequestLog.HTTP_HEADERS).get();
+            methodName = headers.method().name();
+        }
+
+        if (req.hasAttr(RequestLog.RPC_REQUEST)) {
+            methodName = req.attr(RequestLog.RPC_REQUEST).get().method();
+        }
+
+        if (methodName == null) {
+            methodName = MoreObjects.firstNonNull(req.method(), "__UNKNOWN_METHOD__");
+        }
+
+        return MetricRegistry.name(metricNamePrefix, methodName);
+    }
+
+    @SuppressWarnings("unchecked")
+    DropwizardMetricCollectingClient(
+            Client<? super I, ? extends O> delegate,
+            MetricRegistry metricRegistry,
+            BiFunction<? super ClientRequestContext, ? super RequestLog, String> metricNameFunc) {
+        super(delegate, new DropwizardMetricConsumer(
+                metricRegistry, (BiFunction<RequestContext, RequestLog, String>) metricNameFunc));
     }
 }

src/main/java/com/linecorp/armeria/server/AbstractPathMapping.java
@@ -18,6 +18,8 @@ package com.linecorp.armeria.server;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.Optional;
+
 /**
  * A skeletal {@link PathMapping} implementation. Implement {@link #doApply(String)}.
  */
@@ -58,7 +60,7 @@ public abstract class AbstractPathMapping implements PathMapping {
      * @return {@code path}
      *
      * @throws NullPointerException if {@code path} is {@code null}
-     * @throws IllegalArgumentException if {@code path} is not an asbsolute path
+     * @throws IllegalArgumentException if {@code path} is not an absolute path
      */
     protected static String ensureAbsolutePath(String path, String paramName) {
         requireNonNull(path, paramName);
@@ -76,4 +78,68 @@ public abstract class AbstractPathMapping implements PathMapping {
      *         {@code null} if the specified {@code path} does not match this mapping.
      */
     protected abstract String doApply(String path);
+
+    @Override
+    public String loggerName() {
+        return "__UNKNOWN__";
+    }
+
+    static String loggerName(String pathish) {
+        if (pathish == null) {
+            return "__UNKNOWN__";
+        }
+
+        String normalized = pathish;
+        if ("/".equals(normalized)) {
+            return "__ROOT__";
+        }
+        normalized = normalized.substring(1); // Strip the first slash.
+
+        final int end;
+        if (normalized.endsWith("/")) {
+            end = normalized.length() - 1;
+        } else {
+            end = normalized.length();
+        }
+
+        final StringBuilder buf = new StringBuilder(end);
+        boolean start = true;
+        for (int i = 0; i < end; i++) {
+            final char ch = normalized.charAt(i);
+            if (ch != '/') {
+                if (start) {
+                    start = false;
+                    if (Character.isJavaIdentifierStart(ch)) {
+                        buf.append(ch);
+                    } else {
+                        buf.append('_');
+                        if (Character.isJavaIdentifierPart(ch)) {
+                            buf.append(ch);
+                        }
+                    }
+                } else {
+                    if (Character.isJavaIdentifierPart(ch)) {
+                        buf.append(ch);
+                    } else {
+                        buf.append('_');
+                    }
+                }
+            } else {
+                start = true;
+                buf.append('.');
+            }
+        }
+
+        return buf.toString();
+    }
+
+    @Override
+    public String metricName() {
+        return "__UNKNOWN_PATH__";
+    }
+
+    @Override
+    public Optional<String> exactPath() {
+        return Optional.empty();
+    }
 }

src/main/java/com/linecorp/armeria/server/CatchAllPathMapping.java
@@ -23,6 +23,7 @@ final class CatchAllPathMapping extends AbstractPathMapping {
     static final CatchAllPathMapping INSTANCE = new CatchAllPathMapping();
 
     private static final Optional<String> PREFIX_PATH_OPT = Optional.of("/");
+    private static final String LOGGER_NAME = loggerName("/"); // "__ROOT__"
 
     private CatchAllPathMapping() {}
 
@@ -32,8 +33,13 @@ final class CatchAllPathMapping extends AbstractPathMapping {
     }
 
     @Override
-    public Optional<String> prefixPath() {
-        return PREFIX_PATH_OPT;
+    public String loggerName() {
+        return LOGGER_NAME;
+    }
+
+    @Override
+    public String metricName() {
+        return "/**";
     }
 
     @Override

src/main/java/com/linecorp/armeria/server/DefaultServiceRequestContext.java
@@ -28,6 +28,7 @@ import javax.annotation.Nullable;
 import javax.net.ssl.SSLSession;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.linecorp.armeria.common.NonWrappingRequestContext;
 import com.linecorp.armeria.common.SessionProtocol;
@@ -50,11 +51,11 @@ public final class DefaultServiceRequestContext extends NonWrappingRequestContex
     private final Channel ch;
     private final ServiceConfig cfg;
     private final String mappedPath;
-    private final Logger logger;
     private final SSLSession sslSession;
 
     private final DefaultRequestLog requestLog;
     private final DefaultResponseLog responseLog;
+    private final Logger logger;
 
     private long requestTimeoutMillis;
     private long maxRequestLength;
@@ -66,13 +67,12 @@ public final class DefaultServiceRequestContext extends NonWrappingRequestContex
      *
      * @param ch the {@link Channel} that handles the invocation
      * @param sessionProtocol the {@link SessionProtocol} of the invocation
-     * @param logger the {@link Logger} for the invocation
      * @param request the request associated with this context
      * @param sslSession the {@link SSLSession} for this invocation if it is over TLS
      */
     public DefaultServiceRequestContext(
             ServiceConfig cfg, Channel ch, SessionProtocol sessionProtocol,
-            String method, String path, String mappedPath, Logger logger, Object request,
+            String method, String path, String mappedPath, Object request,
             @Nullable SSLSession sslSession) {
 
         super(sessionProtocol, method, path, request);
@@ -80,16 +80,26 @@ public final class DefaultServiceRequestContext extends NonWrappingRequestContex
         this.ch = ch;
         this.cfg = cfg;
         this.mappedPath = mappedPath;
-        this.logger = new RequestContextAwareLogger(this, logger);
         this.sslSession = sslSession;
 
         requestLog = new DefaultRequestLog();
         requestLog.start(ch, sessionProtocol, cfg.virtualHost().defaultHostname(), method, path);
         responseLog = new DefaultResponseLog(requestLog);
+        logger = newLogger(cfg);
 
         final ServerConfig serverCfg = cfg.server().config();
         requestTimeoutMillis = serverCfg.defaultRequestTimeoutMillis();
         maxRequestLength = serverCfg.defaultMaxRequestLength();
+    }
+
+    private RequestContextAwareLogger newLogger(ServiceConfig cfg) {
+        String loggerName = cfg.loggerName().orElse(null);
+        if (loggerName == null) {
+            loggerName = cfg.pathMapping().loggerName();
+        }
+
+        return new RequestContextAwareLogger(this, LoggerFactory.getLogger(
+                cfg.server().config().serviceLoggerPrefix() + '.' + loggerName));
     }
 
     @Override
@@ -100,6 +110,11 @@ public final class DefaultServiceRequestContext extends NonWrappingRequestContex
     @Override
     public VirtualHost virtualHost() {
         return cfg.virtualHost();
+    }
+
+    @Override
+    public PathMapping pathMapping() {
+        return cfg.pathMapping();
     }
 
     @Override

src/main/java/com/linecorp/armeria/server/ExactPathMapping.java
@@ -21,18 +21,30 @@ import java.util.Optional;
 final class ExactPathMapping extends AbstractPathMapping {
 
     private final String exactPath;
+    private final String loggerName;
     private final Optional<String> exactPathOpt;
     private final String strVal;
 
     ExactPathMapping(String exactPath) {
         this.exactPath = ensureAbsolutePath(exactPath, "exactPath");
         exactPathOpt = Optional.of(exactPath);
+        loggerName = loggerName(exactPath);
         strVal = "exact: " + exactPath;
     }
 
     @Override
     protected String doApply(String path) {
         return exactPath.equals(path) ? path : null;
+    }
+
+    @Override
+    public String loggerName() {
+        return loggerName;
+    }
+
+    @Override
+    public String metricName() {
+        return exactPath;
     }
 
     @Override

commit 09eb2cf286afe7658acb853d1cce337e794c2265

Fix DropwizardRequestMetrics leak
Motivation:

Commit c2dad254f19f5b3eccec987efde850b34eac9e32 introduced a regression
where a new DropwizardRequestMetrics instance is created for every
request, because it used RequestLog as a key of the Map that holds
DropwizardRequestMetrics. As a result, the Map will grow infinitely.

Modifications:

- Use String as a key again

Result:

Leak is gone.

src/main/java/com/linecorp/armeria/internal/logging/DropwizardMetricConsumer.java
@@ -40,7 +40,7 @@ public final class DropwizardMetricConsumer implements MessageLogConsumer {
 
     private final MetricRegistry metricRegistry;
     private final BiFunction<RequestContext, RequestLog, String> metricNameFunc;
-    private final Map<RequestLog, DropwizardRequestMetrics> methodRequestMetrics;
+    private final Map<String, DropwizardRequestMetrics> methodRequestMetrics;
 
     /**
      * Creates a new instance.
@@ -100,18 +100,16 @@ public final class DropwizardMetricConsumer implements MessageLogConsumer {
     }
 
     private DropwizardRequestMetrics getRequestMetrics(RequestContext ctx, RequestLog req) {
+        final String metricName = metricNameFunc.apply(ctx, req);
         return methodRequestMetrics.computeIfAbsent(
-                req,
-                reqLog -> {
-                    final String metricName = metricNameFunc.apply(ctx, reqLog);
-                    return new DropwizardRequestMetrics(
-                            metricName,
-                            metricRegistry.timer(MetricRegistry.name(metricName, "requests")),
-                            metricRegistry.meter(MetricRegistry.name(metricName, "successes")),
-                            metricRegistry.meter(MetricRegistry.name(metricName, "failures")),
-                            metricRegistry.counter(MetricRegistry.name(metricName, "activeRequests")),
-                            metricRegistry.meter(MetricRegistry.name(metricName, "requestBytes")),
-                            metricRegistry.meter(MetricRegistry.name(metricName, "responseBytes")));
-                });
+                metricName,
+                name -> new DropwizardRequestMetrics(
+                        name,
+                        metricRegistry.timer(MetricRegistry.name(name, "requests")),
+                        metricRegistry.meter(MetricRegistry.name(name, "successes")),
+                        metricRegistry.meter(MetricRegistry.name(name, "failures")),
+                        metricRegistry.counter(MetricRegistry.name(name, "activeRequests")),
+                        metricRegistry.meter(MetricRegistry.name(name, "requestBytes")),
+                        metricRegistry.meter(MetricRegistry.name(name, "responseBytes"))));
     }
 }

src/main/java/com/linecorp/armeria/internal/logging/DropwizardRequestMetrics.java
@@ -48,10 +48,6 @@ final class DropwizardRequestMetrics {
         this.responseBytes = responseBytes;
     }
 
-    String name() {
-        return name;
-    }
-
     void updateTime(long durationNanos) {
         timer.update(durationNanos, TimeUnit.NANOSECONDS);
     }