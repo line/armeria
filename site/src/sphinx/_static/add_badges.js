62fab90136af16002a81c0ac8d49aa62bdea1332
Trustin Lee <trustin@linecorp.com>
2016-10-20 15:03:42 +0900

    Improve the site design
    - Update sphinx_rtd_theme to the latest version
    - Fix logo and project badge sizes on mobile view
    - Hide the Github fork ribbon on mobile view because it hides the site
      menu button
    - Do not increase the content width infinitely; center the content
      instead because a wide page harms readability.
    - Extract JavaScript code into separate files
    - Add the 'Edit on GitHub' link

---
+++ site/src/sphinx/_static/add_badges.js
@@ -0,0 +1,47 @@
+// Append the Slack invitation and project badges at the end of the menu (sidenav).
+function addSlackInvitation(parent) {
+  var li = document.createElement('li');
+  li.className = 'toctree-l1';
+  var div = document.createElement('div');
+  div.className = 'slack-invitation';
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://slackin-line-armeria.herokuapp.com/slackin.js';
+  div.appendChild(script);
+  li.appendChild(div);
+  parent.appendChild(li);
+}
+
+function addBadge(parent, src, href) {
+  var img = document.createElement('img');
+  img.src = src;
+  var a = document.createElement('a');
+  a.href = href;
+  a.appendChild(img);
+  parent.appendChild(a);
+}
+
+function addBadges(parent) {
+  var li = document.createElement('li');
+  li.className = 'toctree-l1';
+  var div = document.createElement('div');
+  div.className = 'project-badges';
+  addBadge(div, 'https://img.shields.io/maven-central/v/com.linecorp.armeria/armeria.svg?style=flat-square',
+    'http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.linecorp.armeria%22%20AND%20a%3A%22armeria%22');
+  addBadge(div, 'https://img.shields.io/travis/line/armeria/master.svg?style=flat-square',
+    'https://travis-ci.org/line/armeria');
+  addBadge(div, 'https://img.shields.io/codecov/c/github/line/armeria/master.svg?style=flat-square',
+    'https://codecov.io/gh/line/armeria');
+  li.appendChild(div);
+  parent.appendChild(li);
+}
+
+var menus = document.getElementsByClassName("wy-menu wy-menu-vertical");
+if (menus.length > 0) {
+  var menu = menus[0];
+  var lists = menu.getElementsByTagName('ul');
+  if (lists.length > 0) {
+    addSlackInvitation(lists[0]);
+    addBadges(lists[0]);
+  }
+}
