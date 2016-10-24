5f9fada28a01dd860a15f4ae3d7202d3b1ef0e32
Trustin Lee <t@motd.kr>
2016-10-25 00:28:04 +0900

    Center the site page without a flicker
    - Extend the extrahead and footer block in layout.html instead of
      overriding versions.html
      - Did not know I can extend a block in a Sphinx template until now.
    - Center the page before the document body is available
    - Remove the cloak which is not needed anymore
    - Fork me on GitHub -> Fork me at GitHub

--- site/src/sphinx/_static/center_page.js
+++ site/src/sphinx/_static/center_page.js
@@ -1,32 +1,48 @@
-// Center the page dynamically.
+var head = document.head || document.getElementsByTagName('head')[0];
+var style = null;
+
+// Centers the page content dynamically.
 function centerPage() {
-  // Unhide the page hidden by the cloak in the _static/overrides.css.
-  document.body.style.display = 'block';
-
-  var side = document.getElementsByClassName('wy-nav-side')[0];
-  var content = document.getElementsByClassName('wy-nav-content-wrap')[0];
-  var ribbon = document.getElementsByClassName('github-fork-ribbon-wrapper')[0];
-
-  if (typeof side !== 'object' || typeof content !== 'object' || typeof ribbon !== 'object') {
-    return;
+  if (style) {
+    head.removeChild(style);
+    style = null;
   }
 
   var windowWidth = window.innerWidth;
-  var scrollbarWidth = windowWidth - document.body.clientWidth;
+  if (windowWidth <= 768) {
+    return;
+  }
+
   var sideWidth = 300;
   var contentWidth = 800;
+  var ribbonWidth = 150;
+  var leftMargin = Math.max(0, (windowWidth - sideWidth - contentWidth) / 2);
+  var scrollbarWidth = document.body ? windowWidth - document.body.clientWidth : 0;
+  var css = '';
 
-  if (windowWidth > 768) {
-    var leftMargin = Math.max(0, (windowWidth - sideWidth - contentWidth) / 2);
-    side.style.left = leftMargin + 'px';
-    content.style.marginLeft = (300 + leftMargin) + 'px';
-    ribbon.style.marginRight = (leftMargin - scrollbarWidth) + 'px';
+  css += '.wy-nav-side { left: ' + leftMargin + 'px; }';
+  css += "\n";
+  css += '.wy-nav-content-wrap { margin-left: ' + (sideWidth + leftMargin) + 'px; }';
+  css += "\n";
+  css += '.github-fork-ribbon-wrapper.right { left: ' + 
+         (Math.min(windowWidth - scrollbarWidth, sideWidth + contentWidth + leftMargin) - ribbonWidth) + 'px; }';
+  css += "\n";
+
+  var newStyle = document.createElement('style');
+  newStyle.type = 'text/css';
+  if (newStyle.styleSheet) {
+    newStyle.styleSheet.cssText = css;
   } else {
-    side.removeAttribute('style');
-    content.removeAttribute('style');
-    ribbon.removeAttribute('style');
+    newStyle.appendChild(document.createTextNode(css));
   }
+
+  head.appendChild(newStyle);
+  style = newStyle;
 }
 
-window.onresize = centerPage;
 centerPage();
+window.addEventListener('resize', centerPage);
+// Adjust the position of the 'fork me at GitHub' ribbon after document.body is available,
+// so that we can calculate the width of the scroll bar correctly.
+window.addEventListener('DOMContentLoaded', centerPage);
+
