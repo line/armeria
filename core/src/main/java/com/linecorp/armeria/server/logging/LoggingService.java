71a11ac0193029706e99e30af69c561cc85efcc6
Masahiro Ide <imasahiro9@gmail.com>
2017-01-13 15:46:34 +0900

    Add SampledloggingService
    When a server receives many requests, we cannot records all logs
    through LoggingService due to server resource limitation. To adjust
    this, sample logs.

--- core/src/main/java/com/linecorp/armeria/server/logging/LoggingService.java
+++ core/src/main/java/com/linecorp/armeria/server/logging/LoggingService.java
@@ -65,16 +65,15 @@ public class LoggingService<I extends Request, O extends Response> extends Decor
         return delegate().serve(ctx, req);
     }
 
-
     // FIXME(trustin): Format properly.
-    private void logRequest(RequestLog log) {
+    protected void logRequest(RequestLog log) {
         final Logger logger = ((ServiceRequestContext) log.context()).logger();
         if (level.isEnabled(logger)) {
             level.log(logger, REQUEST_FORMAT, log.toStringRequestOnly());
         }
     }
 
-    private void logResponse(RequestLog log) {
+    protected void logResponse(RequestLog log) {
         final Logger logger = ((ServiceRequestContext) log.context()).logger();
         if (level.isEnabled(logger)) {
             level.log(logger, RESPONSE_FORMAT, log.toStringResponseOnly());
