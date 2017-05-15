a124f9c305788eebdb9658d90b6c7936bbb53165
Anuraag Agrawal <anuraaga@gmail.com>
2017-05-15 18:18:55 +0900

    Migrate main tests from ArmeriaServerCall to server tests. (#539)
    ArmeriaServerCallTest are very hard to maintain and reason about since they require running many different callbacks in precise order. While the speed of unit tests is nice, these aren't very useful, so we migrate them to normal client/server tests.
    
    Also defines a separate stub interface for fine-grained unit testing. The copy of the upstream one will still be used for interopability tests, but this should be easier to maintain for unit tests. I don't like how in the upstream ones, many different cases are handled by request parameters which are somewhat opaque.
    
    Also follows AbstractThriftOverHttpTest pattern by using the upstream client to test server behavior and interopability in one go. Uses grpc-okhttp instead of grpc-netty to prevent netty version conflicts so this should be reasonably stable unlike the netty interop tests (I'll probably migrate the ignored integration test at some point to okhttp).
    
    Also allows non-qualified content types for proto since many clients (like okhttp, grpc-web) don't specify `+proto` (hopefully grpc doesn't end up in content-type hell like thrift did...).

--- grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcServiceServerTest.java
+++ grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcServiceServerTest.java
@@ -17,60 +17,112 @@
 package com.linecorp.armeria.server.grpc;
 
 import static com.linecorp.armeria.common.http.HttpSessionProtocols.HTTP;
+import static com.linecorp.armeria.internal.grpc.GrpcTestUtil.REQUEST_MESSAGE;
+import static com.linecorp.armeria.internal.grpc.GrpcTestUtil.RESPONSE_MESSAGE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
-import com.linecorp.armeria.client.Clients;
+import com.google.common.base.Strings;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Ints;
+import com.google.protobuf.ByteString;
+
 import com.linecorp.armeria.client.http.HttpClient;
 import com.linecorp.armeria.client.http.HttpClientFactory;
-import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
 import com.linecorp.armeria.common.http.AggregatedHttpMessage;
 import com.linecorp.armeria.common.http.HttpHeaderNames;
 import com.linecorp.armeria.common.http.HttpHeaders;
 import com.linecorp.armeria.common.http.HttpMethod;
 import com.linecorp.armeria.common.http.HttpStatus;
 import com.linecorp.armeria.grpc.testing.Messages.EchoStatus;
+import com.linecorp.armeria.grpc.testing.Messages.Payload;
 import com.linecorp.armeria.grpc.testing.Messages.SimpleRequest;
 import com.linecorp.armeria.grpc.testing.Messages.SimpleResponse;
 import com.linecorp.armeria.grpc.testing.Messages.StreamingOutputCallRequest;
-import com.linecorp.armeria.grpc.testing.Messages.StreamingOutputCallResponse;
-import com.linecorp.armeria.grpc.testing.TestServiceGrpc;
-import com.linecorp.armeria.grpc.testing.TestServiceGrpc.TestServiceBlockingStub;
-import com.linecorp.armeria.grpc.testing.TestServiceGrpc.TestServiceImplBase;
+import com.linecorp.armeria.grpc.testing.UnitTestServiceGrpc;
+import com.linecorp.armeria.grpc.testing.UnitTestServiceGrpc.UnitTestServiceBlockingStub;
+import com.linecorp.armeria.grpc.testing.UnitTestServiceGrpc.UnitTestServiceImplBase;
+import com.linecorp.armeria.grpc.testing.UnitTestServiceGrpc.UnitTestServiceStub;
 import com.linecorp.armeria.internal.grpc.GrpcHeaderNames;
 import com.linecorp.armeria.internal.grpc.GrpcTestUtil;
+import com.linecorp.armeria.internal.grpc.StreamRecorder;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.testing.server.ServerRule;
 
+import io.grpc.Codec;
+import io.grpc.DecompressorRegistry;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Status;
+import io.grpc.Status.Code;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.ServerCallStreamObserver;
 import io.grpc.stub.StreamObserver;
+import io.netty.util.AsciiString;
 
 public class GrpcServiceServerTest {
 
-    private static class TestServiceImpl extends TestServiceImplBase {
+    private static final int MAX_MESSAGE_SIZE = 16 * 1024 * 1024;
+
+    private static final AsciiString LARGE_PAYLOAD = AsciiString.of(Strings.repeat("a", MAX_MESSAGE_SIZE + 1));
+
+    private static class UnitTestServiceImpl extends UnitTestServiceImplBase {
 
         @Override
-        public void unaryCall(SimpleRequest request, StreamObserver<SimpleResponse> responseObserver) {
-            if (request.hasResponseStatus()) {
-                responseObserver.onError(
-                        new StatusRuntimeException(
-                                Status.fromCodeValue(request.getResponseStatus().getCode())));
+        public void staticUnaryCall(SimpleRequest request, StreamObserver<SimpleResponse> responseObserver) {
+            if (!request.equals(REQUEST_MESSAGE)) {
+                responseObserver.onError(new IllegalArgumentException("Unexpected request: " + request));
                 return;
             }
-            responseObserver.onNext(GrpcTestUtil.RESPONSE_MESSAGE);
+            responseObserver.onNext(RESPONSE_MESSAGE);
             responseObserver.onCompleted();
         }
 
         @Override
-        public void streamingOutputCall(StreamingOutputCallRequest request,
-                                        StreamObserver<StreamingOutputCallResponse> responseObserver) {
-            responseObserver.onNext(
-                    StreamingOutputCallResponse.newBuilder()
-                                               .setPayload(request.getPayload())
-                                               .build());
+        public void staticStreamedOutputCall(SimpleRequest request,
+                                             StreamObserver<SimpleResponse> responseObserver) {
+            if (!request.equals(REQUEST_MESSAGE)) {
+                responseObserver.onError(new IllegalArgumentException("Unexpected request: " + request));
+                return;
+            }
+            responseObserver.onNext(RESPONSE_MESSAGE);
+            responseObserver.onNext(RESPONSE_MESSAGE);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void errorNoMessage(SimpleRequest request, StreamObserver<SimpleResponse> responseObserver) {
+            responseObserver.onError(Status.ABORTED.asException());
+        }
+
+        @Override
+        public void errorWithMessage(SimpleRequest request, StreamObserver<SimpleResponse> responseObserver) {
+            responseObserver.onError(Status.ABORTED.withDescription("aborted call").asException());
+        }
+
+        @Override
+        public void staticUnaryCallSetsMessageCompression(SimpleRequest request,
+                                                          StreamObserver<SimpleResponse> responseObserver) {
+            if (!request.equals(REQUEST_MESSAGE)) {
+                responseObserver.onError(new IllegalArgumentException("Unexpected request: " + request));
+                return;
+            }
+            ServerCallStreamObserver<SimpleResponse> callObserver =
+                    (ServerCallStreamObserver<SimpleResponse>) responseObserver;
+            callObserver.setCompression("gzip");
+            callObserver.setMessageCompression(true);
+            responseObserver.onNext(RESPONSE_MESSAGE);
             responseObserver.onCompleted();
         }
     }
@@ -83,11 +135,103 @@ public class GrpcServiceServerTest {
             sb.port(0, HTTP);
 
             sb.serviceUnder("/", new GrpcServiceBuilder()
-                    .addService(new TestServiceImpl())
+                    .setMaxInboundMessageSizeBytes(MAX_MESSAGE_SIZE)
+                    .addService(new UnitTestServiceImpl())
                     .enableUnframedRequests(true)
                     .build());
         }
     };
+
+    @Rule
+    public Timeout globalTimeout = new Timeout(10, TimeUnit.SECONDS);
+
+    private static ManagedChannel channel;
+
+    private UnitTestServiceBlockingStub blockingClient;
+    private UnitTestServiceStub streamingClient;
+
+    @BeforeClass
+    public static void setUpChannel() {
+        channel = ManagedChannelBuilder.forAddress("127.0.0.1", server.httpPort())
+                                       .usePlaintext(true)
+                                       .build();
+    }
+
+    @AfterClass
+    public static void tearDownChannel() {
+        channel.shutdownNow();
+    }
+
+    @Before
+    public void setUp() {
+        blockingClient = UnitTestServiceGrpc.newBlockingStub(channel);
+        streamingClient = UnitTestServiceGrpc.newStub(channel);
+    }
+
+    @Test
+    public void unary_normal() throws Exception {
+        assertThat(blockingClient.staticUnaryCall(REQUEST_MESSAGE)).isEqualTo(RESPONSE_MESSAGE);
+    }
+
+    @Test
+    public void streamedOutput_normal() throws Exception {
+        StreamRecorder<SimpleResponse> recorder = StreamRecorder.create();
+        streamingClient.staticStreamedOutputCall(REQUEST_MESSAGE, recorder);
+        recorder.awaitCompletion();
+        assertThat(recorder.getValues()).containsExactly(RESPONSE_MESSAGE, RESPONSE_MESSAGE);
+    }
+
+    @Test
+    public void tooLargeRequest_uncompressed() throws Exception {
+        SimpleRequest request = SimpleRequest.newBuilder()
+                                             .setPayload(
+                                                     Payload.newBuilder()
+                                                            .setBody(ByteString.copyFrom(
+                                                                    LARGE_PAYLOAD.toByteArray())))
+                                             .build();
+        StatusRuntimeException t =
+                (StatusRuntimeException) catchThrowable(
+                        () -> blockingClient.staticUnaryCall(request));
+        assertThat(t.getStatus().getCode()).isEqualTo(Code.RESOURCE_EXHAUSTED);
+        assertThat(t.getMessage()).contains("Frame size 16777227 exceeds maximum: 16777216");
+    }
+
+    @Test
+    public void tooLargeRequest_compressed() throws Exception {
+        SimpleRequest request = SimpleRequest.newBuilder()
+                                             .setPayload(
+                                                     Payload.newBuilder()
+                                                            .setBody(ByteString.copyFrom(
+                                                                    LARGE_PAYLOAD.toByteArray())))
+                                             .build();
+        StatusRuntimeException t =
+                (StatusRuntimeException) catchThrowable(
+                        () -> blockingClient.withCompression("gzip").staticUnaryCall(request));
+        assertThat(t.getStatus().getCode()).isEqualTo(Code.INTERNAL);
+        assertThat(t.getMessage())
+                .contains("Compressed frame exceeds maximum frame size: 16777216. Bytes read: 16777227.");
+    }
+
+    @Test
+    public void uncompressedClient_compressedEndpoint() throws Exception {
+        ManagedChannel nonDecompressingChannel =
+                ManagedChannelBuilder.forAddress("127.0.0.1", server.httpPort())
+                                     .decompressorRegistry(
+                                             DecompressorRegistry.emptyInstance()
+                                                                 .with(Codec.Identity.NONE, false))
+                                     .usePlaintext(true)
+                                     .build();
+        UnitTestServiceBlockingStub client = UnitTestServiceGrpc.newBlockingStub(nonDecompressingChannel);
+        assertThat(client.staticUnaryCallSetsMessageCompression(REQUEST_MESSAGE))
+                .isEqualTo(RESPONSE_MESSAGE);
+        nonDecompressingChannel.shutdownNow();
+    }
+
+    @Test
+    public void compressedClient_compressedEndpoint() throws Exception {
+        assertThat(blockingClient.staticUnaryCallSetsMessageCompression(REQUEST_MESSAGE))
+                .isEqualTo(RESPONSE_MESSAGE);
+    }
 
     @Test
     public void unframed() throws Exception {
@@ -95,11 +239,12 @@ public class GrpcServiceServerTest {
                 .newClient("none+" + server.httpUri("/"),
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
-                HttpHeaders.of(HttpMethod.POST, TestServiceGrpc.METHOD_UNARY_CALL.getFullMethodName())
+                HttpHeaders.of(HttpMethod.POST,
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName())
                            .set(HttpHeaderNames.CONTENT_TYPE, "application/protobuf"),
-                GrpcTestUtil.REQUEST_MESSAGE.toByteArray()).aggregate().get();
+                REQUEST_MESSAGE.toByteArray()).aggregate().get();
         SimpleResponse message = SimpleResponse.parseFrom(response.content().array());
-        assertThat(message).isEqualTo(GrpcTestUtil.RESPONSE_MESSAGE);
+        assertThat(message).isEqualTo(RESPONSE_MESSAGE);
     }
 
     @Test
@@ -108,12 +253,13 @@ public class GrpcServiceServerTest {
                 .newClient("none+" + server.httpUri("/"),
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
-                HttpHeaders.of(HttpMethod.POST, TestServiceGrpc.METHOD_UNARY_CALL.getFullMethodName())
+                HttpHeaders.of(HttpMethod.POST,
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName())
                            .set(HttpHeaderNames.CONTENT_TYPE, "application/protobuf")
                            .set(GrpcHeaderNames.GRPC_ACCEPT_ENCODING, "gzip,none"),
-                GrpcTestUtil.REQUEST_MESSAGE.toByteArray()).aggregate().get();
+                REQUEST_MESSAGE.toByteArray()).aggregate().get();
         SimpleResponse message = SimpleResponse.parseFrom(response.content().array());
-        assertThat(message).isEqualTo(GrpcTestUtil.RESPONSE_MESSAGE);
+        assertThat(message).isEqualTo(RESPONSE_MESSAGE);
     }
 
     @Test
@@ -123,7 +269,7 @@ public class GrpcServiceServerTest {
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
                 HttpHeaders.of(HttpMethod.POST,
-                               TestServiceGrpc.METHOD_STREAMING_OUTPUT_CALL.getFullMethodName())
+                               UnitTestServiceGrpc.METHOD_STATIC_STREAMED_OUTPUT_CALL.getFullMethodName())
                            .set(HttpHeaderNames.CONTENT_TYPE, "application/protobuf"),
                 StreamingOutputCallRequest.getDefaultInstance().toByteArray()).aggregate().get();
         assertThat(response.status()).isEqualTo(HttpStatus.BAD_REQUEST);
@@ -136,8 +282,8 @@ public class GrpcServiceServerTest {
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
                 HttpHeaders.of(HttpMethod.POST,
-                               TestServiceGrpc.METHOD_UNARY_CALL.getFullMethodName()),
-                GrpcTestUtil.REQUEST_MESSAGE.toByteArray()).aggregate().get();
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName()),
+                REQUEST_MESSAGE.toByteArray()).aggregate().get();
         assertThat(response.status()).isEqualTo(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
@@ -148,10 +294,10 @@ public class GrpcServiceServerTest {
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
                 HttpHeaders.of(HttpMethod.POST,
-                               TestServiceGrpc.METHOD_UNARY_CALL.getFullMethodName())
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName())
                            .set(HttpHeaderNames.CONTENT_TYPE, "application/protobuf")
                            .set(GrpcHeaderNames.GRPC_ENCODING, "gzip"),
-                GrpcTestUtil.REQUEST_MESSAGE.toByteArray()).aggregate().get();
+                REQUEST_MESSAGE.toByteArray()).aggregate().get();
         assertThat(response.status()).isEqualTo(HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
@@ -162,7 +308,7 @@ public class GrpcServiceServerTest {
                            HttpClient.class);
         AggregatedHttpMessage response = client.execute(
                 HttpHeaders.of(HttpMethod.POST,
-                               TestServiceGrpc.METHOD_UNARY_CALL.getFullMethodName())
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName())
                            .set(HttpHeaderNames.CONTENT_TYPE, "application/protobuf"),
                 SimpleRequest.newBuilder()
                              .setResponseStatus(
@@ -173,11 +319,24 @@ public class GrpcServiceServerTest {
     }
 
     @Test
-    public void framed() throws Exception {
-        TestServiceBlockingStub stub = Clients.newClient(
-                server.httpUri(GrpcSerializationFormats.PROTO, "/"),
-                TestServiceBlockingStub.class);
-        assertThat(stub.unaryCall(GrpcTestUtil.REQUEST_MESSAGE)).isEqualTo(GrpcTestUtil.RESPONSE_MESSAGE);
+    public void grpcWeb() throws Exception {
+        HttpClient client = HttpClientFactory.DEFAULT
+                .newClient("none+" + server.httpUri("/"),
+                           HttpClient.class);
+        AggregatedHttpMessage response = client.execute(
+                HttpHeaders.of(HttpMethod.POST,
+                               UnitTestServiceGrpc.METHOD_STATIC_UNARY_CALL.getFullMethodName())
+                           .set(HttpHeaderNames.CONTENT_TYPE, "application/grpc-web"),
+                GrpcTestUtil.uncompressedFrame(GrpcTestUtil.requestByteBuf())).aggregate().get();
+        byte[] serializedStatusHeader = "grpc-status: 0\r\n".getBytes(StandardCharsets.US_ASCII);
+        byte[] serializedTrailers = Bytes.concat(
+                new byte[] { ArmeriaServerCall.TRAILERS_FRAME_HEADER },
+                Ints.toByteArray(serializedStatusHeader.length),
+                serializedStatusHeader);
+        assertThat(response.content().array()).containsExactly(
+                Bytes.concat(
+                        GrpcTestUtil.uncompressedFrame(
+                                GrpcTestUtil.protoByteBuf(GrpcTestUtil.RESPONSE_MESSAGE)),
+                        serializedTrailers));
     }
-
 }
