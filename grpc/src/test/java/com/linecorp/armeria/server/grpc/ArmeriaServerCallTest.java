cf0bf6d18d4eef93800e6d664d57e3d3328ed1cc
Anuraag Agrawal <anuraaga@gmail.com>
2017-05-15 15:29:57 +0900

    Migrate main tests from ArmeriaServerCall to server tests.

--- grpc/src/test/java/com/linecorp/armeria/server/grpc/ArmeriaServerCallTest.java
+++ grpc/src/test/java/com/linecorp/armeria/server/grpc/ArmeriaServerCallTest.java
@@ -18,63 +18,38 @@ package com.linecorp.armeria.server.grpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-
-import java.nio.charset.StandardCharsets;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.reactivestreams.Subscription;
 
-import com.google.common.base.Strings;
-import com.google.common.io.ByteStreams;
-import com.google.protobuf.ByteString;
-
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
-import com.linecorp.armeria.common.http.HttpData;
-import com.linecorp.armeria.common.http.HttpHeaderNames;
 import com.linecorp.armeria.common.http.HttpHeaders;
-import com.linecorp.armeria.common.http.HttpObject;
 import com.linecorp.armeria.common.http.HttpResponseWriter;
 import com.linecorp.armeria.common.http.HttpStatus;
 import com.linecorp.armeria.common.logging.DefaultRequestLog;
-import com.linecorp.armeria.grpc.testing.Messages.Payload;
 import com.linecorp.armeria.grpc.testing.Messages.SimpleRequest;
 import com.linecorp.armeria.grpc.testing.Messages.SimpleResponse;
 import com.linecorp.armeria.grpc.testing.TestServiceGrpc;
 import com.linecorp.armeria.internal.grpc.ArmeriaMessageDeframer.ByteBufOrStream;
 import com.linecorp.armeria.internal.grpc.GrpcHeaderNames;
 import com.linecorp.armeria.internal.grpc.GrpcTestUtil;
-import com.linecorp.armeria.internal.http.ByteBufHttpData;
 import com.linecorp.armeria.server.ServiceRequestContext;
 
 import io.grpc.CompressorRegistry;
 import io.grpc.DecompressorRegistry;
-import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.Status;
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.ByteBufInputStream;
-import io.netty.buffer.ByteBufOutputStream;
-import io.netty.buffer.ByteBufUtil;
-import io.netty.buffer.UnpooledByteBufAllocator;
-import io.netty.channel.DefaultEventLoop;
 import io.netty.util.AsciiString;
-import io.netty.util.concurrent.DefaultEventExecutor;
 
 // TODO(anuraag): Currently only grpc-protobuf has been published so we only test proto here.
 // Once grpc-thrift is published, add tests for thrift stubs which will not go through the
@@ -128,39 +103,6 @@ public class ArmeriaServerCallTest {
     }
 
     @Test
-    public void nonStreamProtoMessage() throws Exception {
-        ByteBuf request = GrpcTestUtil.requestByteBuf();
-        assertThat(request.refCnt()).isEqualTo(1);
-        call.messageRead(new ByteBufOrStream(request));
-        call.messageReader().onComplete();
-        verify(listener).onMessage(GrpcTestUtil.REQUEST_MESSAGE);
-        assertThat(request.refCnt()).isEqualTo(0);
-    }
-
-    @Test
-    public void streamProtoMessage() throws Exception {
-        ByteBuf uncompressed = GrpcTestUtil.requestByteBuf();
-
-        ByteBuf request = UnpooledByteBufAllocator.DEFAULT.buffer();
-
-        // While any InputStream would be fine, let's go ahead and make sure a closed gzip
-        // stream releases the buffer.
-        try (ByteBufInputStream is = new ByteBufInputStream(uncompressed, true);
-             GZIPOutputStream os = new GZIPOutputStream(new ByteBufOutputStream(request))) {
-            ByteStreams.copy(is, os);
-        }
-
-        assertThat(uncompressed.refCnt()).isEqualTo(0);
-        assertThat(request.refCnt()).isEqualTo(1);
-
-        call.messageRead(new ByteBufOrStream(
-                new GZIPInputStream(new ByteBufInputStream(request, true))));
-        call.messageReader().onComplete();
-        verify(listener).onMessage(GrpcTestUtil.REQUEST_MESSAGE);
-        assertThat(request.refCnt()).isEqualTo(0);
-    }
-
-    @Test
     public void messageReadAfterClose_byteBuf() throws Exception {
         call.close(Status.ABORTED);
 
@@ -189,328 +131,6 @@ public class ArmeriaServerCallTest {
         assertThat(call.isReady()).isTrue();
         call.close(Status.OK);
         assertThat(call.isReady()).isFalse();
-    }
-
-    @Test
-    public void headers_defaults() throws Exception {
-        call.messageReader().cancel();
-        call.sendHeaders(new Metadata());
-        HttpHeaders expectedHeaders = DEFAULT_RESPONSE_HEADERS;
-        verify(res).write(expectedHeaders);
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void headers_noDecompressors() throws Exception {
-        call.messageReader().cancel();
-        call = new ArmeriaServerCall<>(
-                HttpHeaders.of(),
-                TestServiceGrpc.METHOD_UNARY_CALL,
-                CompressorRegistry.getDefaultInstance(),
-                DecompressorRegistry.emptyInstance(),
-                res,
-                MAX_MESSAGE_BYTES,
-                MAX_MESSAGE_BYTES,
-                ctx,
-                GrpcSerializationFormats.PROTO);
-        call.messageReader().cancel();
-        call.sendHeaders(new Metadata());
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-encoding"), "identity");
-        verify(res).write(expectedHeaders);
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void headers_callCompressionClientNoAccepts() throws Exception {
-        call.messageReader().cancel();
-        call.setCompression("gzip");
-        call.setMessageCompression(true);
-        call.sendHeaders(new Metadata());
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-encoding"), "identity")
-                           .set(AsciiString.of("grpc-accept-encoding"),
-                                DecompressorRegistry.getDefaultInstance().getAdvertisedMessageEncodings());
-        verify(res).write(expectedHeaders);
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void headers_callCompressionClientNoMatch() throws Exception {
-        call.messageReader().cancel();
-        call = new ArmeriaServerCall<>(
-                HttpHeaders.of().set(GrpcHeaderNames.GRPC_ACCEPT_ENCODING, "pied-piper,quantum"),
-                TestServiceGrpc.METHOD_UNARY_CALL,
-                CompressorRegistry.getDefaultInstance(),
-                DecompressorRegistry.getDefaultInstance(),
-                res,
-                MAX_MESSAGE_BYTES,
-                MAX_MESSAGE_BYTES,
-                ctx,
-                GrpcSerializationFormats.PROTO);
-        call.messageReader().cancel();
-        call.setCompression("gzip");
-        call.setMessageCompression(true);
-        call.sendHeaders(new Metadata());
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-encoding"), "identity")
-                           .set(AsciiString.of("grpc-accept-encoding"),
-                                DecompressorRegistry.getDefaultInstance().getAdvertisedMessageEncodings());
-        verify(res).write(expectedHeaders);
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void headers_callCompressionClientMatch() throws Exception {
-        call.messageReader().cancel();
-        call = responseCompressionCall();
-        call.messageReader().cancel();
-        call.setCompression("gzip");
-        call.setMessageCompression(true);
-        call.sendHeaders(new Metadata());
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-encoding"), "gzip")
-                           .set(AsciiString.of("grpc-accept-encoding"),
-                                DecompressorRegistry.getDefaultInstance().getAdvertisedMessageEncodings());
-        verify(res).write(expectedHeaders);
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void sendMessage() throws Exception {
-        call.messageReader().cancel();
-        call.sendHeaders(new Metadata());
-        call.sendMessage(GrpcTestUtil.RESPONSE_MESSAGE);
-
-        verify(res).write(isA(HttpHeaders.class));
-        verify(res).write(HttpData.of(GrpcTestUtil.uncompressedResponseBytes()));
-        verifyNoMoreInteractions(res);
-
-        verify(listener, times(1)).onReady();
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void sendCompressedMessage() throws Exception {
-        call.messageReader().cancel();
-        call = responseCompressionCall();
-        call.messageReader().cancel();
-        call.setListener(listener);
-        call.setCompression("gzip");
-        call.setMessageCompression(true);
-        call.sendHeaders(new Metadata());
-        call.sendMessage(GrpcTestUtil.RESPONSE_MESSAGE);
-
-        verify(res).write(isA(HttpHeaders.class));
-        verify(res).write(HttpData.of(GrpcTestUtil.compressedResponseBytes()));
-        verifyNoMoreInteractions(res);
-
-        verify(listener, times(1)).onReady();
-        verifyNoMoreInteractions(listener);
-    }
-
-    @Test
-    public void error_noMessage() throws Exception {
-        call.onError(Status.ABORTED);
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-status"), "10");
-        verify(res).write(expectedHeaders);
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-        verify(listener).onCancel();
-        verify(subscription).cancel();
-    }
-
-    @Test
-    public void error_withMessage() throws Exception {
-        call.onError(Status.ABORTED.withDescription("aborted call"));
-        HttpHeaders expectedHeaders =
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(AsciiString.of("content-type"), "application/grpc+proto")
-                           .set(AsciiString.of("grpc-status"), "10")
-                           .set(AsciiString.of("grpc-message"), "aborted call");
-        verify(res).write(expectedHeaders);
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-        verify(listener).onCancel();
-        verify(subscription).cancel();
-    }
-
-    @Test
-    public void fullCall_success() throws Exception {
-        when(ctx.eventLoop()).thenReturn(new DefaultEventLoop(new DefaultEventExecutor()));
-        call.request(2);
-        ctx.eventLoop().shutdownGracefully().get();
-        call.messageReader().onNext(HttpData.of(GrpcTestUtil.uncompressedFrame(GrpcTestUtil.requestByteBuf())));
-        verify(listener).onMessage(GrpcTestUtil.REQUEST_MESSAGE);
-        call.messageReader().onComplete();
-        verify(listener).onHalfClose();
-        call.sendHeaders(new Metadata());
-        call.sendMessage(GrpcTestUtil.RESPONSE_MESSAGE);
-        verify(listener).onReady();
-        call.close(Status.OK);
-        verify(listener).onComplete();
-        verifyNoMoreInteractions(listener);
-
-        verify(res).write(DEFAULT_RESPONSE_HEADERS);
-        verify(res).write(HttpData.of(GrpcTestUtil.uncompressedResponseBytes()));
-        verify(res).write(HttpHeaders.of().set(AsciiString.of("grpc-status"), "0"));
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-    }
-
-    @Test
-    public void fullCall_error() throws Exception {
-        when(ctx.eventLoop()).thenReturn(new DefaultEventLoop(new DefaultEventExecutor()));
-        call.request(2);
-        ctx.eventLoop().shutdownGracefully().get();
-        call.messageReader().onNext(HttpData.of(GrpcTestUtil.uncompressedFrame(GrpcTestUtil.requestByteBuf())));
-        verify(listener).onMessage(GrpcTestUtil.REQUEST_MESSAGE);
-        call.messageReader().onComplete();
-        verify(listener).onHalfClose();
-        call.sendHeaders(new Metadata());
-        call.sendMessage(GrpcTestUtil.RESPONSE_MESSAGE);
-        verify(listener).onReady();
-        call.close(Status.ABORTED);
-        verify(listener).onCancel();
-        verifyNoMoreInteractions(listener);
-
-        verify(res).write(DEFAULT_RESPONSE_HEADERS);
-        verify(res).write(HttpData.of(GrpcTestUtil.uncompressedResponseBytes()));
-        verify(res).write(HttpHeaders.of().set(AsciiString.of("grpc-status"), "10"));
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-
-        // Error happened after client stream ended.
-        verify(subscription, never()).cancel();
-    }
-
-    @Test
-    public void tooLargeRequest() throws Exception {
-        when(ctx.eventLoop()).thenReturn(new DefaultEventLoop(new DefaultEventExecutor()));
-        SimpleRequest request =
-                SimpleRequest.newBuilder()
-                             .setPayload(Payload.newBuilder()
-                                                .setBody(ByteString.copyFromUtf8(
-                                                        Strings.repeat("a", 1024))))
-                             .build();
-        call.request(2);
-        ctx.eventLoop().shutdownGracefully().get();
-        call.messageReader().onNext(
-                HttpData.of(GrpcTestUtil.uncompressedFrame(GrpcTestUtil.protoByteBuf(request))));
-        verify(res).write(HttpHeaders.of(HttpStatus.OK)
-                                     .set(HttpHeaderNames.CONTENT_TYPE, "application/grpc+proto")
-                                     .set(AsciiString.of("grpc-status"),
-                                          Integer.toString(Status.RESOURCE_EXHAUSTED.getCode().value()))
-                                     .set(AsciiString.of("grpc-message"),
-                                          "com.linecorp.armeria.internal.grpc.ArmeriaMessageDeframer: " +
-                                          "Frame size 1030 exceeds maximum: 1024. "));
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-        verify(listener).onCancel();
-        verifyNoMoreInteractions(listener);
-
-        assertThat(call.messageReader().deframer.isClosed()).isTrue();
-        verify(subscription).cancel();
-    }
-
-    @Test
-    public void tooLargeRequestCompressed() throws Exception {
-        when(ctx.eventLoop()).thenReturn(new DefaultEventLoop(new DefaultEventExecutor()));
-        call.messageReader().cancel();
-        reset(subscription);
-        call = new ArmeriaServerCall<>(
-                HttpHeaders.of().set(GrpcHeaderNames.GRPC_ENCODING, "gzip"),
-                TestServiceGrpc.METHOD_UNARY_CALL,
-                CompressorRegistry.getDefaultInstance(),
-                DecompressorRegistry.getDefaultInstance(), res,
-                MAX_MESSAGE_BYTES,
-                MAX_MESSAGE_BYTES,
-                ctx,
-                GrpcSerializationFormats.PROTO);
-        call.messageReader().onSubscribe(subscription);
-        call.setListener(listener);
-        call.setCompression("gzip");
-        call.setMessageCompression(true);
-        SimpleRequest request =
-                SimpleRequest.newBuilder()
-                             .setPayload(Payload.newBuilder()
-                                                .setBody(ByteString.copyFromUtf8(
-                                                        Strings.repeat("a", 1024))))
-                             .build();
-        call.request(2);
-        ctx.eventLoop().shutdownGracefully().get();
-        call.messageReader().onNext(
-                HttpData.of(GrpcTestUtil.compressedFrame(GrpcTestUtil.protoByteBuf(request))));
-        verify(res).write(
-                HttpHeaders.of(HttpStatus.OK)
-                           .set(HttpHeaderNames.CONTENT_TYPE, "application/grpc+proto")
-                           .set(AsciiString.of("grpc-status"), "13")
-                           .set(AsciiString.of("grpc-message"),
-                                "com.linecorp.armeria.internal.grpc.ArmeriaMessageDeframer: " +
-                                "Compressed frame exceeds maximum frame size: 1024. Bytes read: 1030. "));
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-        verify(listener).onCancel();
-        verifyNoMoreInteractions(listener);
-
-        assertThat(call.messageReader().deframer.isClosed()).isTrue();
-        verify(subscription).cancel();
-    }
-
-    @Test
-    public void grpcWeb() throws Exception {
-        call.messageReader().cancel();
-        call = new ArmeriaServerCall<>(
-                HttpHeaders.of(),
-                TestServiceGrpc.METHOD_UNARY_CALL,
-                CompressorRegistry.getDefaultInstance(),
-                DecompressorRegistry.getDefaultInstance(),
-                res,
-                MAX_MESSAGE_BYTES,
-                MAX_MESSAGE_BYTES,
-                ctx,
-                GrpcSerializationFormats.PROTO_WEB);
-        call.setListener(listener);
-        call.messageReader().onSubscribe(subscription);
-        when(ctx.eventLoop()).thenReturn(new DefaultEventLoop(new DefaultEventExecutor()));
-        call.request(2);
-        ctx.eventLoop().shutdownGracefully().get();
-        call.messageReader().onNext(HttpData.of(GrpcTestUtil.uncompressedFrame(GrpcTestUtil.requestByteBuf())));
-        call.messageReader().onComplete();
-        call.sendHeaders(new Metadata());
-        call.sendMessage(GrpcTestUtil.RESPONSE_MESSAGE);
-        call.close(Status.OK);
-
-        ArgumentCaptor<HttpObject> trailersCaptor = ArgumentCaptor.forClass(HttpObject.class);
-        verify(res, times(3)).write(trailersCaptor.capture());
-        assertThat(trailersCaptor.getAllValues().get(0))
-                .isEqualTo(
-                        HttpHeaders.copyOf(DEFAULT_RESPONSE_HEADERS)
-                                   .set(HttpHeaderNames.CONTENT_TYPE,
-                                        GrpcSerializationFormats.PROTO_WEB.mediaType().toString()));
-        assertThat(trailersCaptor.getAllValues().get(1))
-                .isEqualTo(HttpData.of(GrpcTestUtil.uncompressedResponseBytes()));
-        ByteBufHttpData data = (ByteBufHttpData) trailersCaptor.getAllValues().get(2);
-        assertThat(data.buf().getByte(0)).isEqualTo(ArmeriaServerCall.TRAILERS_FRAME_HEADER);
-        String expectedHeader = "grpc-status: 0\r\n";
-        int length = data.buf().getInt(1);
-        assertThat(length).isEqualTo(expectedHeader.length());
-        assertThat(new String(ByteBufUtil.getBytes(data.buf(), 5, length), StandardCharsets.US_ASCII))
-                .isEqualTo(expectedHeader);
-        verify(res).close();
-        verifyNoMoreInteractions(res);
-        data.buf().release();
     }
 
     private ArmeriaServerCall<SimpleRequest, SimpleResponse> responseCompressionCall() {
