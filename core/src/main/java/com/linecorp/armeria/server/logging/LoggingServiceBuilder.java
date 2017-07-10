0b2ab59962f31a93ab34142553b3339d36834e41
Anuraag Agrawal <anuraaga@gmail.com>
2017-07-10 14:14:10 +0900

    Fix javadoc

--- core/src/main/java/com/linecorp/armeria/server/logging/LoggingServiceBuilder.java
+++ core/src/main/java/com/linecorp/armeria/server/logging/LoggingServiceBuilder.java
@@ -118,7 +118,7 @@ public class LoggingServiceBuilder {
     /**
      * Sets the rate at which to sample requests to log. Any number between {@code 0.0} and {@code 1.0} will
      * cause a random sample of the requests to be logged. The random sampling is appropriate for low-traffic
-     * (ex servers that each receive <100K requests). If unset, all requests will be logged.
+     * (ex servers that each receive &lt;100K requests). If unset, all requests will be logged.
      */
     public LoggingServiceBuilder samplingRate(float samplingRate) {
         checkArgument(0.0 <= samplingRate && samplingRate <= 1.0, "samplingRate must be between 0.0 and 1.0");
