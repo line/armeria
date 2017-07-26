085e3e1759b31a5c7d27f0ff7f4a2eb0dd17f927
Masahiro Ide <imasahiro9@gmail.com>
2017-07-26 12:11:25 +0900

    Customize http status for error response (#691)
    Motivations:
    - The error behavior of HttpResponseSubscriber#onError is hard-coded and cannot customize error code.
    
    Modifications:
    - Add HttpResponseException which makes more customizable error handling
    
    Results:
    - A user can customize error code through HttpResponseException
    - Fixes #543

---
+++ core/src/main/java/com/linecorp/armeria/server/HttpResponseException.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.server;
+
+import static java.util.Objects.requireNonNull;
+
+import com.linecorp.armeria.common.Flags;
+import com.linecorp.armeria.common.HttpStatus;
+
+/**
+ * A {@link RuntimeException} that is raised when an armeria internal http exception has occurred.
+ * This class is the general class of exceptions produced by a failed request or a reset stream.
+ */
+public abstract class HttpResponseException extends RuntimeException {
+    private static final long serialVersionUID = 3487991462085151316L;
+
+    private final HttpStatus httpStatus;
+
+    /**
+     * Creates a new instance.
+     */
+    protected HttpResponseException(HttpStatus httpStatus) {
+        super(requireNonNull(httpStatus, "httpStatus").toString());
+        if (100 <= httpStatus.code() && httpStatus.code() < 400) {
+            throw new IllegalArgumentException(
+                    "httpStatus: " + httpStatus +
+                    " (expected: a status that's neither informational, success nor redirection)");
+        }
+        this.httpStatus = httpStatus;
+    }
+
+    /**
+     * Returns the {@link HttpStatus} that will be sent to a client.
+     */
+    public HttpStatus httpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        if (Flags.verboseExceptions()) {
+            return super.fillInStackTrace();
+        }
+        return this;
+    }
+}
