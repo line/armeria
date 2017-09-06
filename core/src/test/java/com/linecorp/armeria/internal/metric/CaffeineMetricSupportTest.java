a7689ce7247db21994ce85fe3f3430a933966b8a
Trustin Lee <t@motd.kr>
2017-09-06 12:25:34 +0900

    Make CaffineMetricSupport more tolerant to double registration (#747)
    Motivation:
    
    Currently, CaffeineMetricsSupport raises an IllegalStateException when a
    user tries to call setup() with different Cache instances at the same
    metric ID. This can result in the failure of Server startup when a user
    reuses a registry or uses a Metrics.globalRegistry.
    
    Modifications:
    
    - Improve CaffeineMetricsSupport so that it aggregates the stats of all
      Cache instances instead of rejection
    
    Result:
    
    Less surprise to users

--- core/src/test/java/com/linecorp/armeria/internal/metric/CaffeineMetricSupportTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/metric/CaffeineMetricSupportTest.java
@@ -18,19 +18,22 @@ package com.linecorp.armeria.internal.metric;
 
 import static com.linecorp.armeria.internal.metric.CaffeineMetricSupport.UPDATE_INTERVAL_NANOS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 
 import org.junit.Test;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import com.github.benmanes.caffeine.cache.Policy;
 import com.github.benmanes.caffeine.cache.stats.CacheStats;
-import com.google.common.collect.ImmutableList;
 
 import com.linecorp.armeria.common.metric.MeterId;
 import com.linecorp.armeria.common.metric.MoreMeters;
@@ -42,16 +45,13 @@ public class CaffeineMetricSupportTest {
 
     @Test
     public void test() {
-        final LoadingCache<?, ?> cache = mock(LoadingCache.class);
-        when(cache.stats()).thenReturn(new CacheStats(1, 2, 3, 4, 5, 6, 7));
-        when(cache.estimatedSize()).thenReturn(8L);
-
+        final MockLoadingCache cache = new MockLoadingCache(1, 2, 3, 4, 5, 6, 7, 8);
         final AtomicLong ticker = new AtomicLong();
         final MeterRegistry registry = PrometheusMeterRegistries.newRegistry();
         CaffeineMetricSupport.setup(registry, new MeterId("foo"), cache, ticker::get);
 
-        verify(cache, times(1)).stats();
-        verify(cache, times(1)).estimatedSize();
+        assertThat(cache.statsCalls()).isOne();
+        assertThat(cache.estimatedSizeCalls()).isOne();
 
         assertThat(MoreMeters.measureAll(registry))
                 .containsEntry("foo.requests#count{result=hit}", 1.0)
@@ -64,13 +64,12 @@ public class CaffeineMetricSupportTest {
                 .containsEntry("foo.estimatedSize#value", 8.0);
 
         // Make sure Cache.stats() and estimatedSize() are not called since the initial update.
-        verify(cache, times(1)).stats();
-        verify(cache, times(1)).estimatedSize();
+        assertThat(cache.statsCalls()).isOne();
+        assertThat(cache.estimatedSizeCalls()).isOne();
 
         // Advance the ticker so that the next collection triggers stats() and estimatedSize().
         ticker.addAndGet(UPDATE_INTERVAL_NANOS);
-        when(cache.stats()).thenReturn(new CacheStats(9, 10, 11, 12, 13, 14, 15));
-        when(cache.estimatedSize()).thenReturn(16L);
+        cache.update(9, 10, 11, 12, 13, 14, 15, 16);
 
         assertThat(MoreMeters.measureAll(registry))
                 .containsEntry("foo.requests#count{result=hit}", 9.0)
@@ -83,22 +82,19 @@ public class CaffeineMetricSupportTest {
                 .containsEntry("foo.estimatedSize#value", 16.0);
 
         // Make sure Cache.stats() and estimatedSize() were called once more since the initial update.
-        verify(cache, times(2)).stats();
-        verify(cache, times(2)).estimatedSize();
+        assertThat(cache.statsCalls()).isEqualTo(2);
+        assertThat(cache.estimatedSizeCalls()).isEqualTo(2);
     }
 
     @Test
     public void testNonLoadingCache() {
-        final Cache<?, ?> cache = mock(Cache.class);
-        when(cache.stats()).thenReturn(new CacheStats(1, 2, 0, 0, 0, 3, 4));
-        when(cache.estimatedSize()).thenReturn(5L);
-
+        final MockCache cache = new MockCache(1, 2, 3, 4, 5);
         final AtomicLong ticker = new AtomicLong();
         final MeterRegistry registry = PrometheusMeterRegistries.newRegistry();
-        CaffeineMetricSupport.setup(registry, new MeterId("bar", ImmutableList.of()), cache, ticker::get);
+        CaffeineMetricSupport.setup(registry, new MeterId("bar"), cache, ticker::get);
 
-        verify(cache, times(1)).stats();
-        verify(cache, times(1)).estimatedSize();
+        assertThat(cache.statsCalls()).isOne();
+        assertThat(cache.estimatedSizeCalls()).isOne();
 
         assertThat(MoreMeters.measureAll(registry))
                 .containsEntry("bar.requests#count{result=hit}", 1.0)
@@ -112,5 +108,231 @@ public class CaffeineMetricSupportTest {
                 "bar.loads#count{result=success}",
                 "bar.loads#count{result=failure}",
                 "bar.loadDuration#count");
+    }
+
+    @Test
+    public void aggregation() {
+        final MockLoadingCache cache1 = new MockLoadingCache(1, 2, 3, 4, 5, 6, 7, 8);
+        final MockLoadingCache cache2 = new MockLoadingCache(9, 10, 11, 12, 13, 14, 15, 16);
+        final MeterRegistry registry = PrometheusMeterRegistries.newRegistry();
+        final MeterId id = new MeterId("baz");
+
+        // Register two caches at the same meter ID.
+        CaffeineMetricSupport.setup(registry, id, cache1);
+        CaffeineMetricSupport.setup(registry, id, cache2);
+
+        // .. and their stats are aggregated.
+        assertThat(MoreMeters.measureAll(registry))
+                .containsEntry("baz.requests#count{result=hit}", 10.0)
+                .containsEntry("baz.requests#count{result=miss}", 12.0)
+                .containsEntry("baz.loads#count{result=success}", 14.0)
+                .containsEntry("baz.loads#count{result=failure}", 16.0)
+                .containsEntry("baz.loadDuration#count", 18.0)
+                .containsEntry("baz.evictions#count", 20.0)
+                .containsEntry("baz.evictionWeight#count", 22.0)
+                .containsEntry("baz.estimatedSize#value", 24.0);
+    }
+
+    @Test
+    public void aggregationAfterGC() throws Exception {
+        final MockCache cache1 = new MockCache(1, 2, 3, 4, 5);
+        final WeakReference<MockLoadingCache> cache2 =
+                new WeakReference<>(new MockLoadingCache(6, 7, 8, 9, 10, 11, 12, 13));
+        final MeterRegistry registry = PrometheusMeterRegistries.newRegistry();
+        final AtomicLong ticker = new AtomicLong();
+        final MeterId id = new MeterId("baz");
+
+        // Register two caches at the same meter ID.
+        CaffeineMetricSupport.setup(registry, id, cache1, ticker::get);
+        CaffeineMetricSupport.setup(registry, id, cache2.get(), ticker::get);
+
+        assertThat(MoreMeters.measureAll(registry))
+                .containsEntry("baz.requests#count{result=hit}", 7.0)
+                .containsEntry("baz.requests#count{result=miss}", 9.0)
+                .containsEntry("baz.loads#count{result=success}", 8.0)
+                .containsEntry("baz.loads#count{result=failure}", 9.0)
+                .containsEntry("baz.loadDuration#count", 10.0)
+                .containsEntry("baz.evictions#count", 14.0)
+                .containsEntry("baz.evictionWeight#count", 16.0)
+                .containsEntry("baz.estimatedSize#value", 18.0);
+
+        ticker.addAndGet(UPDATE_INTERVAL_NANOS);
+
+        // Ensure the weak reference which held the cache is cleaned up.
+        System.gc();
+        Thread.sleep(1000);
+        assertThat(cache2.get()).isNull();
+
+        // Check if the counters are not decreased after the second cache is GC'd.
+        assertThat(MoreMeters.measureAll(registry))
+                .containsEntry("baz.requests#count{result=hit}", 7.0)
+                .containsEntry("baz.requests#count{result=miss}", 9.0)
+                .containsEntry("baz.loads#count{result=success}", 8.0)
+                .containsEntry("baz.loads#count{result=failure}", 9.0)
+                .containsEntry("baz.loadDuration#count", 10.0)
+                .containsEntry("baz.evictions#count", 14.0)
+                .containsEntry("baz.evictionWeight#count", 16.0)
+                .containsEntry("baz.estimatedSize#value", 5.0); // .. except 'estimatedSize' which is a gauge
+    }
+
+    @Test
+    public void sameCacheTwice() {
+        final MockCache cache = new MockCache(1, 2, 3, 4, 5);
+        final MeterRegistry registry = PrometheusMeterRegistries.newRegistry();
+        final MeterId id = new MeterId("baz");
+
+        // Register the same cache twice at the same meter ID.
+        CaffeineMetricSupport.setup(registry, id, cache);
+        CaffeineMetricSupport.setup(registry, id, cache);
+
+        // .. and check if the stats are *not* doubled.
+        assertThat(MoreMeters.measureAll(registry))
+                .containsEntry("baz.requests#count{result=hit}", 1.0)
+                .containsEntry("baz.requests#count{result=miss}", 2.0)
+                .containsEntry("baz.evictions#count", 3.0)
+                .containsEntry("baz.evictionWeight#count", 4.0)
+                .containsEntry("baz.estimatedSize#value", 5.0);
+    }
+
+    // Not using mocking framework because we need tight control over the life cycle of the mock object.
+    private static class MockCache implements Cache<Object, Object> {
+
+        private CacheStats stats;
+        private long estimatedSize;
+        private int statsCalls;
+        private int estimatedSizeCalls;
+
+        MockCache(long hitCount, long missCount, long evictionCount, long evictionWeight, long estimatedSize) {
+            update(hitCount, missCount, evictionCount, evictionWeight, estimatedSize);
+        }
+
+        MockCache(long hitCount, long missCount, long loadSuccessCount, long loadFailureCount,
+                  long totalLoadTime, long evictionCount, long evictionWeight, long estimatedSize) {
+
+            update(hitCount, missCount, loadSuccessCount, loadFailureCount,
+                   totalLoadTime, evictionCount, evictionWeight, estimatedSize);
+        }
+
+        void update(long hitCount, long missCount,
+                    long evictionCount, long evictionWeight, long estimatedSize) {
+
+            update(hitCount, missCount, 0, 0, 0, evictionCount, evictionWeight, estimatedSize);
+        }
+
+        void update(long hitCount, long missCount, long loadSuccessCount, long loadFailureCount,
+                    long totalLoadTime, long evictionCount, long evictionWeight, long estimatedSize) {
+            stats = new CacheStats(hitCount, missCount, loadSuccessCount, loadFailureCount,
+                                   totalLoadTime, evictionCount, evictionWeight);
+            this.estimatedSize = estimatedSize;
+        }
+
+        int statsCalls() {
+            return statsCalls;
+        }
+
+        int estimatedSizeCalls() {
+            return estimatedSizeCalls;
+        }
+
+        @Nonnull
+        @Override
+        public CacheStats stats() {
+            statsCalls++;
+            return stats;
+        }
+
+        @Override
+        public long estimatedSize() {
+            estimatedSizeCalls++;
+            return estimatedSize;
+        }
+
+        @Override
+        public Object getIfPresent(@Nonnull Object key) {
+            return reject();
+        }
+
+        @Override
+        public Object get(@Nonnull Object key, @Nonnull Function<? super Object, ?> mappingFunction) {
+            return reject();
+        }
+
+        @Nonnull
+        @Override
+        public Map<Object, Object> getAllPresent(@Nonnull Iterable<?> keys) {
+            return reject();
+        }
+
+        @Override
+        public void put(@Nonnull Object key, @Nonnull Object value) {
+            reject();
+        }
+
+        @Override
+        public void putAll(@Nonnull Map<?, ?> map) {
+            reject();
+        }
+
+        @Override
+        public void invalidate(@Nonnull Object key) {
+            reject();
+        }
+
+        @Override
+        public void invalidateAll(@Nonnull Iterable<?> keys) {
+            reject();
+        }
+
+        @Override
+        public void invalidateAll() {
+            reject();
+        }
+
+        @Nonnull
+        @Override
+        public ConcurrentMap<Object, Object> asMap() {
+            return reject();
+        }
+
+        @Override
+        public void cleanUp() {
+            reject();
+        }
+
+        @Nonnull
+        @Override
+        public Policy<Object, Object> policy() {
+            return reject();
+        }
+
+        protected static <T> T reject() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    private static final class MockLoadingCache extends MockCache implements LoadingCache<Object, Object> {
+
+        MockLoadingCache(long hitCount, long missCount, long loadSuccessCount, long loadFailureCount,
+                         long totalLoadTime, long evictionCount, long evictionWeight, long estimatedSize) {
+            super(hitCount, missCount, loadSuccessCount, loadFailureCount,
+                  totalLoadTime, evictionCount, evictionWeight, estimatedSize);
+        }
+
+        @CheckForNull
+        @Override
+        public Object get(@Nonnull Object key) {
+            return reject();
+        }
+
+        @Nonnull
+        @Override
+        public Map<Object, Object> getAll(@Nonnull Iterable<?> keys) {
+            return reject();
+        }
+
+        @Override
+        public void refresh(@Nonnull Object key) {
+            reject();
+        }
     }
 }
