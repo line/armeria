line/armeria

commit 4f0cee147101db3911b36c6c1a8525be69199cfe

Execute stream callbacks directly when executor is the current event loop (#836)
.. for reduced latency between stream elements.

benchmarks/src/jmh/java/com/linecorp/armeria/benchmarks/core/StreamMessageBenchmark.java
@@ -1,0 +1,167 @@
+/*
+ * Copyright 2017 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.benchmarks.core;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import com.linecorp.armeria.benchmarks.shared.EventLoopJmhExecutor;
+import com.linecorp.armeria.common.stream.DefaultStreamMessage;
+
+import io.netty.channel.DefaultEventLoop;
+import io.netty.channel.EventLoop;
+
+@Fork(jvmArgsAppend = { EventLoopJmhExecutor.JVM_ARG_1, EventLoopJmhExecutor.JVM_ARG_2 })
+@State(Scope.Benchmark)
+public class StreamMessageBenchmark {
+
+    private static final EventLoop ANOTHER_EVENT_LOOP = new DefaultEventLoop();
+
+    @State(Scope.Thread)
+    public static class StreamObjects {
+
+        private final ArrayList<Integer> values = new ArrayList<>();
+
+        @Param({ "3", "5", "20", "100", "1000" })
+        private int num;
+
+        private long sum;
+
+        private SummingSubscriber subscriber;
+
+        private CountDownLatch completedLatch;
+
+        @Setup(Level.Invocation)
+        public void setValues() {
+            completedLatch = new CountDownLatch(1);
+            values.clear();
+            sum = 0;
+            for (int i = 0; i < num; i++) {
+                values.add(i);
+                sum += i;
+            }
+            subscriber = new SummingSubscriber(completedLatch);
+        }
+
+        private long computedSum() {
+            long computedSum = subscriber.sum();
+            if (computedSum != sum) {
+                throw new IllegalStateException(
+                        "Did not compute the expected sum, the stream implementation is broken, expected: " +
+                        sum + ", got: " + computedSum);
+            }
+            return computedSum;
+        }
+    }
+
+    @TearDown(Level.Trial)
+    public void closeEventLoop() {
+        ANOTHER_EVENT_LOOP.shutdownGracefully().syncUninterruptibly();
+    }
+
+    @Benchmark
+    public long defaultStreamMessage_noExecutor(StreamObjects streamObjects) {
+        DefaultStreamMessage<Integer> stream = new DefaultStreamMessage<>();
+        stream.subscribe(streamObjects.subscriber);
+        streamObjects.values.forEach(stream::write);
+        stream.close();
+        // No executor, so sum will be updated inline.
+        return streamObjects.computedSum();
+    }
+
+    // Isolates performance of stream operations, but requires the stream to execute events inline or it would
+    // deadlock.
+    @Benchmark
+    public long defaultStreamMessage_jmhEventLoop(StreamObjects streamObjects) {
+        DefaultStreamMessage<Integer> stream = new DefaultStreamMessage<>();
+        stream.subscribe(streamObjects.subscriber, EventLoopJmhExecutor.currentEventLoop());
+        streamObjects.values.forEach(stream::write);
+        stream.close();
+        return streamObjects.computedSum();
+    }
+
+    // Has synchronization overhead, but does not require the stream to execute events inline so can be used
+    // to compare approaches.
+    @Benchmark
+    public long defaultStreamMessage_notJmhEventLoop(StreamObjects streamObjects) throws Exception {
+        ANOTHER_EVENT_LOOP.execute(() -> {
+            DefaultStreamMessage<Integer> stream = new DefaultStreamMessage<>();
+            stream.subscribe(streamObjects.subscriber, ANOTHER_EVENT_LOOP);
+            streamObjects.values.forEach(stream::write);
+            stream.close();
+        });
+        streamObjects.completedLatch.await(10, TimeUnit.SECONDS);
+        return streamObjects.computedSum();
+    }
+
+    private static final class SummingSubscriber implements Subscriber<Integer> {
+
+        private final CountDownLatch completedLatch;
+
+        private long sum;
+        private boolean complete;
+        private Throwable error;
+
+        private SummingSubscriber(CountDownLatch completedLatch) {
+            this.completedLatch = completedLatch;
+        }
+
+        private long sum() {
+            if (!complete) {
+                return -1;
+            }
+            if (error != null) {
+                return -1;
+            }
+            return sum;
+        }
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            s.request(Long.MAX_VALUE);
+        }
+
+        @Override
+        public void onNext(Integer value) {
+            sum += value;
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            error = t;
+            completedLatch.countDown();
+        }
+
+        @Override
+        public void onComplete() {
+            complete = true;
+            completedLatch.countDown();
+        }
+    }
+}

core/src/main/java/com/linecorp/armeria/common/stream/DefaultStreamMessage.java
@@ -40,6 +40,8 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufHolder;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.EventLoop;
+import io.netty.channel.SingleThreadEventLoop;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.ReferenceCounted;
 
@@ -193,7 +195,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             return;
         }
 
-        if (executor != null) {
+        if (shouldRunInExecutor(executor)) {
             // NB: We did not use subscriber.onSubscribe() because that will increase the memory footprint
             //     of the anonymous class by referring to 2 variables in the lambda expression.
             executor.execute(() -> subscription.subscriber().onSubscribe(subscription));
@@ -211,7 +213,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             cause = new IllegalStateException("subscribed by other subscriber already");
         }
 
-        if (executor != null) {
+        if (shouldRunInExecutor(executor)) {
             executor.execute(() -> {
                 lateSubscriber.onSubscribe(NoopSubscription.INSTANCE);
                 lateSubscriber.onError(cause);
@@ -296,7 +298,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
 
         final Executor executor = subscription.executor();
-        if (executor != null) {
+        if (shouldRunInExecutor(executor)) {
             executor.execute(() -> notifySubscriber(subscription, queue));
         } else {
             notifySubscriber(subscription, queue);
@@ -492,6 +494,13 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
     }
 
+    // We directly run callbacks for event loops if we're already on the loop, which applies to the vast
+    // majority of cases.
+    private static boolean shouldRunInExecutor(@Nullable Executor executor) {
+        return executor != null &&
+               (!(executor instanceof SingleThreadEventLoop) || !((EventLoop) executor).inEventLoop());
+    }
+
     private static final class SubscriptionImpl implements Subscription {
 
         private final DefaultStreamMessage<?> publisher;
@@ -592,6 +601,8 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
                     // but we need to ensure the closeFuture is notified and any pending objects are removed.
                     if (publisher.setState(State.CLOSED, State.CLEANUP)) {
                         final Executor executor = executor();
+                        // TODO(anuraag): Consider pushing a cleanup event instead of serializing the activity
+                        // through the event loop.
                         if (executor != null) {
                             executor.execute(publisher::cleanup);
                         } else {
@@ -610,7 +621,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
 
         private void invokeOnError(Throwable cause) {
-            if (executor != null) {
+            if (shouldRunInExecutor(executor)) {
                 executor.execute(() -> subscriber.onError(cause));
             } else {
                 subscriber.onError(cause);

core/src/test/java/com/linecorp/armeria/common/stream/DefaultStreamMessageTest.java
@@ -25,6 +25,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -36,10 +38,24 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufHolder;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.buffer.UnpooledHeapByteBuf;
+import io.netty.channel.DefaultEventLoop;
+import io.netty.channel.EventLoop;
 import io.netty.util.AbstractReferenceCounted;
 import io.netty.util.ReferenceCounted;
 
 public class DefaultStreamMessageTest {
+
+    private static EventLoop eventLoop;
+
+    @BeforeClass
+    public static void startEventLoop() {
+        eventLoop = new DefaultEventLoop();
+    }
+
+    @AfterClass
+    public static void stopEventLoop() {
+        eventLoop.shutdownGracefully().syncUninterruptibly();
+    }
 
     @Test
     public void full_writeFirst() throws Exception {
@@ -132,6 +148,40 @@ public class DefaultStreamMessageTest {
             public void onComplete() {
             }
         });
+        assertThat(result).containsExactlyElementsOf(input);
+    }
+
+    @Test
+    public void flowControlled_writeThenDemandThenProcess_eventLoop() throws Exception {
+        List<Integer> result = new ArrayList<>();
+        DefaultStreamMessage<Integer> stream = new DefaultStreamMessage<>();
+        List<Integer> input = IntStream.range(0, 10).boxed().collect(toImmutableList());
+        input.forEach(stream::write);
+        eventLoop.submit(() ->
+            stream.subscribe(new Subscriber<Integer>() {
+
+                private Subscription subscription;
+
+                @Override
+                public void onSubscribe(Subscription s) {
+                    subscription = s;
+                    subscription.request(1);
+                }
+
+                @Override
+                public void onNext(Integer value) {
+                    subscription.request(1);
+                    result.add(value);
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                }
+
+                @Override
+                public void onComplete() {
+                }
+            }, eventLoop)).syncUninterruptibly();
         assertThat(result).containsExactlyElementsOf(input);
     }
 

benchmarks/src/jmh/java/com/linecorp/armeria/benchmarks/shared/EventLoopJmhExecutor.java
@@ -1,0 +1,64 @@
+/*
+ * Copyright 2017 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.benchmarks.shared;
+
+import java.util.concurrent.Executor;
+
+import io.netty.channel.DefaultEventLoop;
+import io.netty.channel.EventLoop;
+import io.netty.channel.MultithreadEventLoopGroup;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import io.netty.util.concurrent.FastThreadLocal;
+
+/**
+ * A {@link MultithreadEventLoopGroup} that can be used as the JMH executor for benchmarks. This allows
+ * benchmark code to be run from within a event loop thread, which can be useful when the code is specifically
+ * optimized for running inside an event loop. Without this, it would be necessary to switch between threads in
+ * the benchmark which adds significant noise to the benchmark.
+ *
+ * <p>This class is essentially the same as {@link io.netty.channel.DefaultEventLoopGroup} except it stores
+ * a reference to the {@link EventLoop} in a {@link FastThreadLocal} to allow benchmark code to reference it
+ * using {@link EventLoopJmhExecutor#currentEventLoop()}.
+ *
+ * <p>To run on the event loop, the benchmark must specify the JVM args as something like
+ * <pre>{@code
+ *   {@literal @}Fork(jvmArgsAppend = { EventLoopJmhExecutor.JVM_ARG_1, EventLoopJmhExecutor.JMH_ARG_2 })
+ * }</pre>
+ */
+public class EventLoopJmhExecutor extends MultithreadEventLoopGroup {
+
+    public static final String JVM_ARG_1 = "-Djmh.executor=CUSTOM";
+    public static final String JVM_ARG_2 =
+            "-Djmh.executor.class=com.linecorp.armeria.benchmarks.shared.EventLoopJmhExecutor";
+
+    private static final FastThreadLocal<EventLoop> CURRENT_EVENT_LOOP = new FastThreadLocal<>();
+
+    public static EventLoop currentEventLoop() {
+        return CURRENT_EVENT_LOOP.get();
+    }
+
+    public EventLoopJmhExecutor(int numThreads, String threadPrefix) {
+        super(numThreads, new DefaultThreadFactory(threadPrefix));
+    }
+
+    @Override
+    protected EventLoop newChild(Executor executor, Object... args) throws Exception {
+        EventLoop eventLoop = new DefaultEventLoop(this, executor);
+        eventLoop.submit(() -> CURRENT_EVENT_LOOP.set(eventLoop)).syncUninterruptibly();
+        return eventLoop;
+    }
+}

commit d7dd7fce69e32057ad1951fab86536c3f6311b21

Ensure Subscriber.onSubscribe() is invoked before other methods
Motivation:

Commit 4f0cee147101db3911b36c6c1a8525be69199cfe introduced a regression
where Subscriber.onSubscribe() can be invoked after onComplete().

Modifications:

- Reschedule the notification when onSubscribe() is not invoked yet.

Result:

- Correct event order

benchmarks/src/jmh/java/com/linecorp/armeria/benchmarks/core/StreamMessageBenchmark.java
@@ -30,6 +30,8 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.linecorp.armeria.benchmarks.shared.EventLoopJmhExecutor;
 import com.linecorp.armeria.common.stream.DefaultStreamMessage;
@@ -40,6 +42,8 @@ import io.netty.channel.EventLoop;
 @Fork(jvmArgsAppend = { EventLoopJmhExecutor.JVM_ARG_1, EventLoopJmhExecutor.JVM_ARG_2 })
 @State(Scope.Benchmark)
 public class StreamMessageBenchmark {
+
+    private static final Logger logger = LoggerFactory.getLogger(StreamMessageBenchmark.class);
 
     private static final EventLoop ANOTHER_EVENT_LOOP = new DefaultEventLoop();
 
@@ -134,10 +138,12 @@ public class StreamMessageBenchmark {
 
         private long sum() {
             if (!complete) {
+                logger.warn("Stream not completed");
                 return -1;
             }
             if (error != null) {
-                return -1;
+                logger.warn("Stream failed", error);
+                return -2;
             }
             return sum;
         }

core/src/main/java/com/linecorp/armeria/common/stream/DefaultStreamMessage.java
@@ -22,11 +22,10 @@ import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.atomic.AtomicLongFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Supplier;
-
-import javax.annotation.Nullable;
 
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
@@ -41,7 +40,6 @@ import io.netty.buffer.ByteBufHolder;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.EventLoop;
-import io.netty.channel.SingleThreadEventLoop;
 import io.netty.util.ReferenceCountUtil;
 import io.netty.util.ReferenceCounted;
 
@@ -169,7 +167,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
     @Override
     public void subscribe(Subscriber<? super T> subscriber, boolean withPooledObjects) {
         requireNonNull(subscriber, "subscriber");
-        subscribe0(new SubscriptionImpl(this, subscriber, null, withPooledObjects));
+        subscribe(new SubscriptionImpl(this, subscriber, null, withPooledObjects));
     }
 
     @Override
@@ -183,29 +181,27 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
     public void subscribe(Subscriber<? super T> subscriber, Executor executor, boolean withPooledObjects) {
         requireNonNull(subscriber, "subscriber");
         requireNonNull(executor, "executor");
-        subscribe0(new SubscriptionImpl(this, subscriber, executor, withPooledObjects));
+        subscribe(new SubscriptionImpl(this, subscriber, executor, withPooledObjects));
     }
 
-    private void subscribe0(SubscriptionImpl subscription) {
+    private void subscribe(SubscriptionImpl subscription) {
         final Subscriber<Object> subscriber = subscription.subscriber();
         final Executor executor = subscription.executor();
 
         if (!subscriptionUpdater.compareAndSet(this, null, subscription)) {
-            failLateSubscriber(executor, subscriber, this.subscription.subscriber());
+            failLateSubscriber(this.subscription, subscriber);
             return;
         }
 
-        if (shouldRunInExecutor(executor)) {
-            // NB: We did not use subscriber.onSubscribe() because that will increase the memory footprint
-            //     of the anonymous class by referring to 2 variables in the lambda expression.
-            executor.execute(() -> subscription.subscriber().onSubscribe(subscription));
+        if (subscription.needsDirectInvocation()) {
+            subscription.invokeOnSubscribe();
         } else {
-            subscriber.onSubscribe(subscription);
+            executor.execute(subscription::invokeOnSubscribe);
         }
     }
 
-    private static void failLateSubscriber(@Nullable Executor executor,
-                                           Subscriber<?> lateSubscriber, Subscriber<?> oldSubscriber) {
+    private static void failLateSubscriber(SubscriptionImpl subscription, Subscriber<?> lateSubscriber) {
+        final Subscriber<?> oldSubscriber = subscription.subscriber;
         final Throwable cause;
         if (oldSubscriber instanceof AbortingSubscriber) {
             cause = AbortedStreamException.get();
@@ -213,14 +209,14 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             cause = new IllegalStateException("subscribed by other subscriber already");
         }
 
-        if (shouldRunInExecutor(executor)) {
-            executor.execute(() -> {
+        if (subscription.needsDirectInvocation()) {
+            lateSubscriber.onSubscribe(NoopSubscription.INSTANCE);
+            lateSubscriber.onError(cause);
+        } else {
+            subscription.executor().execute(() -> {
                 lateSubscriber.onSubscribe(NoopSubscription.INSTANCE);
                 lateSubscriber.onError(cause);
             });
-        } else {
-            lateSubscriber.onSubscribe(NoopSubscription.INSTANCE);
-            lateSubscriber.onError(cause);
         }
     }
 
@@ -235,6 +231,8 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         final SubscriptionImpl newSubscription = new SubscriptionImpl(
                 this, AbortingSubscriber.get(), null, false);
         if (subscriptionUpdater.compareAndSet(this, null, newSubscription)) {
+            // We don't need to invoke onSubscribe() for AbortingSubscriber because it's just a placeholder.
+            newSubscription.invokedOnSubscribe = true;
             newSubscription.abort();
         } else {
             subscription.abort();
@@ -292,20 +290,18 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             return;
         }
 
-        final Queue<Object> queue = this.queue;
         if (queue.isEmpty()) {
             return;
         }
 
-        final Executor executor = subscription.executor();
-        if (shouldRunInExecutor(executor)) {
-            executor.execute(() -> notifySubscriber(subscription, queue));
+        if (subscription.needsDirectInvocation()) {
+            notifySubscriber0();
         } else {
-            notifySubscriber(subscription, queue);
+            subscription.executor().execute(this::notifySubscriber0);
         }
     }
 
-    private void notifySubscriber(SubscriptionImpl subscription, Queue<Object> queue) {
+    private void notifySubscriber0() {
         if (inOnNext) {
             // Do not let Subscriber.onNext() reenter, because it can lead to weird-looking event ordering
             // for a Subscriber implemented like the following:
@@ -320,6 +316,25 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             //
             // We do not need to worry about synchronizing the access to 'inOnNext' because the subscriber
             // methods must be on the same thread, or synchronized, according to Reactive Streams spec.
+            return;
+        }
+
+        final SubscriptionImpl subscription = this.subscription;
+        if (!subscription.invokedOnSubscribe) {
+            Executor executor = subscription.executor();
+            if (executor == null) {
+                executor = ForkJoinPool.commonPool();
+            }
+
+            // Subscriber.onSubscribe() was not invoked yet.
+            // Reschedule the notification so that onSubscribe() is invoked before other events.
+            //
+            // Note:
+            // The rescheduling will occur at most once because the invocation of onSubscribe() must have been
+            // scheduled already by subscribe(), given that this.subscription is not null at this point and
+            // subscribe() is the only place that sets this.subscription.
+
+            executor.execute(this::notifySubscriber0);
             return;
         }
 
@@ -340,7 +355,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
             }
 
             if (o instanceof AwaitDemandFuture) {
-                if (notifyAwaitDemandFuture(queue)) {
+                if (notifyAwaitDemandFuture()) {
                     // Notified successfully.
                     continue;
                 } else {
@@ -349,14 +364,14 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
                 }
             }
 
-            if (!notifySubscriberWithElements(subscription, queue)) {
+            if (!notifySubscriberWithElements(subscription)) {
                 // Not enough demand.
                 break;
             }
         }
     }
 
-    private boolean notifySubscriberWithElements(SubscriptionImpl subscription, Queue<Object> queue) {
+    private boolean notifySubscriberWithElements(SubscriptionImpl subscription) {
         final Subscriber<Object> subscriber = subscription.subscriber();
         for (;;) {
             final long demand = this.demand;
@@ -408,7 +423,7 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
     }
 
-    private boolean notifyAwaitDemandFuture(Queue<Object> queue) {
+    private boolean notifyAwaitDemandFuture() {
         if (demand == 0) {
             return false;
         }
@@ -494,19 +509,13 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
     }
 
-    // We directly run callbacks for event loops if we're already on the loop, which applies to the vast
-    // majority of cases.
-    private static boolean shouldRunInExecutor(@Nullable Executor executor) {
-        return executor != null &&
-               (!(executor instanceof SingleThreadEventLoop) || !((EventLoop) executor).inEventLoop());
-    }
-
     private static final class SubscriptionImpl implements Subscription {
 
         private final DefaultStreamMessage<?> publisher;
         private Subscriber<Object> subscriber;
         private final Executor executor;
         private final boolean withPooledObjects;
+        private boolean invokedOnSubscribe;
         private volatile boolean cancelRequested;
 
         @SuppressWarnings("unchecked")
@@ -542,6 +551,11 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
 
         boolean cancelRequested() {
             return cancelRequested;
+        }
+
+        void invokeOnSubscribe() {
+            invokedOnSubscribe = true;
+            subscriber().onSubscribe(this);
         }
 
         @Override
@@ -621,11 +635,18 @@ public class DefaultStreamMessage<T> implements StreamMessage<T>, StreamWriter<T
         }
 
         private void invokeOnError(Throwable cause) {
-            if (shouldRunInExecutor(executor)) {
-                executor.execute(() -> subscriber.onError(cause));
-            } else {
+            if (needsDirectInvocation()) {
                 subscriber.onError(cause);
+            } else {
+                executor.execute(() -> subscriber.onError(cause));
             }
+        }
+
+        // We directly run callbacks for event loops if we're already on the loop, which applies to the vast
+        // majority of cases.
+        boolean needsDirectInvocation() {
+            return executor == null ||
+                   executor instanceof EventLoop && ((EventLoop) executor).inEventLoop();
         }
 
         @Override

core/src/test/java/com/linecorp/armeria/common/stream/DefaultStreamMessageTest.java
@@ -23,6 +23,10 @@ import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.junit.AfterClass;
@@ -218,6 +222,46 @@ public class DefaultStreamMessageTest {
         assertThat(result).containsExactlyElementsOf(input);
     }
 
+    /**
+     * Makes sure {@link Subscriber#onComplete()} is always invoked after
+     * {@link Subscriber#onSubscribe(Subscription)} even if
+     * {@link StreamMessage#subscribe(Subscriber, Executor)} is called from non-{@link EventLoop}.
+     */
+    @Test
+    public void onSubscribeBeforeOnComplete() throws Exception {
+        final BlockingQueue<String> queue = new LinkedTransferQueue<>();
+        // Repeat to increase the chance of reproduction.
+        for (int i = 0; i < 8192; i++) {
+            final DefaultStreamMessage<Object> m = new DefaultStreamMessage<>();
+            eventLoop.execute(m::close);
+            m.subscribe(new Subscriber<Object>() {
+                @Override
+                public void onSubscribe(Subscription s) {
+                    queue.add("onSubscribe");
+                    s.request(Long.MAX_VALUE);
+                }
+
+                @Override
+                public void onNext(Object o) {
+                    queue.add("onNext");
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    queue.add("onError");
+                }
+
+                @Override
+                public void onComplete() {
+                    queue.add("onComplete");
+                }
+            }, eventLoop);
+
+            assertThat(queue.poll(5, TimeUnit.SECONDS)).isEqualTo("onSubscribe");
+            assertThat(queue.poll(5, TimeUnit.SECONDS)).isEqualTo("onComplete");
+        }
+    }
+
     @Test
     public void releaseOnConsumption_ByteBuf() throws Exception {
         final DefaultStreamMessage<ByteBuf> m = new DefaultStreamMessage<>();
@@ -357,7 +401,6 @@ public class DefaultStreamMessageTest {
         m.close();
 
         m.subscribe(new Subscriber<Object>() {
-            private Subscription subscription;
             @Override
             public void onSubscribe(Subscription subscription) {
                 // Cancel the subscription when the demand is 0.