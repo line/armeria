line/armeria

commit 6ca23d1faa62412cee90d75a9319d05ad8c5d3dd

Write rpc method name to access log (#1157)
Motivation:
Currently there is no way to get rpc method name from access log.

Modifications:
- Write rpc method name to request line log.

core/src/main/java/com/linecorp/armeria/server/logging/AccessLogComponent.java
@@ -34,6 +34,7 @@ import javax.annotation.Nullable;
 import com.google.common.annotations.VisibleForTesting;
 
 import com.linecorp.armeria.common.HttpHeaders;
+import com.linecorp.armeria.common.RpcRequest;
 import com.linecorp.armeria.common.logging.RequestLog;
 
 import io.netty.util.AsciiString;
@@ -180,10 +181,23 @@ interface AccessLogComponent {
                     return dateTimeFormatter.format(ZonedDateTime.ofInstant(
                             Instant.ofEpochMilli(log.requestStartTimeMillis()), defaultZoneId));
                 case REQUEST_LINE:
+                    final StringBuilder requestLine = new StringBuilder();
+
                     final HttpHeaders headers = log.requestHeaders();
-                    return headers.method().name() + ' ' +
-                           headers.path() + ' ' +
-                           firstNonNull(log.sessionProtocol(), log.context().sessionProtocol()).uriText();
+                    requestLine.append(headers.method().name())
+                               .append(' ')
+                               .append(headers.path());
+
+                    final Object requestContent = log.requestContent();
+                    if (requestContent instanceof RpcRequest) {
+                        requestLine.append('#')
+                                   .append(((RpcRequest) requestContent).method());
+                    }
+
+                    requestLine.append(' ')
+                               .append(firstNonNull(log.sessionProtocol(),
+                                                    log.context().sessionProtocol()).uriText());
+                    return requestLine.toString();
 
                 case RESPONSE_STATUS_CODE:
                     return log.responseHeaders().status().code();

commit aa274f1b2867b178a63f30f04cddccbf53aca158

Avoid RequestLogAvailabilityException while writing access logs
Modifications:
- Fix a bug introduced by #1157

core/src/main/java/com/linecorp/armeria/server/logging/AccessLogComponent.java
@@ -36,6 +36,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.linecorp.armeria.common.HttpHeaders;
 import com.linecorp.armeria.common.RpcRequest;
 import com.linecorp.armeria.common.logging.RequestLog;
+import com.linecorp.armeria.common.logging.RequestLogAvailability;
 
 import io.netty.util.AsciiString;
 import io.netty.util.Attribute;
@@ -180,6 +181,7 @@ interface AccessLogComponent {
                 case REQUEST_TIMESTAMP:
                     return dateTimeFormatter.format(ZonedDateTime.ofInstant(
                             Instant.ofEpochMilli(log.requestStartTimeMillis()), defaultZoneId));
+
                 case REQUEST_LINE:
                     final StringBuilder requestLine = new StringBuilder();
 
@@ -188,12 +190,13 @@ interface AccessLogComponent {
                                .append(' ')
                                .append(headers.path());
 
-                    final Object requestContent = log.requestContent();
-                    if (requestContent instanceof RpcRequest) {
-                        requestLine.append('#')
-                                   .append(((RpcRequest) requestContent).method());
+                    if (log.isAvailable(RequestLogAvailability.REQUEST_CONTENT)) {
+                        final Object requestContent = log.requestContent();
+                        if (requestContent instanceof RpcRequest) {
+                            requestLine.append('#')
+                                       .append(((RpcRequest) requestContent).method());
+                        }
                     }
-
                     requestLine.append(' ')
                                .append(firstNonNull(log.sessionProtocol(),
                                                     log.context().sessionProtocol()).uriText());