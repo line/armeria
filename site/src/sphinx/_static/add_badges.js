c735a69c131b6c0ed3f260a5cab42dff0f4707ff
Trustin Lee <t@motd.kr>
2018-05-18 18:26:32 +0900

    Fix AppVeyor badge URL

--- site/src/sphinx/_static/add_badges.js
+++ site/src/sphinx/_static/add_badges.js
@@ -29,8 +29,8 @@ function addBadges(parent) {
   div.className = 'project-badges';
   addBadge(div, 'https://img.shields.io/travis/line/armeria/master.svg?style=flat-square',
     'https://travis-ci.org/line/armeria');
-  addBadge(div, 'https://img.shields.io/appveyor/ci/trustin/armeria/master.svg?label=appveyor&style=flat-square',
-    'https://ci.appveyor.com/project/trustin/armeria/branch/master');
+  addBadge(div, 'https://img.shields.io/appveyor/ci/line/armeria/master.svg?label=appveyor&style=flat-square',
+    'https://ci.appveyor.com/project/line/armeria/branch/master');
   addBadge(div, 'https://img.shields.io/maven-central/v/com.linecorp.armeria/armeria.svg?style=flat-square',
     'https://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.linecorp.armeria%22%20AND%20a%3A%22armeria%22');
   addBadge(div, 'https://img.shields.io/codecov/c/github/line/armeria/master.svg?style=flat-square',
