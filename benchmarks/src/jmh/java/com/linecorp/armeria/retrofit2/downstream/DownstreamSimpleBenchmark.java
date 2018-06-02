dab1507238e4c2db87b77dfb25bc911c2bca3118
Anuraag Agrawal <anuraaga@gmail.com>
2018-06-02 16:31:16 +0900

    Style

--- benchmarks/src/jmh/java/com/linecorp/armeria/retrofit2/downstream/DownstreamSimpleBenchmark.java
+++ benchmarks/src/jmh/java/com/linecorp/armeria/retrofit2/downstream/DownstreamSimpleBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linecorp.armeria.retrofit2.downstream;
 
 import org.openjdk.jmh.annotations.Scope;
@@ -16,16 +32,17 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 @State(Scope.Benchmark)
 public class DownstreamSimpleBenchmark extends SimpleBenchmarkBase {
 
-  @Override
-  protected SimpleBenchmarkClient client() {
-    ClientFactory factory = new ClientFactoryBuilder()
-            .sslContextCustomizer(ssl -> ssl.trustManager(InsecureTrustManagerFactory.INSTANCE))
-            .build();
-    return new ArmeriaRetrofitBuilder(factory)
-            .baseUrl(baseUrl())
-            .addConverterFactory(JacksonConverterFactory.create())
-            .addCallAdapterFactory(Java8CallAdapterFactory.create())
-            .build()
-            .create(SimpleBenchmarkClient.class);
+    @Override
+    protected SimpleBenchmarkClient client() {
+        ClientFactory factory =
+                new ClientFactoryBuilder()
+                        .sslContextCustomizer(ssl -> ssl.trustManager(InsecureTrustManagerFactory.INSTANCE))
+                        .build();
+        return new ArmeriaRetrofitBuilder(factory)
+                .baseUrl(baseUrl())
+                .addConverterFactory(JacksonConverterFactory.create())
+                .addCallAdapterFactory(Java8CallAdapterFactory.create())
+                .build()
+                .create(SimpleBenchmarkClient.class);
   }
 }
--- benchmarks/src/jmh/java/com/linecorp/armeria/retrofit2/upstream/UpstreamSimpleBenchmark.java
+++ benchmarks/src/jmh/java/com/linecorp/armeria/retrofit2/upstream/UpstreamSimpleBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linecorp.armeria.retrofit2.upstream;
 
 import javax.net.ssl.SSLContext;
@@ -11,7 +27,6 @@ import com.linecorp.armeria.retrofit2.shared.SimpleBenchmarkClient;
 
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import okhttp3.OkHttpClient;
-import okhttp3.internal.platform.Platform;
 import retrofit2.Retrofit;
 import retrofit2.adapter.java8.Java8CallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -19,22 +34,22 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
 @State(Scope.Benchmark)
 public class UpstreamSimpleBenchmark extends SimpleBenchmarkBase {
 
-  @Override
-  protected SimpleBenchmarkClient client() throws Exception {
-    SSLContext context = SSLContext.getInstance("TLS");
-    context.init(null, InsecureTrustManagerFactory.INSTANCE.getTrustManagers(), null);
-    OkHttpClient client = new OkHttpClient.Builder()
-            .sslSocketFactory(context.getSocketFactory(),
-                              (X509TrustManager) InsecureTrustManagerFactory.INSTANCE.getTrustManagers()[0])
-            .hostnameVerifier((s, session) -> true)
-            .build();
+    @Override
+    protected SimpleBenchmarkClient client() throws Exception {
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(null, InsecureTrustManagerFactory.INSTANCE.getTrustManagers(), null);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .sslSocketFactory(context.getSocketFactory(),
+                                  (X509TrustManager) InsecureTrustManagerFactory.INSTANCE.getTrustManagers()[0])
+                .hostnameVerifier((s, session) -> true)
+                .build();
 
-    return new Retrofit.Builder()
-        .baseUrl(baseUrl())
-        .client(client)
-        .addConverterFactory(JacksonConverterFactory.create())
-        .addCallAdapterFactory(Java8CallAdapterFactory.create())
-        .build()
-        .create(SimpleBenchmarkClient.class);
-  }
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl())
+                .client(client)
+                .addConverterFactory(JacksonConverterFactory.create())
+                .addCallAdapterFactory(Java8CallAdapterFactory.create())
+                .build()
+                .create(SimpleBenchmarkClient.class);
+    }
 }
