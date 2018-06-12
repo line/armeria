57e0953a2addaaab5b27bbecc76ba1ba859a5dc2
Trustin Lee <t@motd.kr>
2018-06-12 12:30:48 +0900

    Fix a bug where HttpClient connects to a wrong host when custom authority is used
    Motivation:
    
    When a user specifies both:
    
    - an Endpoint without an IP address and
    - a custom authority
    
    the expected behavior is to use the custom authority as a host name (so
    that it sends the correct server name on a TLS connection) and to
    connect to the IP address of the host specified by the Endpoint.
    
    However, currently, HttpClientDelegate always uses the IP address of the
    host name in the custom authority. As a result, HttpClient ends up with
    connecting to a wrong host.
    
    Modifications:
    
    - Made sure that HttpClientDelegate resolves the IP address of the
      Endpoint.
      - PoolKey.ipAddr is now non-null.
      - HttpSessionChannelFactory now assumes PoolKey always has an IP address.
    - Added test cases
    - Miscellaneous
      - Remove small code duplication in HttpClientDelegate
    
    Result:
    
    - HttpClient does not connect to a wrong host anymore.

--- core/src/main/java/com/linecorp/armeria/client/pool/PoolKey.java
+++ core/src/main/java/com/linecorp/armeria/client/pool/PoolKey.java
@@ -18,10 +18,6 @@ package com.linecorp.armeria.client.pool;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.Objects;
-
-import javax.annotation.Nullable;
-
 import com.google.common.base.MoreObjects;
 
 import com.linecorp.armeria.common.SessionProtocol;
@@ -38,7 +34,6 @@ import com.linecorp.armeria.common.SessionProtocol;
 public final class PoolKey {
 
     private final String host;
-    @Nullable
     private final String ipAddr;
     private final int port;
     private final SessionProtocol sessionProtocol;
@@ -47,9 +42,9 @@ public final class PoolKey {
      * Creates a new key with the specified {@code host}, {@code ipAddr}, {@code port} and
      * {@code sessionProtocol}.
      */
-    public PoolKey(String host, @Nullable String ipAddr, int port, SessionProtocol sessionProtocol) {
+    public PoolKey(String host, String ipAddr, int port, SessionProtocol sessionProtocol) {
         this.host = requireNonNull(host, "host");
-        this.ipAddr = ipAddr;
+        this.ipAddr = requireNonNull(ipAddr, "ipAddr");
         this.port = port;
         this.sessionProtocol = requireNonNull(sessionProtocol, "sessionProtocol");
     }
@@ -63,10 +58,7 @@ public final class PoolKey {
 
     /**
      * Returns the IP address of the server associated with this key.
-     *
-     * @return the IP address, or {@code null} if the host name is not resolved.
      */
-    @Nullable
     public String ipAddr() {
         return ipAddr;
     }
@@ -96,19 +88,18 @@ public final class PoolKey {
         }
 
         final PoolKey that = (PoolKey) o;
-        return host.equals(that.host) && Objects.equals(ipAddr, that.ipAddr) &&
+        return host.equals(that.host) && ipAddr.equals(that.ipAddr) &&
                port == that.port && sessionProtocol == that.sessionProtocol;
     }
 
     @Override
     public int hashCode() {
-        return ((host.hashCode() * 31 + Objects.hashCode(ipAddr)) * 31 + port) * 31 +
-               sessionProtocol.hashCode();
+        return ((host.hashCode() * 31 + ipAddr.hashCode()) * 31 + port) * 31 + sessionProtocol.hashCode();
     }
 
     @Override
     public String toString() {
-        return MoreObjects.toStringHelper(this).omitNullValues()
+        return MoreObjects.toStringHelper(this)
                           .add("sessionProtocol", sessionProtocol.uriText())
                           .add("host", host)
                           .add("ipAddr", ipAddr)
