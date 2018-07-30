bb98c2d6d2f8e08bdeddb1e3b770b3cedeb48d5c
Anuraag Agrawal <rag@istellar.jp>
2018-07-30 13:41:07 +0900

    Fix again

--- core/src/main/java/com/linecorp/armeria/server/docs/DocServiceBuilder.java
+++ core/src/main/java/com/linecorp/armeria/server/docs/DocServiceBuilder.java
@@ -246,7 +246,7 @@ public final class DocServiceBuilder {
 
     /**
      * Adds Javascript scripts to inject into the {@code <head />} of the debug page HTML. This can be used to
-     * customizethe debug page (e.g., to provide a HeaderProvider for enabling authentication based on local
+     * customize the debug page (e.g., to provide a HeaderProvider for enabling authentication based on local
      * storage). All scripts are concatenated into the content of a single script tag.
      *
      * <p>A common use case is to provide authentication of debug requests using a local storage access token,
@@ -264,7 +264,7 @@ public final class DocServiceBuilder {
 
     /**
      * Adds Javascript scripts to inject into the {@code <head />} of the debug page HTML. This can be used to
-     * customizethe debug page (e.g., to provide a HeaderProvider for enabling authentication based on local
+     * customize the debug page (e.g., to provide a HeaderProvider for enabling authentication based on local
      * storage). All scripts are concatenated into the content of a single script tag.
      *
      * <p>A common use case is to provide authentication of debug requests using a local storage access token,
