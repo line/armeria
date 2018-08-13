e5a7382f75eb97ab21bb3dd95f259fc5a70bcf90
hyangtack <hyangtack.lee@linecorp.com>
2018-08-13 15:38:16 +0900

    Add examples of annotated HTTP service (#1330)
    Motivation:
    We provide official document for annotated HTTP service, but it is not ready-to-use on IDE. So, it would be useful to beginners if we provide working examples.
    
    Modifications:
    - Add `example.armeria.server` package

---
+++ examples/annotated-http-service/src/main/java/example/armeria/server/annotated/ExceptionHandlerService.java
@@ -0,0 +1,112 @@
+package example.armeria.server.annotated;
+
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.RequestContext;
+import com.linecorp.armeria.common.logging.LogLevel;
+import com.linecorp.armeria.server.HttpStatusException;
+import com.linecorp.armeria.server.annotation.ExceptionHandler;
+import com.linecorp.armeria.server.annotation.ExceptionHandlerFunction;
+import com.linecorp.armeria.server.annotation.Get;
+import com.linecorp.armeria.server.annotation.Param;
+import com.linecorp.armeria.server.annotation.decorator.LoggingDecorator;
+
+import example.armeria.server.annotated.ExceptionHandlerService.GlobalExceptionHandler;
+
+/**
+ * Examples how to use {@link ExceptionHandler}.
+ *
+ * @see <a href="https://line.github.io/armeria/server-annotated-service.html#handling-exceptions">
+ *      Handling exceptions</a>
+ */
+@LoggingDecorator(
+        requestLogLevel = LogLevel.INFO,            // Log every request sent to this service at INFO level.
+        successfulResponseLogLevel = LogLevel.INFO  // Log every response sent from this service at INFO level.
+)
+@ExceptionHandler(GlobalExceptionHandler.class)
+public class ExceptionHandlerService {
+
+    /**
+     * Throws a {@link LocallySpecificException} which can be handled by the {@link LocalExceptionHandler}.
+     */
+    @Get("/locallySpecific")
+    @ExceptionHandler(LocalExceptionHandler.class)
+    public String exception1() {
+        throw new LocallySpecificException();
+    }
+
+    /**
+     * Throws a {@link LocallyGeneralException} which can be handled by the {@link LocalExceptionHandler}.
+     */
+    @Get("/locallyGeneral")
+    @ExceptionHandler(LocalExceptionHandler.class)
+    public String exception2() {
+        throw new LocallyGeneralException();
+    }
+
+    /**
+     * Throws a {@link GloballyGeneralException} which can be handled by the {@link GlobalExceptionHandler}.
+     */
+    @Get("/globallyGeneral")
+    @ExceptionHandler(LocalExceptionHandler.class)
+    public String exception3() {
+        throw new GloballyGeneralException();
+    }
+
+    /**
+     * Throws an {@link IllegalArgumentException} which can be handled by the default exception handler.
+     */
+    @Get("/default")
+    @ExceptionHandler(LocalExceptionHandler.class)
+    public String exception4() {
+        throw new IllegalArgumentException();
+    }
+
+    /**
+     * Throws an {@link HttpStatusException} which can be handled by the default exception handler.
+     * The {@code status code} of the exception is from the path variable of {@code status}.
+     */
+    @Get("/default/{status}")
+    @ExceptionHandler(LocalExceptionHandler.class)
+    public String exception5(@Param int status) {
+        throw HttpStatusException.of(status);
+    }
+
+    static class GloballyGeneralException extends RuntimeException {
+        private static final long serialVersionUID = 8210080483318166316L;
+    }
+
+    static class LocallyGeneralException extends GloballyGeneralException {
+        private static final long serialVersionUID = -9167203216151475846L;
+    }
+
+    static class LocallySpecificException extends LocallyGeneralException {
+        private static final long serialVersionUID = 5879797158322612975L;
+    }
+
+    static final class GlobalExceptionHandler implements ExceptionHandlerFunction {
+        @Override
+        public HttpResponse handleException(RequestContext ctx, HttpRequest req, Throwable cause) {
+            if (cause instanceof GloballyGeneralException) {
+                return HttpResponse.of(HttpStatus.FORBIDDEN);
+            }
+            // To the next exception handler.
+            return ExceptionHandlerFunction.fallthrough();
+        }
+    }
+
+    static final class LocalExceptionHandler implements ExceptionHandlerFunction {
+        @Override
+        public HttpResponse handleException(RequestContext ctx, HttpRequest req, Throwable cause) {
+            if (cause instanceof LocallySpecificException) {
+                return HttpResponse.of(HttpStatus.SERVICE_UNAVAILABLE);
+            }
+            if (cause instanceof LocallyGeneralException) {
+                return HttpResponse.of(HttpStatus.BAD_REQUEST);
+            }
+            // To the next exception handler.
+            return ExceptionHandlerFunction.fallthrough();
+        }
+    }
+}
---
+++ examples/annotated-http-service/src/main/java/example/armeria/server/annotated/MessageConverterService.java
@@ -0,0 +1,170 @@
+package example.armeria.server.annotated;
+
+import javax.annotation.Nullable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.linecorp.armeria.common.AggregatedHttpMessage;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.logging.LogLevel;
+import com.linecorp.armeria.server.ServiceRequestContext;
+import com.linecorp.armeria.server.annotation.JacksonRequestConverterFunction;
+import com.linecorp.armeria.server.annotation.JacksonResponseConverterFunction;
+import com.linecorp.armeria.server.annotation.Post;
+import com.linecorp.armeria.server.annotation.ProducesJson;
+import com.linecorp.armeria.server.annotation.RequestConverter;
+import com.linecorp.armeria.server.annotation.RequestConverterFunction;
+import com.linecorp.armeria.server.annotation.RequestObject;
+import com.linecorp.armeria.server.annotation.ResponseConverter;
+import com.linecorp.armeria.server.annotation.ResponseConverterFunction;
+import com.linecorp.armeria.server.annotation.decorator.LoggingDecorator;
+
+/**
+ * Examples how to use {@link RequestConverter} and {@link ResponseConverter}.
+ *
+ * @see <a href="https://line.github.io/armeria/server-annotated-service.html#conversion-between-an-http-message-and-a-java-object">
+ *      Conversion between an HTTP message and a Java object</a>
+ */
+@LoggingDecorator(
+        requestLogLevel = LogLevel.INFO,            // Log every request sent to this service at INFO level.
+        successfulResponseLogLevel = LogLevel.INFO  // Log every response sent from this service at INFO level.
+)
+public class MessageConverterService {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * Returns a {@link JsonNode} which is converted from a {@link Response} object. The {@link JsonNode}
+     * will be converted as JSON string by the {@link JacksonResponseConverterFunction}.
+     */
+    @Post("/node/node")
+    public JsonNode json1(@RequestObject JsonNode input) {
+        final JsonNode name = input.get("name");
+        return mapper.valueToTree(new Response(Response.SUCCESS, name.textValue()));
+    }
+
+    /**
+     * Returns a {@link Response} object. The {@link ProducesJson} annotation makes an object be converted
+     * as JSON string by the {@link JacksonResponseConverterFunction}.
+     */
+    @Post("/node/obj")
+    @ProducesJson
+    public Response json2(@RequestObject JsonNode input) {
+        final JsonNode name = input.get("name");
+        return new Response(Response.SUCCESS, name.textValue());
+    }
+
+    /**
+     * Returns a {@link Response} object. A {@link Request} is automatically converted by
+     * {@link JacksonRequestConverterFunction}.
+     *
+     * <p>If you want to use a custom {@link ObjectMapper} for JSON converters, you can register a new
+     * {@link JacksonRequestConverterFunction} with your custom {@link ObjectMapper} when adding an
+     * annotated service as follows:
+     * <pre>{@code
+     * // Create a new JSON request converter with a custom ObjectMapper.
+     * final JacksonRequestConverterFunction requestConverterFunction =
+     *         new JacksonRequestConverterFunction(
+     *                 new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT)
+     *                                   .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS));
+     * // Register the converter when adding an annotated service to the ServerBuilder.
+     * final Server = new ServerBuilder().port(0, SessionProtocol.HTTP)
+     *                                   .annotatedService("/messageConverter", new MessageConverterService(),
+     *                                                     requestConverterFunction)
+     *                                   .build();
+     * }</pre>
+     */
+    @Post("/obj/obj")
+    @ProducesJson
+    public Response json3(@RequestObject Request request) {
+        return new Response(Response.SUCCESS, request.name());
+    }
+
+    /**
+     * Returns a {@link Response} object which will be converted to a text by the
+     * {@link CustomResponseConverter}. A {@link Request} is also automatically converted by
+     * the {@link CustomRequestConverter}.
+     *
+     * <p>If you want to specify converters for every methods in a service class, you can specify them
+     * above the class definition as follows:
+     * <pre>{@code
+     * @RequestConverter(CustomRequestConverter.class)
+     * @ResponseConverter(CustomResponseConverter.class)
+     * public class MyAnnotatedService {
+     *     ...
+     * }
+     * }</pre>
+     */
+    @Post("/custom")
+    @RequestConverter(CustomRequestConverter.class)
+    @ResponseConverter(CustomResponseConverter.class)
+    public Response custom(@RequestObject Request request) {
+        return new Response(Response.SUCCESS, request.name());
+    }
+
+    public static final class Request {
+        private final String name;
+
+        @JsonCreator
+        public Request(@JsonProperty("name") String name) {
+            this.name = name;
+        }
+
+        @JsonProperty
+        public String name() {
+            return name;
+        }
+    }
+
+    public static final class Response {
+        static final String SUCCESS = "success";
+
+        private final String result;
+        private final String from;
+
+        private Response(String result, String from) {
+            this.result = result;
+            this.from = from;
+        }
+
+        @JsonProperty
+        public String result() {
+            return result;
+        }
+
+        @JsonProperty
+        public String from() {
+            return from;
+        }
+    }
+
+    public static final class CustomRequestConverter implements RequestConverterFunction {
+        @Nullable
+        @Override
+        public Object convertRequest(ServiceRequestContext ctx, AggregatedHttpMessage request,
+                                     Class<?> expectedResultType) throws Exception {
+            final MediaType mediaType = request.headers().contentType();
+            if (mediaType != null && mediaType.is(MediaType.PLAIN_TEXT_UTF_8)) {
+                return new Request(request.content().toStringUtf8());
+            }
+            return RequestConverterFunction.fallthrough();
+        }
+    }
+
+    public static final class CustomResponseConverter implements ResponseConverterFunction {
+        @Override
+        public HttpResponse convertResponse(ServiceRequestContext ctx, @Nullable Object result)
+                throws Exception {
+            if (result instanceof Response) {
+                final Response response = (Response) result;
+                return HttpResponse.of(HttpStatus.OK, MediaType.PLAIN_TEXT_UTF_8,
+                                       response.result() + ':' + response.from());
+            }
+            return ResponseConverterFunction.fallthrough();
+        }
+    }
+}
