bb9225c5ab633e565cf214f5e2334d9949d7ddfc
Trustin Lee <t@motd.kr>
2018-10-19 14:45:38 +0900

    Update dependencies / Make sure /dev/epoll and OpenSSL are used during tests (#1394)
    - Brave 5.4.2 -> 5.4.3
    - Guava 26.0 -> 27.0
    - Micrometer 1.0.6 -> 1.0.7
    - Spring Boot 2.0.5 -> 2.0.6, 1.5.16 -> 1.5.17
    - Miscellaneous:
      - Ported the changes in Guava `MediaType`.
      - Added `FlagsTest` which makes sure `Flags.useEpoll()` and
        `useOpenSsl()` are set when expected, so that we always run our
        tests with `/dev/epoll` and OpenSSL enabled.

---
+++ core/src/test/java/com/linecorp/armeria/common/FlagsTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+
+import org.junit.Test;
+
+import com.google.common.base.Ascii;
+
+import io.netty.channel.epoll.Epoll;
+import io.netty.handler.ssl.OpenSsl;
+
+public class FlagsTest {
+
+    private static final String osName = Ascii.toLowerCase(System.getProperty("os.name"));
+
+    /**
+     * Makes sure /dev/epoll is used while running tests on Linux.
+     */
+    @Test
+    public void epollAvailableOnLinux() {
+        assumeTrue(osName.startsWith("linux"));
+        assumeTrue(System.getenv("WSLENV") == null);
+        assumeFalse("false".equals(System.getProperty("com.linecorp.armeria.useEpoll")));
+
+        assertThat(Flags.useEpoll()).isTrue();
+        assertThat(Epoll.isAvailable()).isTrue();
+    }
+
+    /**
+     * Makes sure OpenSSL SSLEngine is used instead of JDK SSLEngine while running tests
+     * on Linux, Windows and OS X.
+     */
+    @Test
+    public void openSslAvailable() {
+        assumeTrue(osName.startsWith("linux") || osName.startsWith("windows") ||
+                   osName.startsWith("macosx") || osName.startsWith("osx"));
+        assumeFalse("false".equals(System.getProperty("com.linecorp.armeria.useOpenSsl")));
+
+        assertThat(Flags.useOpenSsl()).isTrue();
+        assertThat(OpenSsl.isAvailable()).isTrue();
+    }
+}
