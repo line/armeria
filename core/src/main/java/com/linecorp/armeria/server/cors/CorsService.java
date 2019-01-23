2c9591e5cde44fdc2202896697b9e0a0bf11fd60
hyeonjames <tmdgus0118@gmail.com>
2019-01-23 15:46:16 +0900

    Replaced asMutable()

--- core/src/main/java/com/linecorp/armeria/server/cors/CorsService.java
+++ core/src/main/java/com/linecorp/armeria/server/cors/CorsService.java
@@ -88,14 +88,14 @@ public final class CorsService extends SimpleDecoratingService<HttpRequest, Http
                     return obj;
                 }
 
-                final HttpHeaders headers = ((HttpHeaders) obj).asMutable();
+                final HttpHeaders headers = (HttpHeaders) obj;
                 final HttpStatus status = headers.status();
                 if (status == null || status.codeClass() == HttpStatusClass.INFORMATIONAL) {
                     return headers;
                 }
 
-                setCorsResponseHeaders(ctx, req, headers);
-                return obj;
+                setCorsResponseHeaders(ctx, req, headers.asMutable());
+                return headers;
             }
         };
     }
