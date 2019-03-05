e249d4448fa6b70b05fa74fa5c41b67dca6135a5
minwoox <songmw725@gmail.com>
2019-03-05 11:06:42 +0900

    Add tests

--- core/src/test/java/com/linecorp/armeria/server/DecodedHttpRequestTest.java
+++ core/src/test/java/com/linecorp/armeria/server/DecodedHttpRequestTest.java
@@ -17,11 +17,14 @@
 package com.linecorp.armeria.server;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 import java.util.List;
 
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import com.linecorp.armeria.common.HttpData;
 import com.linecorp.armeria.common.HttpHeaderNames;
@@ -29,8 +32,14 @@ import com.linecorp.armeria.common.HttpHeaders;
 import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpObject;
 import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.internal.InboundTrafficController;
 import com.linecorp.armeria.testing.common.EventLoopRule;
+import com.linecorp.armeria.unsafe.ByteBufHttpData;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.util.ReferenceCountUtil;
 
 public class DecodedHttpRequestTest {
 
@@ -92,14 +101,61 @@ public class DecodedHttpRequestTest {
                                             HttpHeaders.of(HttpHeaderNames.of("bar"), "baz"));
     }
 
+    @Test
+    public void contentPreview() {
+        final HttpHeaders headers = HttpHeaders.of(HttpMethod.GET, "/").contentType(MediaType.PLAIN_TEXT_UTF_8);
+        final ServiceRequestContext sctx =
+                ServiceRequestContextBuilder.of(HttpRequest.of(headers))
+                                            .serverConfigurator(sb -> sb.contentPreview(100))
+                                            .build();
+        final DecodedHttpRequest req = decodedHttpRequest(headers, sctx);
+        req.completionFuture().handle((ret, cause) -> {
+            sctx.logBuilder().endRequest();
+            return null;
+        });
+
+        req.subscribe(new ImmediateReleaseSubscriber());
+
+        assertThat(req.tryWrite(new ByteBufHttpData(newBuffer("hello"), false))).isTrue();
+        req.close();
+
+        await().untilAsserted(() -> assertThat(sctx.log().requestContentPreview()).isEqualTo("hello"));
+    }
+
     private static DecodedHttpRequest decodedHttpRequest() {
         final HttpHeaders headers = HttpHeaders.of(HttpMethod.GET, "/");
         final ServiceRequestContext sctx = ServiceRequestContext.of(HttpRequest.of(headers));
+        return decodedHttpRequest(headers, sctx);
+    }
 
+    private static DecodedHttpRequest decodedHttpRequest(HttpHeaders headers, ServiceRequestContext sctx) {
         final DecodedHttpRequest request = new DecodedHttpRequest(sctx.eventLoop(), 1, 1, headers, true,
                                                                   InboundTrafficController.disabled(),
                                                                   sctx.maxRequestLength());
         request.init(sctx);
         return request;
+    }
+
+    private static ByteBuf newBuffer(String content) {
+        return ByteBufAllocator.DEFAULT.buffer().writeBytes(content.getBytes());
+    }
+
+    private static class ImmediateReleaseSubscriber implements Subscriber<HttpObject> {
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            s.request(Integer.MAX_VALUE);
+        }
+
+        @Override
+        public void onNext(HttpObject obj) {
+            ReferenceCountUtil.safeRelease(obj);
+        }
+
+        @Override
+        public void onError(Throwable t) {}
+
+        @Override
+        public void onComplete() {}
     }
 }
