de80157a1192eed9106adba3b70dcac06511022e
Trustin Lee <t@motd.kr>
2019-03-07 11:17:46 +0900

    Add more loggings to `SamlService` (#1629)
    Motivation:
    
    A user has no way to know why a `SamlService` is returning a 400 Bad
    Request response.
    
    Modifications:
    
    - Add logging.
    - Miscellaneous:
      - Add more test cases for `DeferredStreamMessage` which is used by
        `SamlService`, just in case the exception is not propagated.
    
    Result:
    
    - More information when something goes wrong with `SamlService`.

--- saml/src/main/java/com/linecorp/armeria/server/saml/SamlService.java
+++ saml/src/main/java/com/linecorp/armeria/server/saml/SamlService.java
@@ -27,11 +27,15 @@ import java.util.concurrent.CompletionStage;
 
 import javax.annotation.Nullable;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 
 import com.linecorp.armeria.common.AggregatedHttpMessage;
+import com.linecorp.armeria.common.HttpData;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
@@ -43,6 +47,7 @@ import com.linecorp.armeria.server.ServiceConfig;
 import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.ServiceWithPathMappings;
 
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.QueryStringDecoder;
 
 /**
@@ -50,6 +55,20 @@ import io.netty.handler.codec.http.QueryStringDecoder;
  * or handling a logout request from an identity provider.
  */
 final class SamlService implements ServiceWithPathMappings<HttpRequest, HttpResponse> {
+
+    private static final HttpData DATA_INCORRECT_PATH =
+            HttpData.ofUtf8(HttpResponseStatus.BAD_REQUEST + "\nSAML request with an incorrect path");
+
+    private static final HttpData DATA_AGGREGATION_FAILURE =
+            HttpData.ofUtf8(HttpResponseStatus.BAD_REQUEST + "\nSAML request aggregation failure");
+
+    private static final HttpData DATA_NOT_TLS =
+            HttpData.ofUtf8(HttpResponseStatus.BAD_REQUEST + "\nSAML request not from a TLS connection");
+
+    private static final HttpData DATA_NOT_CLEARTEXT =
+            HttpData.ofUtf8(HttpResponseStatus.BAD_REQUEST + "\nSAML request not from a cleartext connection");
+
+    private static final Logger logger = LoggerFactory.getLogger(SamlService.class);
 
     private final SamlServiceProvider sp;
     private final SamlPortConfigAutoFiller portConfigHolder;
@@ -121,7 +140,8 @@ final class SamlService implements ServiceWithPathMappings<HttpRequest, HttpResp
     public HttpResponse serve(ServiceRequestContext ctx, HttpRequest req) throws Exception {
         final SamlServiceFunction func = serviceMap.get(req.path());
         if (func == null) {
-            return HttpResponse.of(HttpStatus.BAD_REQUEST);
+            return HttpResponse.of(HttpStatus.BAD_REQUEST, MediaType.PLAIN_TEXT_UTF_8,
+                                   DATA_INCORRECT_PATH);
         }
 
         final CompletionStage<AggregatedHttpMessage> f;
@@ -132,11 +152,23 @@ final class SamlService implements ServiceWithPathMappings<HttpRequest, HttpResp
         }
         return HttpResponse.from(f.handle((msg, cause) -> {
             if (cause != null) {
-                return HttpResponse.of(HttpStatus.BAD_REQUEST);
+                logger.warn("{} Failed to aggregate a SAML request.", ctx, cause);
+                return HttpResponse.of(HttpStatus.BAD_REQUEST, MediaType.PLAIN_TEXT_UTF_8,
+                                       DATA_AGGREGATION_FAILURE);
             }
+
             final SamlPortConfig portConfig = portConfigHolder.config().get();
-            if (portConfig.scheme().isTls() != ctx.sessionProtocol().isTls()) {
-                return HttpResponse.of(HttpStatus.BAD_REQUEST);
+            final boolean isTls = ctx.sessionProtocol().isTls();
+            if (portConfig.scheme().isTls() != isTls) {
+                if (isTls) {
+                    logger.warn("{} Received a SAML request via a TLS connection.", ctx);
+                    return HttpResponse.of(HttpStatus.BAD_REQUEST, MediaType.PLAIN_TEXT_UTF_8,
+                                           DATA_NOT_CLEARTEXT);
+                } else {
+                    logger.warn("{} Received a SAML request via a cleartext connection.", ctx);
+                    return HttpResponse.of(HttpStatus.BAD_REQUEST, MediaType.PLAIN_TEXT_UTF_8,
+                                           DATA_NOT_TLS);
+                }
             }
 
             // Use user-specified hostname if it exists.
