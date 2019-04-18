0d198a1140bc38c21a1f06922c45121b3230b029
Anuraag Agrawal <anuraaga@gmail.com>
2019-04-18 16:43:04 +0900

    Capitalize

--- grpc/src/main/java/com/linecorp/armeria/internal/grpc/StatusExceptionConverter.java
+++ grpc/src/main/java/com/linecorp/armeria/internal/grpc/StatusExceptionConverter.java
@@ -35,7 +35,7 @@ public final class StatusExceptionConverter {
         // generating a stacktrace is expensive. Ideally, we could call the StatusRuntimeException with the
         // toggle for filling in the stacktrace, but it is not a public API. Other approaches could be to
         // subclass StatusRuntimeException (which will probably just cause upstream to declare the class final
-        // :P) or include ArmeriaStatusException in the cause chain of Status that we return from armeria. Both
+        // :P) or include ArmeriaStatusException in the cause chain of Status that we return from Armeria. Both
         // diverge from upstream behavior slightly and for now we assume the performance hit isn't something to
         // worry about since it only affects exceptions.
         converted.setStackTrace(armeria.getStackTrace());
