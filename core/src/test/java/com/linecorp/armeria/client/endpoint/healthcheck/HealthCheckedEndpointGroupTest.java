0e155f7589446e8df713663008872594c2618e3b
minux <songmw725@gmail.com>
2019-08-12 19:38:28 +0900

    Add a test for HealthCheckedEndpointGroup raising ConcurrentMod… (#1992)
    …ificationException previously
    
    Related #1990

--- core/src/test/java/com/linecorp/armeria/client/endpoint/healthcheck/HealthCheckedEndpointGroupTest.java
+++ core/src/test/java/com/linecorp/armeria/client/endpoint/healthcheck/HealthCheckedEndpointGroupTest.java
@@ -19,8 +19,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.awaitility.Awaitility.await;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
@@ -31,9 +34,50 @@ import com.google.common.collect.ImmutableList;
 
 import com.linecorp.armeria.client.Endpoint;
 import com.linecorp.armeria.client.endpoint.DynamicEndpointGroup;
+import com.linecorp.armeria.common.CommonPools;
 import com.linecorp.armeria.common.util.AsyncCloseable;
 
+import io.netty.channel.EventLoopGroup;
+
 class HealthCheckedEndpointGroupTest {
+
+    @Test
+    void delegateUpdateCandidatesWhileCreatingHealthCheckedEndpointGroup() {
+        final MockEndpointGroup delegate = new MockEndpointGroup();
+        final CompletableFuture<List<Endpoint>> future = delegate.initialEndpointsFuture();
+        future.complete(ImmutableList.of(Endpoint.of("127.0.0.1", 8080), Endpoint.of("127.0.0.1", 8081)));
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // Schedule the task which update the endpoint one second later to ensure that the change is happening
+        // while creating the HealthCheckedEndpointGroup.
+        final EventLoopGroup executors = CommonPools.workerGroup();
+        executors.schedule(
+                () -> {
+                    delegate.set(Endpoint.of("127.0.0.1", 8082));
+                    latch.countDown();
+                }, 1, TimeUnit.SECONDS);
+
+        new AbstractHealthCheckedEndpointGroupBuilder(delegate) {
+            @Override
+            protected Function<? super HealthCheckerContext, ? extends AsyncCloseable> newCheckerFactory() {
+                return (Function<HealthCheckerContext, AsyncCloseable>) ctx -> {
+                    // Call updateHealth after the endpoint is changed so that
+                    // snapshot.forEach(ctx -> ctx.initialCheckFuture.join()); performs the next action.
+                    executors.schedule(() -> {
+                        try {
+                            latch.await();
+                        } catch (InterruptedException e) {
+                            // Ignore
+                        }
+                        ctx.updateHealth(1);
+                    }, 2, TimeUnit.SECONDS);
+                    return CompletableFuture::new;
+                };
+            }
+        }.build();
+    }
+
     @Test
     void disappearedEndpoint() {
         // Start with an endpoint group that has healthy 'foo'.
@@ -74,7 +118,8 @@ class HealthCheckedEndpointGroupTest {
             assertThat(group.healthyEndpoints).isEmpty();
 
             // An attempt to schedule a new task for a disappeared endpoint must fail.
-            assertThatThrownBy(() -> ctx.executor().execute(() -> {}))
+            assertThatThrownBy(() -> ctx.executor().execute(() -> {
+            }))
                     .isInstanceOf(RejectedExecutionException.class)
                     .hasMessageContaining("destroyed");
         }
