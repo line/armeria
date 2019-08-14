71eb65c4ff02b40989b081766762567c617d5693
Trustin Lee <trustin@linecorp.com>
2019-08-14 17:51:33 +0800

    Update dependencies / Fix Netty HTTP/2 security issues (#1996)
    - Brave 5.6.9 -> 5.6.10
    - gRPC 1.22.1 -> 1.22.2
    - Netty 4.1.38 -> 4.1.39
    - SLF4J 1.7.27 -> 1.7.28
    - Use the builder API provided by Netty to construct an
      `Http2ConnectionHandler` so that the security fixes are applied.

---
+++ core/src/main/java/com/linecorp/armeria/server/Http2ServerConnectionHandlerBuilder.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.server;
+
+import com.linecorp.armeria.internal.AbstractHttp2ConnectionHandlerBuilder;
+
+import io.netty.channel.Channel;
+import io.netty.handler.codec.http2.Http2ConnectionDecoder;
+import io.netty.handler.codec.http2.Http2ConnectionEncoder;
+import io.netty.handler.codec.http2.Http2Settings;
+
+final class Http2ServerConnectionHandlerBuilder
+        extends AbstractHttp2ConnectionHandlerBuilder<Http2ServerConnectionHandler,
+                                                      Http2ServerConnectionHandlerBuilder> {
+
+    private final ServerConfig config;
+    private final GracefulShutdownSupport gracefulShutdownSupport;
+    private final String scheme;
+
+    Http2ServerConnectionHandlerBuilder(Channel ch, ServerConfig config,
+                                        GracefulShutdownSupport gracefulShutdownSupport,
+                                        String scheme) {
+        super(ch);
+        this.config = config;
+        this.gracefulShutdownSupport = gracefulShutdownSupport;
+        this.scheme = scheme;
+    }
+
+    @Override
+    protected Http2ServerConnectionHandler build(Http2ConnectionDecoder decoder,
+                                                 Http2ConnectionEncoder encoder,
+                                                 Http2Settings initialSettings) throws Exception {
+        return new Http2ServerConnectionHandler(decoder, encoder, initialSettings, channel(),
+                                                config, gracefulShutdownSupport, scheme);
+    }
+}
