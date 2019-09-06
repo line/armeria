e3ae71bc1de7618194376f85f5e93066ad2ff468
Anuraag Agrawal <anuraaga@gmail.com>
2019-09-06 15:48:04 +0900

    Add CompositeEndpointGroup (#1939)
    Found while working on https://github.com/openzipkin/zipkin/blob/81a481b490399a797bc5d04c00ee7de400a4f015/zipkin-storage/elasticsearch/src/main/java/zipkin2/elasticsearch/ElasticsearchStorage.java#L527
    
    Also makes `Endpoint` implement `EndpointGroup` as a group of itself so APIs can accept both groups and non-groups transparently.
    
    Fixes #1897

--- core/src/test/java/com/linecorp/armeria/client/endpoint/EndpointGroupTest.java
+++ core/src/test/java/com/linecorp/armeria/client/endpoint/EndpointGroupTest.java
@@ -18,20 +18,101 @@ package com.linecorp.armeria.client.endpoint;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableList;
 
 import com.linecorp.armeria.client.Endpoint;
 
-public class EndpointGroupTest {
+class EndpointGroupTest {
+
+    private static final Endpoint FOO = Endpoint.of("foo");
+    private static final Endpoint BAR = Endpoint.of("bar");
+    private static final Endpoint CAT = Endpoint.of("cat");
+    private static final Endpoint DOG = Endpoint.of("dog");
+    private static final Endpoint HELLO = Endpoint.of("hello");
+    private static final Endpoint WORLD = Endpoint.of("world");
+    private static final Endpoint GITHUB = Endpoint.of("github");
+
     @Test
-    public void orElse() throws Exception {
-        final EndpointGroup emptyEndpointGroup = new StaticEndpointGroup();
-        final EndpointGroup endpointGroup1 = new StaticEndpointGroup(Endpoint.of("127.0.0.1", 1234));
-        final EndpointGroup endpointGroup2 = new StaticEndpointGroup(Endpoint.of("127.0.0.1", 2345));
+    void orElse() {
+        final EndpointGroup emptyEndpointGroup = EndpointGroup.empty();
+        final EndpointGroup endpointGroup1 = EndpointGroup.of(Endpoint.of("127.0.0.1", 1234));
+        final EndpointGroup endpointGroup2 = EndpointGroup.of(Endpoint.of("127.0.0.1", 2345));
 
         assertThat(emptyEndpointGroup.orElse(endpointGroup2).endpoints())
                 .isEqualTo(endpointGroup2.endpoints());
         assertThat(endpointGroup1.orElse(endpointGroup2).endpoints())
                 .isEqualTo(endpointGroup1.endpoints());
+    }
+
+    @Test
+    void normal() {
+        final DynamicEndpointGroup group1 = new DynamicEndpointGroup();
+        group1.setEndpoints(ImmutableList.of(FOO, BAR));
+        final DynamicEndpointGroup group2 = new DynamicEndpointGroup();
+        group2.setEndpoints(ImmutableList.of(CAT, DOG));
+        final StaticEndpointGroup group3 = new StaticEndpointGroup(HELLO, WORLD);
+
+        final EndpointGroup composite = EndpointGroup.of(group1, group2, group3, GITHUB);
+        assertThat(composite.endpoints()).containsExactlyInAnyOrder(FOO, BAR, CAT, DOG, HELLO, WORLD, GITHUB);
+        // Same instance of endpoints returned unless there are updates.
+        assertThat(composite.endpoints()).isSameAs(composite.endpoints());
+
+        group1.setEndpoints(ImmutableList.of(FOO));
+        assertThat(composite.endpoints()).containsExactlyInAnyOrder(FOO, CAT, DOG, HELLO, WORLD, GITHUB);
+
+        group1.setEndpoints(ImmutableList.of(FOO, BAR));
+        group2.setEndpoints(ImmutableList.of());
+        assertThat(composite.endpoints()).containsExactlyInAnyOrder(FOO, BAR, HELLO, WORLD, GITHUB);
+    }
+
+    @Test
+    void oneEndpoint() {
+        final EndpointGroup composite = EndpointGroup.of(GITHUB);
+        assertThat(composite.endpoints()).containsExactly(GITHUB);
+        assertThat(composite).isSameAs(GITHUB);
+    }
+
+    @Test
+    void oneGroup() {
+        final EndpointGroup group = EndpointGroup.of(FOO, BAR);
+        final EndpointGroup composite = EndpointGroup.of(group);
+        assertThat(composite.endpoints()).containsExactlyInAnyOrder(FOO, BAR);
+    }
+
+    @Nested
+    class InitialEndpoints {
+        @Test
+        void group1First() throws Exception {
+            final DynamicEndpointGroup group1 = new DynamicEndpointGroup();
+            final DynamicEndpointGroup group2 = new DynamicEndpointGroup();
+            final EndpointGroup composite = EndpointGroup.of(group1, group2);
+            final CompletableFuture<List<Endpoint>> initialEndpoints = composite.initialEndpointsFuture();
+            assertThat(initialEndpoints).isNotCompleted();
+
+            group1.setEndpoints(ImmutableList.of(FOO, BAR));
+            group2.setEndpoints(ImmutableList.of(CAT, DOG));
+            assertThat(initialEndpoints.join()).containsExactlyInAnyOrder(FOO, BAR);
+            assertThat(composite.awaitInitialEndpoints()).containsExactlyInAnyOrder(FOO, BAR);
+        }
+
+        @Test
+        void group2First() throws Exception {
+            final DynamicEndpointGroup group1 = new DynamicEndpointGroup();
+            final DynamicEndpointGroup group2 = new DynamicEndpointGroup();
+            final EndpointGroup composite = EndpointGroup.of(group1, group2);
+            final CompletableFuture<List<Endpoint>> initialEndpoints = composite.initialEndpointsFuture();
+            assertThat(initialEndpoints).isNotCompleted();
+
+            group2.setEndpoints(ImmutableList.of(CAT, DOG));
+            group1.setEndpoints(ImmutableList.of(FOO, BAR));
+            assertThat(initialEndpoints.join()).containsExactlyInAnyOrder(CAT, DOG);
+            assertThat(composite.awaitInitialEndpoints()).containsExactlyInAnyOrder(CAT, DOG);
+        }
     }
 }
