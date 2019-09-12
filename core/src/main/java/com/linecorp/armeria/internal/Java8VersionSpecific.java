85d271492ca7b2033c9cabb1ae6025c3d19f2661
Anuraag Agrawal <anuraaga@gmail.com>
2019-09-12 12:50:44 +0100

    Make java 8 version specific forwards-compatible.

--- core/src/main/java/com/linecorp/armeria/internal/Java8VersionSpecific.java
+++ core/src/main/java/com/linecorp/armeria/internal/Java8VersionSpecific.java
@@ -18,21 +18,81 @@ package com.linecorp.armeria.internal;
 
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
- * Implementation of {@link JavaVersionSpecific} using Java 8 APIs.
+ * Implementation of {@link JavaVersionSpecific} using Java 8 APIs. In general, this class is only used with
+ * Java 8 because we override Java 9+ using a multi-release JAR. But ensure any logic is forwards-compatible on
+ * all Java versions because this class may be used outside the multi-release JAR, e.g., in testing or when a
+ * user shades without creating their own multi-release JAR.
  */
 class Java8VersionSpecific extends JavaVersionSpecific {
+
+    private static final Logger logger = LoggerFactory.getLogger(Java8VersionSpecific.class);
+
+    private static final int JAVA_VERSION;
 
     private static boolean JETTY_ALPN_OPTIONAL_OR_AVAILABLE;
 
     static {
+        //
+        int javaVersion = -1;
         try {
-            // Always use bootstrap class loader.
-            Class.forName("sun.security.ssl.ALPNExtension", true, null);
+            final String spec = System.getProperty("java.specification.version");
+            if (spec != null) {
+                final String[] strValues = spec.split("\\.");
+                final int major;
+                final int minor;
+
+                switch (strValues.length) {
+                    case 0:
+                        major = 0;
+                        minor = 0;
+                        break;
+                    case 1:
+                        major = Integer.parseInt(strValues[0]);
+                        minor = 0;
+                        break;
+                    default:
+                        major = Integer.parseInt(strValues[0]);
+                        minor = Integer.parseInt(strValues[1]);
+                }
+
+                if (major > 1) {
+                    javaVersion = major;
+                } else if (major == 1) {
+                    if (minor == 0) {
+                        javaVersion = 1;
+                    } else if (minor > 0) {
+                        javaVersion = minor;
+                    }
+                }
+            }
+
+            if (javaVersion > 0) {
+                logger.debug("Java version: {}", javaVersion);
+            } else {
+                logger.warn("'java.specification.version' contains an unexpected value: {}", spec);
+            }
+        } catch (Throwable t) {
+            logger.warn("Failed to determine Java version", t);
+        }
+
+        JAVA_VERSION = javaVersion > 0 ? javaVersion : 8;
+
+        // ALPN check from https://github.com/netty/netty/blob/1065e0f26e0d47a67c479b0fad81efab5d9438d9/handler/src/main/java/io/netty/handler/ssl/JettyAlpnSslEngine.java
+        if (JAVA_VERSION >= 9) {
             JETTY_ALPN_OPTIONAL_OR_AVAILABLE = true;
-        } catch (Throwable ignore) {
-            // alpn-boot was not loaded.
-            JETTY_ALPN_OPTIONAL_OR_AVAILABLE = false;
+        } else {
+            try {
+                // Always use bootstrap class loader.
+                Class.forName("sun.security.ssl.ALPNExtension", true, null);
+                JETTY_ALPN_OPTIONAL_OR_AVAILABLE = true;
+            } catch (Throwable ignore) {
+                // alpn-boot was not loaded.
+                JETTY_ALPN_OPTIONAL_OR_AVAILABLE = false;
+            }
         }
     }
 
@@ -43,8 +103,7 @@ class Java8VersionSpecific extends JavaVersionSpecific {
 
     @Override
     public int javaVersion() {
-        // This class is only used on Java 8.
-        return 8;
+        return JAVA_VERSION;
     }
 
     @Override
