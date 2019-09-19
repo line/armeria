81ffb5af7c263d1ea3188e1c74054198059c9ec4
Anuraag Agrawal <anuraaga@gmail.com>
2019-09-19 09:23:58 +0100

    Add Java 9 overrides of platform-specific APIs. (#2069)
    Works around proguard not supporting this by reassembling a multi-release JAR in the end. I guess at some point someone would copy the gradle changes into a `java-mrjar.gradle` in the scripts repo :) When doing so, I would add the `--release` argument globally without requiring any special tag as it's better to specify it than not to get animal-sniffer type checking automatically by javac.
    
    Fixes #2062

---
+++ core/src/main/java/com/linecorp/armeria/internal/JavaVersionSpecific.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.internal;
+
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Contains APIs that are implemented differently based on the version of Java being run. This class implements
+ * the default, using Java 8 APIs, the minimum version supported by Armeria. All implementations in this class
+ * must be forwards-compatible on all Java versions because this class may be used outside the multi-release
+ * JAR, e.g., in testing or when a user shades without creating their own multi-release JAR.
+ */
+public class JavaVersionSpecific {
+
+    private static Logger logger = LoggerFactory.getLogger(JavaVersionSpecific.class);
+
+    private static final JavaVersionSpecific CURRENT = CurrentJavaVersionSpecific.get();
+
+    static {
+        logger.info("Using version specific APIs from {}", CURRENT.getClass().getSimpleName());
+    }
+
+    /**
+     * Returns the {@link JavaVersionSpecific} for the current version of Java.
+     */
+    public static JavaVersionSpecific get() {
+        return CURRENT;
+    }
+
+    /**
+     * Returns the number of microseconds since the epoch (00:00:00, 01-Jan-1970, GMT).
+     */
+    public long currentTimeMicros() {
+        return TimeUnit.MILLISECONDS.toMicros(System.currentTimeMillis());
+    }
+}
