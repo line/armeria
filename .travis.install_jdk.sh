7627d274572e870fb3abaa42579ff7f996e9f64a
Anuraag Agrawal <anuraaga@gmail.com>
2019-09-29 19:22:07 +0900

    Try a different path

--- .travis.install_jdk.sh
+++ .travis.install_jdk.sh
@@ -3,4 +3,5 @@
 # https://docs.travis-ci.com/user/languages/java/#switching-jdks-to-java-10-and-up-within-one-job
 export JAVA_TEST_HOME=$JAVA_HOME
 export JAVA_HOME=$HOME/openjdk11
-$TRAVIS_BUILD_DIR/install-jdk.sh --install openjdk11 --target $JAVA_HOME
+# For some reason, install-jdk.sh isn't available at the location mentioned in docs.
+~/bin/install-jdk.sh --install openjdk11 --target $JAVA_HOME
