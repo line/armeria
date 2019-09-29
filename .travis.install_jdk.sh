d5d93c89b556be177ec6db73349baf6a03799ba7
Anuraag Agrawal <anuraaga@gmail.com>
2019-09-29 19:26:02 +0900

    Use different install-jdk.sh

--- .travis.install_jdk.sh
+++ .travis.install_jdk.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# For some reason, official Travis CI docs for using install-jdk.sh don't work so we use a different one.
 # https://docs.travis-ci.com/user/languages/java/#switching-jdks-to-java-10-and-up-within-one-job
+#
+# https://github.com/sormuras/sormuras.github.io/blob/master/.travis.yml
+
 export JAVA_TEST_HOME=$JAVA_HOME
-export JAVA_HOME=$HOME/openjdk11
-# For some reason, install-jdk.sh isn't available at the location mentioned in docs.
-~/bin/install-jdk.sh --install openjdk11 --target $JAVA_HOME
+wget https://github.com/sormuras/bach/raw/master/install-jdk.sh
+source ./install-jdk.sh --feature 11
