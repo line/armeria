798f1fb351e59104dba074fe6957d846d8007b69
Anuraag Agrawal <anuraaga@gmail.com>
2019-10-01 15:27:33 +0900

    Manually remove temp jdk which is reextracted on second invocation.

--- .travis.install_jdk.sh
+++ .travis.install_jdk.sh
@@ -14,13 +14,16 @@ if [[ -n "$1" ]]; then
   shift 1
   # Can't source twice from install-jdk.sh in same build so we go ahead and manually set up variables.
   export JAVA_TEST_HOME="$HOME/.jdk/openjdk$FEATURE"
-  bash ./install-jdk.sh -v --feature "$FEATURE" --target "$JAVA_TEST_HOME" --workspace "/tmp/openjdk$FEATURE"
+  bash ./install-jdk.sh -v --feature "$FEATURE" --target "$JAVA_TEST_HOME"
+  # install-jdk.sh downloads to this fixed filename, preventing from downloading again
+  rm -f jdk.tar.gz
 else
   export JAVA_TEST_HOME="$JAVA_HOME"
 fi
 
 NEW_JAVA_HOME="$HOME/.jdk/openjdk11"
-bash ./install-jdk.sh -v --feature 11 --target "$NEW_JAVA_HOME" --workspace "/tmp/openjdk11"
+bash ./install-jdk.sh -v --feature 11 --target "$NEW_JAVA_HOME"
+rm -f jdk.tar.gz
 export JAVA_HOME="$NEW_JAVA_HOME"
 export PATH="$JAVA_HOME/bin:$PATH"
 
