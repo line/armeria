72d7be686d4a5afb12cd78b82abea9a8a16b7858
Anuraag Agrawal <anuraaga@gmail.com>
2019-10-01 15:08:08 +0900

    Move openjdk directories.

--- .travis.install_jdk.sh
+++ .travis.install_jdk.sh
@@ -13,13 +13,13 @@ if [[ -n "$1" ]]; then
   FEATURE="$1"
   shift 1
   # Can't source twice from install-jdk.sh in same build
-  export JAVA_TEST_HOME="$HOME/openjdk13"
+  export JAVA_TEST_HOME="$HOME/.jdk/openjdk$FEATURE"
   bash ./install-jdk.sh --feature "$FEATURE" --target "$JAVA_TEST_HOME"
 else
   export JAVA_TEST_HOME="$JAVA_HOME"
 fi
 
-NEW_JAVA_HOME="$HOME/openjdk11"
+NEW_JAVA_HOME="$HOME/.jdk/openjdk11"
 bash ./install-jdk.sh --feature 11 --target "$NEW_JAVA_HOME"
 export JAVA_HOME="$NEW_JAVA_HOME"
 export PATH="$JAVA_HOME/bin:$PATH"
