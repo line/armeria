426a57ccedb3849fc0a0b0b18ef257d24058c132
sohyunpark <sohyun@vendys.co.kr>
2019-10-04 21:26:38 +0900

    myrepository file change

--- examples/spring-boot-tomcat/src/main/java/example/springframework/boot/tomcat/HelloController.java
+++ examples/spring-boot-tomcat/src/main/java/example/springframework/boot/tomcat/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET, path = "/")
     String index() {
-        return "index1";
+        return "index";
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
