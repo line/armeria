c427e0348ef35a081d452948dd7db1299b415842
sivaalli <siva_alli@hotmail.com>
2019-10-07 18:24:13 -0400

    making few fixing, like adding license, adding comments back

--- core/src/main/java/com/linecorp/armeria/server/AbstractServiceBindingBuilder.java
+++ core/src/main/java/com/linecorp/armeria/server/AbstractServiceBindingBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linecorp.armeria.server;
 
 import static com.linecorp.armeria.server.ServerConfig.validateMaxRequestLength;
@@ -18,6 +34,12 @@ import com.linecorp.armeria.common.logging.ContentPreviewerFactory;
 import com.linecorp.armeria.internal.ArmeriaHttpUtil;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 
+/**	public abstract class AbstractServiceBindingBuilder extends AbstractBindingBuilder implements ServiceBuilder {
+ * A builder class for binding a {@link Service} fluently.
+ *
+ * @see ServiceBindingBuilder
+ * @see VirtualHostServiceBindingBuilder
+ */
 public abstract class AbstractServiceBindingBuilder extends AbstractBindingBuilder implements ServiceBuilder {
     @Nullable
     private Long requestTimeoutMillis;
