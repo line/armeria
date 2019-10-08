96c842ee82170eb03dacff6af1ddb24a455a4d5a
sivaalli <siva_alli@hotmail.com>
2019-10-08 18:33:24 -0400

    adding source files.added annotatedServiceBuilder and all the related change. testing is pending. committing before leaving.

---
+++ core/src/main/java/com/linecorp/armeria/server/AnnotatedServiceBindingBuilder.java
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import static java.util.Objects.requireNonNull;
+
+import java.nio.charset.Charset;
+import java.time.Duration;
+import java.util.List;
+import java.util.function.Function;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList.Builder;
+import com.google.common.collect.Iterables;
+
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.logging.ContentPreviewerFactory;
+import com.linecorp.armeria.internal.annotation.AnnotatedHttpServiceElement;
+import com.linecorp.armeria.internal.annotation.AnnotatedHttpServiceFactory;
+import com.linecorp.armeria.server.annotation.ExceptionHandlerFunction;
+import com.linecorp.armeria.server.annotation.RequestConverterFunction;
+import com.linecorp.armeria.server.annotation.ResponseConverterFunction;
+import com.linecorp.armeria.server.logging.AccessLogWriter;
+
+public class AnnotatedServiceBindingBuilder extends AbstractServiceBuilder {
+
+    private final ServerBuilder serverBuilder;
+    private final Builder<ExceptionHandlerFunction> exceptionHandlerFunctionBuilder = ImmutableList.builder();
+    private final Builder<RequestConverterFunction> requestConverterFunctionBuilder = ImmutableList.builder();
+    private final Builder<ResponseConverterFunction> responseConverterFunctionBuilder = ImmutableList.builder();
+    private String pathPrefix = "/";
+
+    public AnnotatedServiceBindingBuilder(ServerBuilder serverBuilder) {
+        this.serverBuilder = requireNonNull(serverBuilder, "serverBuilder");
+    }
+
+    @Override
+    void serviceConfigBuilder(ServiceConfigBuilder serviceConfigBuilder) {
+        serverBuilder.serviceConfigBuilder(serviceConfigBuilder);
+    }
+
+    public AnnotatedServiceBindingBuilder pathPrefix(String pathPrefix) {
+        this.pathPrefix = requireNonNull(pathPrefix, "pathPrefix");
+        return this;
+    }
+
+    public AnnotatedServiceBindingBuilder exceptionHandler(ExceptionHandlerFunction exceptionHandlerFunction) {
+        requireNonNull(exceptionHandlerFunction, "exceptionHandler");
+        exceptionHandlerFunctionBuilder.add(exceptionHandlerFunction);
+        return this;
+    }
+
+    public AnnotatedServiceBindingBuilder responseHandler(ResponseConverterFunction responseConverterFunction) {
+        requireNonNull(responseConverterFunction, "responseConverterFunction");
+        responseConverterFunctionBuilder.add(responseConverterFunction);
+        return this;
+    }
+
+    public AnnotatedServiceBindingBuilder requestHandler(RequestConverterFunction requestConverterFunction) {
+        requireNonNull(requestConverterFunction, "requestConverterFunction");
+        requestConverterFunctionBuilder.add(requestConverterFunction);
+        return this;
+    }
+
+    @Override
+    public <T extends Service<HttpRequest, HttpResponse>, R extends Service<HttpRequest, HttpResponse>>
+    AnnotatedServiceBindingBuilder decorator(Function<T, R> decorator) {
+        return (AnnotatedServiceBindingBuilder) super.decorator(decorator);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder requestTimeout(Duration requestTimeout) {
+        return (AnnotatedServiceBindingBuilder) super.requestTimeout(requestTimeout);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder requestTimeoutMillis(long requestTimeoutMillis) {
+        return (AnnotatedServiceBindingBuilder) super.requestTimeoutMillis(requestTimeoutMillis);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder maxRequestLength(long maxRequestLength) {
+        return (AnnotatedServiceBindingBuilder) super.maxRequestLength(maxRequestLength);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder verboseResponses(boolean verboseResponses) {
+        return (AnnotatedServiceBindingBuilder) super.verboseResponses(verboseResponses);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder requestContentPreviewerFactory(ContentPreviewerFactory factory) {
+        return (AnnotatedServiceBindingBuilder) super.requestContentPreviewerFactory(factory);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder responseContentPreviewerFactory(ContentPreviewerFactory factory) {
+        return (AnnotatedServiceBindingBuilder) super.responseContentPreviewerFactory(factory);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder contentPreview(int length) {
+        return (AnnotatedServiceBindingBuilder) super.contentPreview(length);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder contentPreview(int length, Charset defaultCharset) {
+        return (AnnotatedServiceBindingBuilder) super.contentPreview(length, defaultCharset);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder contentPreviewerFactory(ContentPreviewerFactory factory) {
+        return (AnnotatedServiceBindingBuilder) super.contentPreviewerFactory(factory);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder accessLogFormat(String accessLogFormat) {
+        return (AnnotatedServiceBindingBuilder) super.accessLogFormat(accessLogFormat);
+    }
+
+    @Override
+    public AnnotatedServiceBindingBuilder accessLogWriter(AccessLogWriter accessLogWriter, boolean shutdownOnStop) {
+        return (AnnotatedServiceBindingBuilder) super.accessLogWriter(accessLogWriter, shutdownOnStop);
+    }
+
+    public ServerBuilder build(Object service) {
+        final ImmutableList<ExceptionHandlerFunction> exceptionHandlerFunctions =
+                exceptionHandlerFunctionBuilder.build();
+        final ImmutableList<RequestConverterFunction> requestConverterFunctions =
+                requestConverterFunctionBuilder.build();
+        final ImmutableList<ResponseConverterFunction> responseConverterFunctions =
+                responseConverterFunctionBuilder.build();
+
+        final Iterable<?> exceptionHandlersAndConverters = Iterables.concat(exceptionHandlerFunctions,
+                                                                            requestConverterFunctions,
+                                                                            responseConverterFunctions);
+        final List<AnnotatedHttpServiceElement> elements =
+            AnnotatedHttpServiceFactory.find(pathPrefix, service, exceptionHandlersAndConverters);
+        elements.forEach(e -> {
+            Service<HttpRequest, HttpResponse> s = e.service();
+            // Apply decorators which are specified in the service class.
+            s = e.decorator().apply(s);
+
+            // If there is a decorator, we should add one more decorator which handles an exception
+            // raised from decorators.
+            if (s != e.service()) {
+                s = e.service().exceptionHandlingDecorator().apply(s);
+            }
+            build0(e.route(), s);
+        });
+        return serverBuilder;
+    }
+}
---
+++ core/src/main/java/com/linecorp/armeria/server/ServiceBuilder.java
@@ -0,0 +1,122 @@
+package com.linecorp.armeria.server;
+
+import java.nio.charset.Charset;
+import java.time.Duration;
+import java.util.function.Function;
+
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.logging.ContentPreviewer;
+import com.linecorp.armeria.common.logging.ContentPreviewerFactory;
+import com.linecorp.armeria.server.logging.AccessLogWriter;
+
+interface ServiceBuilder {
+
+    /**
+     * Sets the timeout of an HTTP request. If not set, {@link VirtualHost#requestTimeoutMillis()}
+     * is used.
+     *
+     * @param requestTimeout the timeout. {@code 0} disables the timeout.
+     */
+    ServiceBuilder requestTimeout(Duration requestTimeout);
+
+    /**
+     * Sets the timeout of an HTTP request in milliseconds. If not set,
+     * {@link VirtualHost#requestTimeoutMillis()} is used.
+     *
+     * @param requestTimeoutMillis the timeout in milliseconds. {@code 0} disables the timeout.
+     */
+    ServiceBuilder requestTimeoutMillis(long requestTimeoutMillis);
+
+    /**
+     * Sets the maximum allowed length of an HTTP request. If not set,
+     * {@link VirtualHost#maxRequestLength()} is used.
+     *
+     * @param maxRequestLength the maximum allowed length. {@code 0} disables the length limit.
+     */
+    ServiceBuilder maxRequestLength(long maxRequestLength);
+
+    /**
+     * Sets whether the verbose response mode is enabled. When enabled, the service response will contain
+     * the exception type and its full stack trace, which may be useful for debugging while potentially
+     * insecure. When disabled, the service response will not expose such server-side details to the client.
+     * If not set, {@link VirtualHost#verboseResponses()} is used.
+     */
+    ServiceBuilder verboseResponses(boolean verboseResponses);
+
+    /**
+     * Sets the {@link ContentPreviewerFactory} for an HTTP request of a {@link Service}.
+     * If not set, {@link VirtualHost#requestContentPreviewerFactory()}
+     * is used.
+     */
+    ServiceBuilder requestContentPreviewerFactory(ContentPreviewerFactory factory);
+
+    /**
+     * Sets the {@link ContentPreviewerFactory} for an HTTP response of a {@link Service}.
+     * If not set, {@link VirtualHost#responseContentPreviewerFactory()}
+     * is used.
+     */
+    ServiceBuilder responseContentPreviewerFactory(ContentPreviewerFactory factory);
+    /**
+     * Sets the {@link ContentPreviewerFactory} for creating a {@link ContentPreviewer} which produces the
+     * preview with the maximum {@code length} limit for an HTTP request/response of the {@link Service}.
+     * The previewer is enabled only if the content type of an HTTP request/response meets
+     * any of the following conditions:
+     * <ul>
+     *     <li>when it matches {@code text/*} or {@code application/x-www-form-urlencoded}</li>
+     *     <li>when its charset has been specified</li>
+     *     <li>when its subtype is {@code "xml"} or {@code "json"}</li>
+     *     <li>when its subtype ends with {@code "+xml"} or {@code "+json"}</li>
+     * </ul>
+     * @param length the maximum length of the preview.
+     */
+    ServiceBuilder contentPreview(int length);
+
+    /**
+     * Sets the {@link ContentPreviewerFactory} for creating a {@link ContentPreviewer} which produces the
+     * preview with the maximum {@code length} limit for an HTTP request/response of a {@link Service}.
+     * The previewer is enabled only if the content type of an HTTP request/response meets any of
+     * the following conditions:
+     * <ul>
+     *     <li>when it matches {@code text/*} or {@code application/x-www-form-urlencoded}</li>
+     *     <li>when its charset has been specified</li>
+     *     <li>when its subtype is {@code "xml"} or {@code "json"}</li>
+     *     <li>when its subtype ends with {@code "+xml"} or {@code "+json"}</li>
+     * </ul>
+     * @param length the maximum length of the preview
+     * @param defaultCharset the default charset used when a charset is not specified in the
+     *                       {@code "content-type"} header
+     */
+    ServiceBuilder contentPreview(int length, Charset defaultCharset);
+
+    /**
+     * Sets the {@link ContentPreviewerFactory} for an HTTP request/response of a {@link Service}.
+     */
+    ServiceBuilder contentPreviewerFactory(ContentPreviewerFactory factory);
+
+    /**
+     * Sets the format of this {@link Service}'s access log. The specified {@code accessLogFormat} would be
+     * parsed by {@link AccessLogWriter#custom(String)}.
+     */
+    ServiceBuilder accessLogFormat(String accessLogFormat);
+
+    /**
+     * Sets the access log writer of this {@link Service}. If not set, {@link ServerConfig#accessLogWriter()}
+     * is used.
+     *
+     * @param shutdownOnStop whether to shut down the {@link AccessLogWriter} when the {@link Server} stops
+     */
+    ServiceBuilder accessLogWriter(AccessLogWriter accessLogWriter,
+                                   boolean shutdownOnStop);
+
+    /**
+     * Decorates a {@link Service} with the specified {@code decorator}.
+     *
+     * @param decorator the {@link Function} that decorates the {@link Service}
+     * @param <T> the type of the {@link Service} being decorated
+     * @param <R> the type of the {@link Service} {@code decorator} will produce
+     */
+    <T extends Service<HttpRequest, HttpResponse>, R extends Service<HttpRequest, HttpResponse>>
+    ServiceBuilder decorator(Function<T, R> decorator);
+
+}
