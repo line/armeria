8a13477c049f119a45ffb815d71772de4ec5fd24
Kim Jiyoung <jyblue.kim@gmail.com>
2019-10-18 05:48:27 +0900

    Modify `ArmeriaReactiveWebServerFactory.getWebServer()` not to use `Server` and `Date` headers

--- spring/boot-webflux-autoconfigure/src/main/java/com/linecorp/armeria/spring/web/reactive/ArmeriaReactiveWebServerFactory.java
+++ spring/boot-webflux-autoconfigure/src/main/java/com/linecorp/armeria/spring/web/reactive/ArmeriaReactiveWebServerFactory.java
@@ -131,6 +131,8 @@ public class ArmeriaReactiveWebServerFactory extends AbstractReactiveWebServerFa
     @Override
     public WebServer getWebServer(HttpHandler httpHandler) {
         final ServerBuilder sb = Server.builder();
+        sb.useServerHeader(false);
+        sb.useDateHeader(false);
 
         final SessionProtocol protocol;
         final Ssl ssl = getSsl();
