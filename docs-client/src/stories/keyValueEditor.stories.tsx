c6412e128d8f69925421a8a09fb6a4fae0597a78
tae <taeshindev@gmail.com>
2019-11-07 17:52:38 +0900

    lint

--- docs-client/src/stories/keyValueEditor.stories.tsx
+++ docs-client/src/stories/keyValueEditor.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import KeyValueEditor from "../components/KeyValueEditor";
+import KeyValueEditor from '../components/KeyValueEditor';
 
 export default { title: 'KeyValueEditor' };
 
-export const basic = () => <KeyValueEditor/>;
+export const basic = () => <KeyValueEditor />;
