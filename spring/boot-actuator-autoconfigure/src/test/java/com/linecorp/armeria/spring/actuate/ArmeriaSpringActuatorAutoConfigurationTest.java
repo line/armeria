3ecc8224d71e0a324d323d9f3cfcc0b98d1a836b
Anuraag Agrawal <anuraaga@gmail.com>
2019-11-25 14:55:44 +0900

    Finish

--- spring/boot-actuator-autoconfigure/src/test/java/com/linecorp/armeria/spring/actuate/ArmeriaSpringActuatorAutoConfigurationTest.java
+++ spring/boot-actuator-autoconfigure/src/test/java/com/linecorp/armeria/spring/actuate/ArmeriaSpringActuatorAutoConfigurationTest.java
@@ -16,7 +16,6 @@
 
 package com.linecorp.armeria.spring.actuate;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
