859f5bb35377d81a7edfa47df3ce1338df04eae0
heowc <heowc1992@gmail.com>
2019-11-29 21:11:35 +0900

    Refactor

--- core/src/main/java/com/linecorp/armeria/server/AnnotatedHttpServiceConfigurator.java
+++ core/src/main/java/com/linecorp/armeria/server/AnnotatedHttpServiceConfigurator.java
@@ -40,25 +40,16 @@ final class AnnotatedHttpServiceConfigurator {
     static AnnotatedHttpServiceConfigurator ofExceptionHandlersAndConverters(
             Iterable<?> exceptionHandlersAndConverters) {
 
-        Builder<ExceptionHandlerFunction> exceptionHandlers = null;
-        Builder<RequestConverterFunction> requestConverters = null;
-        Builder<ResponseConverterFunction> responseConverters = null;
+        final Builder<ExceptionHandlerFunction> exceptionHandlers = ImmutableList.builder();
+        final Builder<RequestConverterFunction> requestConverters = ImmutableList.builder();
+        final Builder<ResponseConverterFunction> responseConverters = ImmutableList.builder();
 
         for (final Object o : exceptionHandlersAndConverters) {
             if (o instanceof ExceptionHandlerFunction) {
-                if (exceptionHandlers == null) {
-                    exceptionHandlers = ImmutableList.builder();
-                }
                 exceptionHandlers.add((ExceptionHandlerFunction) o);
             } else if (o instanceof RequestConverterFunction) {
-                if (requestConverters == null) {
-                    requestConverters = ImmutableList.builder();
-                }
                 requestConverters.add((RequestConverterFunction) o);
             } else if (o instanceof ResponseConverterFunction) {
-                if (responseConverters == null) {
-                    responseConverters = ImmutableList.builder();
-                }
                 responseConverters.add((ResponseConverterFunction) o);
             } else {
                 throw new IllegalArgumentException(o.getClass().getName() +
@@ -66,99 +57,46 @@ final class AnnotatedHttpServiceConfigurator {
             }
         }
 
-        final List<ExceptionHandlerFunction> exceptionHandlerFunctions =
-                exceptionHandlers != null ? exceptionHandlers.build() : ImmutableList.of();
-        final List<RequestConverterFunction> requestConverterFunctions =
-                requestConverters != null ? requestConverters.build() : ImmutableList.of();
-        final List<ResponseConverterFunction> responseConverterFunctions =
-                responseConverters != null ? responseConverters.build() : ImmutableList.of();
-
-        return ofExceptionHandlersAndConverters(exceptionHandlerFunctions, requestConverterFunctions,
-                                                responseConverterFunctions);
-    }
-
-    /**
-     * Creates a new instance with the specified {@link ExceptionHandlerFunction}s,
-     * {@link RequestConverterFunction}s and {@link ResponseConverterFunction}s.
-     */
-    static AnnotatedHttpServiceConfigurator ofExceptionHandlersAndConverters(
-            List<ExceptionHandlerFunction> exceptionHandlerFunctions,
-            List<RequestConverterFunction> requestConverterFunctions,
-            List<ResponseConverterFunction> responseConverterFunctions) {
-
         final AnnotatedHttpServiceConfigurator configurator = new AnnotatedHttpServiceConfigurator();
-        configurator.configureExceptionHandlers(exceptionHandlerFunctions);
-        configurator.configureRequestConverters(requestConverterFunctions);
-        configurator.configureResponseConverters(responseConverterFunctions);
+        configurator.exceptionHandlers.addAll(exceptionHandlers.build());
+        configurator.requestConverters.addAll(requestConverters.build());
+        configurator.responseConverters.addAll(responseConverters.build());
         return configurator;
     }
 
     /**
      * The exception handlers of the annotated service.
      */
-    private List<ExceptionHandlerFunction> exceptionHandlers = ImmutableList.of();
+    private final Builder<ExceptionHandlerFunction> exceptionHandlers = ImmutableList.builder();
 
     /**
      * The request converters of the annotated service.
      */
-    private List<RequestConverterFunction> requestConverters = ImmutableList.of();
+    private final Builder<RequestConverterFunction> requestConverters = ImmutableList.builder();
 
     /**
      * The response converters of the annotated service.
      */
-    private List<ResponseConverterFunction> responseConverters = ImmutableList.of();
-
-    /**
-     * Configures the specified {@link ExceptionHandlerFunction}s with the annotated service.
-     */
-    void configureExceptionHandlers(List<ExceptionHandlerFunction> exceptionHandlers) {
-        requireNonNull(exceptionHandlers, "exceptionHandlers");
-        this.exceptionHandlers = ImmutableList.<ExceptionHandlerFunction>builder()
-                .addAll(this.exceptionHandlers)
-                .addAll(ImmutableList.copyOf(exceptionHandlers))
-                .build();
-    }
+    private final Builder<ResponseConverterFunction> responseConverters = ImmutableList.builder();
 
     /**
      * Returns the specified {@link ExceptionHandlerFunction}s with the annotated service.
      */
     List<ExceptionHandlerFunction> exceptionHandlers() {
-        return exceptionHandlers;
-    }
-
-    /**
-     * Configures the specified {@link RequestConverterFunction}s with the annotated service.
-     */
-    void configureRequestConverters(List<RequestConverterFunction> requestConverters) {
-        requireNonNull(requestConverters, "requestConverters");
-        this.requestConverters = ImmutableList.<RequestConverterFunction>builder()
-                .addAll(this.requestConverters)
-                .addAll(ImmutableList.copyOf(requestConverters))
-                .build();
+        return exceptionHandlers.build();
     }
 
     /**
      * Returns the specified {@link RequestConverterFunction}s with the annotated service.
      */
     List<RequestConverterFunction> requestConverters() {
-        return requestConverters;
-    }
-
-    /**
-     * Configures the specified {@link ResponseConverterFunction}s with the annotated service.
-     */
-    void configureResponseConverters(List<ResponseConverterFunction> responseConverters) {
-        requireNonNull(responseConverters, "responseConverters");
-        this.responseConverters = ImmutableList.<ResponseConverterFunction>builder()
-                .addAll(this.responseConverters)
-                .addAll(ImmutableList.copyOf(responseConverters))
-                .build();
+        return requestConverters.build();
     }
 
     /**
      * Returns the specified {@link ResponseConverterFunction}s with the annotated service.
      */
     List<ResponseConverterFunction> responseConverters() {
-        return responseConverters;
+        return responseConverters.build();
     }
 }
