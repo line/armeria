0c6193ccdfc3f046fe84ee8f874f1dbe3c5732e4
Charliocat <carlesguell.06@gmail.com>
2019-12-22 12:53:37 +0100

    header formatting and interface documentation

--- spring/boot-autoconfigure/src/main/java/com/linecorp/armeria/spring/DocServiceConfigurator.java
+++ spring/boot-autoconfigure/src/main/java/com/linecorp/armeria/spring/DocServiceConfigurator.java
@@ -1,18 +1,18 @@
 /*
-Copyright 2019 LINE Corporation
-
-LINE Corporation licenses this file to you under the Apache License,
-version 2.0 (the "License"); you may not use this file except in compliance
-with the License. You may obtain a copy of the License at:
-
-  https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-License for the specific language governing permissions and limitations
-under the License.
-*/
+ * Copyright 2019 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 
 package com.linecorp.armeria.spring;
 
@@ -22,7 +22,13 @@ import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.annotation.Order;
 
 import com.linecorp.armeria.server.docs.DocServiceBuilder;
+import com.linecorp.armeria.server.docs.DocService;
 
+/**
+ * Interface used to configure a {@link DocServiceBuilder} on the default armeria server. Can be
+ * used to register arbitrary builders to customize the {@link DocService}
+ * built by {@link ArmeriaAutoConfiguration}.
+ */
 @FunctionalInterface
 public interface DocServiceConfigurator extends Ordered {
     /**
