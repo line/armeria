42f289310280791d97c482d854bc0af10ff08894
sivaalli <siva_alli@hotmail.com>
2020-01-19 19:07:03 -0500

    fixing checkstyle errors.

--- core/src/main/java/com/linecorp/armeria/internal/Http2KeepAliveHandler.java
+++ core/src/main/java/com/linecorp/armeria/internal/Http2KeepAliveHandler.java
@@ -47,8 +47,8 @@ import io.netty.handler.timeout.IdleStateHandler;
  * This will send {@link Http2PingFrame} when an {@link IdleStateEvent} is emitted by {@link IdleStateHandler}.
  * Specifically, it will write a PING frame to remote and then expects an ACK back within
  * configured {@code pingTimeoutInNanos}. If timeout exceeds then channel will be closed.
- * <p>
- * This class is <b>not</b> thread-safe and all methods are to be called from single thread such as
+ *
+ * <p>This class is <b>not</b> thread-safe and all methods are to be called from single thread such as
  * {@link EventLoop}.
  */
 @NotThreadSafe
