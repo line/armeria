40053b55d3d085959944518c2d657cb328f31fc5
Trustin Lee <trustin@linecorp.com>
2020-02-06 18:24:28 +0900

    Clean up `HttpStatus.is*()` and their tests (#2459)
    This changeset is a follow-up of #2435

--- core/src/test/java/com/linecorp/armeria/common/HttpStatusTest.java
+++ core/src/test/java/com/linecorp/armeria/common/HttpStatusTest.java
@@ -22,49 +22,50 @@ import org.junit.jupiter.api.Test;
 class HttpStatusTest {
     @Test
     void statusIsInformationIfStatusCode2XX() {
+        assertThat(HttpStatus.valueOf(99).isInformational()).isFalse();
         assertThat(HttpStatus.valueOf(100).isInformational()).isTrue();
         assertThat(HttpStatus.valueOf(199).isInformational()).isTrue();
         assertThat(HttpStatus.valueOf(200).isInformational()).isFalse();
-        assertThat(HttpStatus.valueOf(300).isInformational()).isFalse();
-        assertThat(HttpStatus.valueOf(400).isInformational()).isFalse();
-        assertThat(HttpStatus.valueOf(500).isInformational()).isFalse();
     }
 
     @Test
     void statusIsSuccessIfStatusCode2XX() {
-        assertThat(HttpStatus.valueOf(100).isSuccess()).isFalse();
+        assertThat(HttpStatus.valueOf(199).isSuccess()).isFalse();
         assertThat(HttpStatus.valueOf(200).isSuccess()).isTrue();
         assertThat(HttpStatus.valueOf(299).isSuccess()).isTrue();
         assertThat(HttpStatus.valueOf(300).isSuccess()).isFalse();
-        assertThat(HttpStatus.valueOf(400).isSuccess()).isFalse();
-        assertThat(HttpStatus.valueOf(500).isSuccess()).isFalse();
     }
 
     @Test
     void statusIsRedirectionIfStatusCode3XX() {
-        assertThat(HttpStatus.valueOf(100).isRedirection()).isFalse();
-        assertThat(HttpStatus.valueOf(200).isRedirection()).isFalse();
+        assertThat(HttpStatus.valueOf(299).isRedirection()).isFalse();
         assertThat(HttpStatus.valueOf(300).isRedirection()).isTrue();
         assertThat(HttpStatus.valueOf(399).isRedirection()).isTrue();
         assertThat(HttpStatus.valueOf(400).isRedirection()).isFalse();
-        assertThat(HttpStatus.valueOf(500).isRedirection()).isFalse();
     }
 
     @Test
-    void statusIsErrorIfStatusCode4XX() {
-        assertThat(HttpStatus.valueOf(100).isError()).isFalse();
-        assertThat(HttpStatus.valueOf(200).isError()).isFalse();
-        assertThat(HttpStatus.valueOf(300).isError()).isFalse();
+    void statusIsClientErrorIfStatusCode4XX() {
+        assertThat(HttpStatus.valueOf(399).isClientError()).isFalse();
+        assertThat(HttpStatus.valueOf(400).isClientError()).isTrue();
+        assertThat(HttpStatus.valueOf(499).isClientError()).isTrue();
+        assertThat(HttpStatus.valueOf(500).isClientError()).isFalse();
+    }
+
+    @Test
+    void statusIsServerErrorIfStatusCode5XX() {
+        assertThat(HttpStatus.valueOf(499).isServerError()).isFalse();
+        assertThat(HttpStatus.valueOf(500).isServerError()).isTrue();
+        assertThat(HttpStatus.valueOf(599).isServerError()).isTrue();
+        assertThat(HttpStatus.valueOf(600).isServerError()).isFalse();
+    }
+
+    @Test
+    void statusIsErrorIfStatusCode4XXor5XX() {
+        assertThat(HttpStatus.valueOf(399).isError()).isFalse();
         assertThat(HttpStatus.valueOf(400).isError()).isTrue();
-        assertThat(HttpStatus.valueOf(499).isError()).isTrue();
-    }
-
-    @Test
-    void statusIsErrorIfStatusCode5XX() {
-        assertThat(HttpStatus.valueOf(100).isError()).isFalse();
-        assertThat(HttpStatus.valueOf(200).isError()).isFalse();
-        assertThat(HttpStatus.valueOf(300).isError()).isFalse();
         assertThat(HttpStatus.valueOf(500).isError()).isTrue();
         assertThat(HttpStatus.valueOf(599).isError()).isTrue();
+        assertThat(HttpStatus.valueOf(600).isError()).isFalse();
     }
 }
