3f7798b399f56a4d51af6f6b710055f6a7fd6541
Trustin Lee <trustin@linecorp.com>
2020-02-24 18:21:21 +0900

    Push the current context when logging requests and responses (#2528)
    Motivation:
    
    When `Logging{Client,Service}` logs requests and responses via
    `LoggingDecorators`, the context must be pushed so that
    `RequestContextExportingAppender` picks up the context and export the
    properties to the logging framework's MDC.
    
    Modifications:
    
    - Push the current context before logging anything in
      `LoggingDecorators`.
    - Updated `Logging{Client,Service}Test` to:
      - Test if the context is pushed.
      - Test without using mocks excessively.
    
    Result:
    
    - `Logging{Client,Service}` now works well with
      `RequestContextExportingAppender` or any other logging subsystem that
      relies on thread-local variables.

---
+++ core/src/test/java/com/linecorp/armeria/internal/common/logging/LoggingTestUtil.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2020 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.internal.common.logging;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.mockito.invocation.DescribedInvocation;
+import org.mockito.invocation.InvocationOnMock;
+import org.slf4j.Logger;
+
+import com.linecorp.armeria.common.RequestContext;
+import com.linecorp.armeria.common.util.Exceptions;
+
+public final class LoggingTestUtil {
+
+    public static Logger newMockLogger(RequestContext ctx, AtomicReference<Throwable> capturedCause) {
+        return mock(Logger.class, withSettings().invocationListeners(report -> {
+            final DescribedInvocation describedInvocation = report.getInvocation();
+            if (!(describedInvocation instanceof InvocationOnMock)) {
+                return;
+            }
+
+            final InvocationOnMock invocation = (InvocationOnMock) describedInvocation;
+            final Object[] arguments = invocation.getArguments();
+            if (arguments.length == 0) {
+                return;
+            }
+            if (arguments[0] == null) {
+                // Invoked at verification phase
+                return;
+            }
+
+            switch (invocation.getMethod().getName()) {
+                case "trace":
+                case "debug":
+                case "info":
+                case "warn":
+                case "error":
+                    try {
+                        assertThat((RequestContext) RequestContext.current()).isSameAs(ctx);
+                    } catch (Throwable cause) {
+                        capturedCause.set(cause);
+                    }
+            }
+        }));
+    }
+
+    public static void throwIfCaptured(AtomicReference<Throwable> capturedCause) {
+        final Throwable cause = capturedCause.get();
+        if (cause != null) {
+            capturedCause.set(null);
+            Exceptions.throwUnsafely(cause);
+        }
+    }
+
+    private LoggingTestUtil() {}
+}
