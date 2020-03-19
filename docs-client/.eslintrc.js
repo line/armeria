79013424b9452c22afb3f9941ecec07223e149a1
Trustin Lee <t@motd.kr>
2020-03-19 15:43:28 +0900

    Move build-time dependencies to `devDependencies`

--- docs-client/.eslintrc.js
+++ docs-client/.eslintrc.js
@@ -2,4 +2,5 @@ const defaultConfig = require('../settings/eslint/eslintrc');
 
 module.exports = {
   ...defaultConfig,
+  ignorePatterns: [...defaultConfig.ignorePatterns, 'webpack.config.ts'],
 };
