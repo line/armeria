5b4794e5d12a22917930848057f70acd2f38a40a
Ikhun Um <ikhun.um@linecorp.com>
2020-04-02 11:28:33 +0900

    Address comments by @minwoox / Remove a potential flaky test

--- core/src/test/java/com/linecorp/armeria/server/HttpServerKeepAliveHandlerTest.java
+++ core/src/test/java/com/linecorp/armeria/server/HttpServerKeepAliveHandlerTest.java
@@ -17,15 +17,20 @@
 package com.linecorp.armeria.server;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 
 import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import com.google.common.base.Stopwatch;
 
 import com.linecorp.armeria.client.ClientFactory;
 import com.linecorp.armeria.client.ConnectionPoolListener;
@@ -39,8 +44,8 @@ import io.netty.util.AttributeMap;
 
 class HttpServerKeepAliveHandlerTest {
 
-    private static final int serverIdleTimeout = 5000;
-    private static final int serverPingInterval = 1000;
+    private static final long serverIdleTimeout = 5000;
+    private static final long serverPingInterval = 1000;
 
     @RegisterExtension
     static ServerExtension server = new ServerExtension() {
@@ -77,7 +82,7 @@ class HttpServerKeepAliveHandlerTest {
     @ParameterizedTest
     @CsvSource({ "H1C", "H2C" })
     void closeByClientIdleTimeout(SessionProtocol protocol) throws InterruptedException {
-        final int clientIdleTimeout = 2000;
+        final long clientIdleTimeout = 2000;
         final ClientFactory factory = ClientFactory.builder()
                                                    .idleTimeoutMillis(clientIdleTimeout)
                                                    .connectionPoolListener(listener)
@@ -86,16 +91,20 @@ class HttpServerKeepAliveHandlerTest {
                                           .factory(factory)
                                           .build();
 
+        final Stopwatch stopwatch = Stopwatch.createStarted();
         client.get("/").aggregate().join();
+        assertThat(counter).hasValue(1);
 
-        // HTTP/2 PING frame sent by the server cannot prevent to close an idle connection.
-        Thread.sleep(clientIdleTimeout + 1000);
-        assertThat(counter).hasValue(0);
+        // The HTTP/2 PING frames sent by the server should not prevent to close an idle connection.
+        await().untilAtomic(counter, Matchers.is(0));
+        final long elapsed = stopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
+        assertThat(elapsed).isBetween(clientIdleTimeout, serverIdleTimeout - 1000);
     }
 
     @Test
     void http1CloseByServerIdleTimeout() throws InterruptedException {
-        final int clientIdleTimeout = 10000;
+        // longer than the idle timeout of the server.
+        final long clientIdleTimeout = 10000;
         final ClientFactory factory = ClientFactory.builder()
                                                    .idleTimeoutMillis(clientIdleTimeout)
                                                    .connectionPoolListener(listener)
@@ -104,10 +113,13 @@ class HttpServerKeepAliveHandlerTest {
                                           .factory(factory)
                                           .build();
 
+        final Stopwatch stopwatch = Stopwatch.createStarted();
         client.get("/").aggregate().join();
+        assertThat(counter).hasValue(1);
 
         // The connection should be closed by server
-        Thread.sleep(serverIdleTimeout + 1000);
-        assertThat(counter).hasValue(0);
+        await().untilAtomic(counter, Matchers.is(0));
+        final long elapsed = stopwatch.stop().elapsed(TimeUnit.MILLISECONDS);
+        assertThat(elapsed).isBetween(serverIdleTimeout, clientIdleTimeout - 1000);
     }
 }
