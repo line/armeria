461ef6138ac2545c72e31b71d1d89bcb3aa092c6
Trustin Lee <trustin@linecorp.com>
2020-05-14 10:51:53 +0900

    Update dependencies (#2707)
    - Dropwizard 2.0.5 -> 2.0.9
    - Dropwizard Metrics 4.1.5 -> 4.1.7, 3.2.5 -> 3.2.6
    - gRPC 1.28.0 -> 1.29.0
    - Jackson 2.10.3 -> 2.11.0
    - java-jwt 3.10.2 -> 3.10.3
    - Jetty 9.4.27 -> 9.4.28
    - Netty 4.1.48 -> 4.1.50
    - Prometheus simpleclient 0.8.1 -> 0.9.0
    - protobuf-jackson 1.0.0 -> 1.1.0
    - Reactor 3.3.4 -> 3.3.5
    - RxJava 3.0.1 -> 3.0.3
    - snappy-java 1.1.7 -> 1.1.7.5
    - Spring Boot 2.2.6 -> 2.2.7
    - Tomcat 9.0.33 -> 9.0.35, 8.5.53 -> 8.5.55
    - ZooKeeper 3.5.7 -> 3.5.8
    - Shaded dependencies
      - Bouncy Castle 1.64 -> 1.65
      - Caffeine 2.8.1 -> 2.8.2
      - Guava 28.2 -> 29.0
    - Example dependencies
      - Kotlin 1.3.71 -> 1.3.72
      - Kotlin Coroutines 1.3.4 -> 1.3.6
    - Build dependencies
      - AssertJ 3.15.0 -> 3.16.1
      - Checkstyle 8.31 -> 8.32
      - gax-grpc 1.55 -> 1.56
      - Gradle 6.3 -> 6.4
      - Hibernate Validator 6.1.2 -> 6.1.5
      - jOOR 0.9.12 -> 0.9.13
      - json-unit 2.16.2 -> 2.17.0
      - JUnit 5.6.1 -> 5.6.2
      - Node.js 12.16.1 -> 12.16.3

--- examples/grpc-kotlin/gen-src/main/grpc/example/armeria/grpc/kotlin/HelloServiceGrpc.java
+++ examples/grpc-kotlin/gen-src/main/grpc/example/armeria/grpc/kotlin/HelloServiceGrpc.java
@@ -18,7 +18,7 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.28.0)",
+    value = "by gRPC proto compiler (version 1.29.0)",
     comments = "Source: hello.proto")
 public final class HelloServiceGrpc {
 
