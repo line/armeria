0cf936d20cff5de77151e68b5b7dac2c599d6f69
dominhhien <dominhhienbkhn@gmail.com>
2020-05-30 22:41:01 +0700

    Add default servlet output stream

---
+++ servlet/src/main/java/com/linecorp/armeria/server/servlet/DefaultServletOutputStream.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2020 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.server.servlet;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Objects.requireNonNull;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.WriteListener;
+
+import com.linecorp.armeria.common.Cookie;
+import com.linecorp.armeria.common.HttpData;
+import com.linecorp.armeria.common.HttpHeaderNames;
+import com.linecorp.armeria.common.HttpStatus;
+
+/**
+ * Servlets output streams (wrapper classes) that control access to the flow.
+ */
+public class DefaultServletOutputStream extends ServletOutputStream {
+    @Nullable
+    private ServletHttpResponse response;
+
+    /**
+     *  Get servlet response.
+     */
+    public ServletHttpResponse getResponse() {
+        return response;
+    }
+
+    /**
+     *  Set servlet response.
+     */
+    public void setResponse(ServletHttpResponse response) {
+        requireNonNull(response, "response");
+        this.response = response;
+    }
+
+    @Override
+    public boolean isReady() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setWriteListener(WriteListener listener) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void close() throws IOException {
+        response = null;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        final byte[] bytes = new byte[1];
+        bytes[0] = (byte) b;
+        write(bytes);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        requireNonNull(b, "b");
+        checkArgument(off >= 0, "off: %s (expected: >= 0)", off);
+        checkArgument(len >= 0, "len: %s (expected: >= 0)", len);
+        response.getHeadersBuilder().status(HttpStatus.OK);
+        response.getHeadersBuilder().setObject(
+                HttpHeaderNames.SET_COOKIE,
+                response.getCookies().stream().map(x ->
+                                                           Cookie.builder(x.getName(), x.getValue())
+                                                                 .path("/")
+                                                                 .httpOnly(true)
+                                                                 .build().toSetCookieHeader()
+                ).collect(Collectors.toList()));
+        response.getResponseWriter().tryWrite(response.getHeadersBuilder().build());
+        response.getResponseWriter().tryWrite(HttpData.copyOf(Arrays.copyOfRange(b, off, len)));
+        response.getResponseWriter().close();
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        requireNonNull(b, "b");
+        write(b, 0, b.length);
+    }
+}
