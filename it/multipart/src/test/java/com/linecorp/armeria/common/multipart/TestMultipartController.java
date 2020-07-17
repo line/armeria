52f4f353586ed3ec6784b6be9d1d794954afabd1
Ikhun Um <ikhun.um@linecorp.com>
2020-07-17 19:05:25 +0900

    Checkstyle

--- it/multipart/src/test/java/com/linecorp/armeria/common/multipart/TestMultipartController.java
+++ it/multipart/src/test/java/com/linecorp/armeria/common/multipart/TestMultipartController.java
@@ -17,15 +17,12 @@
 package com.linecorp.armeria.common.multipart;
 
 import java.io.IOException;
-import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.google.common.collect.ImmutableList;
 
 @RestController
 @RequestMapping("/multipart")
