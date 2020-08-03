134612dee59f914f39d69738c0d645e9553e20d2
Ikhun Um <ikhun.um@linecorp.com>
2020-08-03 12:53:51 +0900

    Fix typo

--- grpc/src/main/java/com/linecorp/armeria/server/grpc/ArmeriaServerCall.java
+++ grpc/src/main/java/com/linecorp/armeria/server/grpc/ArmeriaServerCall.java
@@ -284,10 +284,10 @@ final class ArmeriaServerCall<I, O> extends ServerCall<I, O>
 
     @Override
     public void close(Status status, Metadata metadata) {
-        // TODO(ikhoon): `onHalfClose()` could call directly Server.close() due to a race condition of Coroutine
-        //               in gRPC-Kotline. By rescheduling the close event, we can avoid the race condition.
-        //               Remove `inHalfClose` flag once https://github.com/grpc/grpc-kotlin/issues/151
-        //               is resolved properly.
+        // TODO(ikhoon): `onHalfClose()` could call directly 'ServerCall.close()' due to a race condition of
+        //               Coroutine in gRPC-Kotline. By rescheduling the close event, we can avoid the race
+        //               condition. Remove `inHalfClose` flag once
+        //               https://github.com/grpc/grpc-kotlin/issues/151 is resolved properly.
         if (ctx.eventLoop().inEventLoop() && !inHalfClose) {
             doClose(status, metadata);
         } else {
