6fb1363df55f226e1a6084d25943c66d6fce88ed
Trustin Lee <trustin@linecorp.com>
2020-08-04 22:38:29 +0900

    Add the new slides by @matsumana (#2966)
    - Added a new article with slides: 'Spring BootユーザのためのArmeria入門'.
    - Updated the `<Article />` component to allow specifying more than one type.
      - Updated some articles to take advantage of this feature.
    - Updated the `<Article />` component to allow specifying HTML in a description.

--- site/src/components/article.tsx
+++ site/src/components/article.tsx
@@ -15,31 +15,59 @@ const { Title, Paragraph } = Typography;
 
 dayjs.extend(relativeTime);
 
+type ArticleType = 'article' | 'slides' | 'video';
+
 interface ArticleProps {
   title: string;
   url: string;
   author: string;
   date: string;
-  type?: 'document' | 'slides' | 'video';
+  type?: ArticleType | ArticleType[];
   description?: string;
 }
 
 const type2icon = {
-  document: <FileOutlined alt="[Article]" aria-label="[Article]" />,
+  article: <FileOutlined alt="[Article]" aria-label="[Article]" />,
   slides: <FundProjectionScreenOutlined alt="[Slides]" aria-label="[Slides]" />,
   video: <PlaySquareOutlined alt="[Video]" aria-label="[Video]" />,
 };
 
 const Article: React.FC<ArticleProps> = (props) => {
+  const typeIcons = [];
+  let typesLabel = '';
+  if (typeof props.type === 'string') {
+    typesLabel = capitalize(props.type);
+    typeIcons.push(type2icon[props.type]);
+  } else if (Array.isArray(props.type)) {
+    if (props.type.length === 1) {
+      typesLabel = capitalize(props.type[0]);
+    } else {
+      typesLabel = props.type
+        .map((value, index) => {
+          if (index === 0) {
+            return capitalize(value);
+          }
+          if (index !== props.type.length - 1) {
+            return `, ${value}`;
+          }
+          return ` and ${value}`;
+        })
+        .join('');
+    }
+
+    props.type.forEach((value) => typeIcons.push(type2icon[value]));
+  }
+
+  if (typesLabel === 'Article') {
+    typesLabel = '';
+  }
+
   return (
     <>
       <Title level={4} id={props.url} className={styles.title}>
         <OutboundLink href={props.url}>{props.title}</OutboundLink>{' '}
         <span className={styles.author}>
-          {(props.type || 'document') !== 'document'
-            ? `${props.type.charAt(0).toUpperCase()}${props.type.substring(1)} `
-            : null}
-          by {props.author},{' '}
+          {typesLabel} by {props.author},{' '}
           <span title={dayjs(props.date).format('YYYY-MM-DD')}>
             {dayjs(props.date).fromNow()}
           </span>
@@ -47,13 +75,22 @@ const Article: React.FC<ArticleProps> = (props) => {
       </Title>
       <Paragraph className={styles.link}>
         <OutboundLink href={props.url}>
-          {type2icon[props.type || 'document']}{' '}
-          {props.url.replace(/^[^:]+:\/\//, '')}
+          {typeIcons} {props.url.replace(/^[^:]+:\/\//, '')}
         </OutboundLink>
       </Paragraph>
-      {props.description ? <Paragraph>{props.description}</Paragraph> : null}
+      {props.description ? (
+        <Paragraph>
+          {/* eslint-disable react/no-danger */}
+          <span dangerouslySetInnerHTML={{ __html: props.description }} />
+          {/* eslint-enable react/no-danger */}
+        </Paragraph>
+      ) : null}
     </>
   );
 };
+
+function capitalize(value: string) {
+  return `${value.charAt(0).toUpperCase()}${value.substring(1)}`;
+}
 
 export default Article;
