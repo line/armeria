0470d345401eee686848bb26d4c3973ccca0d220
Ikhun Um <ikhun.um@linecorp.com>
2020-08-13 13:56:46 +0900

    Revert changed in HttpClientMaxConcurrentStreamTest

--- core/src/test/java/com/linecorp/armeria/client/HttpClientMaxConcurrentStreamTest.java
+++ core/src/test/java/com/linecorp/armeria/client/HttpClientMaxConcurrentStreamTest.java
@@ -139,7 +139,7 @@ public class HttpClientMaxConcurrentStreamTest {
         }
 
         if (clientFactory != null) {
-            clientFactory.closeAsync();
+            clientFactory.close();
         }
 
         await().until(() -> server.server().numConnections() == 0);
