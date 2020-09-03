adda9bf17e3b6c7915f155f54c31166616eae90d
minwoox <songmw725@gmail.com>
2020-09-03 14:49:47 +0900

    Log the exception while making request and response log strings. Motivation: It's not easy to recognize what is the problem if an exception is raised while making request response log strings because there's no log for the exception and of course, request and response logs are not left as well.
    Modification:
    - Log the exception that is raised while making request response log strings.
    
    Result:
    - You can now get the log of the exception which is raised while making requst and response logs.

--- core/src/main/java/com/linecorp/armeria/client/logging/AbstractLoggingClient.java
+++ core/src/main/java/com/linecorp/armeria/client/logging/AbstractLoggingClient.java
@@ -16,6 +16,7 @@
 
 package com.linecorp.armeria.client.logging;
 
+import static com.linecorp.armeria.internal.common.logging.LoggingDecorators.logWhenComplete;
 import static com.linecorp.armeria.internal.common.logging.LoggingDecorators.logRequest;
 import static com.linecorp.armeria.internal.common.logging.LoggingDecorators.logResponse;
 import static java.util.Objects.requireNonNull;
@@ -106,8 +107,7 @@ abstract class AbstractLoggingClient<I extends Request, O extends Response>
     @Override
     public final O execute(ClientRequestContext ctx, I req) throws Exception {
         if (sampler.isSampled(ctx)) {
-            ctx.log().whenRequestComplete().thenAccept(requestLogger);
-            ctx.log().whenComplete().thenAccept(responseLogger);
+            logWhenComplete(logger, ctx, requestLogger, responseLogger);
         }
         return unwrap().execute(ctx, req);
     }
