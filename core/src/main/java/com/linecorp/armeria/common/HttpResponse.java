f0aac93d55a15e5379422986abbea81a9dfac0f6
gyuil-han <cnabro91@gmail.com>
2020-09-14 20:17:12 +0900

    add redirect response in HttpResponse #914

--- core/src/main/java/com/linecorp/armeria/common/HttpResponse.java
+++ core/src/main/java/com/linecorp/armeria/common/HttpResponse.java
@@ -391,6 +391,43 @@ public interface HttpResponse extends Response, StreamMessage<HttpObject> {
     }
 
     /**
+     * Creates a new HTTP response of the temporary redirect to specific location.
+     */
+    static HttpResponse ofRedirect(String location) {
+        requireNonNull(location, "location");
+        return of(ResponseHeaders.of(RedirectType.TEMPORARY.toHttpStatus(), HttpHeaderNames.LOCATION,
+            location));
+    }
+
+    /**
+     * Creates a new HTTP response of the temporary redirect to specific location using string format.
+     */
+    static HttpResponse ofRedirect(String format, Object... args) {
+        requireNonNull(format, "format");
+        return of(ResponseHeaders.of(RedirectType.TEMPORARY.toHttpStatus(), HttpHeaderNames.LOCATION,
+            String.format(format, args)));
+    }
+
+    /**
+     * Creates a new HTTP response of the redirect to specific location.
+     */
+    static HttpResponse ofRedirect(RedirectType redirectType, String location) {
+        requireNonNull(redirectType, "redirectType");
+        requireNonNull(location, "location");
+        return of(ResponseHeaders.of(redirectType.toHttpStatus(), HttpHeaderNames.LOCATION, location));
+    }
+
+    /**
+     * Creates a new HTTP response of the redirect to specific location using string format.
+     */
+    static HttpResponse ofRedirect(RedirectType redirectType, String format, Object... args) {
+        requireNonNull(redirectType, "redirectType");
+        requireNonNull(format, "format");
+        return of(ResponseHeaders.of(redirectType.toHttpStatus(), HttpHeaderNames.LOCATION,
+            String.format(format, args)));
+    }
+
+    /**
      * Creates a new failed HTTP response.
      */
     static HttpResponse ofFailure(Throwable cause) {
