84c55ab9098c3582ade5ba21a5f922c4e9fd227f
Tuan Le <tuanleminh1235@gmail.com>
2020-09-21 00:35:11 -0400

    Add license

--- core/src/main/java/com/linecorp/armeria/client/IgnoreHostsTrustManager.java
+++ core/src/main/java/com/linecorp/armeria/client/IgnoreHostsTrustManager.java
@@ -13,6 +13,21 @@
  *  License for the specific language governing permissions and limitations
  *  under the License.
  */
+/*
+ *  Copyright (C) 2020 Square, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 
 package com.linecorp.armeria.client;
 
@@ -35,6 +50,9 @@ import javax.net.ssl.X509ExtendedTrustManager;
  * An implementation of {@link X509ExtendedTrustManager} that skips verification on whitelisted hosts.
  */
 final class IgnoreHostsTrustManager extends X509ExtendedTrustManager {
+
+    // Forked from okhttp-4.9.0
+    // https://github.com/square/okhttp/blob/1364ea44ae1f1c4b5a1cc32e4e7b51d23cb78517/okhttp-tls/src/main/kotlin/okhttp3/tls/internal/InsecureExtendedTrustManager.kt
 
     /**
      * Returns new {@link IgnoreHostsTrustManager} instance.
