83b73dbeb32f998e7e9e6b6464750374efd35e03
Ikhun Um <ikhun.um@linecorp.com>
2020-10-22 15:50:30 +0900

    Add a release note for 1.2.0
    This PR also includes a way of displaying full parameter information in a method by appending `?full`.
    ```java
    <type://Foo#bar(String, Int)?full>
    ```

--- site/src/components/api-link.tsx
+++ site/src/components/api-link.tsx
@@ -1,4 +1,4 @@
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
+import {OutboundLink} from 'gatsby-plugin-google-analytics';
 import React from 'react';
 
 import styles from './api-link.module.less';
@@ -16,6 +16,16 @@ const TypeLink: React.FC<TypeLinkProps> = (props) => {
     simpleName = simpleName.substring(lastDotIdx + 1);
   }
 
+  let showParams = false;
+  let href = props.href;
+  if (href) {
+    const optionIndex = href.lastIndexOf('?');
+    if (optionIndex > 0) {
+      showParams = href.substring(optionIndex + 1) === "full";
+      href = href.substring(0, optionIndex);
+    }
+  }
+
   let suffix = '';
   if (props.plural) {
     if (simpleName.match(/(ch|s|sh|x|z)$/)) {
@@ -29,13 +39,17 @@ const TypeLink: React.FC<TypeLinkProps> = (props) => {
   if (simpleName.indexOf('#') > 0) {
     const replaced = simpleName.replace('#', '.');
     title = replaced;
-    simpleName = replaced.replace(/ *\([^)]*\)*/, '()');
+    if (showParams) {
+      simpleName = replaced;
+    } else {
+      simpleName = replaced.replace(/ *\([^)]*\)*/, '()');
+    }
   } else {
     title = simpleName;
   }
 
-  const simpleTypeNameWithHref = props.href ? (
-    <OutboundLink href={props.href} title={title}>
+  const simpleTypeNameWithHref = href ? (
+    <OutboundLink href={href} title={title}>
       {simpleName}
     </OutboundLink>
   ) : (
