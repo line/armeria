b9fe16d7d040897899a539bf1fbb8feee53116aa
Hirotaka Kawata <kawata.hirotaka@linecorp.com>
2020-11-05 00:18:17 +0900

    Allow colon character in first segment of absolute path

--- core/src/test/java/com/linecorp/armeria/internal/common/PathAndQueryTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/common/PathAndQueryTest.java
@@ -144,14 +144,6 @@ public class PathAndQueryTest {
     }
 
     @Test
-    public void colon() {
-        assertThat(PathAndQuery.parse("/:")).isNull();
-        assertThat(PathAndQuery.parse("/:/")).isNull();
-        assertThat(PathAndQuery.parse("/a/:")).isNotNull();
-        assertThat(PathAndQuery.parse("/a/:/")).isNotNull();
-    }
-
-    @Test
     public void rawUnicode() {
         // 2- and 3-byte UTF-8
         final PathAndQuery res1 = PathAndQuery.parse("/\u00A2?\u20AC"); // ¢ and €
