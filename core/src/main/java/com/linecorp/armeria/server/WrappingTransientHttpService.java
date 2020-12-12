baff2cae0b7ad4bb33c518257eb1381a0a4b6396
gihwan.kim <gihwan.kim@linecorp.com>
2020-12-12 23:38:17 +0900

    Enable existing service to be as `TransientService`

---
+++ core/src/main/java/com/linecorp/armeria/server/WrappingTransientHttpService.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2020 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import java.util.Set;
+import java.util.function.Function;
+
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+
+/**
+ * Decorated a {@link HttpService} to be treated as {@link TransientService} without inheritance.
+ */
+public final class WrappingTransientHttpService
+        extends SimpleDecoratingHttpService implements TransientHttpService {
+
+    /**
+     * Returns a new {@link HttpService} decorator which makes as {@link TransientService}.
+     */
+    public static Function<? super HttpService, WrappingTransientHttpService> newDecorator() {
+        return delegate -> builder().build(delegate);
+    }
+
+    /**
+     * Returns a new {@link WrappingTransientHttpServiceBuilder}.
+     */
+    public static WrappingTransientHttpServiceBuilder builder() {
+        return new WrappingTransientHttpServiceBuilder();
+    }
+
+    private final Set<TransientServiceOption> transientServiceOptions;
+
+    WrappingTransientHttpService(HttpService delegate, Set<TransientServiceOption> transientServiceOptions) {
+        super(delegate);
+        this.transientServiceOptions = transientServiceOptions;
+    }
+
+    @Override
+    public HttpResponse serve(ServiceRequestContext ctx, HttpRequest req) throws Exception {
+        return unwrap().serve(ctx, req);
+    }
+
+    @Override
+    public Set<TransientServiceOption> transientServiceOptions() {
+        return transientServiceOptions;
+    }
+}
---
+++ core/src/test/java/com/linecorp/armeria/server/WrappingTransientHttpServiceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.linecorp.armeria.common.HttpResponse;
+
+class WrappingTransientHttpServiceTest {
+
+    static final HttpService FOO = (ctx, req) -> HttpResponse.of("foo");
+
+    @Test
+    void extractTransientServiceOptions() {
+        final HttpService wrapped = FOO.decorate(
+                WrappingTransientHttpService.builder()
+                                            .transientServiceOptions(TransientServiceOption.WITH_ACCESS_LOGGING)
+                                            .newDecorator());
+
+        @SuppressWarnings("rawtypes")
+        final TransientService transientService = wrapped.as(TransientService.class);
+        assertThat(transientService).isNotNull();
+
+        @SuppressWarnings("unchecked")
+        final Set<TransientServiceOption> transientServiceOptions =
+                (Set<TransientServiceOption>) transientService.transientServiceOptions();
+        assertThat(transientServiceOptions).containsExactly(TransientServiceOption.WITH_ACCESS_LOGGING);
+    }
+}
