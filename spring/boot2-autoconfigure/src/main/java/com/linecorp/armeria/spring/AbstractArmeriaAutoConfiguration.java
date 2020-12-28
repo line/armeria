ea4e7f5e5fc09806bca6355e1d36dedbd6aa3a18
Koji Lin <koji.lin@gmail.com>
2020-12-28 12:25:27 +0900

    Make Armeria server can shutdown before unmanaged tomcat in springboot. (#3226)
    Checked the document https://docs.spring.io/spring-boot/docs/2.3.1.RELEASE/reference/htmlsingle/#boot-features-graceful-shutdown, the graceful shutdown also use SmartLifeCycle, so if we make `Server` utilize it, we can start Armeria's graceful shutdown before the tomcat or at the same time(depends on the springboot's graceful setting). So there is no 503 when we have a request to tomcat during Armeria's graceful shutdown.
    
    There is one implementation detail that our phase is `Integer.MAX_VALUE`. And spring provides 2 types of web lifecycle. `WebServerStartStopLifecycle` & `WebServerGracefulShutdownLifecycle`. 
    * `WebServerStartStopLifecycle`'s phase is `Integer.MAX_VALUE-1`, so it will wait for Armria shutdown complete.
    * Unfortunately, the phase of `WebServerGracefulShutdownLifecycle` is Integer.MAX_VALUE. so it means Tomcat & Armeria will enter graceful shutdown at the same time when we enable `server.shutdown: graceful`. But luckily, the implementation of `WebServerGracefulShutdownLifecycle` is just to close the protocol/port but not setting the service in the connector as null(different with `WebServerStartStopLifecycle`). So even enter the graceful shutdown at the same time, the tomcat connector is still alive and can be used by Armeria. It may break after future implementation change of spring side.
    
    --
    
    I test the code using example/spring-boot-tomcat with `while sleep 1; do curl "http://localhost:8080"; done` and send `kill -TERM` to test, maybe good to try and let me know if I miss something.

--- spring/boot2-autoconfigure/src/main/java/com/linecorp/armeria/spring/AbstractArmeriaAutoConfiguration.java
+++ spring/boot2-autoconfigure/src/main/java/com/linecorp/armeria/spring/AbstractArmeriaAutoConfiguration.java
@@ -31,6 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.SmartLifecycle;
 import org.springframework.context.annotation.Bean;
 
 import com.google.common.base.Strings;
@@ -126,6 +127,14 @@ public abstract class AbstractArmeriaAutoConfiguration {
         }).join();
         logger.info("Armeria server started at ports: {}", server.activePorts());
         return server;
+    }
+
+    /**
+     * Wrap {@link Server} with {@link SmartLifecycle}.
+     */
+    @Bean
+    public SmartLifecycle armeriaServerGracefulShutdownLifecycle(Server server) {
+        return new ArmeriaServerGracefulShutdownLifecycle(server);
     }
 
     /**
