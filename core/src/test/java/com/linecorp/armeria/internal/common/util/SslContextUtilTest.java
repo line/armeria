44aba7ac71ca0ca91fe222752c4b286b5d8683ad
Trustin Lee <t@motd.kr>
2021-01-20 17:55:43 +0900

    Fix test failure

--- core/src/test/java/com/linecorp/armeria/internal/common/util/SslContextUtilTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/common/util/SslContextUtilTest.java
@@ -72,7 +72,7 @@ class SslContextUtilTest {
                                  .build();
             factory.closeAsync();
         }).isInstanceOf(IllegalStateException.class)
-          .hasMessageContaining("Attempting to configure a server or HTTP/2 client without");
+          .hasMessageContaining("TLS without the TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256 cipher suite");
 
         final ClientFactory factory =
                 ClientFactory.builder()
