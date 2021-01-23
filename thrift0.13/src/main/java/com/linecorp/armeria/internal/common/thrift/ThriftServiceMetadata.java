a2a169809c7136a54183ba29b9735b36e736d5fb
richieyan <richieyan@gmail.com>
2021-01-23 09:43:23 +0800

    fix: check style

--- thrift0.13/src/main/java/com/linecorp/armeria/internal/common/thrift/ThriftServiceMetadata.java
+++ thrift0.13/src/main/java/com/linecorp/armeria/internal/common/thrift/ThriftServiceMetadata.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-
 import javax.annotation.Nullable;
 
 import org.apache.thrift.AsyncProcessFunction;
