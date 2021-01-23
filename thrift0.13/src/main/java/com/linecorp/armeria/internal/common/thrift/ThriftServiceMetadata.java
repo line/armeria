66e786915c1e6dd3b8d14d46d76ace4b96eae6a8
richieyan <richieyan@gmail.com>
2021-01-23 09:26:08 +0800

    style: clean useless lock

--- thrift0.13/src/main/java/com/linecorp/armeria/internal/common/thrift/ThriftServiceMetadata.java
+++ thrift0.13/src/main/java/com/linecorp/armeria/internal/common/thrift/ThriftServiceMetadata.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.locks.ReentrantLock;
 
 import javax.annotation.Nullable;
 
