de536873c9cf7378f8aad10c58f09fab8fb00ae7
Ikhun Um <ih.pert@gmail.com>
2021-01-27 21:27:12 +0900

    Provide a way to register a custom gRPC client stub factory (#3294)
    Motivation:
    
    A factory for gRPC clients is hard-coded and a user can not plug-in a custom factory.
    He/she has to send a PR for adding a new client factory to the existing factory logic.
    See #3214 for more information.
    
    Modifications:
    
    - Add `GrpcClientStubFactory` and load it via SPI.
    - The unified gRPC client factory is split into each client provider.
    - Add `GrpcClientOptions.GRPC_CLIENT_STUB_FACTORY` for registering a `GrpcClientStubFactory` without SPI.
      - This is useful if a gRPC client factory has a runtime dependency.
        ```java
        Clients.builder("gproto+http://127.0.0.1")
               .option(GrpcClientOptions.GRPC_CLIENT_STUB_FACTORY.newValue(new GrpcClientStubFactory() {
                   @Override
                   public ServiceDescriptor findServiceDescriptor(Class<?> clientType) {
                       return HelloServiceGrpc.SERVICE;
                   }
    
                   @Override
                   public Object newClientStub(Class<?> clientType, Channel channel) {
                       // Need to specify an IO type when calling a stub factory.
                       return HelloServiceFs2Grpc.stub[IO](channel);
                   }
               }))
               .build(TestServiceStub.class);
        ```
    - Fix a bug where the gRPC client factory failed to find `ServiceDescriptor` for Reactive-gRPC through reflection.
    
    Result:
    
    - You can now add your own gRPC client factory using SPI.
    - Fix a bug where the gRPC client factory failed to find `ServiceDescriptor` for Reactive-gRPC through reflection.
    - Fixes #3214

---
+++ grpc/src/main/java/com/linecorp/armeria/internal/client/grpc/NullGrpcClientStubFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.internal.client.grpc;
+
+import javax.annotation.Nullable;
+
+import com.linecorp.armeria.client.grpc.GrpcClientStubFactory;
+
+import io.grpc.Channel;
+import io.grpc.ServiceDescriptor;
+
+public enum NullGrpcClientStubFactory implements GrpcClientStubFactory {
+
+    INSTANCE;
+
+    @Override
+    public ServiceDescriptor findServiceDescriptor(Class<?> clientType) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Nullable
+    @Override
+    public Object newClientStub(Class<?> clientType, Channel channel) {
+        throw new UnsupportedOperationException();
+    }
+}
