5c826511385619f1003df0a6a363ea5b00d6d6ee
hexoul <crosien@gmail.com>
2021-04-05 23:03:15 +0900

    Address a comment by @minwoox

--- core/src/main/java/com/linecorp/armeria/server/ServiceConfigBuilder.java
+++ core/src/main/java/com/linecorp/armeria/server/ServiceConfigBuilder.java
@@ -117,11 +117,13 @@ final class ServiceConfigBuilder implements ServiceConfigSetters {
     public ServiceConfigBuilder defaultServiceName(String defaultServiceName) {
         requireNonNull(defaultServiceName, "defaultServiceName");
         this.defaultServiceName = defaultServiceName;
-        return defaultServiceNaming(ServiceNaming.of(defaultServiceName));
+        this.defaultServiceNaming = ServiceNaming.of(defaultServiceName);
+        return this;
     }
 
     @Override
     public ServiceConfigBuilder defaultServiceNaming(ServiceNaming defaultServiceNaming) {
+        this.defaultServiceName = null;
         this.defaultServiceNaming = requireNonNull(defaultServiceNaming, "defaultServiceNaming");
         return this;
     }
