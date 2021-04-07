ff6e5079e70de49d8c6a9379eabf12158d416cf4
Ikhun Um <ih.pert@gmail.com>
2021-04-07 11:51:51 +0900

    Fix a bug where a Spring `SecurityContext` is not propagated to upstream
    Motivation:
    
    A `SecurityContext` of Spring Security is injected using Reactor's [Context](https://projectreactor.io/docs/core/release/reference/#context).
    As `ArmeriaServerHttpResponse.write(Flux<DataBuffer>)` returns a `Mono` that wraps a `CompletableFuture`,
    the injected Reactor Context was not passed to upstream.
    https://github.com/line/armeria/blob/fbd9fc72b8f5d715072d1d71d8e485f78c0fb306/spring/boot2-webflux-autoconfigure/src/main/java/com/linecorp/armeria/spring/web/reactive/ArmeriaServerHttpResponse.java#L203-L214
    
    Modifications:
    
    - Explicitly pass `ContextView` to upstream `Flux` when subscribed.
    
    Result:
    
    - `ArmeriaServerHttpResponse` of Spring integration now correctly propagates
      Reactor's [`Context`](https://projectreactor.io/docs/core/release/reference/#context).
    - Fixes #3439
    
    Co-authored-by: probe <re2rar@gmail.com>

---
+++ it/spring/webflux-security/src/test/java/com/linecorp/armeria/spring/security/SecureResourceTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License
+ */
+
+package com.linecorp.armeria.spring.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.client.cookie.CookieClient;
+import com.linecorp.armeria.client.logging.LoggingClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.MediaType;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SecureResourceTest {
+    @LocalServerPort
+    int port;
+
+    WebClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = WebClient.builder("http://127.0.0.1:" + port)
+                          .responseTimeoutMillis(0)
+                          .decorator(LoggingClient.newDecorator())
+                          .decorator(CookieClient.newDecorator())
+                          .build();
+
+        final AggregatedHttpResponse response =
+                client.prepare()
+                      .post("/login")
+                      .content(MediaType.FORM_DATA, "username=user&password=password")
+                      .execute()
+                      .aggregate().join();
+        assertThat(response.headers().cookies().stream().anyMatch(cookie -> "SESSION".equals(cookie.name())))
+                .isTrue();
+    }
+
+    @Test
+    void testMono() {
+        final AggregatedHttpResponse response = client.get("/api/mono").aggregate().join();
+        assertThat(response.contentUtf8()).contains("Username=user");
+    }
+
+    @Test
+    void testFlux() throws InterruptedException {
+        final AggregatedHttpResponse response = client.get("/api/flux").aggregate().join();
+        assertThat(response.contentUtf8()).contains("Username=user");
+    }
+}
