5a82b4d7f2e37524bf294c64de47201ec2ea715b
Ikhun Um <ih.pert@gmail.com>
2021-04-19 12:56:36 +0900

    Add an HTTP service that dumps all active threads
    Motivation:
    
    It would be useful to monitor the state of active threads using a REST API.
    This API will be reused when Armeria introduces a dashboard service #90
    
    Modifications:
    
    - Add `ThreadDumpService` that dumps all live threads with stack trace.
    
    Result:
    
    You now see the state of all live threads using `ThreadDumpSerivce`.

---
+++ core/src/main/java/com/linecorp/armeria/server/metric/ThreadDumpService.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License
+ */
+
+package com.linecorp.armeria.server.metric;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+
+import java.lang.Thread.State;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.base.Splitter;
+import com.google.common.collect.Streams;
+
+import com.linecorp.armeria.common.HttpHeaderNames;
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.common.annotation.UnstableApi;
+import com.linecorp.armeria.server.AbstractHttpService;
+import com.linecorp.armeria.server.HttpService;
+import com.linecorp.armeria.server.ServiceRequestContext;
+
+import sun.jvm.hotspot.runtime.Thread;
+
+/**
+ * An {@link HttpService} that dumps the thread info for all live threads with stack trace.
+ * If {@link MediaType#JSON} is specified in {@link HttpHeaderNames#ACCEPT_ENCODING}, the thread info will be
+ * converted as a JSON array.
+ */
+@UnstableApi
+public final class ThreadDumpService extends AbstractHttpService {
+
+    private static final ObjectMapper mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+
+    private static final ThreadDumpService INSTANCE = new ThreadDumpService();
+    private static final Splitter ACCEPT_ENCODING_SPLITTER = Splitter.on(',').trimResults();
+
+    /**
+     * Returns a singleton {@link ThreadDumpService}.
+     * @return
+     */
+    public static ThreadDumpService of() {
+        return INSTANCE;
+    }
+
+    ThreadDumpService() {}
+
+    @Override
+    protected HttpResponse doGet(ServiceRequestContext ctx, HttpRequest req) throws Exception {
+
+        boolean hasJson = false;
+        final List<String> acceptEncodings = req.headers().getAll(HttpHeaderNames.ACCEPT_ENCODING);
+        if (acceptEncodings != null) {
+            hasJson = acceptEncodings.stream().anyMatch(acceptEncoding -> {
+                return Streams.stream(ACCEPT_ENCODING_SPLITTER.split(acceptEncoding))
+                              .anyMatch(accept -> MediaType.JSON.is(MediaType.parse(accept)));
+            });
+        }
+
+        if (hasJson) {
+            final List<ThreadInfo> threadInfos = Thread.getAllStackTraces().entrySet().stream().map(entry -> {
+                final Thread thread = entry.getKey();
+                final List<String> stack = Arrays.stream(entry.getValue())
+                                                 .map(StackTraceElement::toString)
+                                                 .collect(toImmutableList());
+
+                // java.lang.management.ThreadInfo.isDaemon() and getPriority() added at Java 9
+                return new ThreadInfo(thread.getId(), thread.getName(), thread.isDaemon(),
+                                      thread.getState(), thread.getPriority(), stack);
+            }).collect(toImmutableList());
+            return HttpResponse.of(HttpStatus.OK, MediaType.JSON, mapper.writeValueAsBytes(threadInfos));
+        } else {
+            final java.lang.management.ThreadInfo[] threadInfos =
+                    ManagementFactory.getThreadMXBean().dumpAllThreads(true, true);
+            final String threadDump = Arrays.stream(threadInfos)
+                                            .map(Objects::toString)
+                                            .collect(Collectors.joining());
+            return HttpResponse.of(HttpStatus.OK, MediaType.PLAIN_TEXT, threadDump);
+        }
+    }
+
+    static final class ThreadInfo {
+        private final long id;
+        private final String name;
+        private final boolean daemon;
+        private final State state;
+        private final int priority;
+        private final List<String> stack;
+
+        ThreadInfo(long id, String name, boolean daemon, State state, int priority,
+                   List<String> stack) {
+            this.id = id;
+            this.name = name;
+            this.daemon = daemon;
+            this.state = state;
+            this.priority = priority;
+            this.stack = stack;
+        }
+
+        @JsonProperty
+        long id() {
+            return id;
+        }
+
+        @JsonProperty
+        String name() {
+            return name;
+        }
+
+        @JsonProperty
+        boolean daemon() {
+            return daemon;
+        }
+
+        @JsonProperty
+        State state() {
+            return state;
+        }
+
+        @JsonProperty
+        int priority() {
+            return priority;
+        }
+
+        @JsonProperty
+        List<String> stack() {
+            return stack;
+        }
+    }
+}
---
+++ core/src/test/java/com/linecorp/armeria/server/metric/ThreadDumpServiceTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License
+ */
+
+package com.linecorp.armeria.server.metric;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpHeaderNames;
+import com.linecorp.armeria.common.MediaType;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
+
+class ThreadDumpServiceTest {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @RegisterExtension
+    static ServerExtension server = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) {
+            sb.service("/internal/threads", ThreadDumpService.of());
+        }
+    };
+
+    @Test
+    void threadDump() throws InterruptedException {
+        final WebClient client = WebClient.of(server.httpUri());
+        final AggregatedHttpResponse response = client.get("/internal/threads").aggregate().join();
+        assertThat(response.contentType()).isEqualTo(MediaType.PLAIN_TEXT);
+        assertThat(response.contentUtf8()).contains(Thread.currentThread().getName());
+    }
+
+    @Test
+    void threadDumpWithJson() throws Exception {
+        final WebClient client = WebClient.of(server.httpUri());
+        final AggregatedHttpResponse response =
+                client.prepare()
+                      .get("/internal/threads")
+                      .header(HttpHeaderNames.ACCEPT_ENCODING, MediaType.JSON)
+                      .execute().aggregate().join();
+
+        assertThat(response.contentType()).isEqualTo(MediaType.JSON);
+        final String content = response.contentUtf8();
+        assertThat(content).contains(Thread.currentThread().getName());
+        assertThat(mapper.readTree(content).isArray()).isTrue();
+    }
+}
