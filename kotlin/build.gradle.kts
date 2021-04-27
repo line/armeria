f749fa42d8deac21a5161a7659af6b2e6d72deca
Ikhun Um <ih.pert@gmail.com>
2021-04-27 15:45:13 +0900

    Support Kotlin's data class and Scala's case class in the default Jackson converter of annotated service (#3458)
    Motivation:
    
    Kotlin and Scala users should have to use a custom Jackson `Module` for `Jackson{Request,Response}ConverterFunction` such as:
    - https://github.com/FasterXML/jackson-module-kotlin
    - https://github.com/FasterXML/jackson-module-scala
    
    The custom `Jackson{Request,Response}ConverterFunction`s should be declared to a service builder every time. For example:
    ```scala
    val scalaObjectMapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
    Server
      .builder()
      .annotatedService()
      .requestConverters(new JacksonRequestConverterFunction(scalaObjectMapper))
      .responseConverters(new JacksonResponseConverterFunction(scalaObjectMapper))
      .build(myScalaService)
      ...
    ```
    It would be boilerplate code if users just want to use the custom modules with default configurations.
    
    Modifications:
    
    - Add `com.fasterxml.jackson.module:jackson-module-kotlin` dependency to `armeria-kotlin` module
    - Add `com.fasterxml.jackson.module:jackson-module-scala` dependency to `armeria-scala` module
    - Register `DefaultScalaModule` or `KotlinModule` to the default `ObjectMapper` if they are in the classpath.
    
    Result:
    
    - You no longer have to specify a custom `Jackson{Request,Response}ConverterFunction` for
      Kotlin data class and Scala case class if you add `com.fasterxml.jackson.module:jackson-module-kotlin` or
      `com.fasterxml.jackson.module:jackson-module-scala` as a dependency.

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -28,6 +28,8 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
 
 dependencies {
     implementation(project(":core"))
+    // Added for supporting Kotlin types in Jackson{Request,Response}ConverterFunction
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
