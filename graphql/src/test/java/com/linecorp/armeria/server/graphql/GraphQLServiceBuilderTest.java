2ee77d3b29829b97d220c83d4861e3ab2713b52b
heowc <heowc1992@gmail.com>
2021-05-09 14:01:11 +0900

    Remove unused import

--- graphql/src/test/java/com/linecorp/armeria/server/graphql/GraphQLServiceBuilderTest.java
+++ graphql/src/test/java/com/linecorp/armeria/server/graphql/GraphQLServiceBuilderTest.java
@@ -18,7 +18,6 @@ package com.linecorp.armeria.server.graphql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
 
 import java.io.File;
 
