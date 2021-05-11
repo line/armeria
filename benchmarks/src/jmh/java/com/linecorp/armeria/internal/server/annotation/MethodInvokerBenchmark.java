471839cb0d86cac3c48e52659a3f7e586710f9f6
max904 <max904@yandex.ru>
2021-05-11 15:09:59 +0200

    - fixed checkstyle issues

--- benchmarks/src/jmh/java/com/linecorp/armeria/internal/server/annotation/MethodInvokerBenchmark.java
+++ benchmarks/src/jmh/java/com/linecorp/armeria/internal/server/annotation/MethodInvokerBenchmark.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.linecorp.armeria.internal.server.annotation;
 
 import static java.util.Objects.requireNonNull;
@@ -12,7 +27,6 @@ import javax.annotation.Nullable;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -26,8 +40,6 @@ import org.openjdk.jmh.infra.Blackhole;
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
 @Warmup(iterations = 1)
-//@Measurement(iterations = 3)
-@Fork(1)
 @SuppressWarnings("NotNullFieldNotInitialized")
 public class MethodInvokerBenchmark {
 
