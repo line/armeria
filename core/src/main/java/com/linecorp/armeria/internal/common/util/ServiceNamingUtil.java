207c5e038f59802dca769936a50e219a5fe308ea
Ikhun Um <ih.pert@gmail.com>
2021-05-12 18:40:21 +0900

    Use a precomputed serviceName and method name instead of splitting a full method name on demand (#3537)
    Motivation:
    
    A service name and method name are extracted from a full method name for every request.
    It consumes a few CPU cycles. 😆
    ![image](https://user-images.githubusercontent.com/1866157/117612495-42e32c00-b1a0-11eb-8275-5b5f3b90083e.png)
    
    Actually, the service names and their method names are good to cache because they are not changed at all.
    
    Modifications:
    
    - Add `serviceName()` to `RpcRequest` in addition to `serviceType()`
    - Add some fatory methods taking a service name to `RpcRequest`.
    - Early build simple method names and reuse them for better performance
    - Remove `ServiceNamingUtil.fullTypeRpcServiceName()` in favor of `RpcRequest.serviceName()`
    
    Result:
    
    Optimized code and slightly better performance

--- core/src/main/java/com/linecorp/armeria/internal/common/util/ServiceNamingUtil.java
+++ core/src/main/java/com/linecorp/armeria/internal/common/util/ServiceNamingUtil.java
@@ -15,7 +15,6 @@
  */
 package com.linecorp.armeria.internal.common.util;
 
-import com.linecorp.armeria.common.RpcRequest;
 import com.linecorp.armeria.common.util.Unwrappable;
 import com.linecorp.armeria.internal.server.annotation.AnnotatedService;
 import com.linecorp.armeria.server.HttpService;
@@ -23,17 +22,6 @@ import com.linecorp.armeria.server.HttpService;
 public final class ServiceNamingUtil {
 
     public static final String GRPC_SERVICE_NAME = "com.linecorp.armeria.internal.common.grpc.GrpcLogUtil";
-
-    public static String fullTypeRpcServiceName(RpcRequest rpcReq) {
-        final String serviceType = rpcReq.serviceType().getName();
-        if (GRPC_SERVICE_NAME.equals(serviceType)) {
-            // Parse gRPC serviceName and methodName
-            final String fullMethodName = rpcReq.method();
-            return fullMethodName.substring(0, fullMethodName.lastIndexOf('/'));
-        } else {
-            return serviceType;
-        }
-    }
 
     public static String fullTypeHttpServiceName(HttpService service) {
         Unwrappable unwrappable = service;
