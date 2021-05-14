0277a5fe8bb9ce6f33dddb1c0c31f900ffe7baa0
Mickey Donaghy <michael.donaghy@linecorp.com>
2021-05-14 14:39:14 +0900

    Remove no-longer-used import

--- core/src/main/java/com/linecorp/armeria/common/AbstractContextAwareExecutorService.java
+++ core/src/main/java/com/linecorp/armeria/common/AbstractContextAwareExecutorService.java
@@ -27,7 +27,6 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
 
