3da58c2a36607caba16e43eb8fe4e22713ebb0ef
sooj-hwang <sooj.hwang@linecorp.com>
2021-06-03 21:55:12 +0900

    Remove commented block out of step structure

--- site/src/components/steps.tsx
+++ site/src/components/steps.tsx
@@ -58,29 +58,4 @@ const TutorialSteps:React.FC<TutorialStepProps> = (props) => {
     );
 };
 
-/*
-const TutorialSteps:React.FC<TutorialStepProps> = (props) => {
-    return (
-        <AntdSteps
-            {...props}
-            type='default'
-            direction='horizontal'
-            responsive='true'
-            size='small'
-            progressDot
-            initial={1}
-            className={styles.tutorialTheme}
-        >
-            <Step title="Step 1" description="Create server"/>
-            <Step title="Step 2" description="Prepare files"/>
-            <Step title="Step 3" description="Implement CREATE"/>
-            <Step title="Step 4" description="Implement READ"/>
-            <Step title="Step 5" description="Implement PUT"/>
-            <Step title="Step 6" description="Implement DELETE"/>
-            <Step title="Step 7" description="Add service to server"/>
-        </AntdSteps>
-    );
-};
- */
-
 export default TutorialSteps;
