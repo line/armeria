df2362390f47ca604eb00acef3ccdc22611b9dc2
Ikhun Um <ih.pert@gmail.com>
2021-06-08 21:04:20 +0900

    Use SPI for loading the Jackson modules automatically
    Motivation:
    
    If users want to register a new Jackson Module with annotated services,
    they have to create a custom `Jackson{Request,Response}ConverterFunction` or
    `ServerBuilder.annotatedServiceExtensions()`. It would be invenient.
    Most of the users want to register a module automatically.
    
    For automation, we can use a Jackson's API that the loads all available modules via SPI.
    https://fasterxml.github.io/jackson-databind/javadoc/2.11/com/fasterxml/jackson/databind/cfg/MapperBuilder.html#findAndAddModules--
    However, the API has a downside. It can not exclude specifc modules.
    The problem could be solved providing a custom `JacksonModuleProvider` that can explicitly
    specify Jackson's modules.
    
    Motifications:
    
    - Modify `JacksonUtil` to find all available modules with `JsonMapper.Builder.findAndAddModules()`
    - Add `JacksonModuleProvider` that can override the default Jackson
      modules via SPI.
    
    Result:
    
    - You can now specify a default Jackson modules with
      `JacksonModuleProvider`.
    - All available Jackson moudules are automatically registered to the default
      Jackson modules for annotated services via SPI.
    - Fixes #3594

---
+++ it/jackson-provider/src/test/java/com/linecorp/armeria/server/CustomJacksonModuleProvider.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
+import com.google.common.collect.ImmutableList;
+
+public final class CustomJacksonModuleProvider implements JacksonModuleProvider {
+    @Override
+    public List<Module> modules() {
+        return ImmutableList.of(new KotlinModule());
+    }
+}
---
+++ it/jackson-provider/src/test/java/com/linecorp/armeria/server/JacksonModuleProviderTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.linecorp.armeria.internal.server.JacksonUtil;
+
+class JacksonModuleProviderTest {
+
+    @Test
+    void overrideDefaultJacksonModules() {
+        final ObjectMapper objectMapper = JacksonUtil.newDefaultObjectMapper();
+        final Set<Object> ids = objectMapper.getRegisteredModuleIds();
+        assertThat(ids).hasSize(1);
+        assertThat(ids).first().isEqualTo("com.fasterxml.jackson.module.kotlin.KotlinModule");
+    }
+}
