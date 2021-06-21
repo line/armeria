7110bdf05a366cb0a0e35a9fc052f651b1d18e87
kezhenxu94 <kezhenxu94@apache.org>
2021-06-21 21:52:33 +0800

    Add captor and enhance extension to record `ServiceRequestContext`s easily
    Motivation:
    
    Provide a way to record `ServiceRequestContext`s easily.
    
    Modifications:
    
    - Provide a decorator implementation that helps capturing `ServiceRequestContext`s.
    - Enhance `ServerExtension` so they are capable of collecting `ServiceRequestContext`s.
    
    Result:
    
    - Closes #3644.
    - Users can capture `ServiceRequestContext`s in tests more easily.

---
+++ junit5/src/test/java/com/linecorp/armeria/testing/junit5/server/ServerExtensionTest.java
@@ -0,0 +1,36 @@
+package com.linecorp.armeria.testing.junit5.server;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.ServerBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.fail;
+
+class ServerExtensionTest {
+    @RegisterExtension
+    static ServerExtension server = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) {
+            sb.service("/hello", (ctx, req) -> HttpResponse.of(200));
+        }
+    };
+
+    @Test
+    void requestContextCaptor() {
+        WebClient client = WebClient.of(server.httpUri());
+        client.get("/hello").aggregate().join();
+
+        ServiceRequestContextCaptor captor = server.requestContextCaptor();
+        assertThat(captor.size()).isEqualTo(1);
+
+        try {
+            assertThat(captor.take().request().uri().getPath()).isEqualTo("/hello");
+        } catch (InterruptedException e) {
+            fail("Should not fail");
+        }
+    }
+}
