dbee3f092db3608a8c25a25efe805b1d4b5ef4ca
Trustin Lee <t@motd.kr>
2021-06-23 19:01:09 +0900

    Checkstyle

--- core/src/main/java/com/linecorp/armeria/client/Http1ResponseDecoder.java
+++ core/src/main/java/com/linecorp/armeria/client/Http1ResponseDecoder.java
@@ -27,7 +27,6 @@ import com.linecorp.armeria.common.ClosedSessionException;
 import com.linecorp.armeria.common.ContentTooLargeException;
 import com.linecorp.armeria.common.HttpData;
 import com.linecorp.armeria.common.ProtocolViolationException;
-import com.linecorp.armeria.common.ResponseHeaders;
 import com.linecorp.armeria.common.stream.ClosedStreamException;
 import com.linecorp.armeria.internal.common.ArmeriaHttpUtil;
 import com.linecorp.armeria.internal.common.InboundTrafficController;
