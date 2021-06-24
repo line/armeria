2dcae398a87337efe0281bb0bf68d4c2e0df6ac1
Trustin Lee <t@motd.kr>
2021-06-24 19:59:14 +0900

    Checkstyle

--- testing-internal/src/main/java/com/linecorp/armeria/internal/testing/webapp/WebAppContainerTest.java
+++ testing-internal/src/main/java/com/linecorp/armeria/internal/testing/webapp/WebAppContainerTest.java
@@ -40,7 +40,6 @@ import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import com.linecorp.armeria.client.ClientFactory;
 import com.linecorp.armeria.client.ClientRequestContextCaptor;
