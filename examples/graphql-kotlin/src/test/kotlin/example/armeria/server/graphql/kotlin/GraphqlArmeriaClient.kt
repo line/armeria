2f84e2f82aead0ddf41203030dfd754180dca1c6
heowc <heowc1992@gmail.com>
2021-07-13 20:27:38 +0900

    Add example for `graphql-kotlin`

---
+++ examples/graphql-kotlin/src/test/kotlin/example/armeria/server/graphql/kotlin/GraphqlArmeriaClient.kt
@@ -0,0 +1,51 @@
+package example.armeria.server.graphql.kotlin
+
+import com.expediagroup.graphql.client.GraphQLClient
+import com.expediagroup.graphql.client.serializer.GraphQLClientSerializer
+import com.expediagroup.graphql.client.serializer.defaultGraphQLSerializer
+import com.expediagroup.graphql.client.types.GraphQLClientRequest
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
+import com.linecorp.armeria.client.WebClient
+import com.linecorp.armeria.common.HttpMethod
+import com.linecorp.armeria.common.HttpRequest
+import com.linecorp.armeria.common.HttpRequestBuilder
+import com.linecorp.armeria.common.MediaType
+import java.io.Closeable
+import java.net.URL
+
+class GraphqlArmeriaClient(
+    private val url: URL,
+    private val client: WebClient = WebClient.of(),
+    private val serializer: GraphQLClientSerializer = defaultGraphQLSerializer()
+) : GraphQLClient<HttpRequestBuilder>, Closeable {
+
+    override suspend fun <T : Any> execute(
+        request: GraphQLClientRequest<T>,
+        requestCustomizer: HttpRequestBuilder.() -> Unit
+    ): GraphQLClientResponse<T> {
+        val response = client.execute(HttpRequest.builder()
+                                                 .apply(requestCustomizer)
+                                                 .path(url.toString())
+                                                 .method(HttpMethod.POST)
+                                                 .content(MediaType.JSON_UTF_8, serializer.serialize(request))
+                                                 .build())
+                             .aggregate().join()
+        return serializer.deserialize(response.contentUtf8(), request.responseType());
+    }
+
+    override suspend fun execute(
+        requests: List<GraphQLClientRequest<*>>,
+        requestCustomizer: HttpRequestBuilder.() -> Unit
+    ): List<GraphQLClientResponse<*>> {
+        val response = client.execute(HttpRequest.builder()
+                                                 .apply(requestCustomizer)
+                                                 .path(url.toString())
+                                                 .method(HttpMethod.POST)
+                                                 .content(MediaType.JSON_UTF_8, serializer.serialize(requests))
+                                                 .build())
+                             .aggregate().join()
+        return serializer.deserialize(response.contentUtf8(), requests.map { it.responseType() })
+    }
+
+    override fun close() {}
+}
---
+++ examples/graphql-kotlin/src/test/kotlin/example/armeria/server/graphql/kotlin/GraphqlServiceTest.kt
@@ -0,0 +1,55 @@
+package example.armeria.server.graphql.kotlin
+
+import com.expediagroup.graphql.client.jackson.GraphQLClientJacksonSerializer
+import com.expediagroup.graphql.client.types.GraphQLClientRequest
+import com.linecorp.armeria.server.ServerBuilder
+import com.linecorp.armeria.testing.junit5.server.ServerExtension
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.net.URL
+import kotlin.reflect.KClass
+
+class GraphqlServiceTest {
+
+    companion object {
+
+        @JvmField
+        @RegisterExtension
+        val server: ServerExtension = object : ServerExtension() {
+            @Throws(Exception::class)
+            override fun configure(sb: ServerBuilder) {
+                configureService(sb)
+            }
+        }
+
+        private fun client(): GraphqlArmeriaClient {
+            return GraphqlArmeriaClient(
+                    url = URL("http://localhost:${server.server().activeLocalPort()}/graphql"),
+                    serializer = GraphQLClientJacksonSerializer())
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+            "1,hero",
+            "2,human",
+            "3,droid"
+    )
+    fun testUserDataFetch(id: String, expected: String) {
+        runBlocking {
+            val response = client().execute(UserNameQuery(id))
+            assertThat(response.data?.userById?.name).isEqualTo(expected)
+        }
+    }
+
+    data class UserNameResult(val userById: User)
+
+    class UserNameQuery(id: String) : GraphQLClientRequest<UserNameResult> {
+        override val query: String = "query {userById(id: $id) {name}}"
+        override val operationName: String? = null
+        override fun responseType(): KClass<UserNameResult> = UserNameResult::class
+    }
+}
