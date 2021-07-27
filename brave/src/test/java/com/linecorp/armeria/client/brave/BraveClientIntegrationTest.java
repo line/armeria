4c43e68064a160219be67021e53e926a7acac708
Ikhun Um <ih.pert@gmail.com>
2021-07-27 16:50:38 +0900

    Fix indent

--- brave/src/test/java/com/linecorp/armeria/client/brave/BraveClientIntegrationTest.java
+++ brave/src/test/java/com/linecorp/armeria/client/brave/BraveClientIntegrationTest.java
@@ -159,11 +159,11 @@ public class BraveClientIntegrationTest extends ITHttpAsyncClient<WebClient> {
             final ClientRequestContext ctx = ctxCaptor.get();
             res.aggregate().handle((response, cause) -> {
                 try (SafeCloseable ignored = ctx.push()) {
-            if (cause == null) {
-                callback.accept(response.status().code(), null);
-            } else {
-                callback.accept(null, cause);
-            }
+                    if (cause == null) {
+                        callback.accept(response.status().code(), null);
+                    } else {
+                        callback.accept(null, cause);
+                    }
                 }
             return null;
         });
