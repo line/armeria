0484d43d1d8e1b628c987d124c3b4cfc8c8bc6e4
Ikhun Um <ih.pert@gmail.com>
2021-08-04 23:39:42 +0900

    Ignore `CancelledSubscriptionException` when WebFlux's WebClient cancels the upstream
    Motivation:
    
    When 1) an error is raised from WebFlux's `WebClient`, the `WebClient`
    immediately cancels the upstream. If `whenComplete()` completes with an
    `CancelledSubscriptionException`, the actual error raised in 1) will be
    ignored and users can't see the actual error.
    See: https://github.com/line/armeria/issues/3730
    
    Modifications:
    
    - Silently ignore `CancelledSubscriptionException` and let WebFlux's
      `WebClient` propagate the original error.
    - Add `:it:spring:boot2-kotlin` for testing compatibility between
      Spring WebFlux integration and Kotlin
    
    Result:
    
    - You no longer see `CancelledSubscriptionException` when WebFlux's
      `WebClient` receives an unexpected `Content-Type` header.
    - Closes #3730

---
+++ it/spring/boot2-kotlin/src/test/kotlin/com/linecorp/armeria/spring/kotlin/AbnormalController.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.spring.kotlin
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+class AbnormalController(private val objectMapper: ObjectMapper) {
+
+    @GetMapping(value = ["/abnormal"], produces = ["text/plain;charset=utf-8"])
+    @ResponseBody
+    fun abnormal(): ResponseEntity<String> {
+        return ResponseEntity.ok()
+            .body(objectMapper.writeValueAsString(Abnormal(abnormal = true, dummyText = "output")))
+    }
+
+}
+
+data class Abnormal(val abnormal: Boolean, val dummyText: String)
---
+++ it/spring/boot2-kotlin/src/test/kotlin/com/linecorp/armeria/spring/kotlin/ClientCoroutineTest.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.spring.kotlin
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.http.client.reactive.ClientHttpConnector
+import org.springframework.web.reactive.function.UnsupportedMediaTypeException
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.awaitBody
+import javax.annotation.PostConstruct
+import javax.inject.Inject
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ClientCoroutineTest {
+
+    @LocalServerPort
+    private var port = 0
+
+    @Inject
+    private lateinit var connector: ClientHttpConnector
+
+    private lateinit var client: WebClient
+
+    @PostConstruct
+    fun setUp() {
+        client = WebClient.builder()
+            .baseUrl("http://127.0.0.1:$port")
+            .clientConnector(connector)
+            .build()
+    }
+
+    @Test
+    fun shouldRaiseWebClientResponseException() {
+        runBlocking {
+            try {
+                client.get()
+                    .uri("/abnormal")
+                    .retrieve()
+                    .awaitBody<Abnormal>()
+            } catch (ex: WebClientResponseException) {
+                assertThat(ex.rawStatusCode).isEqualTo(200)
+                assertThat(ex.cause).isInstanceOf(UnsupportedMediaTypeException::class.java)
+                    .hasMessageContaining("Content type 'text/plain;charset=utf-8' not supported for " +
+                                          "bodyType=com.linecorp.armeria.spring.kotlin.Abnormal")
+            }
+        }
+    }
+}
