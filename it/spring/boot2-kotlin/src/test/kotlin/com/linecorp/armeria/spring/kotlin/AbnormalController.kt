0eb5f1da36bfa1a3600dd35229d5242ab3e3c510
Ikhun Um <ih.pert@gmail.com>
2021-08-09 17:04:58 +0900

    Address comments by @trustin

--- it/spring/boot2-kotlin/src/test/kotlin/com/linecorp/armeria/spring/kotlin/AbnormalController.kt
+++ it/spring/boot2-kotlin/src/test/kotlin/com/linecorp/armeria/spring/kotlin/AbnormalController.kt
@@ -21,6 +21,9 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
+/**
+ * This controller returns a JSON with an incorrect content type header.
+ */
 @Controller
 class AbnormalController(private val objectMapper: ObjectMapper) {
 
