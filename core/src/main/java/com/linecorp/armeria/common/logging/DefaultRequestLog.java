e08b6eabcf9f8ddf231b8744026dd5dbd6342271
Trustin Lee <t@motd.kr>
2021-08-18 23:14:38 +0900

    Invalidate `RequestLog.toString*()` cache when sanitizer functions are different (#3758)
    Motivation:
    
    `RequestLog.toStringRequestOnly()` and `toStringResponseOnly()`
    currently doesn't invalidate its cached string representation when a
    user specified a different sanitizer function. This can lead to an
    unintended leak of sensitive information when the string is cached with
    a wrong sanitizer function. For example:
    
        RequestLog log = ...;
        String a = log.toStringRequestOnly();
        String b = log.toStringRequestOnly(headersSanitizer, contentSanitizer);
        assert !a.equals(b); // This fails currently.
    
    Modifications:
    
    - Kept the sanitizer functions used for generating the cached string.
    - Invalidated the cache when the cached string was generated with
      different sanitizer functions.
    - Miscellaneous:
      - `RequestLog.toString()` produces a tad bit prettier string when it has
        children.
    
    Result:
    
    - No more unintended leakage of sensitive information when logging a
      `RequestLog`.

--- core/src/main/java/com/linecorp/armeria/common/logging/DefaultRequestLog.java
+++ core/src/main/java/com/linecorp/armeria/common/logging/DefaultRequestLog.java
@@ -158,8 +158,21 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
     @Nullable
     private Object rawResponseContent;
 
+    // Fields for caching the string representation.
     private volatile int requestStrFlags = -1;
+    @Nullable
+    private Object requestStrHeadersSanitizer;
+    @Nullable
+    private Object requestStrContentSanitizer;
+    @Nullable
+    private Object requestStrTrailersSanitizer;
     private volatile int responseStrFlags = -1;
+    @Nullable
+    private Object responseStrHeadersSanitizer;
+    @Nullable
+    private Object responseStrContentSanitizer;
+    @Nullable
+    private Object responseStrTrailersSanitizer;
     @Nullable
     private String requestStr;
     @Nullable
@@ -1363,29 +1376,38 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
     public String toString() {
         final String req = toStringRequestOnly();
         final String res = toStringResponseOnly();
-
-        // Create a StringBuilder with the initial capacity not considering the children's log length.
-        // The children will be empty in most of the cases, so it is OK.
-        final StringBuilder buf = new StringBuilder(5 + req.length() + 6 + res.length() + 1);
-        buf.append("{req=")  // 5 chars
-           .append(req)
-           .append(", res=") // 6 chars
-           .append(res)
-           .append('}');     // 1 char
         final int numChildren = children != null ? children.size() : 0;
-        if (numChildren > 0) {
-            buf.append(", {");
-            for (int i = 0; i < numChildren; i++) {
-                buf.append('[');
-                buf.append(children.get(i));
-                buf.append(']');
-                if (i != numChildren - 1) {
-                    buf.append(", ");
-                }
+
+        if (numChildren == 0) {
+            try (TemporaryThreadLocals ttl = TemporaryThreadLocals.acquire()) {
+                return toStringWithoutChildren(ttl.stringBuilder(), req, res).toString();
             }
-            buf.append('}');
+        } else {
+            return toStringWithChildren(req, res, numChildren);
+        }
+    }
+
+    private String toStringWithChildren(String req, String res, int numChildren) {
+        assert children != null;
+
+        final StringBuilder buf = toStringWithoutChildren(new StringBuilder(1024), req, res);
+        buf.append(System.lineSeparator())
+           .append("Children:");
+
+        for (int i = 0; i < numChildren; i++) {
+            buf.append(System.lineSeparator());
+            buf.append('\t');
+            buf.append(children.get(i));
         }
         return buf.toString();
+    }
+
+    private static StringBuilder toStringWithoutChildren(StringBuilder buf, String req, String res) {
+        return buf.append("{req=")
+                  .append(req)
+                  .append(", res=")
+                  .append(res)
+                  .append('}');
     }
 
     @Override
@@ -1403,7 +1425,10 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
 
         // Only interested in the bits related with request.
         final int flags = this.flags & RequestLogProperty.FLAGS_REQUEST_COMPLETE;
-        if (requestStrFlags == flags) {
+        if (requestStrFlags == flags &&
+            requestStrHeadersSanitizer == headersSanitizer &&
+            requestStrContentSanitizer == contentSanitizer &&
+            requestStrTrailersSanitizer == trailersSanitizer) {
             assert requestStr != null;
             return requestStr;
         }
@@ -1492,6 +1517,10 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
 
             requestStr = buf.toString();
         }
+
+        requestStrHeadersSanitizer = headersSanitizer;
+        requestStrContentSanitizer = contentSanitizer;
+        requestStrTrailersSanitizer = trailersSanitizer;
         requestStrFlags = flags;
 
         return requestStr;
@@ -1512,7 +1541,10 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
 
         // Only interested in the bits related with response.
         final int flags = this.flags & RequestLogProperty.FLAGS_RESPONSE_COMPLETE;
-        if (responseStrFlags == flags) {
+        if (responseStrFlags == flags &&
+            responseStrHeadersSanitizer == headersSanitizer &&
+            responseStrContentSanitizer == contentSanitizer &&
+            responseStrTrailersSanitizer == trailersSanitizer) {
             assert responseStr != null;
             return responseStr;
         }
@@ -1597,6 +1629,10 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
 
             responseStr = buf.toString();
         }
+
+        responseStrHeadersSanitizer = headersSanitizer;
+        responseStrContentSanitizer = contentSanitizer;
+        responseStrTrailersSanitizer = trailersSanitizer;
         responseStrFlags = flags;
 
         return responseStr;
