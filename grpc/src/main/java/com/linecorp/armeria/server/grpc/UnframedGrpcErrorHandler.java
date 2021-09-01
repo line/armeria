59a49da5686a9a6e985e47355d1b694ac074ace8
Vivek Kothari <vivekkothari2304@gmail.com>
2021-09-01 15:05:50 +0530

    Add comments and integration test

--- grpc/src/main/java/com/linecorp/armeria/server/grpc/UnframedGrpcErrorHandler.java
+++ grpc/src/main/java/com/linecorp/armeria/server/grpc/UnframedGrpcErrorHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.linecorp.armeria.server.grpc;
 
 import com.google.common.collect.ImmutableMap;
@@ -9,26 +24,37 @@ import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.common.ResponseHeaders;
+import com.linecorp.armeria.common.annotation.UnstableApi;
 import com.linecorp.armeria.common.grpc.protocol.GrpcHeaderNames;
 import com.linecorp.armeria.internal.common.grpc.GrpcStatus;
 import com.linecorp.armeria.server.ServiceRequestContext;
 
 import io.grpc.Status;
 
+/**
+ * Error handler which maps grpc response to {@link HttpStatus}.
+ */
 @FunctionalInterface
+@UnstableApi
 public interface UnframedGrpcErrorHandler {
 
+    /**
+     * Returns a plain text or json response based on the content type.
+     */
     static UnframedGrpcErrorHandler of() {
         return (ctx, status, response) -> {
             final MediaType grpcMediaType = response.contentType();
             if (grpcMediaType != null && grpcMediaType.isJson()) {
-                return ofJson().handler(ctx, status, response);
+                return ofJson().handle(ctx, status, response);
             } else {
-                return ofPlain().handler(ctx, status, response);
+                return ofPlainText().handle(ctx, status, response);
             }
         };
     }
 
+    /**
+     * Returns a json response.
+     */
     static UnframedGrpcErrorHandler ofJson() {
         return (ctx, status, response) -> {
             final HttpHeaders headers = !response.trailers().isEmpty() ?
@@ -44,13 +70,17 @@ public interface UnframedGrpcErrorHandler {
             }
             final ResponseHeaders responseHeaders = ResponseHeaders.builder(httpStatus)
                                                                    .contentType(MediaType.JSON_UTF_8)
-                                                                   .add(GrpcHeaderNames.GRPC_STATUS, grpcStatusCode)
+                                                                   .add(GrpcHeaderNames.GRPC_STATUS,
+                                                                        grpcStatusCode)
                                                                    .build();
             return HttpResponse.ofJson(responseHeaders, builder.build());
         };
     }
 
-    static UnframedGrpcErrorHandler ofPlain() {
+    /**
+     * Returns a plain text response.
+     */
+    static UnframedGrpcErrorHandler ofPlainText() {
         return (ctx, status, response) -> {
             final HttpHeaders headers = !response.trailers().isEmpty() ?
                                         response.trailers() : response.headers();
@@ -77,5 +107,14 @@ public interface UnframedGrpcErrorHandler {
         };
     }
 
-    HttpResponse handler(ServiceRequestContext ctx, Status status, AggregatedHttpResponse response);
+    /**
+     * Maps the gRPC error response to the http response.
+     *
+     * @param ctx the service context.
+     * @param status the gRPC {@link Status} code.
+     * @param response the gRPC response.
+     *
+     * @return the http response.
+     */
+    HttpResponse handle(ServiceRequestContext ctx, Status status, AggregatedHttpResponse response);
 }
