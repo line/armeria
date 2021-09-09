d409851635685387cf1df08c30c8bfb73144b2ef
K.S. Yim <point1304@gmail.com>
2021-09-09 22:41:20 +0900

    Add reactive TCK tests for `FlowCollectingPublisher`

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -6,4 +6,15 @@ dependencies {
     // kotlin
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    testImplementation("org.reactivestreams:reactive-streams-tck")
 }
+
+val testNg by tasks.registering(Test::class) {
+    group = "Verification"
+    description = "Runs the TestNG unit tests"
+    useTestNG()
+}
+
+tasks.shadedTest { finalizedBy(testNg) }
+tasks.check { dependsOn(testNg) }
--- kotlin/src/test/kotlin/com/linecorp/armeria/server/kotlin/FlowCollectingPublisherTest.kt
+++ kotlin/src/test/kotlin/com/linecorp/armeria/server/kotlin/FlowCollectingPublisherTest.kt
@@ -20,170 +20,32 @@ import com.linecorp.armeria.common.HttpMethod
 import com.linecorp.armeria.common.HttpRequest
 import com.linecorp.armeria.internal.common.kotlin.FlowCollectingPublisher
 import com.linecorp.armeria.server.ServiceRequestContext
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.flow.buffer
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
-import org.assertj.core.api.Assertions.assertThat
-import org.awaitility.Awaitility.await
-import org.junit.jupiter.api.Test
-import org.reactivestreams.Subscriber
-import org.reactivestreams.Subscription
-import reactor.test.StepVerifier
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedTransferQueue
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicReference
+import org.reactivestreams.tck.PublisherVerification
+import org.reactivestreams.tck.TestEnvironment
+import org.testng.annotations.Test
 
 private val ctx = ServiceRequestContext.of(HttpRequest.of(HttpMethod.GET, "/"))
 
-class FlowCollectingPublisherTest {
-    @Test
-    fun test_shouldCompleteAfterConsumingAllElements() {
-        val pub = FlowCollectingPublisher(flowOf(1, 2), ctx)
-        StepVerifier.create(pub)
-            .expectNext(1, 2)
-            .verifyComplete()
-    }
-
-    @Test
-    fun test_backPressureOnFlow() {
-        val queue: BlockingQueue<Any> = LinkedTransferQueue()
-        val executor = Executors.newSingleThreadScheduledExecutor()
-
+@Test
+internal class FlowCollectingPublisherTest : PublisherVerification<Long>(TestEnvironment(5000L, 2000L)) {
+    override fun createPublisher(elements: Long): FlowCollectingPublisher<Long> =
         FlowCollectingPublisher(
             flow {
-                (0 until 3).forEach {
+                (0 until elements).onEach {
                     emit(it)
-                    queue.add(it)
                 }
             },
             ctx
-        ).subscribe(object : Subscriber<Int> {
-            private lateinit var subscription: Subscription
+        )
 
-            override fun onSubscribe(s: Subscription) {
-                subscription = s
-                subscription.request(1L)
-            }
-
-            override fun onNext(t: Int) {
-                executor.schedule({ subscription.request(1L) }, 2, TimeUnit.SECONDS)
-            }
-
-            override fun onError(t: Throwable) {}
-
-            override fun onComplete() {}
-        })
-
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(0)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isNull()
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(1)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isNull()
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(2)
-    }
-
-    @Test
-    fun test_backPressuresWithBuffer() {
-        val queue: BlockingQueue<Any> = LinkedTransferQueue()
-        val executor = Executors.newSingleThreadScheduledExecutor()
-
+    override fun createFailedPublisher(): FlowCollectingPublisher<Long> =
         FlowCollectingPublisher(
             flow {
-                (0 until 7).forEach {
-                    emit(it)
-                    queue.add(it)
-                }
-            }.buffer(capacity = 1),
-            ctx
-        ).subscribe(object : Subscriber<Int> {
-            private lateinit var subscription: Subscription
-
-            override fun onSubscribe(s: Subscription) {
-                subscription = s
-                subscription.request(1L)
-            }
-
-            override fun onNext(t: Int) {
-                executor.schedule({ subscription.request(1L) }, 2, TimeUnit.SECONDS)
-            }
-
-            override fun onError(t: Throwable) {}
-
-            override fun onComplete() {}
-        })
-
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(0)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(1)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(2)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isNull()
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(3)
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isNull()
-        assertThat(queue.poll(1500L, TimeUnit.MILLISECONDS)).isEqualTo(4)
-    }
-
-    @Test
-    fun test_emitsOnErrorSignalUponUpstreamCancellation() {
-        val pub = FlowCollectingPublisher(
-            flow {
-                emit("onNext")
                 currentCoroutineContext().cancel()
             },
             ctx
         )
-        StepVerifier.create(pub)
-            .expectNext("onNext")
-            .expectError(CancellationException::class.java)
-            .verify()
-    }
-
-    @Test
-    fun test_errorAfterCancellationIsNotSignaled() {
-        val pub = FlowCollectingPublisher(
-            flow {
-                try {
-                    emit("onNext")
-                    emit("onNext")
-                } catch (t: CancellationException) {
-                    // do nothing
-                } finally {
-                    throw RuntimeException()
-                }
-            },
-            ctx
-        )
-        StepVerifier.create(pub)
-            .expectSubscription()
-            .expectNext("onNext")
-            .thenCancel()
-            .verify()
-    }
-
-    @Test
-    fun test_propagatesCoroutineContext() {
-        val context = CoroutineName("custom-context")
-        val coroutineNameCaptor = AtomicReference<CoroutineName>()
-
-        FlowCollectingPublisher(
-            flow = flow {
-                coroutineNameCaptor.set(currentCoroutineContext()[CoroutineName])
-                emit(1)
-            },
-            ctx = ctx,
-            context = context
-        ).subscribe(object : Subscriber<Int> {
-            override fun onSubscribe(s: Subscription) {}
-
-            override fun onNext(t: Int) {}
-
-            override fun onError(t: Throwable) {}
-
-            override fun onComplete() {}
-        })
-        await().untilAsserted { assertThat(coroutineNameCaptor.get()).isEqualTo(context) }
-    }
 }
