cd49994087c5412b50086d562fd81643efa94825
Ikhun Um <ih.pert@gmail.com>
2021-10-13 15:43:09 +0900

    Provide a way to write a `StreamMessage<HttpData>` into a file
    Motivation:
    
    If the size of `StreamMessage` is big, it could not fit in the memory.
    Users might want to write the data into a local file system and use it
    later.
    
    Modifications:
    
    - Add `AsyncFileWriter` that asynchronously write a file with
      backpressure.
    - Add `StreamMessages` as a utility class of `StreamMessage<T>`
    
    Result:
    
    - You can now write a `StreamMessage<HttpData>` into a file using
    `StreamMessages.writeTo()`.
      ```java
      StreamMessage<HttpData> stream = ...;
      Path destination = ...;
      StreamMessages.writeTo(stream, destination);
      ```
    - Fixes #3870

---
+++ core/src/main/java/com/linecorp/armeria/common/stream/AsyncFileWriter.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.common.stream;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+
+import com.linecorp.armeria.common.HttpData;
+import com.linecorp.armeria.common.annotation.Nullable;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.util.concurrent.EventExecutor;
+
+/**
+ * Asynchronously writes a {@code StreamMessage<HttpMessage>} to a {@link File}.
+ */
+final class AsyncFileWriter implements Subscriber<HttpData>,
+                                       CompletionHandler<Integer, Entry<ByteBuffer, ByteBuf>> {
+
+    private static final Logger logger = LoggerFactory.getLogger(AsyncFileWriter.class);
+
+    private final CompletableFuture<Path> completionFuture = new CompletableFuture<>();
+    private final Path path;
+    private final EventExecutor eventExecutor;
+    private final Set<OpenOption> options;
+    private final ExecutorService blockingTaskExecutor;
+    private final StreamMessage<? extends HttpData> publisher;
+
+    @Nullable
+    private AsynchronousFileChannel fileChannel;
+    @Nullable
+    private Subscription subscription;
+
+    // state
+    private long position;
+    private boolean writing;
+    private boolean closing;
+
+    AsyncFileWriter(StreamMessage<? extends HttpData> publisher, Path path, Set<OpenOption> options,
+                    EventExecutor eventExecutor, ExecutorService blockingTaskExecutor) {
+        this.path = path;
+        this.eventExecutor = eventExecutor;
+        this.options = options;
+        this.blockingTaskExecutor = blockingTaskExecutor;
+        this.publisher = publisher;
+        publisher.subscribe(this, eventExecutor, SubscriptionOption.WITH_POOLED_OBJECTS);
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        requireNonNull(subscription, "subscription");
+        this.subscription = subscription;
+
+        try {
+            fileChannel = AsynchronousFileChannel.open(path, options, blockingTaskExecutor);
+        } catch (IOException e) {
+            maybeCloseFileChannel(e);
+            return;
+        }
+
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(HttpData httpData) {
+        if (httpData.isEmpty()) {
+            httpData.close();
+            subscription.request(1);
+        } else {
+            final ByteBuf byteBuf = httpData.byteBuf();
+            final ByteBuffer byteBuffer = byteBuf.nioBuffer();
+            writing = true;
+            fileChannel.write(byteBuffer, position, Maps.immutableEntry(byteBuffer, byteBuf), this);
+        }
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        maybeCloseFileChannel(t);
+    }
+
+    @Override
+    public void onComplete() {
+        if (!writing) {
+            maybeCloseFileChannel(null);
+        } else {
+            closing = true;
+        }
+    }
+
+    public CompletableFuture<Path> whenComplete() {
+        return completionFuture;
+    }
+
+    @Override
+    public void completed(Integer result, Entry<ByteBuffer, ByteBuf> attachment) {
+        assert subscription != null;
+        eventExecutor.execute(() -> {
+            if (result > -1) {
+                position += result;
+                final ByteBuffer byteBuffer = attachment.getKey();
+                final ByteBuf byteBuf = attachment.getValue();
+
+                if (byteBuffer.hasRemaining()) {
+                    fileChannel.write(byteBuffer, position, attachment, this);
+                } else {
+                    byteBuf.release();
+                    writing = false;
+
+                    if (closing) {
+                        maybeCloseFileChannel(null);
+                    } else {
+                        subscription.request(1);
+                    }
+                }
+            } else {
+                subscription.cancel();
+                maybeCloseFileChannel(new IOException(
+                        "Unexpected exception while writing data to '" + path + "' + : result " + result));
+            }
+        });
+    }
+
+    @Override
+    public void failed(Throwable cause, Entry<ByteBuffer, ByteBuf> attachment) {
+        assert subscription != null;
+        subscription.cancel();
+        attachment.getValue().release();
+        maybeCloseFileChannel(cause);
+    }
+
+    private void maybeCloseFileChannel(@Nullable Throwable cause) {
+        if (completionFuture.isDone()) {
+            // no-op
+        } else {
+            if (cause == null) {
+                completionFuture.complete(path);
+            } else {
+                publisher.abort(cause);
+                completionFuture.completeExceptionally(cause);
+            }
+
+            if (fileChannel != null && fileChannel.isOpen()) {
+                try {
+                    fileChannel.close();
+                } catch (IOException e) {
+                    logger.warn("Failed to close '" + path + '\'', e);
+                }
+            }
+        }
+    }
+}
---
+++ core/src/main/java/com/linecorp/armeria/common/stream/StreamMessages.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.common.stream;
+
+import static java.util.Objects.requireNonNull;
+
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+
+import com.google.common.collect.ImmutableSet;
+
+import com.linecorp.armeria.common.CommonPools;
+import com.linecorp.armeria.common.HttpData;
+import com.linecorp.armeria.common.RequestContext;
+import com.linecorp.armeria.common.annotation.UnstableApi;
+import com.linecorp.armeria.server.ServiceRequestContext;
+
+import io.netty.util.concurrent.EventExecutor;
+
+/**
+ * A utility class for {@link StreamMessage}.
+ */
+@UnstableApi
+public final class StreamMessages {
+
+    /**
+     * Write the specified {@link  StreamMessage} to the given {@link Path} with {@link OpenOption}.
+     * If the {@link OpenOption} is not specified, defaults to {@link StandardOpenOption#CREATE},
+     * {@link StandardOpenOption#TRUNCATE_EXISTING} and {@link StandardOpenOption#WRITE}.
+     *
+     * @param publisher the source of {@link HttpData} to be written
+     * @param destination the {@link Path} to write to
+     * @param options the {@link OpenOption} specifying how the file is opened
+     * @return a {@link CompletableFuture} that completes with the specified {@link Path} or an error
+     */
+    public static CompletableFuture<Path> writeTo(StreamMessage<? extends HttpData> publisher,
+                                                  Path destination, OpenOption... options) {
+        requireNonNull(publisher, "publisher");
+        requireNonNull(destination, "destination");
+        requireNonNull(options, "options");
+
+        final RequestContext ctx = RequestContext.currentOrNull();
+        EventExecutor eventExecutor = null;
+        ExecutorService blockingTaskExecutor = null;
+        if (ctx != null) {
+            eventExecutor = ctx.eventLoop();
+            if (ctx instanceof ServiceRequestContext) {
+                blockingTaskExecutor = ((ServiceRequestContext) ctx).blockingTaskExecutor();
+            }
+        }
+        if (eventExecutor == null) {
+            eventExecutor = CommonPools.workerGroup().next();
+        }
+        if (blockingTaskExecutor == null) {
+            blockingTaskExecutor = CommonPools.blockingTaskExecutor();
+        }
+
+        return writeTo(publisher, destination, eventExecutor, blockingTaskExecutor, options);
+    }
+
+    /**
+     * Write the specified {@link  StreamMessage} to the given {@link Path} with {@link OpenOption}.
+     * If the {@link OpenOption} is not specified, defaults to {@link StandardOpenOption#CREATE},
+     * {@link StandardOpenOption#TRUNCATE_EXISTING} and {@link StandardOpenOption#WRITE}.
+     *
+     * @param publisher the source of {@link HttpData} to be written
+     * @param destination the {@link Path} to write to
+     * @param eventExecutor the {@link EventExecutor} to subscribe to the given publisher
+     * @param blockingTaskExecutor the {@link ExecutorService} to which blocking tasks are submitted to handle
+     *                             file I/O events and write operations
+     * @param options the {@link OpenOption} specifying how the file is opened
+     * @return a {@link CompletableFuture} that completes with the specified {@link Path} or an error
+     */
+    public static CompletableFuture<Path> writeTo(StreamMessage<? extends HttpData> publisher, Path destination,
+                                                  EventExecutor eventExecutor,
+                                                  ExecutorService blockingTaskExecutor, OpenOption... options) {
+        requireNonNull(publisher, "publisher");
+        requireNonNull(destination, "destination");
+        requireNonNull(eventExecutor, "eventExecutor");
+        requireNonNull(blockingTaskExecutor, "blockingTaskExecutor");
+        requireNonNull(options, "options");
+
+        final Set<OpenOption> writeOptions = checkWriteOptions(options);
+        return new AsyncFileWriter(publisher, destination, writeOptions, eventExecutor,
+                                   blockingTaskExecutor).whenComplete();
+    }
+
+    private static Set<OpenOption> checkWriteOptions(OpenOption[] options) {
+        final int length = options.length;
+        final ImmutableSet.Builder<OpenOption> writeOptions =
+                ImmutableSet.builderWithExpectedSize(length + 3);
+
+        if (length == 0) {
+            writeOptions.add(StandardOpenOption.CREATE);
+            writeOptions.add(StandardOpenOption.TRUNCATE_EXISTING);
+        } else {
+            for (OpenOption opt : options) {
+                if (opt == StandardOpenOption.READ) {
+                    throw new IllegalArgumentException("READ not allowed");
+                }
+                writeOptions.add(opt);
+            }
+        }
+        writeOptions.add(StandardOpenOption.WRITE);
+        return writeOptions.build();
+    }
+
+    private StreamMessages() {}
+}
