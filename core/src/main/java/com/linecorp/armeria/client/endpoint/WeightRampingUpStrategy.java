eb73fc6c546a6cdbccad78174af11fdc99323291
Ikhun Um <ih.pert@gmail.com>
2021-12-09 17:43:50 +0900

    checkstyle

--- core/src/main/java/com/linecorp/armeria/client/endpoint/WeightRampingUpStrategy.java
+++ core/src/main/java/com/linecorp/armeria/client/endpoint/WeightRampingUpStrategy.java
@@ -261,7 +261,7 @@ final class WeightRampingUpStrategy implements EndpointSelectionStrategy {
             final Map<Endpoint, Endpoint> newEndpointsMap = deduplicateEndpoints(newEndpoints);
 
             final List<Endpoint> replacedEndpoints = new ArrayList<>();
-            for (final Iterator<Endpoint> i = endpointsFinishedRampingUp.iterator(); i.hasNext(); ) {
+            for (final Iterator<Endpoint> i = endpointsFinishedRampingUp.iterator(); i.hasNext();) {
                 final Endpoint endpointFinishedRampingUp = i.next();
                 final Endpoint newEndpoint = newEndpointsMap.remove(endpointFinishedRampingUp);
                 if (newEndpoint == null) {
@@ -289,7 +289,7 @@ final class WeightRampingUpStrategy implements EndpointSelectionStrategy {
             }
 
             for (final Iterator<EndpointsRampingUpEntry> i = endpointsRampingUp.iterator();
-                 i.hasNext(); ) {
+                 i.hasNext();) {
                 final EndpointsRampingUpEntry endpointsRampingUpEntry = i.next();
 
                 final Set<EndpointAndStep> endpointAndSteps = endpointsRampingUpEntry.endpointAndSteps();
@@ -314,7 +314,7 @@ final class WeightRampingUpStrategy implements EndpointSelectionStrategy {
         private void filterOldEndpoints(Set<EndpointAndStep> endpointAndSteps,
                                         Map<Endpoint, Endpoint> newEndpointsMap) {
             final List<EndpointAndStep> replacedEndpoints = new ArrayList<>();
-            for (final Iterator<EndpointAndStep> i = endpointAndSteps.iterator(); i.hasNext(); ) {
+            for (final Iterator<EndpointAndStep> i = endpointAndSteps.iterator(); i.hasNext();) {
                 final EndpointAndStep endpointAndStep = i.next();
                 final Endpoint rampingUpEndpoint = endpointAndStep.endpoint();
                 final Endpoint newEndpoint = newEndpointsMap.remove(rampingUpEndpoint);
@@ -371,7 +371,7 @@ final class WeightRampingUpStrategy implements EndpointSelectionStrategy {
         }
 
         private void updateWeightAndStep(Set<EndpointAndStep> endpointAndSteps) {
-            for (final Iterator<EndpointAndStep> i = endpointAndSteps.iterator(); i.hasNext(); ) {
+            for (final Iterator<EndpointAndStep> i = endpointAndSteps.iterator(); i.hasNext();) {
                 final EndpointAndStep endpointAndStep = i.next();
                 final int step = endpointAndStep.incrementAndGetStep();
                 final Endpoint endpoint = endpointAndStep.endpoint();
