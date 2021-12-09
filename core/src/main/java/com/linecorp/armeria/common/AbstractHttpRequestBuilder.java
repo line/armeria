b34fe092e9addbb50d86f278f260599b91d0b803
policeman-kh <policeman_kh@msn.com>
2021-12-09 17:12:44 +0900

    Deprecate Cookie.builder and introduce Cookie.secureBuilder (#3939)
    Motivation:
    
    `Cookie.builder` makes it easy for users to create insecure cookies with `secure=false` and `httpOnly=false`.
    It should provide a method to create a secure cookie with `secure=true` and `httpOnly=true`, as described in [the issue](https://github.com/line/armeria/issues/3788).
    
    Modifications:
    
    - Add `Cookie.secureBuilder` and `Cookie.ofSecure`.
    - Add `@deprecated` into `Cookie.builder` and `Cookie.of`.
    - Replace `Cookie.builder` and `Cookie.of` to `Cookie.secureBuilder` and `Cookie.ofSecure` in test cases and example.
    
    Result:
    
    - Closes #3788.

--- core/src/main/java/com/linecorp/armeria/common/AbstractHttpRequestBuilder.java
+++ core/src/main/java/com/linecorp/armeria/common/AbstractHttpRequestBuilder.java
@@ -349,7 +349,7 @@ public abstract class AbstractHttpRequestBuilder {
      * <pre>{@code
      * HttpRequest.builder()
      *            .get("/")
-     *            .cookie(Cookie.of("cookie", "foo"))
+     *            .cookie(Cookie.ofSecure("cookie", "foo"))
      *            .build();
      * }</pre>
      *
@@ -369,8 +369,8 @@ public abstract class AbstractHttpRequestBuilder {
      * <pre>{@code
      * HttpRequest.builder()
      *            .get("/")
-     *            .cookies(Cookies.of(Cookie.of("cookie1", "foo"),
-     *                                Cookie.of("cookie2", "bar")))
+     *            .cookies(Cookies.ofSecure(Cookie.ofSecure("cookie1", "foo"),
+     *                                      Cookie.ofSecure("cookie2", "bar")))
      *            .build();
      * }</pre>
      *
