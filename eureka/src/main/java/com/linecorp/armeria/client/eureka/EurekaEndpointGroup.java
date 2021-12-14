fe79e342501f0a0a88ae2663d68684aebf3b6336
eisig <eisig.g@live.com>
2021-12-14 09:42:18 +0800

    Update eureka/src/main/java/com/linecorp/armeria/client/eureka/EurekaEndpointGroup.java
    Co-authored-by: Ikhun Um <ih.pert@gmail.com>

--- eureka/src/main/java/com/linecorp/armeria/client/eureka/EurekaEndpointGroup.java
+++ eureka/src/main/java/com/linecorp/armeria/client/eureka/EurekaEndpointGroup.java
@@ -93,7 +93,7 @@ public final class EurekaEndpointGroup extends DynamicEndpointGroup {
      * The {@link AttributeKey} that store eureka {@link InstanceInfo}.
      * Use {@link Endpoint#attr(AttributeKey)} to retrieve the value.
      */
-    private static final AttributeKey<InstanceInfo> INSTANCE_INFO = AttributeKey.valueOf("instanceInfo");
+    private static final AttributeKey<InstanceInfo> INSTANCE_INFO = AttributeKey.valueOf(EurekaEndpointGroup.class, "instanceInfo");
 
     /**
      * Returns a new {@link EurekaEndpointGroup} that retrieves the {@link Endpoint} list from the specified
