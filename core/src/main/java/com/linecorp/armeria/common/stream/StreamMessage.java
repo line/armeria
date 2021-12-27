e56d06b28ce28a1260131c76ef46c46a747cc3a5
minux <songmw725@gmail.com>
2021-12-27 15:07:14 +0900

    Fix lint

--- core/src/main/java/com/linecorp/armeria/common/stream/StreamMessage.java
+++ core/src/main/java/com/linecorp/armeria/common/stream/StreamMessage.java
@@ -555,7 +555,8 @@ public interface StreamMessage<T> extends Publisher<T> {
      *
      * <p>Example:<pre>{@code
      * StreamMessage<Integer> streamMessage = StreamMessage.of(1, 2, 3, 4, 5);
-     * StreamMessage<Integer> transformed = streamMessage.mapAsync(x -> CompletableFuture.completedFuture(x + 1));
+     * StreamMessage<Integer> transformed =
+     *     streamMessage.mapAsync(x -> CompletableFuture.completedFuture(x + 1));
      * }</pre>
      */
     default <U> StreamMessage<U> mapAsync(
