810446cfe39f4ad9ec60c852aacf052816a2c468
Ikhun Um <ih.pert@gmail.com>
2022-01-12 16:16:56 +0900

    fix test

--- scala/scala_2.13/src/test/scala/com/linecorp/armeria/client/ScalaResponseAs.scala
+++ scala/scala_2.13/src/test/scala/com/linecorp/armeria/client/ScalaResponseAs.scala
@@ -14,17 +14,40 @@
  * under the License.
  */
 
-package com.linecorp.armeria.common
+package com.linecorp.armeria.client
 
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.{ClassTagExtensions, JavaTypeable}
-import com.linecorp.armeria.client.{ResponseAs, WebClient}
+import com.linecorp.armeria.client.ScalaResponseAs.json
+import com.linecorp.armeria.common.{HttpResponse, ResponseEntity}
 import com.linecorp.armeria.internal.common.JacksonUtil
 import com.linecorp.armeria.scala.implicits._
+import com.linecorp.armeria.server.{ServerBuilder, ServerSuite}
+import munit.FunSuite
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 
-object FutureResponseAs {
+class ScalaResponseAsSuite extends FunSuite with ServerSuite {
+  override protected def configureServer: ServerBuilder => Unit = { sb =>
+    sb.service("/foo", (_, _) => HttpResponse.ofJson(MyClass("hello")))
+  }
+
+  test("should be able to convert a response into a case class") {
+    val client = WebClient.of(server.httpUri())
+    val response: Future[MyClass] =
+      client
+        .prepare()
+        .get("/foo")
+        .as(json[MyClass])
+        .execute()
+        .map(_.content())
+
+    assertEquals(Await.result(response, Duration.Inf), MyClass("hello"))
+  }
+}
+
+object ScalaResponseAs {
 
   private val jsonMapper = JacksonUtil.newDefaultObjectMapper().asInstanceOf[JsonMapper]
   private val mapper = jsonMapper :: ClassTagExtensions
@@ -36,23 +59,12 @@ object FutureResponseAs {
     response => ResponseAs.string().as(response).toScala
 
   def json[A: JavaTypeable]: ResponseAs[HttpResponse, Future[ResponseEntity[A]]] =
-    _.aggregate().thenApply { agg =>
-      val content: A = mapper.readValue(agg.content().array())
-      ResponseEntity.of(agg.headers(), content, agg.trailers())
-    }.toScala
+    _.aggregate()
+      .thenApply[ResponseEntity[A]] { agg =>
+        val content: A = mapper.readValue(agg.content().array())
+        ResponseEntity.of(agg.headers(), content, agg.trailers())
+      }
+      .toScala
 }
 
-object Test {
-  def main(args: Array[String]): Unit = {
-    val client = WebClient.of()
-    val response: Future[ResponseEntity[MyClass]] =
-      client
-        .prepare()
-        .get("/foo")
-        .as(FutureResponseAs.json[MyClass])
-        .execute()
-    response.map(x => x)
-  }
-
-  case class MyClass(id: String)
-}
+case class MyClass(id: String)
