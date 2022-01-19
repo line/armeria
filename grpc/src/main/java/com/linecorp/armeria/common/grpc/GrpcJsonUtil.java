7be0eaa5890088d8990b03fc3d25fc53f731fae2
jrhee17 <guins_j@guins.org>
2022-01-19 16:21:03 +0900

    let users know removing grpc serialization formats is also an option

--- grpc/src/main/java/com/linecorp/armeria/common/grpc/GrpcJsonUtil.java
+++ grpc/src/main/java/com/linecorp/armeria/common/grpc/GrpcJsonUtil.java
@@ -66,7 +66,8 @@ final class GrpcJsonUtil {
         try {
             return builder.build();
         } catch (RuntimeException e) {
-            logger.warn("Failed to instantiate a json marshaller for {}. Consider using {}.ofGson() instead.",
+            logger.warn("Failed to instantiate a json marshaller for {}. Consider " +
+                        "disabling gRPC-JSON serialization or using {}.ofGson() instead.",
                         methods, GrpcJsonMarshaller.class.getName(), e);
             throw e;
         }
