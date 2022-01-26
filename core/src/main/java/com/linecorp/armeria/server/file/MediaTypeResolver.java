f480b0f01f7ed34e10cb5e75616b5bb04da419b1
Ikhun Um <ih.pert@gmail.com>
2022-01-26 18:15:10 +0900

    Update Javadoc

--- core/src/main/java/com/linecorp/armeria/server/file/MediaTypeResolver.java
+++ core/src/main/java/com/linecorp/armeria/server/file/MediaTypeResolver.java
@@ -44,7 +44,7 @@ public interface MediaTypeResolver {
      * @param contentEncoding the content encoding, such as {@code "gzip"} and {@code "br"}, as defined
      *                        in <a href="https://datatracker.ietf.org/doc/rfc2616/"> the section 3.5,
      *                        RFC 2616</a>.
-     *                        {@code null} to use a file extension as is.
+     *                        {@code null} for an uncompressed file.
      * @return the resolved {@link MediaType}
      */
     @Nullable
