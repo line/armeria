d26d7e244865efb14c575c1baa30848e510200a9
kojilin <koji.lin@gmail.com>
2022-03-22 23:36:09 +0900

    Address the comments

--- core/src/main/java/com/linecorp/armeria/server/annotation/decorator/LoggingDecorator.java
+++ core/src/main/java/com/linecorp/armeria/server/annotation/decorator/LoggingDecorator.java
@@ -66,7 +66,8 @@ public @interface LoggingDecorator {
     /**
      * The rate at which to sample success requests to log. Any number between {@code 0.0} and {@code 1.0} will
      * cause a random sample of the requests to be logged. The random sampling is appropriate for low-traffic
-     * (ex servers that each receive &lt;100K requests). If unset, all requests will be logged.
+     * (ex servers that each receive &lt;100K requests). If unset, {@link #samplingRate()} will be used
+     * to sample successful requests.
      * If the value of this attribute is negative, the value from {@link #samplingRate()}
      * will be used as a fallback.
      */
@@ -75,7 +76,8 @@ public @interface LoggingDecorator {
     /**
      * The rate at which to sample failed requests to log. Any number between {@code 0.0} and {@code 1.0} will
      * cause a random sample of the requests to be logged. The random sampling is appropriate for low-traffic
-     * (ex servers that each receive &lt;100K requests). If unset, all requests will be logged.
+     * (ex servers that each receive &lt;100K requests). If unset, {@link #samplingRate()} will be used
+     * to sample failed requests.
      * If the value of this attribute is negative, the value from {@link #samplingRate()}
      * will be used as a fallback.
      */
