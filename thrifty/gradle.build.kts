44ba5cb41bcdab440bcd2062ef57877425a4a2c4
Jiayu Liu <jiayu@hey.com>
2022-03-31 00:00:39 +0800

    add thrift common module

--- thrifty/gradle.build.kts
+++ thrifty/gradle.build.kts
@@ -1,4 +1,6 @@
 dependencies {
+    // Common thrift classes
+    api(project(":thrift-common"))
     api("javax.annotation:javax.annotation-api")
     // https://mvnrepository.com/artifact/com.microsoft.thrifty/thrifty-runtime
     api("com.microsoft.thrifty:thrifty-runtime:3.0.0")
