fa7c20f3056b74cad01e1b304b0b8685d2596e32
jsing <joosing711@gmail.com>
2022-04-02 09:56:32 +0900

    Use queryParams.add() instead of set() in method queryParams()

--- core/src/main/java/com/linecorp/armeria/common/AbstractHttpRequestBuilder.java
+++ core/src/main/java/com/linecorp/armeria/common/AbstractHttpRequestBuilder.java
@@ -228,7 +228,7 @@ public abstract class AbstractHttpRequestBuilder
         if (this.queryParams == null) {
             this.queryParams = QueryParams.builder();
         }
-        this.queryParams.set(queryParams);
+        this.queryParams.add(queryParams);
         return this;
     }
 
