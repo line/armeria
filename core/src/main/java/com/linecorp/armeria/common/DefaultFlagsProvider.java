24b88e7f4b7904afc4297cc29ff33e13cfbcb133
waritboonmasiri <warit.boonmasiri@gmail.com>
2022-04-12 01:47:44 +0700

    Correct maxNumConnections default value

--- core/src/main/java/com/linecorp/armeria/common/DefaultFlagsProvider.java
+++ core/src/main/java/com/linecorp/armeria/common/DefaultFlagsProvider.java
@@ -38,7 +38,7 @@ final class DefaultFlagsProvider implements FlagsProvider {
     static final DefaultFlagsProvider INSTANCE = new DefaultFlagsProvider();
 
     static final String VERBOSE_EXCEPTION_SAMPLER = "rate-limit=10";
-    static final int MAX_NUM_CONNECTIONS = 1;
+    static final int MAX_NUM_CONNECTIONS = Integer.MAX_VALUE;
     static final int NUM_COMMON_BLOCKING_TASK_THREADS = 200; // from Tomcat default maxThreads
     static final long DEFAULT_MAX_REQUEST_LENGTH = 10 * 1024 * 1024; // 10 MiB
     static final long DEFAULT_MAX_RESPONSE_LENGTH = 10 * 1024 * 1024; // 10 MiB
@@ -89,7 +89,7 @@ final class DefaultFlagsProvider implements FlagsProvider {
 
     @Override
     public Sampler<Class<? extends Throwable>> verboseExceptionSampler() {
-        return new ExceptionSampler(VERBOSE_EXCEPTION_SAMPLER);
+        return new ExceptionSampler("rate-limit=10");
     }
 
     @Override
