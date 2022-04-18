line/armeria

commit d336d8981f03001f5a12ecae3363e98b698bb08b

Add prefix to `HttpServiceWithRoutes.routes()` in `ServerBuilder.serviceUnder` (#4086)
Motivation:
It's very common that a `GrpcService` is added to a `Server` with a path prefix:
```java
GrpcService grpcService = ...
Server.builder().serviceUnder("/foo", grpcService);
```
If so, the `GrpcService` is bound using the prefix route, which means:
- the `GrpcService.routes()` are ignored so we cannot easily recognize which routes are added to the server
  (When a server starts up, it prints every `Route` added to the router.)
- the `GrpcService` has to implement the router by itself in its method.
- `HttpJsonTranscodingService` does not work because the `Route`s for rest endpoints are not added.

If we prepend the path prefix to the `GrpcService.routes()` and use the recreated `Routes`, the problems will be gone.

Modifications:
- `ServerBuilder.serviceUnder(String, HttpService)` now adds the prefix to the `HttpServiceWithRoutes.routes()` if the `HttpService` is an `HttpServiceWithRoutes`.
- Add `Route.withPrefix(String)` to easily recreate a `Route` with a prefix.
- Add `reportMaskedRoutes` property not to log the masked routes warning

Result:
- An `HttpJsonTranscodingService` works correctly when it's added to a `Server` with a prefix.
- You can disable the masked routes warning log by specifying -Dcom.linecorp.armeria.reportMaskedRoutes=false.

core/src/main/java/com/linecorp/armeria/server/AbstractPathMapping.java
@@ -26,6 +26,17 @@ import com.linecorp.armeria.common.annotation.Nullable;
  */
 abstract class AbstractPathMapping implements PathMapping {
 
+    @Override
+    public final PathMapping withPrefix(String prefix) {
+        prefix = ensureAbsolutePath(prefix, "prefix");
+        if ("/".equals(prefix)) {
+            return this;
+        }
+        return doWithPrefix(prefix.charAt(prefix.length() - 1) == '/' ? prefix : prefix + '/');
+    }
+
+    abstract PathMapping doWithPrefix(String prefix);
+
     /**
      * {@inheritDoc} This method performs sanity checks on the specified {@code path} and calls
      * {@link #doApply(RoutingContext)}.

core/src/main/java/com/linecorp/armeria/server/ExactPathMapping.java
@@ -16,6 +16,7 @@
 
 package com.linecorp.armeria.server;
 
+import static com.linecorp.armeria.internal.common.ArmeriaHttpUtil.concatPaths;
 import static com.linecorp.armeria.internal.server.RouteUtil.ensureAbsolutePath;
 
 import java.util.List;
@@ -34,6 +35,11 @@ final class ExactPathMapping extends AbstractPathMapping {
     ExactPathMapping(String exactPath) {
         this.exactPath = ensureAbsolutePath(exactPath, "exactPath");
         paths = ImmutableList.of(exactPath, exactPath);
+    }
+
+    @Override
+    PathMapping doWithPrefix(String prefix) {
+        return new ExactPathMapping(concatPaths(prefix, exactPath));
     }
 
     @Nullable

core/src/main/java/com/linecorp/armeria/server/GlobPathMapping.java
@@ -18,6 +18,7 @@ package com.linecorp.armeria.server;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.linecorp.armeria.internal.server.RouteUtil.GLOB;
+import static com.linecorp.armeria.server.RouteBuilder.globPathMapping;
 
 import java.util.List;
 import java.util.Set;
@@ -33,6 +34,7 @@ import com.linecorp.armeria.internal.common.util.StringUtil;
 final class GlobPathMapping extends AbstractPathMapping {
 
     private final String glob;
+    private final int numGroupsToSkip;
     private final Pattern pattern;
     private final int numParams;
     private final Set<String> paramNames;
@@ -44,6 +46,7 @@ final class GlobPathMapping extends AbstractPathMapping {
         final PatternAndParamCount patternAndParamCount = globToRegex(glob, numGroupsToSkip);
 
         this.glob = glob;
+        this.numGroupsToSkip = numGroupsToSkip;
         pattern = patternAndParamCount.pattern;
         numParams = patternAndParamCount.numParams;
 
@@ -60,6 +63,11 @@ final class GlobPathMapping extends AbstractPathMapping {
         final String aGlob = glob.startsWith("/") ? glob : "/**/" + glob;
         pathPattern = aGlob;
         paths = ImmutableList.of(pattern.pattern(), aGlob);
+    }
+
+    @Override
+    PathMapping doWithPrefix(String prefix) {
+        return globPathMapping(prefix, glob, numGroupsToSkip);
     }
 
     @Nullable

core/src/main/java/com/linecorp/armeria/server/ParameterizedPathMapping.java
@@ -16,6 +16,7 @@
 
 package com.linecorp.armeria.server;
 
+import static com.linecorp.armeria.internal.common.ArmeriaHttpUtil.concatPaths;
 import static java.util.Objects.requireNonNull;
 
 import java.util.ArrayList;
@@ -214,6 +215,11 @@ final class ParameterizedPathMapping extends AbstractPathMapping {
      */
     String skeleton() {
         return skeleton;
+    }
+
+    @Override
+    PathMapping doWithPrefix(String prefix) {
+        return new ParameterizedPathMapping(concatPaths(prefix, pathPattern));
     }
 
     @Override

core/src/main/java/com/linecorp/armeria/server/RegexPathMappingWithPrefix.java
@@ -16,6 +16,7 @@
 
 package com.linecorp.armeria.server;
 
+import static com.linecorp.armeria.internal.common.ArmeriaHttpUtil.concatPaths;
 import static com.linecorp.armeria.internal.server.RouteUtil.PREFIX;
 import static java.util.Objects.requireNonNull;
 
@@ -50,6 +51,11 @@ final class RegexPathMappingWithPrefix extends AbstractPathMapping {
             normalizedPathPrefix = pathPrefix;
         }
         pathPattern = normalizedPathPrefix + patternString;
+    }
+
+    @Override
+    PathMapping doWithPrefix(String prefix) {
+        return new RegexPathMappingWithPrefix(concatPaths(prefix, pathPrefix), mapping);
     }
 
     @Nullable

core/src/main/java/com/linecorp/armeria/server/RouteBuilder.java
@@ -134,14 +134,17 @@ public final class RouteBuilder {
 
         if (pathPattern.startsWith(GLOB)) {
             final String glob = pathPattern.substring(GLOB.length());
-            if (glob.startsWith("/")) {
-                return glob(concatPaths(prefix, glob));
-            } else {
-                return glob(concatPaths(prefix + "**/", glob), 1);
-            }
+            return pathMapping(globPathMapping(prefix, glob, 0));
         }
 
         return pathMapping(new RegexPathMappingWithPrefix(prefix, getPathMapping(pathPattern)));
+    }
+
+    static PathMapping globPathMapping(String prefix, String glob, int numGroupsToSkip) {
+        if (glob.startsWith("/")) {
+            return globPathMapping(concatPaths(prefix, glob), numGroupsToSkip);
+        }
+        return globPathMapping(concatPaths(prefix + "**/", glob), numGroupsToSkip + 1);
     }
 
     private static PathMapping globPathMapping(String glob, int numGroupsToSkip) {
@@ -543,7 +546,7 @@ public final class RouteBuilder {
         return new ParameterizedPathMapping(pathPattern);
     }
 
-    private static PathMapping prefixPathMapping(String prefix, boolean stripPrefix) {
+    static PathMapping prefixPathMapping(String prefix, boolean stripPrefix) {
         if ("/".equals(prefix)) {
             // Every path starts with '/'.
             return CatchAllPathMapping.INSTANCE;

benchmarks/jmh/src/jmh/java/com/linecorp/armeria/server/RoutersBenchmark.java
@@ -50,18 +50,21 @@ public class RoutersBenchmark {
         final String defaultLogName = null;
         final String defaultServiceName = null;
         final ServiceNaming defaultServiceNaming = ServiceNaming.of("Service");
+        final Route route1 = Route.builder().exact("/grpc.package.Service/Method1").build();
+        final Route route2 = Route.builder().exact("/grpc.package.Service/Method2").build();
         final Path multipartUploadsLocation = Flags.defaultMultipartUploadsLocation();
         SERVICES = ImmutableList.of(
-                new ServiceConfig(Route.builder().exact("/grpc.package.Service/Method1").build(),
+                new ServiceConfig(route1, route1,
                                   SERVICE, defaultLogName, defaultServiceName, defaultServiceNaming, 0, 0,
                                   false, AccessLogWriter.disabled(), false, CommonPools.blockingTaskExecutor(),
                                   true, SuccessFunction.always(), multipartUploadsLocation),
-                new ServiceConfig(Route.builder().exact("/grpc.package.Service/Method2").build(),
+                new ServiceConfig(route2, route2,
                                   SERVICE, defaultLogName, defaultServiceName, defaultServiceNaming, 0, 0,
                                   false, AccessLogWriter.disabled(), false, CommonPools.blockingTaskExecutor(),
                                   true, SuccessFunction.always(), multipartUploadsLocation)
         );
-        FALLBACK_SERVICE = new ServiceConfig(Route.ofCatchAll(), SERVICE, defaultLogName, defaultServiceName,
+        FALLBACK_SERVICE = new ServiceConfig(Route.ofCatchAll(), Route.ofCatchAll(), SERVICE,
+                                             defaultLogName, defaultServiceName,
                                              defaultServiceNaming, 0, 0, false, AccessLogWriter.disabled(),
                                              false, CommonPools.blockingTaskExecutor(), true,
                                              SuccessFunction.always(), multipartUploadsLocation);

commit dd5b5ff2f8f84e5c0508302636c399aa7adb7595

Make `RoutingResult.path()` correctly return the mapped path
Motivation:

`RoutingResult.path()` should return a mapped path by `Route`.
Let's say a service which routes to `/users/{userId}` is prefixed
with `/api/v1`. If a request path is `/api/v1/users/1234`,
- `ServiceRequestContext.path()` is equal to `/api/v1/users/1234`
- `ServiceRequestContext.mappedPath()` should be equal to `/users/1234`

Before #4086, a prefixed path is internally mapped with
`PrefixPathMapping`. So a prefix path is correctly stripped.
https://github.com/line/armeria/blob/d336d8981f03001f5a12ecae3363e98b698bb08b/core/src/main/java/com/linecorp/armeria/server/PrefixPathMapping.java#L67
\#4086 introduced `Route.withPrefix()` and all `PathMapping`s are able
to be prefixed. Consequentely, other `PathMapping` implmentations such
as `ExactPathMapping`, should remove the prefix when building
`RoutingResult.path()`.

Modifications:

- Remove the prefix of a request path when a `RoutingContext` is applied
  to:
  - `ExactPathMapping`
  - `GlobPathMapping`,
  - `ParameterizedPathMapping`
  - `RegexPathMappingWithPrefix`

Result:

Fix a regression introduced by #4086

core/src/main/java/com/linecorp/armeria/server/AbstractPathMapping.java
@@ -26,6 +26,25 @@ import com.linecorp.armeria.common.annotation.Nullable;
  */
 abstract class AbstractPathMapping implements PathMapping {
 
+    /**
+     * Returns the path with its prefix removed.
+     */
+    static String mappedPath(String prefix, String path) {
+        int length = prefix.length();
+        if (length == 0) {
+            return path;
+        }
+        if ("/".equals(prefix)) {
+            return path;
+        }
+
+        if (prefix.charAt(length - 1) == '/') {
+            // Make sure the mappedPath starts with '/'
+            length -= 1;
+        }
+        return path.substring(length);
+    }
+
     @Override
     public final PathMapping withPrefix(String prefix) {
         prefix = ensureAbsolutePath(prefix, "prefix");

core/src/main/java/com/linecorp/armeria/server/ExactPathMapping.java
@@ -29,24 +29,30 @@ import com.linecorp.armeria.common.annotation.Nullable;
 
 final class ExactPathMapping extends AbstractPathMapping {
 
+    private final String prefix;
     private final String exactPath;
     private final List<String> paths;
 
     ExactPathMapping(String exactPath) {
+        this("", exactPath);
+    }
+
+    private ExactPathMapping(String prefix, String exactPath) {
+        this.prefix = prefix;
         this.exactPath = ensureAbsolutePath(exactPath, "exactPath");
         paths = ImmutableList.of(exactPath, exactPath);
     }
 
     @Override
     PathMapping doWithPrefix(String prefix) {
-        return new ExactPathMapping(concatPaths(prefix, exactPath));
+        return new ExactPathMapping(prefix, concatPaths(prefix, exactPath));
     }
 
     @Nullable
     @Override
     RoutingResultBuilder doApply(RoutingContext routingCtx) {
         return exactPath.equals(routingCtx.path()) ? RoutingResult.builder()
-                                                                  .path(routingCtx.path())
+                                                                  .path(mappedPath(prefix, routingCtx.path()))
                                                                   .query(routingCtx.query())
                                                    : null;
     }

core/src/main/java/com/linecorp/armeria/server/GlobPathMapping.java
@@ -17,8 +17,8 @@
 package com.linecorp.armeria.server;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.linecorp.armeria.internal.common.ArmeriaHttpUtil.concatPaths;
 import static com.linecorp.armeria.internal.server.RouteUtil.GLOB;
-import static com.linecorp.armeria.server.RouteBuilder.globPathMapping;
 
 import java.util.List;
 import java.util.Set;
@@ -33,6 +33,7 @@ import com.linecorp.armeria.internal.common.util.StringUtil;
 
 final class GlobPathMapping extends AbstractPathMapping {
 
+    private final String prefix;
     private final String glob;
     private final int numGroupsToSkip;
     private final Pattern pattern;
@@ -43,8 +44,13 @@ final class GlobPathMapping extends AbstractPathMapping {
     private final List<String> paths;
 
     GlobPathMapping(String glob, int numGroupsToSkip) {
+        this("", glob, numGroupsToSkip);
+    }
+
+    private GlobPathMapping(String prefix, String glob, int numGroupsToSkip) {
         final PatternAndParamCount patternAndParamCount = globToRegex(glob, numGroupsToSkip);
 
+        this.prefix = prefix;
         this.glob = glob;
         this.numGroupsToSkip = numGroupsToSkip;
         pattern = patternAndParamCount.pattern;
@@ -67,7 +73,15 @@ final class GlobPathMapping extends AbstractPathMapping {
 
     @Override
     PathMapping doWithPrefix(String prefix) {
-        return globPathMapping(prefix, glob, numGroupsToSkip);
+        final String prefixGlob;
+        int numGroupsToSkip = this.numGroupsToSkip;
+        if (glob.startsWith("/")) {
+            prefixGlob = concatPaths(prefix, glob);
+        } else {
+            prefixGlob = concatPaths(prefix + "**/", glob);
+            numGroupsToSkip++;
+        }
+        return new GlobPathMapping(prefix, prefixGlob, numGroupsToSkip);
     }
 
     @Nullable
@@ -79,7 +93,7 @@ final class GlobPathMapping extends AbstractPathMapping {
         }
 
         final RoutingResultBuilder builder = RoutingResult.builderWithExpectedNumParams(numParams)
-                                                          .path(routingCtx.path())
+                                                          .path(mappedPath(prefix, routingCtx.path()))
                                                           .query(routingCtx.query());
         for (int i = 1; i <= numParams; i++) {
             final String value = firstNonNull(m.group(i), "");

core/src/main/java/com/linecorp/armeria/server/ParameterizedPathMapping.java
@@ -54,6 +54,7 @@ final class ParameterizedPathMapping extends AbstractPathMapping {
 
     private static final Splitter PATH_SPLITTER = Splitter.on('/');
 
+    private final String prefix;
     /**
      * The original path pattern specified in the constructor.
      */
@@ -99,6 +100,20 @@ final class ParameterizedPathMapping extends AbstractPathMapping {
      * @throws IllegalArgumentException if the {@code pathPattern} is invalid.
      */
     ParameterizedPathMapping(String pathPattern) {
+        this("", pathPattern);
+    }
+
+    /**
+     * Create a {@link ParameterizedPathMapping} instance from given {@code pathPattern}.
+     *
+     * @param pathPattern the {@link String} that contains path params.
+     *                    e.g. {@code /users/{name}}, {@code /users/:name}, {@code /users/{*name}} or
+     *                    {@code /users/:*name}
+     *
+     * @throws IllegalArgumentException if the {@code pathPattern} is invalid.
+     */
+    private ParameterizedPathMapping(String prefix, String pathPattern) {
+        this.prefix = prefix;
         requireNonNull(pathPattern, "pathPattern");
 
         if (!pathPattern.startsWith("/")) {
@@ -219,7 +234,7 @@ final class ParameterizedPathMapping extends AbstractPathMapping {
 
     @Override
     PathMapping doWithPrefix(String prefix) {
-        return new ParameterizedPathMapping(concatPaths(prefix, pathPattern));
+        return new ParameterizedPathMapping(prefix, concatPaths(prefix, pathPattern));
     }
 
     @Override
@@ -251,7 +266,7 @@ final class ParameterizedPathMapping extends AbstractPathMapping {
         }
 
         final RoutingResultBuilder builder = RoutingResult.builderWithExpectedNumParams(paramNameArray.length)
-                                                          .path(routingCtx.path())
+                                                          .path(mappedPath(prefix, routingCtx.path()))
                                                           .query(routingCtx.query());
 
         for (int i = 0; i < paramNameArray.length; i++) {

core/src/main/java/com/linecorp/armeria/server/RegexPathMappingWithPrefix.java
@@ -66,14 +66,7 @@ final class RegexPathMappingWithPrefix extends AbstractPathMapping {
             return null;
         }
 
-        final RoutingResultBuilder builder =
-                mapping.apply(routingCtx.overridePath(path.substring(pathPrefix.length() - 1)));
-        if (builder != null) {
-            // Replace the path.
-            builder.path(path);
-        }
-
-        return builder;
+        return mapping.apply(routingCtx.overridePath(path.substring(pathPrefix.length() - 1)));
     }
 
     @Override

core/src/main/java/com/linecorp/armeria/server/RouteBuilder.java
@@ -140,7 +140,7 @@ public final class RouteBuilder {
         return pathMapping(new RegexPathMappingWithPrefix(prefix, getPathMapping(pathPattern)));
     }
 
-    static PathMapping globPathMapping(String prefix, String glob, int numGroupsToSkip) {
+    private static PathMapping globPathMapping(String prefix, String glob, int numGroupsToSkip) {
         if (glob.startsWith("/")) {
             return globPathMapping(concatPaths(prefix, glob), numGroupsToSkip);
         }

grpc/src/main/java/com/linecorp/armeria/server/grpc/GrpcDecoratingService.java
@@ -95,12 +95,17 @@ final class GrpcDecoratingService extends SimpleDecoratingHttpService implements
     }
 
     @Override
+    public Map<Route, ServerMethodDefinition<?, ?>> methodsByRoute() {
+        return delegate.methodsByRoute();
+    }
+
+    @Override
     public Set<SerializationFormat> supportedSerializationFormats() {
         return delegate.supportedSerializationFormats();
     }
 
     @Nullable
-    HttpService lookup(ServiceRequestContext ctx) {
+    private HttpService lookup(ServiceRequestContext ctx) {
         final ServerMethodDefinition<?, ?> method = lookupMethodFromAttribute ? ctx.attr(RESOLVED_GRPC_METHOD)
                                                                               : null;
         if (method == null || method.getMethodDescriptor() == null) {

core/src/test/java/com/linecorp/armeria/server/MappedPathTest.java
@@ -1,0 +1,99 @@
+/*
+ *  Copyright 2022 LINE Corporation
+ *
+ *  LINE Corporation licenses this file to you under the Apache License,
+ *  version 2.0 (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at:
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package com.linecorp.armeria.server;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import com.google.common.collect.ImmutableSet;
+
+import com.linecorp.armeria.client.BlockingWebClient;
+import com.linecorp.armeria.common.HttpRequest;
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
+
+class MappedPathTest {
+    @RegisterExtension
+    static ServerExtension server = new ServerExtension() {
+        @SuppressWarnings("OverlyStrongTypeCast")
+        @Override
+        protected void configure(ServerBuilder sb) {
+            sb.serviceUnder("/exact",
+                            (SimpleHttpServiceWithRoute) () -> Route.builder()
+                                                                    .path("/users/0000")
+                                                                    .build());
+
+            sb.serviceUnder("/regex",
+                            (SimpleHttpServiceWithRoute) () -> Route.builder()
+                                                                    .regex("^/users/(?<userId>[0-9]+)$")
+                                                                    .build());
+            sb.serviceUnder("/glob",
+                            (SimpleHttpServiceWithRoute) () -> Route.builder()
+                                                                    .glob("/users/**/profile")
+                                                                    .build());
+
+            sb.serviceUnder("/parameterized",
+                            (SimpleHttpServiceWithRoute) () -> Route.builder()
+                                                                    .path("/users/{userId}")
+                                                                    .build());
+
+            sb.serviceUnder("/prefix1",
+                            (SimpleHttpServiceWithRoute) () -> Route.builder()
+                                                                    .pathPrefix("/prefix2")
+                                                                    .build());
+
+            sb.serviceUnder("/catch-all",
+                            (SimpleHttpServiceWithRoute) Route::ofCatchAll);
+
+        }
+    };
+
+    @CsvSource({
+            "/exact/users/0000,         /users/0000",
+            "/regex/users/0001,         /users/0001",
+            "/glob/users/0002/profile,  /users/0002/profile",
+            "/parameterized/users/0003, /users/0003",
+            "/prefix1/prefix2/0004,        /0004",
+            "/catch-all/a/n/y,          /a/n/y",
+    })
+    @ParameterizedTest
+    void mappedPath(String path, String mappedPath) {
+        final BlockingWebClient client = server.blockingWebClient();
+        assertThat(client.get(path).contentUtf8()).isEqualTo(mappedPath);
+    }
+
+    @FunctionalInterface
+    private interface SimpleHttpServiceWithRoute extends HttpServiceWithRoutes {
+
+        Route route();
+
+        @Override
+        default Set<Route> routes() {
+            return ImmutableSet.of(route());
+        }
+
+        @Override
+        default HttpResponse serve(ServiceRequestContext ctx, HttpRequest req) throws Exception {
+            return HttpResponse.of(ctx.mappedPath());
+        }
+    }
+}

grpc/src/test/java/com/linecorp/armeria/it/grpc/GrpcDecoratingServiceTest.java
@@ -18,6 +18,9 @@ package com.linecorp.armeria.it.grpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.concurrent.LinkedBlockingDeque;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -56,23 +59,36 @@ class GrpcDecoratingServiceTest {
         }
     };
 
-    private static String FIRST_TEST_RESULT = "";
-    private static String SECOND_TEST_RESULT = "";
+    private static final LinkedBlockingDeque<String> decorators = new LinkedBlockingDeque<>();
+
+    @BeforeEach
+    void setUp() {
+        decorators.clear();
+    }
 
     @Test
     void methodDecorators() {
         final TestServiceBlockingStub client =
                 GrpcClients.newClient(server.httpUri(), TestServiceBlockingStub.class);
         client.unaryCall(SimpleRequest.getDefaultInstance());
-        assertThat(FIRST_TEST_RESULT)
-                .isEqualTo("FirstDecorator/MethodFirstDecorator");
+        final String first = decorators.poll();
+        final String second = decorators.poll();
+        assertThat(first).isEqualTo("FirstDecorator");
+        assertThat(second).isEqualTo("MethodFirstDecorator");
+        assertThat(decorators).isEmpty();
+    }
 
-        final TestServiceBlockingStub prefixClient = GrpcClients
-                .builder(server.httpUri())
-                .pathPrefix("/grpc")
-                .build(TestServiceBlockingStub.class);
+    @Test
+    void methodDecoratorsWithPrefix() {
+        final TestServiceBlockingStub prefixClient = GrpcClients.builder(server.httpUri())
+                                                                .responseTimeoutMillis(0)
+                                                                .pathPrefix("/grpc")
+                                                                .build(TestServiceBlockingStub.class);
         prefixClient.unaryCall(SimpleRequest.getDefaultInstance());
-        assertThat(SECOND_TEST_RESULT).isEqualTo("SecondDecorator/MethodSecondDecorator");
+        final String first = decorators.poll();
+        final String second = decorators.poll();
+        assertThat(first).isEqualTo("SecondDecorator");
+        assertThat(second).isEqualTo("MethodSecondDecorator");
     }
 
     private static class FirstDecorator implements DecoratingHttpServiceFunction {
@@ -80,7 +96,7 @@ class GrpcDecoratingServiceTest {
         public HttpResponse serve(HttpService delegate,
                                   ServiceRequestContext ctx,
                                   HttpRequest req) throws Exception {
-            FIRST_TEST_RESULT += "FirstDecorator/";
+            decorators.offer("FirstDecorator");
             return delegate.serve(ctx, req);
         }
     }
@@ -90,7 +106,7 @@ class GrpcDecoratingServiceTest {
         public HttpResponse serve(HttpService delegate,
                                   ServiceRequestContext ctx,
                                   HttpRequest req) throws Exception {
-            FIRST_TEST_RESULT += "MethodFirstDecorator";
+            decorators.offer("MethodFirstDecorator");
             return delegate.serve(ctx, req);
         }
     }
@@ -100,7 +116,7 @@ class GrpcDecoratingServiceTest {
         public HttpResponse serve(HttpService delegate,
                                   ServiceRequestContext ctx,
                                   HttpRequest req) throws Exception {
-            SECOND_TEST_RESULT += "SecondDecorator/";
+            decorators.offer("SecondDecorator");
             return delegate.serve(ctx, req);
         }
     }
@@ -110,7 +126,7 @@ class GrpcDecoratingServiceTest {
         public HttpResponse serve(HttpService delegate,
                                   ServiceRequestContext ctx,
                                   HttpRequest req) throws Exception {
-            SECOND_TEST_RESULT += "MethodSecondDecorator";
+            decorators.offer("MethodSecondDecorator");
             return delegate.serve(ctx, req);
         }
     }