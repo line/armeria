ba26d5838f7c273c60c60e665039dce434c30497
Gyuil Han <cnabro91@gmail.com>
2022-06-19 11:32:45 +0900

    Show path mappings in Services

--- docs-client/src/containers/App/index.tsx
+++ docs-client/src/containers/App/index.tsx
@@ -224,7 +224,7 @@ const AppDrawer: React.FunctionComponent<AppDrawerProps> = ({
                         )
                       }
                     >
-                      <Grid container alignItems="center" spacing={2}>
+                      <Grid container alignItems="center" spacing={1}>
                         <Grid item xs="auto">
                           <Typography
                             className={httpMethodClass(method.httpMethod)}
@@ -240,6 +240,15 @@ const AppDrawer: React.FunctionComponent<AppDrawerProps> = ({
                           >
                             <code>{`${method.name}()`}</code>
                           </ListItemText>
+                          {method.endpoints.map((endpoint) => (
+                            <ListItemText
+                              primaryTypographyProps={{
+                                variant: 'caption',
+                              }}
+                            >
+                              {endpoint.pathMapping}
+                            </ListItemText>
+                          ))}
                         </Grid>
                       </Grid>
                     </ListItem>
