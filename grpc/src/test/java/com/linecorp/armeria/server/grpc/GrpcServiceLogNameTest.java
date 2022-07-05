97e33d6081b8972f253c282a48d226fc889e220d
Ikhun Um <ih.pert@gmail.com>
2022-07-05 21:04:22 +0900

    Remove unused imports

--- grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcServiceLogNameTest.java
+++ grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcServiceLogNameTest.java
@@ -25,7 +25,6 @@ import java.util.concurrent.Executors;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.mockito.ArgumentCaptor;
