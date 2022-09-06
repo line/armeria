b4f813531bf416887d2ff77c25c123bb3aaeaca8
Ikhun <ih.pert@gmail.com>
2022-09-06 15:41:58 +0900

    Revert Logback

--- logback/src/main/java/com/linecorp/armeria/common/logback/RequestContextExportingAppender.java
+++ logback/src/main/java/com/linecorp/armeria/common/logback/RequestContextExportingAppender.java
@@ -20,13 +20,11 @@ import static com.google.common.base.Preconditions.checkState;
 import static java.util.Objects.requireNonNull;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
 import org.slf4j.MDC;
 import org.slf4j.Marker;
-import org.slf4j.event.KeyValuePair;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Splitter;
@@ -329,21 +327,6 @@ public final class RequestContextExportingAppender
         }
 
         @Override
-        public int getNanoseconds() {
-            return event.getNanoseconds();
-        }
-
-        @Override
-        public long getSequenceNumber() {
-            return event.getSequenceNumber();
-        }
-
-        @Override
-        public List<KeyValuePair> getKeyValuePairs() {
-            return event.getKeyValuePairs();
-        }
-
-        @Override
         public StackTraceElement[] getCallerData() {
             return event.getCallerData();
         }
@@ -356,11 +339,6 @@ public final class RequestContextExportingAppender
         @Override
         public Marker getMarker() {
             return event.getMarker();
-        }
-
-        @Override
-        public List<Marker> getMarkerList() {
-            return event.getMarkerList();
         }
 
         @Override
