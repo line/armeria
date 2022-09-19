21795fadc59990b5aebc045cd8d62ad4879838fb
Bogyeong Kim <boggyhint@gmail.com>
2022-09-19 12:07:07 +0900

    Update graphql/src/main/java/com/linecorp/armeria/server/graphql/GraphqlErrorsHandler.java
    Co-authored-by: minux <songmw725@gmail.com>

--- graphql/src/main/java/com/linecorp/armeria/server/graphql/GraphqlErrorsHandler.java
+++ graphql/src/main/java/com/linecorp/armeria/server/graphql/GraphqlErrorsHandler.java
@@ -27,7 +27,7 @@ import graphql.ExecutionResult;
 import graphql.GraphQLError;
 
 /**
- * Error handler which map a GraphQL errors to an {@link HttpResponse}.
+ * A handler that maps GraphQL errors to an {@link HttpResponse}.
  */
 @FunctionalInterface
 public interface GraphqlErrorsHandler {
