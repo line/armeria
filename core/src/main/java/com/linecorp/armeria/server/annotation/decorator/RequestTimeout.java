e47426df004f8c052f2f7f0c1b43a6a70cfb002b
Minsoo Cheong <54794500+j-min5u@users.noreply.github.com>
2022-11-23 01:27:57 +0900

    fix checkstyle violations

--- core/src/main/java/com/linecorp/armeria/server/annotation/decorator/RequestTimeout.java
+++ core/src/main/java/com/linecorp/armeria/server/annotation/decorator/RequestTimeout.java
@@ -21,7 +21,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
-import com.linecorp.armeria.common.util.TimeoutMode;
 import com.linecorp.armeria.server.annotation.DecoratorFactory;
 
 /**
--- core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.linecorp.armeria.internal.server.annotation;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,7 +29,6 @@ import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.RequestHeaders;
-import com.linecorp.armeria.common.util.TimeoutMode;
 import com.linecorp.armeria.internal.server.DefaultServiceRequestContext;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.ServiceRequestContext;
@@ -28,7 +43,6 @@ public class RequestTimeoutAnnotationTest {
         @Override
         protected void configure(ServerBuilder sb) throws Exception {
             sb.annotatedService("/myService", new MyAnnotationService());
-
         }
     };
 
@@ -77,9 +91,9 @@ public class RequestTimeoutAnnotationTest {
     public void testCancellationSchedulerInit() {
         final BlockingWebClient client = BlockingWebClient.of(server.httpUri());
 
-        final AggregatedHttpResponse response = client.execute(RequestHeaders.of(HttpMethod.GET, "/myService/timeoutMillis"));
+        final AggregatedHttpResponse response = client.execute(
+                RequestHeaders.of(HttpMethod.GET, "/myService/timeoutMillis"));
         assertThat(response.status()).isEqualTo(HttpStatus.OK);
         assertThat(Boolean.parseBoolean(response.contentUtf8())).isEqualTo(true);
     }
-
 }
