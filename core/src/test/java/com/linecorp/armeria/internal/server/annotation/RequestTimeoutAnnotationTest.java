d8c66ca4f29f4e9e2a2ebfb7b84de6a89d2dbe2d
Minsoo Cheong <54794500+j-min5u@users.noreply.github.com>
2022-11-23 02:01:06 +0900

    remove unneeded line

--- core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
 package com.linecorp.armeria.internal.server.annotation;
 
 import static org.assertj.core.api.Assertions.assertThat;
