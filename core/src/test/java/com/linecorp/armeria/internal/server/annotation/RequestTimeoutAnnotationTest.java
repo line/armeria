5f0f0e9de512c53c5841080ef40254985517dcbf
Minsoo Cheong <54794500+j-min5u@users.noreply.github.com>
2022-12-06 16:40:55 +0900

    remove unused import

--- core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/server/annotation/RequestTimeoutAnnotationTest.java
@@ -28,7 +28,6 @@ import com.linecorp.armeria.common.HttpMethod;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.RequestHeaders;
-import com.linecorp.armeria.internal.server.DefaultServiceRequestContext;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.annotation.Get;
