39d73bab4c774a4929ab25840caf73346768f85c
jrhee17 <guins_j@guins.org>
2022-12-23 17:32:10 +0900

    use resilience4j 2

--- examples/resilience4j-spring/src/test/java/example/armeria/resilience4j/spring/CircuitBreakerTest.java
+++ examples/resilience4j-spring/src/test/java/example/armeria/resilience4j/spring/CircuitBreakerTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.entry;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
@@ -16,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.client.circuitbreaker.CircuitBreakerClient;
 import com.linecorp.armeria.client.circuitbreaker.CircuitBreakerRule;
-import com.linecorp.armeria.client.retrofit2.ArmeriaRetrofit;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.metric.MoreMeters;
 import com.linecorp.armeria.resilience4j.circuitbreaker.client.Resilience4JCircuitBreakerClientHandler;
@@ -26,10 +23,7 @@ import com.linecorp.armeria.server.Server;
 import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.retrofit.CircuitBreakerCallAdapter;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import retrofit2.HttpException;
-import retrofit2.http.GET;
 
 @SpringBootTest(classes = Main.class)
 class CircuitBreakerTest {
@@ -87,36 +81,6 @@ class CircuitBreakerTest {
         }
         assertThatThrownBy(() -> client.blocking().get("500-2")).isInstanceOf(CallNotPermittedException.class);
     }
-
-    interface Service {
-
-        @GET("/500-1")
-        CompletableFuture<Void> serverError1();
-    }
-
-    @Test
-    void testRetrofitClient() {
-        final WebClient client = WebClient.of("http://localhost:" + server.activeLocalPort());
-        final CircuitBreaker r4jCircuitBreaker =
-                circuitBreakerRegistry.circuitBreaker("retrofit", "defaultA");
-        final Service service = ArmeriaRetrofit
-                .builder(client)
-                .addCallAdapterFactory(CircuitBreakerCallAdapter.of(r4jCircuitBreaker))
-                .build()
-                .create(Service.class);
-
-        final int windowSize = circuitBreakerRegistry.getConfiguration("defaultA")
-                                                     .orElseThrow().getSlidingWindowSize();
-        for (int i = 0; i < windowSize; i++) {
-            assertThatThrownBy(() -> service.serverError1().join())
-                    .isInstanceOf(CompletionException.class)
-                    .hasRootCauseInstanceOf(HttpException.class);
-        }
-        assertThatThrownBy(() -> service.serverError1().join())
-                .isInstanceOf(CompletionException.class)
-                .hasRootCauseInstanceOf(CallNotPermittedException.class);
-    }
-
     @Test
     void testMicrometerIntegration() {
         final CircuitBreaker r4jCircuitBreaker =
