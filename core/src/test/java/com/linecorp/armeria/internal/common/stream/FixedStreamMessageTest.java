7e247ee75356a19c732a8ec22317300d458eb4b7
Ikhun Um <ih.pert@gmail.com>
2023-02-06 18:12:23 +0900

    Fix a `NullPointerException` raised while an aborted `FixedStreamMessage` is being collected.
    Motivation:
    
    We got a report about `NullPointerException` from LINE internally.
    ```
    java.lang.NullPointerException: at index 0
    	at com.linecorp.armeria.internal.shaded.guava.collect.ObjectArrays.checkElementNotNull(ObjectArrays.java:229)
    	at com.linecorp.armeria.internal.shaded.guava.collect.ObjectArrays.checkElementsNotNull(ObjectArrays.java:219)
    	at com.linecorp.armeria.internal.shaded.guava.collect.ObjectArrays.checkElementsNotNull(ObjectArrays.java:213)
    	at com.linecorp.armeria.internal.shaded.guava.collect.ImmutableList.construct(ImmutableList.java:353)
    	at com.linecorp.armeria.internal.shaded.guava.collect.ImmutableList.of(ImmutableList.java:109)
    	at com.linecorp.armeria.internal.common.stream.TwoElementFixedStreamMessage.drainAll(TwoElementFixedStreamMessage.java:75)
    	at com.linecorp.armeria.internal.common.stream.FixedStreamMessage.collect(FixedStreamMessage.java:201)
    	at com.linecorp.armeria.internal.common.stream.FixedStreamMessage.lambda$collect$2(FixedStreamMessage.java:188)
    	at io.netty.util.concurrent.AbstractEventExecutor.runTask(AbstractEventExecutor.java:174)
    	at io.netty.util.concurrent.AbstractEventExecutor.safeExecute(AbstractEventExecutor.java:167)
    	at io.netty.util.concurrent.SingleThreadEventExecutor.runAllTasks(SingleThreadEventExecutor.java:470)
    	at io.netty.channel.epoll.EpollEventLoop.run(EpollEventLoop.java:391)
    	at io.netty.util.concurrent.SingleThreadEventExecutor$4.run(SingleThreadEventExecutor.java:997)
    	at io.netty.util.internal.ThreadExecutorMap$2.run(ThreadExecutorMap.java:74)
    	at io.netty.util.concurrent.FastThreadLocalRunnable.run(FastThreadLocalRunnable.java:30)
    	at java.lang.Thread.run(Thread.java:748)
    ```
    
    The error occured because the elements of a `StreamMessage` are null
    when a `StreamMessage.collect()` is called. Double subscriptions aren't
    the cause of the problem since it is atomiclaly checked by CAS
    operations.
    
    One possible senario is `abort()` is called first and `collect()` is
    called right after. The two method calls are not thread safe because the
    `abort()` called first can't get a executor which is assigned when
    `subscribe()` is called. As a result, a direct executor is used and then
    two threads may access the elements simultaneously.
    
    Modifications:
    
    - Remove `subscribedUpdater` and use `executorUpdater` as a latch for a
      concurrent execution.
    - Make `abort()` executed directly only when explicitly there is no
      subscription.
      - If a executor has already been assigned by another thread, the
        executor is used to avoid the race condition.
    
    Result:
    
    You no longer see `NullPointerException` when an aborted `StreamMessage`
    is being collected.

--- core/src/test/java/com/linecorp/armeria/internal/common/stream/FixedStreamMessageTest.java
+++ core/src/test/java/com/linecorp/armeria/internal/common/stream/FixedStreamMessageTest.java
@@ -17,13 +17,18 @@
 package com.linecorp.armeria.internal.common.stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.awaitility.Awaitility.await;
 
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
@@ -31,9 +36,9 @@ import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
-import com.google.common.collect.ImmutableList;
-
 import com.linecorp.armeria.common.stream.StreamMessage;
+import com.linecorp.armeria.internal.testing.AnticipatedException;
+import com.linecorp.armeria.testing.junit5.common.EventLoopExtension;
 
 import io.netty.util.concurrent.ImmediateEventExecutor;
 
@@ -41,14 +46,15 @@ class FixedStreamMessageTest {
 
     private static final Integer[] EMPTY_INTEGERS = new Integer[0];
 
-    @ArgumentsSource(IntsProvider.class)
+    @RegisterExtension
+    static EventLoopExtension eventLoop = new EventLoopExtension();
+
+    @ArgumentsSource(FixedStreamMessageProvider.class)
     @ParameterizedTest
-    void spec_306_requestAfterCancel(List<Integer> nums) throws InterruptedException {
-        final Integer[] array = nums.toArray(EMPTY_INTEGERS);
-        final StreamMessage<Integer> message = StreamMessage.of(array);
+    void spec_306_requestAfterCancel(StreamMessage<Integer> stream) throws InterruptedException {
         final CompletableFuture<Subscription> subscriptionFuture = new CompletableFuture<>();
         final AtomicInteger received = new AtomicInteger();
-        message.subscribe(new Subscriber<Integer>() {
+        stream.subscribe(new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 subscriptionFuture.complete(s);
@@ -74,15 +80,69 @@ class FixedStreamMessageTest {
         assertThat(received).hasValue(0);
     }
 
-    private static final class IntsProvider implements ArgumentsProvider {
+    @ArgumentsSource(FixedStreamMessageProvider.class)
+    @ParameterizedTest
+    void raceBetweenSubscriptionAndAbort(StreamMessage<Integer> stream) {
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicReference<Throwable> causeRef = new AtomicReference<>();
+        final AtomicInteger delivered = new AtomicInteger();
+        stream.subscribe(new Subscriber<Integer>() {
+
+            @Override
+            public void onSubscribe(Subscription s) {
+                try {
+                    // Wait for `abort()` to be called.
+                    latch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onNext(Integer integer) {}
+
+            @Override
+            public void onError(Throwable t) {
+                causeRef.set(t);
+            }
+
+            @Override
+            public void onComplete() {}
+        }, eventLoop.get());
+
+        final AnticipatedException abortCause = new AnticipatedException("Abort a fixed stream");
+        stream.abort(abortCause);
+        latch.countDown();
+
+        // EmptyStreamMessage performs nothing.
+        if (!stream.isEmpty()) {
+            await().untilAsserted(() -> {
+                assertThat(causeRef).hasValue(abortCause);
+            });
+            assertThatThrownBy(() -> stream.whenComplete().join())
+                    .isInstanceOf(CompletionException.class)
+                    .hasCause(abortCause);
+        }
+        assertThat(delivered).hasValue(0);
+    }
+
+    private static final class FixedStreamMessageProvider implements ArgumentsProvider {
 
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
-            return Stream.of(ImmutableList.of(),
-                             ImmutableList.of(1),
-                             ImmutableList.of(1, 2),
-                             ImmutableList.of(1, 2, 3),
-                             ImmutableList.of(1, 2, 3, 4))
+            final AggregatingStreamMessage<Integer> aggregatingStreamMessage =
+                    new AggregatingStreamMessage<>(5);
+            aggregatingStreamMessage.write(1);
+            aggregatingStreamMessage.write(2);
+            aggregatingStreamMessage.write(3);
+            aggregatingStreamMessage.write(4);
+            aggregatingStreamMessage.close();
+            return Stream.of(StreamMessage.of(),           // EmptyFixedStreamMessage
+                             StreamMessage.of(1),          // OneElementFixedStreamMessage
+                             StreamMessage.of(1, 2),       // TwoElementFixedStreamMessage
+                             StreamMessage.of(1, 2, 3),    // ThreeElementFixedStreamMessage
+                             StreamMessage.of(1, 2, 3, 4), // RegularFixedStreamMessage
+                             aggregatingStreamMessage)
                          .map(Arguments::of);
         }
     }
