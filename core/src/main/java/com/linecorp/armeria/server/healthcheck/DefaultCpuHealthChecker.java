08c706ef800c2d4ab4483352dce64b229ab23492
Bue-von-hon <dkssudvn2@gmail.com>
2023-03-01 00:19:46 +0900

    Fixed javadoc and lint based on code review
    Modifications:
    
    - Fixed DefaultCpuHealthChecker
    added copyright header, added javadocs,
    Change health check from range-based to ratio-based,
    added nullable annotations,
    added requireNonNull method for nullable method calls

--- core/src/main/java/com/linecorp/armeria/server/healthcheck/DefaultCpuHealthChecker.java
+++ core/src/main/java/com/linecorp/armeria/server/healthcheck/DefaultCpuHealthChecker.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.linecorp.armeria.server.healthcheck;
 
 import static java.util.Objects.requireNonNull;
@@ -9,7 +24,12 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
-public class DefaultCpuHealthChecker implements HealthChecker{
+import com.linecorp.armeria.common.annotation.Nullable;
+
+/**
+ * Forked from <a href="https://github.com/micrometer-metrics/micrometer/blob/8339d57bef8689beb8d7a18b429a166f6595f2af/micrometer-core/src/main/java/io/micrometer/core/instrument/binder/system/ProcessorMetrics.java">ProcessorMetrics.java in the micrometer core</a>
+ */
+public class DefaultCpuHealthChecker implements HealthChecker {
 
     private static final List<String> OPERATING_SYSTEM_BEAN_CLASS_NAMES = Arrays.asList(
             "com.ibm.lang.management.OperatingSystemMXBean", // J9
@@ -24,49 +44,62 @@ public class DefaultCpuHealthChecker implements HealthChecker{
 
     private final double targetCpuUsage;
 
-    public DefaultCpuHealthChecker(final double targetCpuUsage) {
-        this.targetCpuUsage = targetCpuUsage;
+    private final Method processCpuUsage;
+
+    private final double targetProcessCpuLoad;
+
+    /**
+     * Instantiates a new Default cpu health checker.
+     *
+     * @param cpuUsage the cpu usage
+     * @param cpuIdle the cpu idle
+     * @param processCpuUsage the process cpu usage
+     * @param processCpuIdle the process cpu idle
+     */
+    public DefaultCpuHealthChecker(final int cpuUsage, final int cpuIdle,
+                                   final int processCpuUsage, final int processCpuIdle) {
+        this.targetCpuUsage = (double) cpuUsage / cpuIdle;
+        this.targetProcessCpuLoad = (double) processCpuUsage / processCpuIdle;
         this.operatingSystemBean = ManagementFactory.getOperatingSystemMXBean();
-        this.operatingSystemBeanClass = getFirstClassFound(OPERATING_SYSTEM_BEAN_CLASS_NAMES);
-        this.systemCpuUsage = detectMethod("getSystemCpuLoad");
+        this.operatingSystemBeanClass = requireNonNull(getFirstClassFound(OPERATING_SYSTEM_BEAN_CLASS_NAMES));
+        this.systemCpuUsage = requireNonNull(detectMethod("getSystemCpuLoad"));
+        this.processCpuUsage = requireNonNull(detectMethod("getProcessCpuLoad"));
     }
 
     @Override
     public boolean isHealthy() {
-        double cpuUsage = invoke(systemCpuUsage);
-        return cpuUsage <= targetCpuUsage;
+        final double currentSystemCpuUsage = invoke(systemCpuUsage);
+        final double currentProcessCpuUsage = invoke(processCpuUsage);
+        return currentSystemCpuUsage <= targetCpuUsage && currentProcessCpuUsage <= targetProcessCpuLoad;
     }
 
     private double invoke(final Method method) {
         try {
             return (double) method.invoke(operatingSystemBean);
-        }
-        catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
             return Double.NaN;
         }
     }
 
+    @Nullable
     private Method detectMethod(final String name) {
         requireNonNull(name);
-        if (operatingSystemBeanClass == null) {
-            return null;
-        }
         try {
             // ensure the Bean we have is actually an instance of the interface
             requireNonNull(operatingSystemBeanClass.cast(operatingSystemBean));
             return operatingSystemBeanClass.getMethod(name);
-        }
-        catch (ClassCastException | NoSuchMethodException | SecurityException e) {
+        } catch (ClassCastException | NoSuchMethodException | SecurityException e) {
             return null;
         }
     }
 
+    @Nullable
     private Class<?> getFirstClassFound(final List<String> classNames) {
         for (String className : classNames) {
             try {
                 return Class.forName(className);
-            }
-            catch (ClassNotFoundException ignore) {
+            } catch (ClassNotFoundException ignore) {
+
             }
         }
         return null;
