573d15cc0b9a07d9673e94943d7bb10c73d9466e
Seokjae Lee <seokjae.lee@linecorp.com>
2023-03-06 17:31:38 +0900

    Add CoroutineServerInterceptor

--- kotlin-grpc/build.gradle.kts
+++ kotlin-grpc/build.gradle.kts
@@ -1,3 +1,10 @@
 dependencies {
     implementation(project(":kotlin"))
+    implementation(project(":grpc"))
+
+    // kotlin
+    implementation(libs.kotlin.coroutines.jdk8)
+    implementation(libs.kotlin.reflect)
+
+    testImplementation(libs.reactivestreams.tck)
 }
---
+++ kotlin-grpc/src/main/kotlin/CoroutineServerInterceptor.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import com.linecorp.armeria.server.grpc.AsyncServerInterceptor
+import io.grpc.Metadata
+import io.grpc.ServerCall
+import io.grpc.ServerCallHandler
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.future.future
+import java.util.concurrent.CompletableFuture
+
+/**
+ * A [ServerInterceptor][io.grpc.ServerInterceptor] that is able to asynchronously execute the interceptor without blocking the
+ * caller thread.
+ * For example:
+ * ```kotlin
+ * class AuthServerInterceptor : CoroutineServerInterceptor {
+ *     override suspend fun <ReqT, RespT> suspendedInterceptCall(
+ *             call: ServerCall<ReqT, RespT>,
+ *             headers: Metadata,
+ *             next: ServerCallHandler<ReqT, RespT>): ServerCall.Listener<ReqT> {
+ *         return authorizer.authorize(headers).thenApply { result ->
+ *             if (result) {
+ *                 return next.startCall(call, headers)
+ *             } else {
+ *                 throw AuthenticationException("Invalid access")
+ *             }
+ *         }
+ *     }
+ * }
+ * ```
+ */
+interface CoroutineServerInterceptor : AsyncServerInterceptor {
+
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun <I : Any, O : Any> asyncInterceptCall(
+        call: ServerCall<I, O>,
+        headers: Metadata,
+        next: ServerCallHandler<I, O>
+    ): CompletableFuture<ServerCall.Listener<I>> {
+        return GlobalScope.future {
+            suspendedInterceptCall(call, headers, next)
+        }
+    }
+
+    suspend fun <ReqT, RespT> suspendedInterceptCall(
+        call: ServerCall<ReqT, RespT>,
+        headers: Metadata,
+        next: ServerCallHandler<ReqT, RespT>
+    ): ServerCall.Listener<ReqT>
+}
