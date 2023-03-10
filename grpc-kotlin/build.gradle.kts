20eaf41893b6d39316a0667cd9617cd3cda4dfa9
Seokjae Lee <seokjae.lee@linecorp.com>
2023-03-10 14:27:49 +0900

    Add grpc-kotlin dependency

--- grpc-kotlin/build.gradle.kts
+++ grpc-kotlin/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":kotlin"))
     implementation(project(":grpc"))
 
+    implementation(libs.grpc.kotlin)
     implementation(libs.kotlin.coroutines.jdk8)
 
     testImplementation(libs.kotlin.coroutines.test)
