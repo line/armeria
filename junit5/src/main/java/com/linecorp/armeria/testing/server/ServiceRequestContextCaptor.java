de36d9edd35768f31a154bcbedbab95f27fa03a2
Valentin Mitrofanov <mitrofmep@gmail.com>
2023-03-21 22:14:47 +0800

    Add ServiceRequestContextCaptor.isEmpty()

--- junit5/src/main/java/com/linecorp/armeria/testing/server/ServiceRequestContextCaptor.java
+++ junit5/src/main/java/com/linecorp/armeria/testing/server/ServiceRequestContextCaptor.java
@@ -172,4 +172,11 @@ public final class ServiceRequestContextCaptor {
     public ServiceRequestContext poll(long timeout, TimeUnit unit) throws InterruptedException {
         return serviceContexts.poll(timeout, unit);
     }
+
+    /**
+     * Returns whether the {@link #size()} of this data is 0.
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
 }
