8d258595bb9573695da01db8729a8951e72eaff0
Ikhun Um <ih.pert@gmail.com>
2023-03-26 15:28:26 +0900

    Fix test code

--- core/src/test/java/com/linecorp/armeria/server/ServerRequestIdTest.java
+++ core/src/test/java/com/linecorp/armeria/server/ServerRequestIdTest.java
@@ -21,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linecorp.armeria.client.BlockingWebClient;
 import com.linecorp.armeria.client.ClientFactory;
@@ -28,8 +29,26 @@ import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.RequestId;
 import com.linecorp.armeria.internal.testing.MockAddressResolverGroup;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
 
 public class ServerRequestIdTest {
+    @RegisterExtension
+    static ServerExtension server = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) {
+            sb.requestIdGenerator(ctx -> RequestId.of(1L))   // for default
+              .service("/default_virtual_host",
+                       (ctx, req) -> HttpResponse.of(ctx.id().toString()))
+              .virtualHost("foo.com")
+              .requestIdGenerator(ctx -> RequestId.of(2L))  // for virtual host
+              .service("/custom_virtual_host",
+                       (ctx, req) -> HttpResponse.of(ctx.id().toString()))
+              .route()
+              .requestIdGenerator(ctx -> RequestId.of(3L)) // for service
+              .get("/service_config")
+              .build((ctx, req) -> HttpResponse.of(ctx.id().toString()));
+        }
+    };
     private static ClientFactory clientFactory;
 
     @BeforeAll
@@ -46,47 +65,27 @@ public class ServerRequestIdTest {
 
     @Test
     void hierarchyRequestIdGeneratorConfiguration() {
-        final Server server = Server.builder()
-                                    .requestIdGenerator((ctx) -> RequestId.of(1L))   // for default
-                                    .service("/default_virtual_host",
-                                             (ctx, req) -> HttpResponse.of(ctx.id().toString()))
-                                    .virtualHost("foo.com")
-                                    .requestIdGenerator((ctx) -> RequestId.of(2L))  // for virtual host
-                                    .service("/custom_virtual_host",
-                                             (ctx, req) -> HttpResponse.of(ctx.id().toString()))
-                                    .route()
-                                    .requestIdGenerator((ctx) -> RequestId.of(3L)) // for service
-                                    .get("/service_config")
-                                    .build((ctx, req) -> HttpResponse.of(ctx.id().toString()))
-                                    .and()
-                                    .build();
-        server.start().join();
+        final BlockingWebClient client = WebClient.builder(server.httpUri())
+                                                  .factory(clientFactory)
+                                                  .build()
+                                                  .blocking();
+        final BlockingWebClient fooClient = WebClient.builder("http://foo.com:" + server.httpPort())
+                                                     .factory(clientFactory)
+                                                     .build()
+                                                     .blocking();
 
-        try {
-            final BlockingWebClient client = WebClient.builder("http://127.0.0.1:" + server.activeLocalPort())
-                                                      .factory(clientFactory)
-                                                      .build()
-                                                      .blocking();
-            final BlockingWebClient fooClient = WebClient.builder("http://foo.com:" + server.activeLocalPort())
-                                                         .factory(clientFactory)
-                                                         .build()
-                                                         .blocking();
+        // choose from default server config
+        assertThat(client.get("/default_virtual_host").contentUtf8())
+                .isEqualTo(RequestId.of(1L).text());
 
-            // choose from default server config
-            assertThat(client.get("/default_virtual_host").contentUtf8())
-                    .isEqualTo(RequestId.of(1L).text());
-
-            // choose from 'foo.com' virtual host
-            assertThat(fooClient.get("/default_virtual_host").contentUtf8())
-                    .isEqualTo(RequestId.of(2L).text());
-            // choose from 'foo.com' virtual host
-            assertThat(fooClient.get("/custom_virtual_host").contentUtf8())
-                    .isEqualTo(RequestId.of(2L).text());
-            // choose from 'foo.com/service_config' service
-            assertThat(fooClient.get("/service_config").contentUtf8())
-                    .isEqualTo(RequestId.of(3L).text());
-        } finally {
-            server.stop();
-        }
+        // choose from 'foo.com' virtual host
+        assertThat(fooClient.get("/default_virtual_host").contentUtf8())
+                .isEqualTo(RequestId.of(2L).text());
+        // choose from 'foo.com' virtual host
+        assertThat(fooClient.get("/custom_virtual_host").contentUtf8())
+                .isEqualTo(RequestId.of(2L).text());
+        // choose from 'foo.com/service_config' service
+        assertThat(fooClient.get("/service_config").contentUtf8())
+                .isEqualTo(RequestId.of(3L).text());
     }
 }
