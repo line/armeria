c50acfc736f55a6d7a962c79175c5ade149bb996
shimbo <takaaki.shimbo@linecorp.com>
2023-03-29 15:22:17 +0900

    Remove DeferredListener and Executor

--- grpc/src/main/java/com/linecorp/armeria/internal/client/grpc/DeferredClientCall.java
+++ grpc/src/main/java/com/linecorp/armeria/internal/client/grpc/DeferredClientCall.java
@@ -19,53 +19,39 @@ package com.linecorp.armeria.internal.client.grpc;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
 import java.util.concurrent.locks.ReentrantLock;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.util.concurrent.MoreExecutors;
-
-import com.linecorp.armeria.common.CommonPools;
 import com.linecorp.armeria.common.annotation.Nullable;
 import com.linecorp.armeria.common.annotation.UnstableApi;
 
 import io.grpc.ClientCall;
 import io.grpc.Metadata;
-import io.grpc.Status;
 
 @UnstableApi
 public final class DeferredClientCall<I, O> extends ClientCall<I, O> {
-
-    private static final List<Runnable> NOOP_TASKS = ImmutableList.of();
 
     private List<Runnable> pendingTasks = new ArrayList<>();
 
     private final ReentrantLock lock = new ReentrantLock();
 
-    private final Executor executor;
-
     @Nullable
-    private DeferredListner<O> deferredListner;
-
-    @Nullable
-    private volatile ArmeriaClientCall<I, O> delegate;
+    private volatile ClientCall<I, O> delegate;
 
     public DeferredClientCall(CompletableFuture<ClientCall<I, O>> clientFuture) {
-        executor = MoreExecutors.newSequentialExecutor(CommonPools.blockingTaskExecutor());
-        clientFuture.handleAsync((delegate, cause) -> {
+        clientFuture.handle((delegate, cause) -> {
             if (cause != null) {
                 // TODO
                 return null;
             }
             lock.lock();
             try {
-                this.delegate = (ArmeriaClientCall<I, O>) delegate;
+                this.delegate = delegate;
             } finally {
                 lock.unlock();
             }
             drainPendingTasks();
             return null;
-        }, executor);
+        });
     }
 
     @Override
@@ -75,8 +61,7 @@ public final class DeferredClientCall<I, O> extends ClientCall<I, O> {
             if (delegate != null) {
                 delegate.start(responseListener, headers);
             } else {
-                deferredListner = new DeferredListner<>(responseListener);
-                pendingTasks.add(() -> delegate.start(deferredListner, headers));
+                pendingTasks.add(() -> delegate.start(responseListener, headers));
             }
         } finally {
             lock.unlock();
@@ -141,106 +126,27 @@ public final class DeferredClientCall<I, O> extends ClientCall<I, O> {
 
     private void drainPendingTasks() {
         List<Runnable> runnables = new ArrayList<>();
-        DeferredListner<O> deferredListner;
         while (true) {
             lock.lock();
             try {
                 if (pendingTasks.isEmpty()) {
-                    pendingTasks = NOOP_TASKS;
-                    deferredListner = this.deferredListner;
                     break;
                 }
             } finally {
                 lock.unlock();
             }
-            final List<Runnable> tmp = runnables;
-            runnables = pendingTasks;
-            pendingTasks = tmp;
+            lock.lock();
+            try {
+                final List<Runnable> tmp = runnables;
+                runnables = pendingTasks;
+                pendingTasks = tmp;
+            } finally {
+                lock.unlock();
+            }
             for (Runnable runnable : runnables) {
                 runnable.run();
             }
             runnables.clear();
-        }
-        if (deferredListner != null) {
-            executor.execute(delegate.ctx().makeContextAware(deferredListner::drainPendingTasks));
-        }
-    }
-
-    class DeferredListner<O> extends Listener<O> {
-        private final Listener<O> listener;
-        private final ReentrantLock lock = new ReentrantLock();
-        private List<Runnable> pendingTasks = new ArrayList<>();
-        private volatile boolean passThrough;
-
-        DeferredListner(Listener<O> listener) {
-            this.listener = listener;
-        }
-
-        private void deferredOrExecute(Runnable runnable) {
-            lock.lock();
-            try {
-                if (!passThrough) {
-                    pendingTasks.add(runnable);
-                    return;
-                }
-            } finally {
-                lock.unlock();
-            }
-            runnable.run();
-        }
-
-        @Override
-        public void onHeaders(Metadata headers) {
-            if (passThrough) {
-                listener.onHeaders(headers);
-            } else {
-                deferredOrExecute(() -> listener.onHeaders(headers));
-            }
-        }
-
-        @Override
-        public void onMessage(O message) {
-            if (passThrough) {
-                listener.onMessage(message);
-            } else {
-                deferredOrExecute(() -> listener.onMessage(message));
-            }
-        }
-
-        @Override
-        public void onClose(Status status, Metadata trailers) {
-            deferredOrExecute(() -> listener.onClose(status, trailers));
-        }
-
-        @Override
-        public void onReady() {
-            if (passThrough) {
-                listener.onReady();
-            } else {
-                deferredOrExecute(listener::onReady);
-            }
-        }
-
-        void drainPendingTasks() {
-            List<Runnable> runnables = new ArrayList<>();
-            while (true) {
-                lock.lock();
-                try {
-                    if (pendingTasks.isEmpty()) {
-                        passThrough = true;
-                        break;
-                    }
-                    final List<Runnable> tmp = runnables;
-                    runnables = pendingTasks;
-                    pendingTasks = tmp;
-                    for (Runnable runnable : runnables) {
-                        runnable.run();
-                    }
-                    runnables.clear();
-                } finally {
-                    lock.unlock();
-                }
-            }
         }
     }
 }
