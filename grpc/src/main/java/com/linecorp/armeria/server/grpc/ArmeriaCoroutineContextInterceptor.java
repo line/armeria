01fff08dfa9a079b1ab336044fce1c1d63249051
Ikhun Um <ih.pert@gmail.com>
2023-05-09 18:42:35 +0900

    Provide a way to customize `CoroutineContext` for gRPC-Kotlin
    Related issue: #4842
    
    Motivation:
    
    As Armeria gRPC service inejcts an event loop or a blocking task
    executor as the default `CoroutineDispatcher`, users can't override the
    dispatcher with the `CoroutineContext` set to the constructor of a stub.
    
    In addition, we didn't document the default `CoroutineContext` that was
    automatically by an Armeria gRPC service.
    
    Modifications:
    
    - Fixed ArmeriaCoroutineContextInterceptor to load
      avaiable `CoroutineContextProvider`s via SPI
      - The loaded providers are used to override the default
        `CoroutineDispatcher` for gRPC-Kotlin.
    - Created `Kotlin integration` documentation and migratedd the Coroutine
      support part in annotated service
      - Some sections for gRPC-Kotlin are new added such as basic usage of
        gRPC services, gRPC clients and `CoroutineContextProvider`
    
    Result:
    
    - You can now customize a `CoroutineContext` for gRPC-Kotlin with
      `CoroutinContextProvider`.
    
      ```kotlin
      class CustomCoroutineContextProvider : CoroutineContextProvider {
        override fun provide(ctx: ServiceRequestContext): CoroutineContext {
          return ... // A custom CoroutineContext
        }
      }
      ```
      Note that a custom `CoroutineContextProvider` should be registered as a
      Java SPI to `META-INF/services/com.linecorp.armeria.common.kotlin.CoroutineContextProvider`.
    - Closes: #4842

--- grpc/src/main/java/com/linecorp/armeria/server/grpc/ArmeriaCoroutineContextInterceptor.java
+++ grpc/src/main/java/com/linecorp/armeria/server/grpc/ArmeriaCoroutineContextInterceptor.java
@@ -16,8 +16,19 @@
 
 package com.linecorp.armeria.server.grpc;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.List;
+import java.util.ServiceLoader;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
+
+import com.linecorp.armeria.common.annotation.Nullable;
 import com.linecorp.armeria.server.ServiceRequestContext;
 
 import io.grpc.Metadata;
@@ -28,6 +39,32 @@ import kotlinx.coroutines.ExecutorsKt;
 
 final class ArmeriaCoroutineContextInterceptor extends CoroutineContextServerInterceptor {
 
+    private static final Logger logger = LoggerFactory.getLogger(ArmeriaCoroutineContextInterceptor.class);
+
+    private static final List<?> COROUTINE_CONTEXT_PROVIDERS;
+    @Nullable
+    private static final MethodHandle PROVIDE_METHOD;
+
+    static {
+        List<?> providers = ImmutableList.of();
+        MethodHandle provideMethod = null;
+        try {
+            final Class<?> clazz = Class.forName("com.linecorp.armeria.common.kotlin.CoroutineContextProvider");
+            providers = ImmutableList.copyOf(
+                    ServiceLoader.load(clazz, ArmeriaCoroutineContextInterceptor.class.getClassLoader()));
+            final MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
+            final MethodType mt = MethodType.methodType(CoroutineContext.class, ServiceRequestContext.class);
+            provideMethod = publicLookup.findVirtual(clazz, "provide", mt);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException ignored) {
+        }
+
+        if (!providers.isEmpty()) {
+            logger.debug("Available CoroutineContextProviders: {}", providers);
+        }
+        COROUTINE_CONTEXT_PROVIDERS = providers;
+        PROVIDE_METHOD = provideMethod;
+    }
+
     private final boolean useBlockingTaskExecutor;
 
     ArmeriaCoroutineContextInterceptor(boolean useBlockingTaskExecutor) {
@@ -37,13 +74,27 @@ final class ArmeriaCoroutineContextInterceptor extends CoroutineContextServerInt
     @Override
     public CoroutineContext coroutineContext(ServerCall<?, ?> serverCall, Metadata metadata) {
         final ServiceRequestContext ctx = ServiceRequestContext.current();
-        final ArmeriaRequestCoroutineContext coroutineContext = new ArmeriaRequestCoroutineContext(ctx);
-        final ScheduledExecutorService executor;
-        if (useBlockingTaskExecutor) {
-            executor = ctx.blockingTaskExecutor().withoutContext();
+        CoroutineContext coroutineContext = new ArmeriaRequestCoroutineContext(ctx);
+        if (PROVIDE_METHOD != null && !COROUTINE_CONTEXT_PROVIDERS.isEmpty()) {
+            for (Object provider : COROUTINE_CONTEXT_PROVIDERS) {
+                try {
+                    coroutineContext =
+                            coroutineContext.plus((CoroutineContext) PROVIDE_METHOD.invoke(provider, ctx));
+                } catch (Throwable e) {
+                    throw new IllegalStateException("Failed to invoke CoroutineContextProvider#provide()", e);
+                }
+            }
+            return coroutineContext;
         } else {
-            executor = ctx.eventLoop().withoutContext();
+            // No custom context is specified. Use the context aware executor as the default Coroutine
+            // dispatcher.
+            final ScheduledExecutorService executor;
+            if (useBlockingTaskExecutor) {
+                executor = ctx.blockingTaskExecutor().withoutContext();
+            } else {
+                executor = ctx.eventLoop().withoutContext();
+            }
+            return ExecutorsKt.from(executor).plus(coroutineContext);
         }
-        return ExecutorsKt.from(executor).plus(coroutineContext);
     }
 }
