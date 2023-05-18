d4ed172c220f5fa3bfea0f420a85e10d1e8cf33b
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2023-05-18 15:29:43 +0900

    add license

--- spring/boot3-actuator-autoconfigure/src/main/java/com/linecorp/armeria/spring/actuate/MappingWebEndpointPathMapper.java
+++ spring/boot3-actuator-autoconfigure/src/main/java/com/linecorp/armeria/spring/actuate/MappingWebEndpointPathMapper.java
@@ -13,6 +13,21 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
+/*
+ * Copyright 2012-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.linecorp.armeria.spring.actuate;
 
@@ -28,7 +43,7 @@ import org.springframework.util.StringUtils;
 import com.linecorp.armeria.common.annotation.Nullable;
 
 /**
- * Fork of <a href="https://github.com/be-hase/spring-boot/blob/24925c3daef169deaa9b2107ef02be4a67edb886/spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/MappingWebEndpointPathMapper.java">MappingWebEndpointPathMapper</a>.
+ * Fork of <a href="https://github.com/spring-projects/spring-boot/blob/e3aac5913ed3caf53b34eb7750138a4ed6839549/spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/MappingWebEndpointPathMapper.java">MappingWebEndpointPathMapper</a>.
  */
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class MappingWebEndpointPathMapper implements PathMapper {
