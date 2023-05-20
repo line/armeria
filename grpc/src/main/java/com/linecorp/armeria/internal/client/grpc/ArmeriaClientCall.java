87b4824dcf58d90d3af890584178993ca0ec98ed
Vlad Kostyukov <vlad.kostyukov@databricks.com>
2023-05-20 09:33:31 -0700

    optimize for lower allocations

--- grpc/src/main/java/com/linecorp/armeria/internal/client/grpc/ArmeriaClientCall.java
+++ grpc/src/main/java/com/linecorp/armeria/internal/client/grpc/ArmeriaClientCall.java
@@ -57,6 +57,7 @@ import com.linecorp.armeria.common.logging.RequestLogBuilder;
 import com.linecorp.armeria.common.logging.RequestLogProperty;
 import com.linecorp.armeria.common.stream.StreamMessage;
 import com.linecorp.armeria.common.stream.SubscriptionOption;
+import com.linecorp.armeria.common.util.SafeCloseable;
 import com.linecorp.armeria.common.util.TimeoutMode;
 import com.linecorp.armeria.internal.client.DefaultClientRequestContext;
 import com.linecorp.armeria.internal.client.grpc.protocol.InternalGrpcWebUtil;
@@ -104,59 +105,6 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
             pendingTaskUpdater = AtomicReferenceFieldUpdater.newUpdater(
             ArmeriaClientCall.class, Runnable.class, "pendingTask");
 
-    /**
-     * Offloads execution of Listener callbacks to contextAwareExecutor, while
-     * also handling callbacks' exceptions to gracefully close the stream.
-     */
-    private final class OffloadingListener extends Listener<O> {
-
-        private final Listener<O> delegate;
-
-        OffloadingListener(Listener<O> delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        public void onHeaders(Metadata metadata) {
-            contextAwareExecutor.execute(() -> {
-                try {
-                    delegate.onHeaders(metadata);
-                } catch (Throwable t) {
-                    closeWhenListenerThrows(t);
-                }
-            });
-        }
-
-        @Override
-        public void onMessage(O message) {
-            contextAwareExecutor.execute(() -> {
-                try {
-                    delegate.onMessage(message);
-                } catch (Throwable t) {
-                    closeWhenListenerThrows(t);
-                }
-            });
-        }
-
-        @Override
-        public void onClose(Status status, Metadata trailers) {
-            contextAwareExecutor.execute(() -> {
-                delegate.onClose(status, trailers);
-            });
-        }
-
-        @Override
-        public void onReady() {
-            contextAwareExecutor.execute(() -> {
-                try {
-                    delegate.onReady();
-                } catch (Throwable t) {
-                    closeWhenListenerThrows(t);
-                }
-            });
-        }
-    }
-
     private final DefaultClientRequestContext ctx;
     private final EndpointGroup endpointGroup;
     private final HttpClient httpClient;
@@ -169,7 +117,7 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
     private final CompressorRegistry compressorRegistry;
     private final SerializationFormat serializationFormat;
     private final boolean unsafeWrapResponseBuffers;
-    private final Executor contextAwareExecutor;
+    private final Executor executor;
     private final DecompressorRegistry decompressorRegistry;
     private final int maxInboundMessageSizeBytes;
     private final boolean grpcWebText;
@@ -233,12 +181,11 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
                                                  unsafeWrapResponseBuffers);
 
         if (callOptions.getExecutor() == null) {
-            contextAwareExecutor = ctx.makeContextAware(MoreExecutors.directExecutor());
+            executor = MoreExecutors.directExecutor();
         } else {
             // We wrap CallOptions' executor with SequentialExecutor to linearize the events, just like it's
             // done in vanilla grpc-java.
-            contextAwareExecutor =
-                    ctx.makeContextAware(MoreExecutors.newSequentialExecutor(callOptions.getExecutor()));
+            executor = MoreExecutors.newSequentialExecutor(callOptions.getExecutor());
         }
 
         req.whenComplete().handle((unused1, unused2) -> {
@@ -257,7 +204,7 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
 
         // The listener is set by the callers thread. The safe-publishing (aka visibility)
         // to event-loop is guaranteed via semantics of EventLoop.execute(...) call.
-        listener = new OffloadingListener(responseListener);
+        listener = responseListener;
 
         final Compressor compressor;
         if (callOptions.getCompressor() != null) {
@@ -313,8 +260,14 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
             });
         }
 
-        assert listener != null;
-        listener.onReady();
+        executor.execute(() -> {
+            try (SafeCloseable ignored = ctx.push()) {
+                assert listener != null;
+                listener.onReady();
+            } catch (Throwable t) {
+                closeWhenListenerThrows(t);
+            }
+        });
     }
 
     @Override
@@ -412,8 +365,14 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
             req.write(requestFramer.writePayload(serialized));
             req.whenConsumed().thenRun(() -> {
                 if (pendingMessagesUpdater.decrementAndGet(this) == 0) {
-                    assert listener != null;
-                    listener.onReady();
+                    executor.execute(() -> {
+                        try (SafeCloseable ignored = ctx.push()) {
+                            assert listener != null;
+                            listener.onReady();
+                        } catch (Throwable t) {
+                            closeWhenListenerThrows(t);
+                        }
+                    });
                 }
             });
         } catch (Throwable t) {
@@ -477,8 +436,14 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
                 GrpcUnsafeBufferUtil.storeBuffer(buf, msg, ctx);
             }
 
-            assert listener != null;
-            listener.onMessage(msg);
+            executor.execute(() -> {
+                try (SafeCloseable ignored = ctx.push()) {
+                    assert listener != null;
+                    listener.onMessage(msg);
+                } catch (Throwable t) {
+                    closeWhenListenerThrows(t);
+                }
+            });
         } catch (Throwable t) {
             close(GrpcStatus.fromThrowable(t), new Metadata());
         }
@@ -496,13 +461,22 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
 
     @Override
     public void transportReportStatus(Status status, Metadata metadata) {
-        contextAwareExecutor.execute(() -> closeWhenEos(status, metadata));
+        // This method is invoked in ctx.eventLoop and we need to bounce it through
+        // CallOptions executor (in case it's configured) to serialize with closes
+        // that were potentially triggered when Listener throws (closeWhenListenerThrows).
+        executor.execute(() -> closeWhenEos(status, metadata));
     }
 
     @Override
     public void transportReportHeaders(Metadata metadata) {
-        assert listener != null;
-        listener.onHeaders(metadata);
+        executor.execute(() -> {
+            try (SafeCloseable ignored = ctx.push()) {
+                assert listener != null;
+                listener.onHeaders(metadata);
+            } catch (Throwable t) {
+                closeWhenListenerThrows(t);
+            }
+        });
     }
 
     private void prepareHeaders(Compressor compressor, Metadata metadata, long remainingNanos) {
@@ -573,8 +547,14 @@ final class ArmeriaClientCall<I, O> extends ClientCall<I, O>
 
         // We want to close the user-facing ClientCall.Listener right away, before we move on to recycling
         // underlying resources.
-        assert listener != null;
-        listener.onClose(statusAndMetadata.status(), statusAndMetadata.metadata());
+        executor.execute(() -> {
+            try (SafeCloseable ignored = ctx.push()) {
+                assert listener != null;
+                listener.onClose(statusAndMetadata.status(), statusAndMetadata.metadata());
+            } catch (Throwable t) {
+                closeWhenListenerThrows(t);
+            }
+        });
 
         final RequestLogBuilder logBuilder = ctx.logBuilder();
         logBuilder.responseContent(GrpcLogUtil.rpcResponse(statusAndMetadata, firstResponse), null);
