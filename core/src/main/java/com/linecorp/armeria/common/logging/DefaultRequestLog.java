line/armeria

commit b78d9515dc8b4b997dd65787f717443b38a21869

Add WebSocketService (#3904)
Motivation:
It would be nice if we can provide a service that supports [The
WebSocket Protocol](https://datatracker.ietf.org/doc/html/rfc6455)

Modifications:
- Add `WebSocketService` and `WebSocketHandler` to implement the
WebSocket service using back pressure.
- Add `WebSocketFrame` that represents the web socket frames.
- Add `RequestLogBuilder.responseCause(cause)` to set the cause while
sending a normal response.
- Forked `HttpServerCodec` from netty to support WebSocket upgrade.
- Forked WebSocker encoder and decoder from Netty to use it without
channels.

Result:
- You can now use `WebSocketService` to send WebSocket messages.
  ```
  ServerBuilder sb = ...
  WebSocketHandler backpressureHandler = (ctx, messages) -> { 
      WebSocketWriter webSocketWriter = WebSocket.streaming();
      // Write frames using back pressure.
      return webSocketWriter;
  };
  sb.service("/chat", WebSocketService.of(backpressureHandler));
  ```

Todos:
- Provide another abstract class for the `WebSocketHandler` so that a
user can implement WebSocket easily without considering back pressure.
(next PR)
- Support WebSocketClient. (next PR)
- Support extensions. (next PR)

core/src/main/java/com/linecorp/armeria/common/logging/DefaultRequestLog.java
@@ -1188,6 +1188,17 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
     }
 
     @Override
+    public void responseCause(Throwable cause) {
+        if (isAvailable(RequestLogProperty.RESPONSE_CAUSE)) {
+            return;
+        }
+
+        requireNonNull(cause, "cause");
+        responseCause = cause;
+        updateFlags(RequestLogProperty.RESPONSE_CAUSE);
+    }
+
+    @Override
     public long responseLength() {
         ensureAvailable(RequestLogProperty.RESPONSE_LENGTH);
         return responseLength;
@@ -1281,9 +1292,9 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
             if (!rpcResponse.isDone()) {
                 throw new IllegalArgumentException("responseContent must be complete: " + responseContent);
             }
-            if (rpcResponse.cause() != null) {
-                responseCause = rpcResponse.cause();
-                updateFlags(RequestLogProperty.RESPONSE_CAUSE);
+            final Throwable cause = rpcResponse.cause();
+            if (cause != null) {
+                responseCause(cause);
             }
         }
 

commit 4d65efcb76730dfc1eaca720576a8796be3a12a6

Fix a bug where `RequestLog.responseCause()` isn't recorded for RPC responses
Motivation:

Recently, i've encountered the following test failure repeatedly when I
was running tests locally.
```
GrpcClientTest > errorNoMessage() FAILED
    java.lang.AssertionError:
    Expecting actual not to be null
        at com.linecorp.armeria.client.grpc.GrpcClientTest.lambda$errorNoMessage$44(GrpcClientTest.java:1639)
        at com.linecorp.armeria.client.grpc.GrpcClientTest.checkRequestLogError(GrpcClientTest.java:1863)
        at com.linecorp.armeria.client.grpc.GrpcClientTest.errorNoMessage(GrpcClientTest.java:1634)
```

That regression was caused by the change in #3904 that added
`responseCause(Throwable)`. The PR also migrated the way a response
cause is set from `RpcResponse`
https://github.com/line/armeria/pull/3904/files#diff-91096a8a0f87541eb2debf804754395c3000432ccc7af9167fd99d93e4241628

`RpcResponse` could be set after an HTTP response is ended.
This is because the decode process takes place at the RPC layer.

Note:

This regression has not yet been released, so there is no impact on
users.

Modifications:

- Direcly set `RpcResponse.cause()` to `responseCause` if it is null.

Result:

`RpcResponse.cause()` is correctly propagated to
`RequestLog.responseCause()`.

core/src/main/java/com/linecorp/armeria/common/logging/DefaultRequestLog.java
@@ -1284,9 +1284,13 @@ final class DefaultRequestLog implements RequestLog, RequestLogBuilder {
             if (!rpcResponse.isDone()) {
                 throw new IllegalArgumentException("responseContent must be complete: " + responseContent);
             }
-            final Throwable cause = rpcResponse.cause();
-            if (cause != null) {
-                responseCause(cause);
+
+            if (rpcResponse.cause() != null && responseCause == null) {
+                // Don't use 'responseCause(Throwable)' to set responseCause because if the HTTP response was
+                // ended successfully the RequestLogProperty.RESPONSE_CAUSE flag becomes available with a null
+                // value.
+                responseCause = rpcResponse.cause();
+                updateFlags(RequestLogProperty.RESPONSE_CAUSE);
             }
         }
 