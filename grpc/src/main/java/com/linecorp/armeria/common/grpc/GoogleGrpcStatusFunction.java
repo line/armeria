b76cb0a161bfb3048d96ba0d93905aa819b86d48
shimbo <takaaki.shimbo@linecorp.com>
2023-07-07 16:35:28 +0900

    Move GRPC_STATUS_DETAILS_BIN_KEY to MetadataUtil

--- grpc/src/main/java/com/linecorp/armeria/common/grpc/GoogleGrpcStatusFunction.java
+++ grpc/src/main/java/com/linecorp/armeria/common/grpc/GoogleGrpcStatusFunction.java
@@ -16,7 +16,7 @@
 
 package com.linecorp.armeria.common.grpc;
 
-import static com.linecorp.armeria.internal.common.grpc.GrpcMetadataKeys.GRPC_STATUS_DETAILS_BIN_KEY;
+import static com.linecorp.armeria.internal.common.grpc.MetadataUtil.GRPC_STATUS_DETAILS_BIN_KEY;
 import static java.util.Objects.requireNonNull;
 
 import com.linecorp.armeria.common.RequestContext;
