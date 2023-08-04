cb72fa14eabb7576a2f3236a2f4f1264278e15fa
Ikhun Um <ih.pert@gmail.com>
2023-08-04 21:22:18 +0900

    Upload Gradle build scan
    Motivation:
    
    It must be useful to see test results on a website and share the link
    with other folks. Since Gradle enterprise is activated in the Armeria
    project, we can upload build scans to https://ge.armeria.dev.
    
    An access token is required to upload build scans. As GitHub secrets are
    not exposed to the pull requests from public forks, it is not possible to
    directly upload the build scans in a CI build workflow while keeping the
    access token securely.
    
    @ribafish suggested a good idea that uploads `build-scan-data` to GitHub
    Actions artifacts and then publishing the backup scan data in a sperate
    workflow run on Armeria repository later.
    
    Modifications:
    
    - Added a new step that uploads Gradle build scan data to GitHub Actions
      artifacts to the CI build.
    - Redefined the build job name as a deterministic name.
      - A job name is used to fetch the job result from the JOB API.
      - GitHub does not provide a way to get a job ID or a job name in a workflow,
        - https://github.com/orgs/community/discussions/8945
        - https://stackoverflow.com/questions/71593861/how-to-get-github-actions-matrix-job-name-within-job
    - Added some useful GitHub links to the build scan.
    - Added `gradle-build-scan.yml` for downloading and executing the
      previous build scan.
    - Added `get-pr-number-by-sha.ts` to find a pull request number based on
      the given commit SHA that was triggered the build.
      - The pull request number is used to comment the scan result on a pull
        request.
    - Miscellaneous)
      - Bump `actions/checkout` to v3 from v2
      - Bump `actions/setup-java` to v3 from v2
    
    Result:
    
    You can now see Gradle build scan of a pull request on https://ge.armeria.dev

---
+++ .github/actions/comment-build-scan.ts
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Octokit } from 'octokit';
+
+main();
+
+async function main(): Promise<void> {
+  const octokit = new Octokit({auth: process.env.GITHUB_TOKEN});
+  const buildScans = process.env.BUILD_SCANS.split(",");
+  const prNumber = parseInt(process.env.PR_NUMBER);
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+  console.log(`💻 Getting jobs for ${process.env.RUN_ID} ...`)
+  const {data: {jobs}} = await octokit.rest.actions.listJobsForWorkflowRun({
+    owner: owner,
+    repo: repo,
+    run_id: parseInt(process.env.RUN_ID),
+  });
+
+  console.log(`💻 Getting comments for #${prNumber} ...`)
+  const comments = await octokit.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: prNumber,
+  })
+
+  let commentBody = `## 🔍 Build Scan® (commit: ${process.env.COMMIT_SHA})\n\n`;
+  commentBody += `| Job name | Status | Build Scan® |\n`;
+  commentBody += `| --- | --- | --- |\n`;
+  for (const scan of buildScans) {
+    if (scan.trim().length === 0) {
+      continue;
+    }
+    // build scan string pattern: "<job-name> https://ge.armeria.dev/xxxxxx"
+    const [jobName, scanUrl]= scan.split(" ");
+    const job = jobs.find(job => job.name === jobName);
+    if (job.conclusion === 'success') {
+      commentBody += `| [${job.name}](${job.url}) | ✅| ${scanUrl} |\n`;
+    } else {
+      commentBody += `| [${job.name}](${job.url}) | ❌ (${job.conclusion}) | ${scanUrl} |\n`;
+    }
+  }
+
+  const scanComment = comments.data.find(comment =>
+    comment.user.login === "github-actions[bot]" && comment.body.includes('Build Scan®'))
+  if (scanComment) {
+    // Update the previous comment
+    console.log(`📝 Updating the previous comment: ${scanComment.html_url} ...`)
+    await octokit.rest.issues.updateComment({
+      owner,
+      repo,
+      comment_id: scanComment.id,
+      body: commentBody
+    })
+  } else {
+    // If no previous comment, create a new one
+    console.log(`📝 Creating a new comment for #${prNumber} ...`)
+    const { data: newComment } = await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body: commentBody
+    })
+    console.log(`💬 A new comment has been created: ${newComment.html_url}`)
+  }
+}
