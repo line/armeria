6694c7efa1026d590db8775a0ba93c4a2ec8ab71
Hannam Rhee <jrhee17@linecorp.com>
2023-08-17 00:10:00 +0900

    address comment by @ikhoon

--- core/src/main/java/com/linecorp/armeria/server/RouteCache.java
+++ core/src/main/java/com/linecorp/armeria/server/RouteCache.java
@@ -128,7 +128,8 @@ final class RouteCache {
             }
 
             final Routed<V> result = delegate.find(routingCtx);
-            if (result.isPresent() && !dynamicPredicateRoutes.contains(result.route())) {
+            if (result.isPresent() && result.route().isCacheable() &&
+                !dynamicPredicateRoutes.contains(result.route())) {
                 findCache.put(routingCtx, result.value());
             }
             return result;
