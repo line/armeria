37714caff378bc8ce9ca27b95614948fd005e876
Hannam Rhee <jrhee17@linecorp.com>
2023-08-22 17:47:34 +0900

    try increasing timeout

--- core/src/test/java/com/linecorp/armeria/common/multipart/MultipartEncoderTckTest.java
+++ core/src/test/java/com/linecorp/armeria/common/multipart/MultipartEncoderTckTest.java
@@ -49,7 +49,7 @@ public class MultipartEncoderTckTest extends PublisherVerification<HttpData> {
     // Forked from https://github.com/oracle/helidon/blob/9d209a1a55f927e60e15b061700384e438ab5a01/media/multipart/src/test/java/io/helidon/media/multipart/MultiPartEncoderTckTest.java
 
     public MultipartEncoderTckTest() {
-        super(new TestEnvironment(200));
+        super(new TestEnvironment(5000));
     }
 
     @Override
