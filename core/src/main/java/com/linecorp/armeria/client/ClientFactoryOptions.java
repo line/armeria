63523aeb0a5ddea79afd5071f47befaf128ba67f
Ikhun Um <ih.pert@gmail.com>
2023-09-05 11:53:02 +0900

    .1

--- core/src/main/java/com/linecorp/armeria/client/ClientFactoryOptions.java
+++ core/src/main/java/com/linecorp/armeria/client/ClientFactoryOptions.java
@@ -526,7 +526,7 @@ public final class ClientFactoryOptions
     }
 
     /**
-     * Returns whether to use HTTP/1 instead of HTTP/2 . If {@code true}, the client will not attempt to upgrade
+     * Returns whether to use HTTP/1.1 instead of HTTP/2 . If {@code true}, the client will not attempt to upgrade
      * to HTTP/2 for {@link SessionProtocol#HTTP} and {@link SessionProtocol#HTTPS}.
      */
     @UnstableApi
