77b1f948b52d26becc5cdec4e68586cb2a4ba7f5
minux <minu.song@linecorp.com>
2023-09-26 11:25:10 +0900

    Fix NPE from `HttpSessionHander.isAcquirable()` (#5162)
    Motivation:
    When the client sends an options request for upgrade, the server can respond with the following data in the order:
    - 101 switching protocol
    - HTTP/2 settings frame
    - HTTP/2 response for the options request
    
    When the client process the HTTP/2 response, an NPE is raised because:
    - the end of stream of the response for options request is set to true and it triggers `Http2ResponseDecoder.onStreamClosed()`
    - [HttpSessionHandler.isAcquirable()](https://github.com/line/armeria/blob/main/core/src/main/java/com/linecorp/armeria/client/HttpSessionHandler.java#L285) is also called but NPE is raised because `responseDecoder` isn't set.
    - The task for setting the response decoder is in the event loop queue: https://github.com/line/armeria/blob/863e27c5268631360c14e50e13cd3e38d84c9db5/core/src/main/java/com/linecorp/armeria/client/HttpClientPipelineConfigurator.java#L421
    
    Modifications:
    - Add `HttpSessionHandler.isAcquirable(KeepAliveHandler)` method so that we can check the healthiness when the response decoder isn't set.
    
    Result:
    - No more `NullPointerException` from `HttpSessionHander.isAcquirable()`.

--- core/src/main/java/com/linecorp/armeria/client/AbstractHttpResponseDecoder.java
+++ core/src/main/java/com/linecorp/armeria/client/AbstractHttpResponseDecoder.java
@@ -24,7 +24,6 @@ import com.linecorp.armeria.common.annotation.Nullable;
 import com.linecorp.armeria.internal.client.DecodedHttpResponse;
 import com.linecorp.armeria.internal.client.HttpSession;
 import com.linecorp.armeria.internal.common.InboundTrafficController;
-import com.linecorp.armeria.internal.common.KeepAliveHandler;
 
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoop;
@@ -65,10 +64,7 @@ abstract class AbstractHttpResponseDecoder implements HttpResponseDecoder {
                 new HttpResponseWrapper(res, eventLoop, ctx,
                                         ctx.responseTimeoutMillis(), ctx.maxResponseLength());
         final HttpResponseWrapper oldRes = responses.put(id, newRes);
-        final KeepAliveHandler keepAliveHandler = keepAliveHandler();
-        if (keepAliveHandler != null) {
-            keepAliveHandler.increaseNumRequests();
-        }
+        keepAliveHandler().increaseNumRequests();
 
         assert oldRes == null : "addResponse(" + id + ", " + res + ", " + ctx + "): " + oldRes;
         onResponseAdded(id, eventLoop, newRes);
@@ -142,11 +138,6 @@ abstract class AbstractHttpResponseDecoder implements HttpResponseDecoder {
             return httpSession;
         }
         return httpSession = HttpSession.get(channel);
-    }
-
-    @Override
-    public boolean needsToDisconnectNow() {
-        return !session().isAcquirable() && !hasUnfinishedResponses();
     }
 
     static ContentTooLargeException contentTooLargeException(HttpResponseWrapper res, long transferred) {
