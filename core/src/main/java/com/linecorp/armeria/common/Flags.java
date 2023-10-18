71373dbc5c61d2bf209c1634cfdc9b8bd2191cd1
Ikhun Um <ikhun.um@linecorp.com>
2023-10-18 16:24:17 +0900

    Add metrics for client certificates (#5155)
    Motivation:
    
    Armeria only provides metrics for server certificates. mTLS is also widely used in Kubernetes service mesh or for enhanced security.
    
    If we provide validity metrics for client certificates, users will easily monitor expiration of the certificates.
    
    Modifications:
    
    - Add `CerfiticateMetrics` to easily register metrics for `X509Certificate`
    - Set up metrics for client certificates when `HttpClientFactory` is created.
      - Client certificates in `SslContextBuilder` are illegally accessed via reflections.
      - TODO: Open an issue to Netty to expose `keyCertChain` in `SslContextBuilder` or introduce Armeria `SslContextProvider` 
    - Move `CertificateUtil` to internal to use in the client modules.
    - Fix `EventLoopMetrics` to take `MeterIdPrefix` which makes users easily add additional tags to metrics.
    - Refactor `Server.setupTlsMetrics` to use `CerficateMetrics`.
    
    Result:
    
    You can now monitor client certificates in the following metrics:
    - `armeria.client.tls.certificates.validity`
    - `armeria.client.tls.certificates.validity.days`

--- core/src/main/java/com/linecorp/armeria/common/Flags.java
+++ core/src/main/java/com/linecorp/armeria/common/Flags.java
@@ -560,7 +560,7 @@ public final class Flags {
                     SslContextBuilder::forClient,
                     /* forceHttp1 */ false,
                     /* tlsAllowUnsafeCiphers */ false,
-                    ImmutableList.of()).newEngine(ByteBufAllocator.DEFAULT);
+                    ImmutableList.of(), null).newEngine(ByteBufAllocator.DEFAULT);
             logger.info("All available SSL protocols: {}",
                         ImmutableList.copyOf(engine.getSupportedProtocols()));
             logger.info("Default enabled SSL protocols: {}", SslContextUtil.DEFAULT_PROTOCOLS);
