02cbc3421086e80fe64128d6b86b5ea5330679cc
jrhee17 <guins_j@guins.org>
2023-12-27 10:41:55 +0900

    Introduce xDS module and SotW stream (#5342)
    Motivation:
    
    We would like to introduce a xDS integration module.
    In order to fetch resources from xDS module, I would like to introduce a
    `Stream` object which represents a single persistent connection to a
    remote control plane server. The `Stream` will query a
    `SubscriberStorage` to check which resources are subscribed and update
    the request accordingly.
    
    Once a response is recieved, the type will be used to fetch a
    `ResourceParser` which will later parse the response. Afterwards, the
    parameters will be sent over to a `ResponseHandler` which will handle
    the response and send either an `ackResponse` or `nackResponse` to keep
    the feedback loop open. Also, a `resourcesUpdated` can be sent to
    indicate the subscribed resources have been changed.
    
    Modifications:
    
    - Introduced a `SotwXdsStream` which represents a persistent connection
    to a control plane server
    - Introduced a `ResourceParser` which will be responsible for parsing a
    response in a format readily sendable in a nack response
    - Introduced a `XdsStreamSubscriber` and `SubscriberStorage` which
    represent the currently subscribed resources
    
    Result:
    
    - A basic xDS stream implementation is added
    - Step 1 of https://github.com/line/armeria/pull/5336 is merged

---
+++ xds/src/main/java/com/linecorp/armeria/xds/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Provides integrations with the
+ * <a href="https://www.envoyproxy.io/docs/envoy/latest/api-docs/xds_protocol">v3 gRPC-xDS protocol</a>.
+ */
+@NonNullByDefault
+package com.linecorp.armeria.xds;
+
+import com.linecorp.armeria.common.annotation.NonNullByDefault;
