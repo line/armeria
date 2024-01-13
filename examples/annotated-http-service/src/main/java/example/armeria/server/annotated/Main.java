3a734fcd94b10410ad6d080d6ee57b9c0963fa35
Klemen Košir <klemen.kosir@kream.io>
2024-01-13 23:03:03 +0900

    Support wrapped `HttpResult` response in annotated services

--- examples/annotated-http-service/src/main/java/example/armeria/server/annotated/Main.java
+++ examples/annotated-http-service/src/main/java/example/armeria/server/annotated/Main.java
@@ -40,6 +40,7 @@ public class Main {
           .annotatedService("/file", new FileUploadService())
           .annotatedService("/messageConverter", new MessageConverterService())
           .annotatedService("/exception", new ExceptionHandlerService())
+          .annotatedService("/reactive", new ReactiveService())
           .serviceUnder("/docs",
                         DocService.builder()
                                   .examplePaths(PathPatternService.class,
