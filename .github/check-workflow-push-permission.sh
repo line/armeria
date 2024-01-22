4a258c18da5c822936fc753a9b731adbfbe3714b
Ikhun Um <ih.pert@gmail.com>
2024-01-22 19:33:06 +0900

    gh cli is used to get diff

--- .github/check-workflow-push-permission.sh
+++ .github/check-workflow-push-permission.sh
@@ -6,9 +6,15 @@ set -euo pipefail
 # The script is triggered when a job has been assigned to a runner, but before the job starts running.
 # https://docs.github.com/en/actions/hosting-your-own-runners/managing-self-hosted-runners/running-scripts-before-or-after-a-job#triggering-the-scripts
 
-cd "$GITHUB_WORKSPACE"
+if [[ $GITHUB_REF != refs/pull* ]]; then
+  echo "Not a pull request. Skipping"
+  exit 0
+fi
+
+PR_NUMBER=$(echo "$GITHUB_REF" | awk -F / '{print $3}')
 # Check if there are any changes in .github/actions or .github/workflows
-if git diff --quiet HEAD main -- .github/actions .github/workflows; then
+WORKFLOW_CHANGES=$(gh pr diff "$PR_NUMBER" --name-only | grep -c '^.github/workflows\|^.github/actions')
+if [[ "$WORKFLOW_CHANGES" -eq "0" ]]; then
   echo "No changes in .github/actions or .github/workflows. Skipping."
   exit 0
 fi
@@ -17,10 +23,10 @@ MAINTAINERS=("ikhoon" "jrhee17" "minwoox" "trustin")
 for maintainer in "${MAINTAINERS[@]}"
 do
   if [[ $maintainer == "$GITHUB_ACTOR" ]]; then
-    echo "$GITHUB_ACTOR is a maintainer. Allowed to change GitHub Actions workflows."
+    echo "@$GITHUB_ACTOR is a maintainer. Allowed to change GitHub Actions workflows."
     exit 0
   fi
 done
 
-echo "$GITHUB_ACTOR is not a maintainer. Disallowed to change GitHub Actions workflows."
+echo "@$GITHUB_ACTOR is not a maintainer. Disallowed to change GitHub Actions workflows."
 exit 1
