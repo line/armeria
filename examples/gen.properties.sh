94837f6df47fecaabbeffb05c708a0440995b9e7
xiao kun <xshalk@163.com>
2024-04-20 21:05:15 +0800

    clean some script

--- examples/gen.properties.sh
+++
@@ -1,3 +0,0 @@
-
-find . -name "build.gradle" | xargs grep --no-filename  " project(\'\:" |awk '{print $2 }' | sort  | uniq -c | awk '{print $2}' > project.properties
-find . -name "build.gradle" | xargs grep --no-filename  ' libs.' |awk '{print $2 }'| sort | uniq -c  | awk '{print $2}' > lib.properties
