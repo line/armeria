2033e2671a84c44daeabab7c1b5daf00f2d69aa2
taisichi <taisichi@lycorp.co.jp>
2024-04-26 11:15:02 +0900

    add test

--- core/src/test/java/com/linecorp/armeria/server/cors/CorsServerErrorHanderTest.java
+++ core/src/test/java/com/linecorp/armeria/server/cors/CorsServerErrorHanderTest.java
@@ -18,16 +18,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CorsServerErrorHanderTest {
     private static final ClientFactory clientFactory = ClientFactory.ofDefault();
 
-    @CorsDecorators(value = {
-            @CorsDecorator(origins = "http://example.com", exposedHeaders = "expose_header_1")
-    }, shortCircuit = true)
-    private static class MyAnnotatedService {
-        @Get("/index")
-        @StatusCode(200)
-        public void index() {
-        }
-    }
-
     @RegisterExtension
     static final ServerExtension server = new ServerExtension() {
         @Override
@@ -53,53 +43,47 @@ public class CorsServerErrorHanderTest {
                     return HttpResponse.of(HttpStatus.OK);
                 }
             };
-            sb.service("/cors_status_exception", myService.decorate(
-                    CorsService.builder("http://example.com")
-                            .allowRequestMethods(HttpMethod.POST, HttpMethod.GET)
-                            .allowRequestHeaders("allow_request_header")
-                            .exposeHeaders("expose_header_1", "expose_header_2")
-                            .preflightResponseHeader("x-preflight-cors", "Hello CORS")
-                            .newDecorator())
-                    .decorate(new DecoratingHttpServiceFunction() {
-                @Override
-                public HttpResponse serve(HttpService delegate, ServiceRequestContext ctx, HttpRequest req) throws Exception {
-                    throw HttpStatusException.of(HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            })
-            );
+            addCorsServiceWithException(sb, myService, "/cors_status_exception", HttpStatusException.of(HttpStatus.INTERNAL_SERVER_ERROR));
+            addCorsServiceWithException(sb, myService, "/cors_response_exception", HttpResponseException.of(HttpResponse.of(HttpStatus.INTERNAL_SERVER_ERROR)));
         }
     };
 
+    private static void addCorsServiceWithException(ServerBuilder sb, HttpService myService, String pathPattern, Exception exception) {
+        sb.service(pathPattern, myService.decorate(CorsService.builder("http://example.com").allowRequestMethods(HttpMethod.POST, HttpMethod.GET).allowRequestHeaders("allow_request_header").exposeHeaders("expose_header_1", "expose_header_2").preflightResponseHeader("x-preflight-cors", "Hello CORS").newDecorator()).decorate(new DecoratingHttpServiceFunction() {
+            @Override
+            public HttpResponse serve(HttpService delegate, ServiceRequestContext ctx, HttpRequest req) throws Exception {
+                throw exception;
+            }
+        }));
+    }
+
     static WebClient client() {
-        return WebClient.builder(server.httpUri())
-                .factory(clientFactory)
-                .build();
+        return WebClient.builder(server.httpUri()).factory(clientFactory).build();
     }
 
-    static AggregatedHttpResponse request(WebClient client, HttpMethod method, String path, String origin,
-                                          String requestMethod) {
-        return client.execute(
-                RequestHeaders.of(method, path,
-                        HttpHeaderNames.ACCEPT, "utf-8",
-                        HttpHeaderNames.ORIGIN, origin,
-                        HttpHeaderNames.ACCESS_CONTROL_REQUEST_METHOD, requestMethod)
-        ).aggregate().join();
+    static AggregatedHttpResponse request(WebClient client, HttpMethod method, String path, String origin, String requestMethod) {
+        return client.execute(RequestHeaders.of(method, path, HttpHeaderNames.ACCEPT, "utf-8", HttpHeaderNames.ORIGIN, origin, HttpHeaderNames.ACCESS_CONTROL_REQUEST_METHOD, requestMethod)).aggregate().join();
     }
 
-    static AggregatedHttpResponse preflightRequest(WebClient client, String path, String origin,
-                                                   String requestMethod) {
+    static AggregatedHttpResponse preflightRequest(WebClient client, String path, String origin, String requestMethod) {
         return request(client, HttpMethod.OPTIONS, path, origin, requestMethod);
     }
 
-    @Test
-    void testCorsHeaderWhenHttpStatusException() {
+    void testCorsHeaderWithException(String path) {
         final WebClient client = client();
-        final AggregatedHttpResponse response = preflightRequest(client, "/cors_status_exception", "http://example.com",
-                "GET");
-//        assertThat(response.status()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
-        assertThat(response.headers().get(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS))
-                .isEqualTo("allow_request_header");
-        assertThat(response.headers().get(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN))
-                .isEqualTo("http://example.com");
+        final AggregatedHttpResponse response = preflightRequest(client, "/cors_status_exception", "http://example.com", "GET");
+        assertThat(response.status()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+        assertThat(response.headers().get(HttpHeaderNames.ACCESS_CONTROL_ALLOW_HEADERS)).isEqualTo("allow_request_header");
+        assertThat(response.headers().get(HttpHeaderNames.ACCESS_CONTROL_ALLOW_ORIGIN)).isEqualTo("http://example.com");
+    }
+
+    @Test
+    void testCorsHeaderWhenStatusException() {
+        testCorsHeaderWithException("/cors_status_exception");
+    }
+
+    @Test
+    void testCorsHeaderWhenResponseException() {
+        testCorsHeaderWithException("/cors_response_exception");
     }
 }
