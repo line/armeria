e16264425fc3abfc7b8ecda804831983be6280ee
eunho ko <eunho.dev@gmail.com>
2024-06-01 20:25:07 +0900

    Clean up

--- core/src/test/java/com/linecorp/armeria/server/websocket/WebSocketIdleTimeoutTest.java
+++ core/src/test/java/com/linecorp/armeria/server/websocket/WebSocketIdleTimeoutTest.java
@@ -1,23 +1,29 @@
+/*
+ * Copyright 2022 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.linecorp.armeria.server.websocket;
 
-import static com.google.common.collect.ImmutableList.toImmutableList;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.Duration;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linecorp.armeria.client.websocket.WebSocketClient;
-import com.linecorp.armeria.client.websocket.WebSocketInboundTestHandler;
 import com.linecorp.armeria.client.websocket.WebSocketSession;
-import com.linecorp.armeria.common.ClosedSessionException;
-import com.linecorp.armeria.common.HttpResponse;
-import com.linecorp.armeria.common.SessionProtocol;
-import com.linecorp.armeria.common.websocket.WebSocketFrame;
-import com.linecorp.armeria.common.websocket.WebSocketWriter;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.websocket.WebSocketServiceTest.AbstractWebSocketHandler;
 import com.linecorp.armeria.testing.junit5.server.ServerExtension;
