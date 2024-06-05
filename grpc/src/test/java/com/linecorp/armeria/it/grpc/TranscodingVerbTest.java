line/armeria

commit baa68298f34a7092417f63ceeb0241c5c73f37fb

Fix a bug when gRPC transcoding is used with parameterized paths (#5679)
Motivation:

This bug was found while analyzing the code for #5676.

Our gRPC-transcoding path parser contains logic which uses the parameter name when constructing the mapped path.
While doing so, we use an arbitrary name prefixed with a 'p' if we are unable to determine a suitable parameter name.

https://github.com/line/armeria/blob/1737482b82255d2ff4728927bdf91631bf1b23e9/grpc/src/main/java/com/linecorp/armeria/server/grpc/HttpJsonTranscodingPathParser.java#L385

This can be problematic since a path may be constructed in a different way from what the user expected.

e.g. Assume we use the following path pattern:
- path: `/v1/conflict/{id=hello/*}/{p0}/test`

The above path will currently be mapped to:
- `/v1/conflict/hello/:p0/:p0/test`.

However, there is no guarantee that `id` and `p0` should be equal, and hence should be built to the following path:
- `/v1/conflict/hello/:p0/:p1/test`.

I propose that we prepend a '@' to the generated fields. The reasoning for this is:
- Protobuf doesn't allow field names other than letters, digits, underscores in identifiers.
  - https://protobuf.dev/reference/protobuf/proto3-spec/#identifiers
  - https://protobuf.dev/reference/protobuf/proto2-spec/#identifiers
- '@' is a valid character in a path segment, so it stays the same after `RequestTarget#forServer` is called. This is useful when displaying the paths in `DocService` (called by `MethodInfo`).
  - https://datatracker.ietf.org/doc/html/rfc3986#section-3.3

As long as the above is satisfied, I think any character is fine.
I actually believe any `sub-delims` character can be used, so let me know if any character should be preferred more.

>  sub-delims  = "!" / "$" / "&" / "'" / "(" / ")"
                  / "*" / "+" / "," / ";" / "="

Modifications:

- Modified to prepend a '@' character in front of generated fields.

Result:

grpc/src/main/java/com/linecorp/armeria/server/grpc/HttpJsonTranscodingPathParser.java
@@ -382,7 +382,7 @@ final class HttpJsonTranscodingPathParser {
                 if (parentFieldPath != null) {
                     return parentFieldPath;
                 } else {
-                    return 'p' + StringUtil.toString(pathVarIndex);
+                    return "@p" + StringUtil.toString(pathVarIndex);
                 }
             } else {
                 return StringUtil.toString(pathVarIndex);

grpc/src/test/java/com/linecorp/armeria/internal/server/grpc/GrpcDocServicePluginTest.java
@@ -348,7 +348,7 @@ class GrpcDocServicePluginTest {
 
         // Expected generated routes. See 'transcoding.proto' file.
         final List<Route> routes = ImmutableList.of(
-                Route.builder().methods(HttpMethod.GET).path("/v1/messages/:p0").build(),
+                Route.builder().methods(HttpMethod.GET).path("/v1/messages/:@p0").build(),
                 Route.builder().methods(HttpMethod.GET).path("/v2/messages/{message_id}").build(),
                 Route.builder().methods(HttpMethod.GET).path("/v3/messages/{message_id}").build(),
                 Route.builder().methods(HttpMethod.PATCH).path("/v1/messages/{message_id}").build(),
@@ -379,7 +379,7 @@ class GrpcDocServicePluginTest {
                                                    .findFirst().get();
         assertThat(getMessageV1.httpMethod()).isEqualTo(HttpMethod.GET);
         assertThat(getMessageV1.endpoints()).containsAll(ImmutableSet.of(
-                EndpointInfo.builder(virtualHostNamePattern, "/v1/messages/:p0")
+                EndpointInfo.builder(virtualHostNamePattern, "/v1/messages/:@p0")
                             .availableMimeTypes(MediaType.JSON_UTF_8).build()));
         assertThat(getMessageV1.parameters()).containsAll(ImmutableList.of(
                 FieldInfo.builder("name", TypeSignature.ofBase(JavaType.STRING.name()))

grpc/src/test/java/com/linecorp/armeria/internal/server/grpc/GrpcDocServicePrefixTest.java
@@ -78,7 +78,7 @@ class GrpcDocServicePrefixTest {
                 "/route/innerPrefix/GetMessageV1");
         getMessageV1 = findMethod(httpMethods, "GetMessageV1");
         assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder(
-                "/v1/messages/:p0", "/outerPrefix/v1/messages/:p0");
+                "/v1/messages/:@p0", "/outerPrefix/v1/messages/:@p0");
 
         JsonNode getMessageV2 = findMethod(methods, "GetMessageV2");
         assertThat(pathMapping(getMessageV2)).containsExactlyInAnyOrder(

grpc/src/test/java/com/linecorp/armeria/it/grpc/HttpJsonTranscodingTest.java
@@ -76,6 +76,7 @@ import io.grpc.Status.Code;
 import io.grpc.stub.StreamObserver;
 import testing.grpc.HttpJsonTranscodingTestServiceGrpc.HttpJsonTranscodingTestServiceBlockingStub;
 import testing.grpc.HttpJsonTranscodingTestServiceGrpc.HttpJsonTranscodingTestServiceImplBase;
+import testing.grpc.Transcoding.ConflictMessage;
 import testing.grpc.Transcoding.EchoAnyRequest;
 import testing.grpc.Transcoding.EchoAnyResponse;
 import testing.grpc.Transcoding.EchoFieldMaskRequest;
@@ -331,6 +332,13 @@ public class HttpJsonTranscodingTest {
                                            StreamObserver<EchoNestedMessageResponse> responseObserver) {
             responseObserver
                     .onNext(EchoNestedMessageResponse.newBuilder().setNested(request.getNested()).build());
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void paramNameConflict(ConflictMessage request,
+                                      StreamObserver<ConflictMessage> responseObserver) {
+            responseObserver.onNext(request);
             responseObserver.onCompleted();
         }
     }
@@ -947,8 +955,8 @@ public class HttpJsonTranscodingTest {
 
         final JsonNode getMessageV1 = findMethod(methods, "GetMessageV1");
         assertThat(getMessageV1.get("httpMethod").asText()).isEqualTo("GET");
-        assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder("/v1/messages/:p0",
-                                                                        "/foo/v1/messages/:p0");
+        assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder("/v1/messages/:@p0",
+                                                                        "/foo/v1/messages/:@p0");
 
         final JsonNode getMessageV2 = findMethod(methods, "GetMessageV2");
         assertThat(getMessageV2.get("httpMethod").asText()).isEqualTo("GET");
@@ -1077,6 +1085,14 @@ public class HttpJsonTranscodingTest {
         assertThat(response.contentType()).isEqualTo(MediaType.JSON_UTF_8);
         assertThat(nested).isNotNull().matches(v -> ((TreeNode) v).isObject());
         assertThat(nested.get("name").asText()).isEqualTo("Armeria");
+    }
+
+    @Test
+    void conflictingParamNamesReturnsCorrectly() {
+        final AggregatedHttpResponse res = server.blockingWebClient().get("/v1/conflict/hello/1/a/test");
+        assertThat(res.status().code()).isEqualTo(200);
+        assertThatJson(res.contentUtf8()).node("id").isEqualTo("hello/1");
+        assertThatJson(res.contentUtf8()).node("p0").isEqualTo("a");
     }
 
     public static JsonNode findMethod(JsonNode methods, String name) {

grpc/src/test/java/com/linecorp/armeria/server/grpc/HttpJsonTranscodingPathParserTest.java
@@ -89,9 +89,9 @@ class HttpJsonTranscodingPathParserTest {
                               ImmutableMap.of("user_id", "1", "message_id", "2"),
                               ImmutableMap.of("user_id", "1", "message_id", "2")),
                     arguments("/v1/{name=messages/*}",
-                              "/v1/messages/:p0",
+                              "/v1/messages/:@p0",
                               PathMappingType.PARAMETERIZED,
-                              ImmutableMap.of("p0", "1"),
+                              ImmutableMap.of("@p0", "1"),
                               ImmutableMap.of("name", "messages/1")),
                     arguments("/v1/{name=messages/{name2=*}}",
                               "/v1/messages/:name2",
@@ -99,9 +99,9 @@ class HttpJsonTranscodingPathParserTest {
                               ImmutableMap.of("name2", "1"),
                               ImmutableMap.of("name", "messages/1", "name2", "1")),
                     arguments("/v1/{name=messages/{name2=*/foo/{name3=*}}}",
-                              "/v1/messages/:p0/foo/:name3",
+                              "/v1/messages/:@p0/foo/:name3",
                               PathMappingType.PARAMETERIZED,
-                              ImmutableMap.of("p0", "1", "name3", "2"),
+                              ImmutableMap.of("@p0", "1", "name3", "2"),
                               ImmutableMap.of("name", "messages/1/foo/2", "name2", "1/foo/2", "name3", "2")),
                     arguments("/v1/messages/{message_id}:verb",
                               "/v1/messages/:message_id:verb",

grpc/src/test/proto/testing/grpc/transcoding.proto
@@ -224,6 +224,12 @@ service HttpJsonTranscodingTestService {
       }
     };
   }
+
+  rpc ParamNameConflict(ConflictMessage) returns (ConflictMessage) {
+    option(google.api.http) = {
+      get: "/v1/conflict/{id=hello/*}/{p0}/test"
+    };
+  }
 }
 
 message GetMessageRequestV1 {
@@ -406,4 +412,9 @@ message EchoNestedMessageRequest {
 
 message EchoNestedMessageResponse {
   TopLevelMessage.NestedMessage nested = 1;
+}
+
+message ConflictMessage {
+  string p0 = 1;
+  string id = 2;
 }

commit cc47a3190a3e4348fd12a65430adfd8ac8735ba7

Fix a regession where an invalid path pattern is generated for gRPC trancoding (#5729)
Motivation:

PR #5679 allows groups name in a regex path pattern to start with `@`. However, group names of a regular expression must start with a letter. https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#groupname

Modifications:

- Use `p\d` as a group name for `PathMappingType.REGEX`
  - `@p\d` will stay unchanged for `PathMappingType.PARAMETERIZED`

Result:

Fix a regression caused by #5679

grpc/src/main/java/com/linecorp/armeria/server/grpc/HttpJsonTranscodingPathParser.java
@@ -385,14 +385,22 @@ final class HttpJsonTranscodingPathParser {
 
         @Override
         public String pathVariable(PathMappingType type) {
-            if (type == PathMappingType.PARAMETERIZED || type == PathMappingType.REGEX) {
-                if (parentFieldPath != null) {
-                    return parentFieldPath;
-                } else {
-                    return "@p" + StringUtil.toString(pathVarIndex);
-                }
+            switch (type) {
+                case PARAMETERIZED:
+                    if (parentFieldPath != null) {
+                        return parentFieldPath;
+                    } else {
+                        return 'p' + StringUtil.toString(pathVarIndex);
+                    }
+                case REGEX:
+                    // A group name for regex must start with a letter and contain only letters, digits.
+                    // parentFieldPath may contain non-alphanumeric characters, so we prepend 'p' to the index.
+                    return 'p' + StringUtil.toString(pathVarIndex);
+                case GLOB:
+                    return StringUtil.toString(pathVarIndex);
             }
-            return StringUtil.toString(pathVarIndex);
+            // Should not reach here.
+            throw new Error();
         }
 
         WildcardPathSegment withParentFieldPath(String parentFieldPath) {

grpc/src/test/java/com/linecorp/armeria/internal/server/grpc/GrpcDocServicePluginTest.java
@@ -354,7 +354,7 @@ class GrpcDocServicePluginTest {
 
         // Expected generated routes. See 'transcoding.proto' file.
         final List<Route> routes = ImmutableList.of(
-                Route.builder().methods(HttpMethod.GET).path("/v1/messages/:@p0").build(),
+                Route.builder().methods(HttpMethod.GET).path("/v1/messages/:p0").build(),
                 Route.builder().methods(HttpMethod.GET).path("/v2/messages/{message_id}").build(),
                 Route.builder().methods(HttpMethod.GET).path("/v3/messages/{message_id}").build(),
                 Route.builder().methods(HttpMethod.PATCH).path("/v1/messages/{message_id}").build(),
@@ -385,7 +385,7 @@ class GrpcDocServicePluginTest {
                                                    .findFirst().get();
         assertThat(getMessageV1.httpMethod()).isEqualTo(HttpMethod.GET);
         assertThat(getMessageV1.endpoints()).containsAll(ImmutableSet.of(
-                EndpointInfo.builder(virtualHostNamePattern, "/v1/messages/:@p0")
+                EndpointInfo.builder(virtualHostNamePattern, "/v1/messages/:p0")
                             .availableMimeTypes(MediaType.JSON_UTF_8).build()));
         assertThat(getMessageV1.parameters()).containsAll(ImmutableList.of(
                 FieldInfo.builder("name", TypeSignature.ofBase(JavaType.STRING.name()))
@@ -457,7 +457,7 @@ class GrpcDocServicePluginTest {
 
     @Test
     void pathParamRegexIsConvertedCorrectly() {
-        assertThat(GrpcDocServicePlugin.convertRegexPath("/a/(?<p0>[^/]+):get"))
+        assertThat(convertRegexPath("/a/(?<p0>[^/]+):get"))
                 .isEqualTo("/a/p0:get");
     }
 }

grpc/src/test/java/com/linecorp/armeria/internal/server/grpc/GrpcDocServicePrefixTest.java
@@ -78,7 +78,7 @@ class GrpcDocServicePrefixTest {
                 "/route/innerPrefix/GetMessageV1");
         getMessageV1 = findMethod(httpMethods, "GetMessageV1");
         assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder(
-                "/v1/messages/:@p0", "/outerPrefix/v1/messages/:@p0");
+                "/v1/messages/:p0", "/outerPrefix/v1/messages/:p0");
 
         JsonNode getMessageV2 = findMethod(methods, "GetMessageV2");
         assertThat(pathMapping(getMessageV2)).containsExactlyInAnyOrder(

grpc/src/test/java/com/linecorp/armeria/it/grpc/HttpJsonTranscodingTest.java
@@ -76,7 +76,6 @@ import io.grpc.Status.Code;
 import io.grpc.stub.StreamObserver;
 import testing.grpc.HttpJsonTranscodingTestServiceGrpc.HttpJsonTranscodingTestServiceBlockingStub;
 import testing.grpc.HttpJsonTranscodingTestServiceGrpc.HttpJsonTranscodingTestServiceImplBase;
-import testing.grpc.Transcoding.ConflictMessage;
 import testing.grpc.Transcoding.EchoAnyRequest;
 import testing.grpc.Transcoding.EchoAnyResponse;
 import testing.grpc.Transcoding.EchoFieldMaskRequest;
@@ -332,13 +331,6 @@ public class HttpJsonTranscodingTest {
                                            StreamObserver<EchoNestedMessageResponse> responseObserver) {
             responseObserver
                     .onNext(EchoNestedMessageResponse.newBuilder().setNested(request.getNested()).build());
-            responseObserver.onCompleted();
-        }
-
-        @Override
-        public void paramNameConflict(ConflictMessage request,
-                                      StreamObserver<ConflictMessage> responseObserver) {
-            responseObserver.onNext(request);
             responseObserver.onCompleted();
         }
     }
@@ -955,8 +947,8 @@ public class HttpJsonTranscodingTest {
 
         final JsonNode getMessageV1 = findMethod(methods, "GetMessageV1");
         assertThat(getMessageV1.get("httpMethod").asText()).isEqualTo("GET");
-        assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder("/v1/messages/:@p0",
-                                                                        "/foo/v1/messages/:@p0");
+        assertThat(pathMapping(getMessageV1)).containsExactlyInAnyOrder("/v1/messages/:p0",
+                                                                        "/foo/v1/messages/:p0");
 
         final JsonNode getMessageV2 = findMethod(methods, "GetMessageV2");
         assertThat(getMessageV2.get("httpMethod").asText()).isEqualTo("GET");
@@ -1085,14 +1077,6 @@ public class HttpJsonTranscodingTest {
         assertThat(response.contentType()).isEqualTo(MediaType.JSON_UTF_8);
         assertThat(nested).isNotNull().matches(v -> ((TreeNode) v).isObject());
         assertThat(nested.get("name").asText()).isEqualTo("Armeria");
-    }
-
-    @Test
-    void conflictingParamNamesReturnsCorrectly() {
-        final AggregatedHttpResponse res = server.blockingWebClient().get("/v1/conflict/hello/1/a/test");
-        assertThat(res.status().code()).isEqualTo(200);
-        assertThatJson(res.contentUtf8()).node("id").isEqualTo("hello/1");
-        assertThatJson(res.contentUtf8()).node("p0").isEqualTo("a");
     }
 
     public static JsonNode findMethod(JsonNode methods, String name) {

grpc/src/test/java/com/linecorp/armeria/it/grpc/TranscodingVerbTest.java
@@ -24,12 +24,14 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 
 import com.linecorp.armeria.common.AggregatedHttpResponse;
 import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.grpc.GrpcService;
 import com.linecorp.armeria.testing.junit5.server.ServerExtension;
 
 import io.grpc.stub.StreamObserver;
 import testing.grpc.TranscodingVerbServiceGrpc;
 import testing.grpc.Verb.Simple;
+import testing.grpc.Verb.UnderScore;
 
 class TranscodingVerbTest {
 
@@ -55,6 +57,12 @@ class TranscodingVerbTest {
 
         @Override
         public void noParamsVerb(Simple request, StreamObserver<Simple> responseObserver) {
+            responseObserver.onNext(request);
+            responseObserver.onCompleted();
+        }
+
+        @Override
+        public void underScoreVerb(UnderScore request, StreamObserver<UnderScore> responseObserver) {
             responseObserver.onNext(request);
             responseObserver.onCompleted();
         }
@@ -98,5 +106,17 @@ class TranscodingVerbTest {
     void testNoParamsVerb() throws Exception {
         final AggregatedHttpResponse response = server.blockingWebClient().get("/v1/simple/1/noparam:verb");
         assertThat(response.contentUtf8()).isEqualTo("{}");
+    }
+
+    @Test
+    void testUnderScoreVariableWithVerb() throws InterruptedException {
+        server.requestContextCaptor().clear();
+        final AggregatedHttpResponse response = server.blockingWebClient().get("/v1/underscore/a/b:verb");
+        final ServiceRequestContext ctx = server.requestContextCaptor().take();
+        assertThatJson(response.contentUtf8()).node("firstMessage").isEqualTo("a");
+        assertThatJson(response.contentUtf8()).node("secondMessage").isEqualTo("b");
+        // Verb pattern uses p\d for path variables
+        assertThat(ctx.pathParam("p0")).isEqualTo("a");
+        assertThat(ctx.pathParam("p1")).isEqualTo("b");
     }
 }

grpc/src/test/java/com/linecorp/armeria/server/grpc/HttpJsonTranscodingPathParserTest.java
@@ -17,6 +17,7 @@ package com.linecorp.armeria.server.grpc;
 
 import static com.linecorp.armeria.server.grpc.HttpJsonTranscodingPathParser.Stringifier.segmentsToPath;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.mock;
@@ -24,6 +25,7 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
@@ -47,6 +49,11 @@ class HttpJsonTranscodingPathParserTest {
                                            PathMappingType typeAnswer,
                                            Map<String, String> pathParams,
                                            Map<String, String> pathVariablesAnswer) {
+        if (typeAnswer == PathMappingType.REGEX) {
+            // Make sure the generated path is a valid regex pattern.
+            assertThatCode(() -> Pattern.compile(generatedPathAnswer))
+                    .doesNotThrowAnyException();
+        }
         final List<HttpJsonTranscodingPathParser.PathSegment> segments =
                 HttpJsonTranscodingPathParser.parse(originalPath);
 
@@ -82,9 +89,9 @@ class HttpJsonTranscodingPathParserTest {
                               ImmutableMap.of("user_id", "1", "message_id", "2"),
                               ImmutableMap.of("user_id", "1", "message_id", "2")),
                     arguments("/v1/{name=messages/*}",
-                              "/v1/messages/:@p0",
+                              "/v1/messages/:p0",
                               PathMappingType.PARAMETERIZED,
-                              ImmutableMap.of("@p0", "1"),
+                              ImmutableMap.of("p0", "1"),
                               ImmutableMap.of("name", "messages/1")),
                     arguments("/v1/{name=messages/{name2=*}}",
                               "/v1/messages/:name2",
@@ -92,14 +99,14 @@ class HttpJsonTranscodingPathParserTest {
                               ImmutableMap.of("name2", "1"),
                               ImmutableMap.of("name", "messages/1", "name2", "1")),
                     arguments("/v1/{name=messages/{name2=*/foo/{name3=*}}}",
-                              "/v1/messages/:@p0/foo/:name3",
+                              "/v1/messages/:p0/foo/:name3",
                               PathMappingType.PARAMETERIZED,
-                              ImmutableMap.of("@p0", "1", "name3", "2"),
+                              ImmutableMap.of("p0", "1", "name3", "2"),
                               ImmutableMap.of("name", "messages/1/foo/2", "name2", "1/foo/2", "name3", "2")),
                     arguments("/v1/messages/{message_id}:verb",
-                              "/v1/messages/(?<message_id>[^/]+):verb",
+                              "/v1/messages/(?<p0>[^/]+):verb",
                               PathMappingType.REGEX,
-                              ImmutableMap.of("message_id", "1"),
+                              ImmutableMap.of("p0", "1"),
                               ImmutableMap.of("message_id", "1")),
                     arguments("/v1/messages/{message_id=hello/*}:verb",
                               "/v1/messages/hello/(?<p0>[^/]+):verb",
@@ -107,9 +114,9 @@ class HttpJsonTranscodingPathParserTest {
                               ImmutableMap.of("p0", "1"),
                               ImmutableMap.of("message_id", "hello/1")),
                     arguments("/v1/messages/{first=a/{second=b/*/{third}}}:verb",
-                              "/v1/messages/a/b/(?<p0>[^/]+)/(?<third>[^/]+):verb",
+                              "/v1/messages/a/b/(?<p0>[^/]+)/(?<p1>[^/]+):verb",
                               PathMappingType.REGEX,
-                              ImmutableMap.of("p0", "1", "third", "2"),
+                              ImmutableMap.of("p0", "1", "p1", "2"),
                               ImmutableMap.of("first", "a/b/1/2", "second", "b/1/2", "third", "2")),
                     arguments("/v1/messages/{name=**}",
                               "/v1/messages/**",

grpc/src/test/proto/testing/grpc/transcoding.proto
@@ -224,12 +224,6 @@ service HttpJsonTranscodingTestService {
       }
     };
   }
-
-  rpc ParamNameConflict(ConflictMessage) returns (ConflictMessage) {
-    option(google.api.http) = {
-      get: "/v1/conflict/{id=hello/*}/{p0}/test"
-    };
-  }
 }
 
 message GetMessageRequestV1 {
@@ -412,9 +406,4 @@ message EchoNestedMessageRequest {
 
 message EchoNestedMessageResponse {
   TopLevelMessage.NestedMessage nested = 1;
-}
-
-message ConflictMessage {
-  string p0 = 1;
-  string id = 2;
 }

grpc/src/test/proto/testing/grpc/verb.proto
@@ -30,10 +30,21 @@ service TranscodingVerbService {
       get: "/v1/simple/1/noparam:verb"
     };
   }
+
+  rpc UnderScoreVerb(UnderScore) returns (UnderScore) {
+    option (google.api.http) = {
+      get: "/v1/underscore/{first_message}/{second_message}:verb"
+    };
+  }
 }
 
 message Simple {
   string first = 1;
   string second = 2;
   string third = 3;
+}
+
+message UnderScore {
+  string first_message = 1;
+  string second_message = 2;
 }