f94cf66065ab14c06e5623ca979145fe9fe8680a
KonaEspresso94 <konaespresso94@kakao.com>
2024-06-16 15:25:40 +0900

    Copyright header: Line Corporation -> LY Corporation

--- nacos/src/main/java/com/linecorp/armeria/server/nacos/NacosUpdatingListenerBuilder.java
+++ nacos/src/main/java/com/linecorp/armeria/server/nacos/NacosUpdatingListenerBuilder.java
@@ -1,7 +1,7 @@
 /*
- * Copyright 2024 LINE Corporation
- *
- * LINE Corporation licenses this file to you under the Apache License,
+ * Copyright 2024 LY Corporation
+
+ * LY Corporation licenses this file to you under the Apache License,
  * version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at:
  *
