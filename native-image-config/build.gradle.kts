06fbe64fe0defd05fbae2cc677fa9e6e7697f2fc
Trustin Lee <trustin@linecorp.com>
2024-06-20 20:31:05 +0900

    Use `ModuleVersionSpecifier` instead of `Dependency` so we don't need `!!` anymore

--- native-image-config/build.gradle.kts
+++ native-image-config/build.gradle.kts
@@ -12,9 +12,9 @@ buildscript {
     dependencies {
         // TODO(trustin): Use platform(libs.boms.jackson) once Gradle supports platform() for build dependencies.
         //                https://github.com/gradle/gradle/issues/21788
-        val jacksonDatabind: Dependency = libs.jackson.databind.get()
+        val jacksonDatabind: ModuleVersionSelector = libs.jackson.databind.get()
         classpath(
-            group = jacksonDatabind.group!!,
+            group = jacksonDatabind.group,
             name = jacksonDatabind.name,
             version = libs.boms.jackson.get().version
         )
