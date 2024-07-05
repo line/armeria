d8c97f1ca91777f81b2c184a39408c56b60c4642
jrhee17 <guins_j@guins.org>
2024-07-05 11:03:49 +0900

    lint

--- core/src/main/java/com/linecorp/armeria/client/endpoint/WeightRampingUpStrategy.java
+++ core/src/main/java/com/linecorp/armeria/client/endpoint/WeightRampingUpStrategy.java
@@ -33,7 +33,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
