d793adec4a8079914a81e590e066e691c244c6e4
jrhee17 <guins_j@guins.org>
2024-07-25 15:20:12 +0900

    Modify behavior of `Endpoint#withAttrs` to merge attributes (#5802)
    Motivation:
    
    It seems inconsistent that `Endpoint#withAttr` merges the attribute to
    the existing attributes, whereas `Endpoint#withAttrs` simply replaces
    the previous attributes.
    I propose that we modify the behavior of `Endpoint#withAttrs` to be
    aligned with `Endpoint#withAttr`. Additionally, in order to support the
    previous behavior, I propose a new API `Endpoint#replaceAttrs` be added.
    
    I've confirmed that there are no uses of this API at least internally.
    
    ref: https://github.com/line/armeria/pull/5785#discussion_r1654225883
    
    Modifications:
    
    - Modified the behavior of `Endpoint#withAttrs` to merge attributes
    - Added a new API `Endpoint#replaceAttrs` to support the previous
    behavior
    
    Result:
    
    - More consistent APIs
    - Preparation for https://github.com/line/armeria/pull/5785
    
    <!--
    Visit this URL to learn more about how to write a pull request
    description:
    
    https://armeria.dev/community/developer-guide#how-to-write-pull-request-description
    -->

--- core/src/main/java/com/linecorp/armeria/client/Endpoint.java
+++ core/src/main/java/com/linecorp/armeria/client/Endpoint.java
@@ -696,7 +696,7 @@ public final class Endpoint implements Comparable<Endpoint>, EndpointGroup {
             if (value == null) {
                 return this;
             }
-            return withAttrs(Attributes.of(key, value));
+            return replaceAttrs(Attributes.of(key, value));
         }
 
         if (attributes.attr(key) == value) {
@@ -704,8 +704,32 @@ public final class Endpoint implements Comparable<Endpoint>, EndpointGroup {
         } else {
             final AttributesBuilder attributesBuilder = attributes.toBuilder();
             attributesBuilder.set(key, value);
-            return withAttrs(attributesBuilder.build());
+            return replaceAttrs(attributesBuilder.build());
         }
+    }
+
+    /**
+     * Returns a new {@link Endpoint} with the specified {@link Attributes}.
+     * Note that the {@link #attrs()} of this {@link Endpoint} is merged with the specified
+     * {@link Attributes}. For attributes with the same {@link AttributeKey}, the attribute
+     * in {@param newAttributes} has higher precedence.
+     */
+    @UnstableApi
+    @SuppressWarnings("unchecked")
+    public Endpoint withAttrs(Attributes newAttributes) {
+        requireNonNull(newAttributes, "newAttributes");
+        if (newAttributes.isEmpty()) {
+            return this;
+        }
+        if (attrs().isEmpty()) {
+            return replaceAttrs(newAttributes);
+        }
+        final AttributesBuilder builder = attrs().toBuilder();
+        newAttributes.attrs().forEachRemaining(entry -> {
+            final AttributeKey<Object> key = (AttributeKey<Object>) entry.getKey();
+            builder.set(key, entry.getValue());
+        });
+        return new Endpoint(type, host, ipAddr, port, weight, builder.build());
     }
 
     /**
@@ -714,7 +738,7 @@ public final class Endpoint implements Comparable<Endpoint>, EndpointGroup {
      * {@link Attributes}.
      */
     @UnstableApi
-    public Endpoint withAttrs(Attributes newAttributes) {
+    public Endpoint replaceAttrs(Attributes newAttributes) {
         requireNonNull(newAttributes, "newAttributes");
         if (attrs().isEmpty() && newAttributes.isEmpty()) {
             return this;
