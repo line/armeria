c32df98ebb474802bf7aeee705c53de153db0107
Ikhun Um <ih.pert@gmail.com>
2025-02-04 15:01:21 +0900

    add more tests

--- grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcTranscodingJsonNameTest.java
+++ grpc/src/test/java/com/linecorp/armeria/server/grpc/GrpcTranscodingJsonNameTest.java
@@ -16,11 +16,17 @@
 
 package com.linecorp.armeria.server.grpc;
 
+import static com.linecorp.armeria.server.grpc.HttpJsonTranscodingQueryParamMatchRule.JSON_NAME;
+import static com.linecorp.armeria.server.grpc.HttpJsonTranscodingQueryParamMatchRule.LOWER_CAMEL_CASE;
+import static com.linecorp.armeria.server.grpc.HttpJsonTranscodingQueryParamMatchRule.ORIGINAL_FIELD;
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import com.linecorp.armeria.client.BlockingWebClient;
 import com.linecorp.armeria.common.AggregatedHttpResponse;
@@ -39,33 +45,91 @@ class GrpcTranscodingJsonNameTest {
     static ServerExtension server = new ServerExtension() {
         @Override
         protected void configure(ServerBuilder sb) {
-            final HttpJsonTranscodingOptions options =
-                    HttpJsonTranscodingOptions.builder()
-                                              .queryParamMatchRules(
-                                                      HttpJsonTranscodingQueryParamMatchRule.JSON_NAME)
-                                              .build();
-            final GrpcService grpcService = GrpcService.builder()
-                                                       .addService(new JsonNameTestService())
-                                                       .enableHttpJsonTranscoding(options)
-                                                       .build();
-            sb.service(grpcService);
+            final JsonNameTestService jsonNameService = new JsonNameTestService();
+            final HashMap<String, HttpJsonTranscodingOptions> transcodingOptions = new HashMap<>();
+            transcodingOptions.put("/original",
+                                   HttpJsonTranscodingOptions.builder()
+                                                             .queryParamMatchRules(ORIGINAL_FIELD)
+                                                             .build());
+            transcodingOptions.put("/camel",
+                                   HttpJsonTranscodingOptions.builder()
+                                                             .queryParamMatchRules(LOWER_CAMEL_CASE)
+                                                             .build());
+            transcodingOptions.put("/jsonNameOrOriginal",
+                                   HttpJsonTranscodingOptions.builder()
+                                                             .queryParamMatchRules(ORIGINAL_FIELD, JSON_NAME)
+                                                             .build());
+            transcodingOptions.put("/jsonNameOrCamel",
+                                   HttpJsonTranscodingOptions.builder()
+                                                             .queryParamMatchRules(LOWER_CAMEL_CASE, JSON_NAME)
+                                                             .build());
+            transcodingOptions.put("/all",
+                                   HttpJsonTranscodingOptions.builder()
+                                                             .queryParamMatchRules(ORIGINAL_FIELD,
+                                                                                   LOWER_CAMEL_CASE, JSON_NAME)
+                                                             .build());
+            transcodingOptions.forEach((path, options) -> {
+                sb.serviceUnder(path, GrpcService.builder()
+                                                 .addService(jsonNameService)
+                                                 .enableHttpJsonTranscoding(options)
+                                                 .build());
+            });
         }
     };
 
-    @Test
-    void shouldIgnoreJsonNameForPathVariable() {
+    @CsvSource({
+            // ORIGINAL_FIELD: Use the original fields
+            "/original, json_name_query_param=third&query_param=fourth, first|second|third|fourth",
+            // ORIGINAL_FIELD: Ignore the json name
+            "/original, customJsonNameQueryParam=third&query_param=fourth, first|second||fourth",
+            // ORIGINAL_FIELD: Ignore the camel case
+            "/original, json_name_query_param=third&queryParam=fourth, first|second|third|",
+            // LOWER_CAMEL_CASE: Use the camel case
+            "/camel, jsonNameQueryParam=third&queryParam=fourth, first|second|third|fourth",
+            // LOWER_CAMEL_CASE: Ignore the json name
+            "/camel, customJsonNameQueryParam=third&queryParam=fourth, first|second||fourth",
+            // LOWER_CAMEL_CASE: Ignore the original fields
+            "/camel, json_name_query_param=third&query_param=fourth, first|second||",
+            // ORIGINAL, JSON_NAME: Use the original fields
+            "/jsonNameOrOriginal, json_name_query_param=third&query_param=fourth, first|second|third|fourth",
+            // ORIGINAL, JSON_NAME: Use both the original fields and json names
+            "/jsonNameOrOriginal, customJsonNameQueryParam=third&query_param=fourth, first|second|third|fourth",
+            // ORIGINAL, JSON_NAME: Ignore the camel case
+            "/jsonNameOrOriginal, customJsonNameQueryParam=third&queryParam=fourth, first|second|third|",
+            // ORIGINAL, JSON_NAME: Ignore the camel case
+            "/jsonNameOrOriginal, jsonNameQueryParam=third&query_param=fourth, first|second||fourth",
+            // LOWER_CAMEL_CASE, JSON_NAME: Use the camel case
+            "/jsonNameOrCamel, jsonNameQueryParam=third&queryParam=fourth, first|second|third|fourth",
+            // LOWER_CAMEL_CASE, JSON_NAME: Use both the camel case and json names
+            "/jsonNameOrCamel, customJsonNameQueryParam=third&queryParam=fourth, first|second|third|fourth",
+            // LOWER_CAMEL_CASE, JSON_NAME: Ignore the original fields
+            "/jsonNameOrCamel, customJsonNameQueryParam=third&query_param=fourth, first|second|third|",
+            // LOWER_CAMEL_CASE, JSON_NAME: Ignore the original fields
+            "/jsonNameOrCamel, json_name_query_param=third&queryParam=fourth, first|second||fourth",
+            // ALL: Use the camel case
+            "/all, jsonNameQueryParam=third&queryParam=fourth, first|second|third|fourth",
+            // ALL: Use both the camel case and json names
+            "/all, customJsonNameQueryParam=third&queryParam=fourth, first|second|third|fourth",
+            // ALL: Use both the original fields and the camel case
+            "/all, customJsonNameQueryParam=third&query_param=fourth, first|second|third|fourth",
+            // ALL: Use the original fields
+            "/all, json_name_query_param=third&query_param=fourth, first|second|third|fourth",
+    })
+    @ParameterizedTest
+    void testOriginalField(String prefix, String query, String expected) {
         final BlockingWebClient client = server.blockingWebClient();
-        final AggregatedHttpResponse response = client.get("/v1/hello/first/second?query_param=third");
+        final AggregatedHttpResponse response = client.get(prefix + "/v1/hello/first/second?" + query);
         assertThat(response.status()).isEqualTo(HttpStatus.OK);
         assertThatJson(response.contentUtf8())
-                .isEqualTo("{\"message\":\"first + second + third\"}");
+                .isEqualTo("{\"message\": \"" + expected + "\"}");
     }
 
     private static final class JsonNameTestService extends JsonNameTestServiceGrpc.JsonNameTestServiceImplBase {
         @Override
         public void hello(JsonNameRequest request, StreamObserver<JsonNameReply> responseObserver) {
-            final String message = request.getJsonNameInput() + " + " +
-                                   request.toBuilder().getOriginalName() + " + " +
+            final String message = request.getJsonNamePathVariable() + '|' +
+                                   request.getPathVariable() + '|' +
+                                   request.getJsonNameQueryParam() + '|' +
                                    request.getQueryParam();
             final JsonNameReply reply = JsonNameReply.newBuilder()
                                                      .setMessage(message)
