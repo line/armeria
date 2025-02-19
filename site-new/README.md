4fc19f83ef999e457df0f50490ed883ce0296f1c
Hanee Park <haneepark@linecorp.com>
2025-02-19 13:27:31 +0900

    Initialize a Docusaurus project for the new Armeria site (#6098)
    Motivation:
    
    We're preparing for the migration of the Armeria website to reduce
    maintenance costs. The new website is built with Docusaurus, which
    offers features and components specifically designed for documentation.
    This approach alleviates the need for us to develop and maintain the
    website on our own.
    
    Modifications:
    
    - Initialize a Docusaurus project
    - Configure some initial settings
    - Set up eslint and prettier
    - Set up antd and less
    
    Result:
    
    - The new project is ready for development.
    - Note that it has some peer dependency issues, which are expected to be
    resolved in the future Docusaurus releases (e.g.
    https://github.com/facebook/docusaurus/issues/10817)

---
+++ site-new/README.md
@@ -0,0 +1,43 @@
+# Armeria website
+
+> WARNING: This new project is now under construction, and will replace the current Gatsby project.
+
+This directory contains the source code for the official Armeria website, built with Docusaurus.
+
+### Installation
+
+```
+$ npm install
+```
+
+### Local Development
+
+```
+$ npm run start
+```
+
+This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
+
+### Build
+
+```
+$ npm run build
+```
+
+This command generates static content into the `build` directory and can be served using any static contents hosting service.
+
+### Deployment
+
+Using SSH:
+
+```
+$ USE_SSH=true npm run deploy
+```
+
+Not using SSH:
+
+```
+$ GIT_USER=<Your GitHub username> yarn deploy
+```
+
+If you are using GitHub pages for hosting, this command is a convenient way to build the website and push to the `gh-pages` branch.
---
+++ site-new/src/pages/index.tsx
@@ -0,0 +1,48 @@
+import React, { type ReactNode } from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.less';
+
+const HomepageHeader = () => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Home: React.FC = (): ReactNode => {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />"
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+};
+
+export default Home;
