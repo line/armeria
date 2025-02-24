7191d155a2261ea009d360a1deb1fa26c6a44a73
Hanee Park <haneepark@linecorp.com>
2025-02-24 15:36:20 +0900

    Handle method signatures and options in api links

--- site-new/src/components/api-link.tsx
+++ site-new/src/components/api-link.tsx
@@ -8,7 +8,22 @@ interface ApiLinkProps {
 }
 
 const ApiLink: React.FC<ApiLinkProps> = (props) => {
-  const simpleName = props.name;
+  // Decode escaped strings such as &lt;init&gt; that represents a constructor.
+  let simpleName = decodeURIComponent(props.name);
+  const lastDotIdx = simpleName.lastIndexOf('.');
+  if (lastDotIdx >= 0) {
+    simpleName = simpleName.substring(lastDotIdx + 1);
+  }
+
+  let showParams = false;
+  let href = props.href;
+  if (href) {
+    const optionIndex = href.lastIndexOf('?');
+    if (optionIndex > 0) {
+      showParams = href.substring(optionIndex + 1) === 'full';
+      href = href.substring(0, optionIndex);
+    }
+  }
 
   let suffix = '';
   if (props.plural) {
@@ -19,10 +34,23 @@ const ApiLink: React.FC<ApiLinkProps> = (props) => {
     }
   }
 
+  let title = '';
+  if (simpleName.indexOf('#') > 0) {
+    const replaced = simpleName.replace('#', '.');
+    title = replaced;
+    if (showParams) {
+      simpleName = replaced;
+    } else {
+      simpleName = replaced.replace(/ *\([^)]*\)*/, '()');
+    }
+  } else {
+    title = simpleName;
+  }
+
   return (
     <code>
-      <a href={props.href}>
-        {props.name}
+      <a href={href} title={title}>
+        {simpleName}
         <span className={styles.suffix}>{suffix}</span>
       </a>
     </code>
--- site-new/src/remark/remark-api-link.js
+++ site-new/src/remark/remark-api-link.js
@@ -6,16 +6,34 @@ const plugin = (options) => {
   const transformer = (markdownAST) => {
     visit(markdownAST, 'link', (node) => {
       const typeInput = node.url;
-
       if (typeInput !== 'type' && typeInput !== 'typeplural') {
         return;
       }
 
-      const typeName = node.children[0].value;
-      const href = apiIndex[typeName];
+      const typeNameInput = node.children[0].value;
+      let optionIndex = typeNameInput.lastIndexOf('?');
+      let option;
+      if (optionIndex < 0) {
+        optionIndex = typeNameInput.length;
+      } else {
+        option = typeNameInput.substring(optionIndex);
+      }
+      // Exclude option string from typeName
+      let typeName = typeNameInput.substring(0, optionIndex);
+      // Decode escaped string such as &lt;init&gt; that represents a constructor.
+      typeName = decodeURIComponent(typeName);
+      const href = typeName.startsWith('@')
+        ? apiIndex[typeName.substring(1)]
+        : apiIndex[typeName];
       if (href) {
         // eslint-disable-next-line no-param-reassign
+        node.children[0].value = typeName;
+        // eslint-disable-next-line no-param-reassign
         node.url = `${typeInput}://${href}`;
+        if (option) {
+          // eslint-disable-next-line no-param-reassign
+          node.url += option;
+        }
       }
     });
   };
