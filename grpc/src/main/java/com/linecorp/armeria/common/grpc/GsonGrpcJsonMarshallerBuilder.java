97720ee31b4152e36303d2506ff1550f8866c289
Andrei Alikov <andrei.alikov@gmail.com>
2025-03-09 20:24:27 +0900

    fixes customizer logic in the GsonGrpcJsonMarshallerBuilder
    JsonFormat.Parser and JsonFormat.Printer classes are immutable and
    calling any method in customizer creates new object.
    Because of that result of the customer is ignored so we need a way
    to use new object created in the customizer.
    Replaces Consumer<T> with the Function<T, T> customizer which
    returns customized object.

--- grpc/src/main/java/com/linecorp/armeria/common/grpc/GsonGrpcJsonMarshallerBuilder.java
+++ grpc/src/main/java/com/linecorp/armeria/common/grpc/GsonGrpcJsonMarshallerBuilder.java
@@ -18,7 +18,7 @@ package com.linecorp.armeria.common.grpc;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
@@ -32,23 +32,24 @@ import com.linecorp.armeria.common.annotation.Nullable;
 public final class GsonGrpcJsonMarshallerBuilder {
 
     @Nullable
-    private Consumer<JsonFormat.Parser> jsonParserCustomizer;
+    private Function<JsonFormat.Parser, JsonFormat.Parser> jsonParserCustomizer;
 
     @Nullable
-    private Consumer<JsonFormat.Printer> jsonPrinterCustomizer;
+    private Function<JsonFormat.Printer, JsonFormat.Printer> jsonPrinterCustomizer;
 
     GsonGrpcJsonMarshallerBuilder() {}
 
     /**
-     * Adds a {@link Consumer} that can customize the {@link JsonFormat.Parser}
+     * Adds a {@link Function} that returns customized the {@link JsonFormat.Parser}
      * used when deserializing a JSON payload into a {@link Message}.
      */
     public GsonGrpcJsonMarshallerBuilder jsonParserCustomizer(
-            Consumer<? super JsonFormat.Parser> jsonParserCustomizer) {
+            Function<? super JsonFormat.Parser, JsonFormat.Parser> jsonParserCustomizer) {
         requireNonNull(jsonParserCustomizer, "jsonParserCustomizer");
         if (this.jsonParserCustomizer == null) {
             @SuppressWarnings("unchecked")
-            final Consumer<JsonFormat.Parser> cast = (Consumer<JsonFormat.Parser>) jsonParserCustomizer;
+            final Function<JsonFormat.Parser, JsonFormat.Parser> cast =
+                    (Function<JsonFormat.Parser, JsonFormat.Parser>) jsonParserCustomizer;
             this.jsonParserCustomizer = cast;
         } else {
             this.jsonParserCustomizer = this.jsonParserCustomizer.andThen(jsonParserCustomizer);
@@ -57,15 +58,16 @@ public final class GsonGrpcJsonMarshallerBuilder {
     }
 
     /**
-     * Adds a {@link Consumer} that can customize the {@link JsonFormat.Printer}
+     * Adds a {@link Function} that returns customized the {@link JsonFormat.Printer}
      * used when serializing a {@link Message} into a JSON payload.
      */
     public GsonGrpcJsonMarshallerBuilder jsonPrinterCustomizer(
-            Consumer<? super JsonFormat.Printer> jsonPrinterCustomizer) {
+            Function<? super JsonFormat.Printer, JsonFormat.Printer> jsonPrinterCustomizer) {
         requireNonNull(jsonPrinterCustomizer, "jsonPrinterCustomizer");
         if (this.jsonPrinterCustomizer == null) {
             @SuppressWarnings("unchecked")
-            final Consumer<JsonFormat.Printer> cast = (Consumer<JsonFormat.Printer>) jsonPrinterCustomizer;
+            final Function<JsonFormat.Printer, JsonFormat.Printer> cast =
+                    (Function<JsonFormat.Printer, JsonFormat.Printer>) jsonPrinterCustomizer;
             this.jsonPrinterCustomizer = cast;
         } else {
             this.jsonPrinterCustomizer = this.jsonPrinterCustomizer.andThen(jsonPrinterCustomizer);
@@ -77,14 +79,14 @@ public final class GsonGrpcJsonMarshallerBuilder {
      * Returns a newly-created {@link GrpcJsonMarshaller}.
      */
     public GrpcJsonMarshaller build() {
-        final JsonFormat.Printer printer = JsonFormat.printer().omittingInsignificantWhitespace();
+        JsonFormat.Printer printer = JsonFormat.printer().omittingInsignificantWhitespace();
         if (jsonPrinterCustomizer != null) {
-            jsonPrinterCustomizer.accept(printer);
+            printer = jsonPrinterCustomizer.apply(printer);
         }
 
-        final JsonFormat.Parser parser = JsonFormat.parser().ignoringUnknownFields();
+        JsonFormat.Parser parser = JsonFormat.parser().ignoringUnknownFields();
         if (jsonParserCustomizer != null) {
-            jsonParserCustomizer.accept(parser);
+            parser = jsonParserCustomizer.apply(parser);
         }
         return new GsonGrpcJsonMarshaller(printer, parser);
     }
