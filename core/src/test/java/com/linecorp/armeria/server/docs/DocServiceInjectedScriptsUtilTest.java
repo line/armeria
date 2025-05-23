c25ee87ea5458cc4cda47a46f51563f8a1f79971
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-05-23 12:07:21 +0200

    feat: add improvements from maintainers

--- core/src/test/java/com/linecorp/armeria/server/docs/DocServiceInjectedScriptsUtilTest.java
+++ core/src/test/java/com/linecorp/armeria/server/docs/DocServiceInjectedScriptsUtilTest.java
@@ -1,7 +1,7 @@
 /*
- *  Copyright 2025 LINE Corporation
+ *  Copyright 2025 LY Corporation
  *
- *  LINE Corporation licenses this file to you under the Apache License,
+ *  LY Corporation licenses this file to you under the Apache License,
  *  version 2.0 (the "License"); you may not use this file except in compliance
  *  with the License. You may obtain a copy of the License at:
  *
@@ -18,71 +18,106 @@ package com.linecorp.armeria.server.docs;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DocServiceInjectedScriptsUtilTest {
 
-    @Test
-    void withTitleBackground_givenValidColor_returnsScriptWithColor() {
-
-        final String color = "#ff0089";
-        final String absentColor = "#ff9dc3";
+    @ParameterizedTest
+    @ValueSource(strings = { "#ff0089", "#ff9dc3", "#3a3"})
+    void withTitleBackground_givenValidColor_returnsScriptWithColor(String color) {
 
         final String result = DocServiceInjectedScriptsUtil.withTitleBackground(color);
 
-        assertThat(result).contains(color).doesNotContain(absentColor);
+        assertThat(result).isNotBlank().contains(color);
     }
 
-    @Test
-    void withTitleBackground_givenInvalidColor_throwsException() {
-
-        final String color = "#1234567";
+    @ParameterizedTest
+    @ValueSource(strings = { "#1234567", "#ABCDEFA", "#7654321"})
+    void withTitleBackground_givenTooLongColor_throwsException(String color) {
 
         assertThatThrownBy(() -> DocServiceInjectedScriptsUtil.withGotoBackground(color))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("length exceeds");
     }
 
-    @Test
-    void withGotoBackground_givenValidColor_returnsScriptWithColor() {
+    @ParameterizedTest
+    @ValueSource(strings = { "#12345Z", "#ZABCDE", "#@12345"})
+    void withTitleBackground_givenInvalidColor_throwsException(String color) {
 
-        final String color = "#ff9dc3";
-        final String absentColor = "#ff0089";
+        assertThatThrownBy(() -> DocServiceInjectedScriptsUtil.withGotoBackground(color))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("not in hex format");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "#ff0089", "#ff9dc3", "#3a3"})
+    void withGotoBackground_givenValidColor_returnsScriptWithColor(String color) {
 
         final String result = DocServiceInjectedScriptsUtil.withGotoBackground(color);
 
-        assertThat(result).contains(color).doesNotContain(absentColor);
+        assertThat(result).isNotBlank().contains(color);
     }
 
-    @Test
-    void withFavicon_givenValidUrl_returnsScriptWithUrl() {
+    @ParameterizedTest
+    @MethodSource("getStreamOfValidUri")
+    void withFavicon_givenValidUri_returnsScriptWithUri(String uri, String expectedUri) {
 
-        final String url = "https://armeria.dev/static/icon.png";
-        final String absentUrl = "https://line.com/static/icon.png";
+        final String result = DocServiceInjectedScriptsUtil.withFavicon(uri);
 
-        final String result = DocServiceInjectedScriptsUtil.withFavicon(url);
-
-        assertThat(result).contains(url).doesNotContain(absentUrl);
+        assertThat(result).contains(expectedUri).doesNotContain(uri);
     }
 
-    @Test
-    void withFavicon_givenInvalidUrl_throwsException() {
+    private static Stream<Arguments> getStreamOfValidUri() {
+        return Stream.of(
+                Arguments.of("https://armeria.dev/static/icon.png", "https:\\/\\/armeria.dev\\/static\\/icon.png"),
+                Arguments.of("/images/icon.svg", "\\/images\\/icon.svg")
+        );
+    }
 
-        final String url = "https://armeria.dev/static/icon.js";
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "javascript://armeria.dev/static/icon.svg",
+            "data://line.com/static/icon.svg",
+            "https://\\evil.corp/image/icon.svg"
+    })
+    void withFavicon_givenInvalidUri_throwsException(String uri) {
 
-        assertThatThrownBy(() -> DocServiceInjectedScriptsUtil.withFavicon(url))
+        assertThatThrownBy(() -> DocServiceInjectedScriptsUtil.withFavicon(uri))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("uri invalid");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "https://armeria.dev/static/icon.js",
+            "https://line.com/static/icon.zip",
+            "https://evil.corp/image/icon.jpeg"
+    })
+    void withFavicon_givenBadImageExtension_throwsException(String uri) {
+
+        assertThatThrownBy(() -> DocServiceInjectedScriptsUtil.withFavicon(uri))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("extension not allowed");
     }
 
-    @Test
-    void withFavicon_givenEvilUrl_returnsScriptWithUrl() {
+    @ParameterizedTest
+    @MethodSource("getStreamOfEvilUri")
+    void withFavicon_givenEvilUri_returnsScriptWithCleanUri(String uri, String expectedUri) {
 
-        final String url = "https://line.com/static/ico\\n';mal\"ici;ous\n\r.png";
-        final String expectedUrl = "https://line.com/static/ico\\\\n\\'mal\\\"icious.png";
+        final String result = DocServiceInjectedScriptsUtil.withFavicon(uri);
 
-        final String result = DocServiceInjectedScriptsUtil.withFavicon(url);
+        assertThat(result).contains(expectedUri).doesNotContain(uri);
+    }
 
-        assertThat(result).contains(expectedUrl).doesNotContain(url);
+    private static Stream<Arguments> getStreamOfEvilUri() {
+        return Stream.of(
+                Arguments.of("https://line.cóm&/static=/'icon.png", "https:\\/\\/line.c\\u00F3m\\u0026\\/static\\u003D\\/\\'icon.png"),
+                Arguments.of("';alert(1);//.ico", "\\';alert(1);\\/\\/.ico")
+        );
     }
 }
