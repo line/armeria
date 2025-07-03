8c78e0b78e97081091b26e55c8e8ee5f36d74a2d
minwoox <songmw725@gmail.com>
2025-07-03 12:21:21 +0900

    WIP

--- testing-internal/src/main/java/com/linecorp/armeria/internal/testing/InternalTestingBlockHoundIntegration.java
+++ testing-internal/src/main/java/com/linecorp/armeria/internal/testing/InternalTestingBlockHoundIntegration.java
@@ -76,6 +76,12 @@ public final class InternalTestingBlockHoundIntegration implements BlockHoundInt
         builder.allowBlockingCallsInside("com.linecorp.armeria.client.ClientFactory", "ofDefault");
         builder.allowBlockingCallsInside("io.envoyproxy.controlplane.cache.SimpleCache", "createWatch");
         builder.allowBlockingCallsInside("io.grpc.netty.shaded.io.netty.util.Version", "identify");
+        builder.allowBlockingCallsInside(
+                "io.fabric8.kubernetes.client.server.mock.WatchEventsListener", "onClosed");
+        builder.allowBlockingCallsInside(
+                "io.fabric8.kubernetes.client.server.mock.WatchEventsListener", "onFailure");
+        builder.allowBlockingCallsInside(
+                "io.fabric8.mockwebserver.internal.WebSocketSession", "onOpen");
 
         // prints the exception which makes it easier to debug issues
         builder.blockingMethodCallback(this::writeBlockingMethod);
