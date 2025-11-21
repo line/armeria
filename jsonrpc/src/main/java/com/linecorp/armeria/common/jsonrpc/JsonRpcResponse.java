3230db82d42ec96d23c1dd86f62c2de6f0cd6267
Ikhun Um <ikhun.um@linecorp.com>
2025-11-21 12:55:35 +0900

    Introduce JSON-RPC service (#6504)
    This PR contains JSON-RPC service implementations. It was previously
    reviewed in:
    - #6289 
    - #6487
    
    In addition, the JSON-RPC code is split into the `armeria-jsonrpc`
    module in 7c3f556a9930b433706de6a681564b58c6ce194e
    
    Co-authored-by: Eunho Ko <eunho.dev@gmail.com>
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    * **New Features**
    * Added comprehensive JSON-RPC 2.0 protocol support with
    request/response handling, error responses, and customizable method
    handlers.
    * Added streaming support for JSON-RPC responses via Server-Sent Events.
    * Added custom HTTP status predicate overloads for JSON deserialization
    to control success/failure logic.
    * Added pluggable exception handlers and status mapping for JSON-RPC
    services.
    
    * **Bug Fixes / Refactoring**
      * Removed nullable annotation from HttpEntity interface.
      * Added missing @Override annotation to StreamWriter.
    
    <sub>✏️ Tip: You can customize this high-level summary in your review
    settings.</sub>
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    ---------
    
    Co-authored-by: Eunho Ko <eunho.dev@gmail.com>

---
+++ jsonrpc/src/main/java/com/linecorp/armeria/common/jsonrpc/JsonRpcResponse.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2025 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.linecorp.armeria.common.jsonrpc;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.linecorp.armeria.common.jsonrpc.JsonRpcMessageUtil.objectMapper;
+import static java.util.Objects.requireNonNull;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.linecorp.armeria.common.annotation.Nullable;
+import com.linecorp.armeria.common.annotation.UnstableApi;
+import com.linecorp.armeria.server.jsonrpc.JsonRpcParseException;
+import com.linecorp.armeria.server.jsonrpc.JsonRpcService;
+
+/**
+ * A JSON-RPC response.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@UnstableApi
+public interface JsonRpcResponse extends JsonRpcMessage {
+
+    /**
+     * Creates a new instance with the specified {@code id} and {@code result}.
+     * Note that the {@code id} can be of type {@link String}, {@link Long}, {@link Integer}.
+     */
+    static JsonRpcResponse ofSuccess(Object id, Object result) {
+        requireNonNull(id, "id");
+        requireNonNull(result, "result");
+        checkArgument(!(result instanceof JsonRpcError),
+                      "result.class: %s (expected: not JsonRpcError)", result.getClass());
+        return new DefaultJsonRpcResponse(id, result, null);
+    }
+
+    /**
+     * Creates a new instance with {@code result}.
+     * The {@code id} field is automatically filled if this response is sent via {@link JsonRpcService}.
+     */
+    static JsonRpcResponse ofSuccess(Object result) {
+        requireNonNull(result, "result");
+        return new DefaultJsonRpcResponse(null, result, null);
+    }
+
+    /**
+     * Creates a new instance with the specified {@code id} and {@code result}.
+     * Note that the {@code id} can be of type {@link String}, {@link Long}, {@link Integer}.
+     */
+    static JsonRpcResponse ofFailure(Object id, JsonRpcError error) {
+        requireNonNull(id, "id");
+        requireNonNull(error, "error");
+        return new DefaultJsonRpcResponse(id, null, error);
+    }
+
+    /**
+     * Creates a new instance with the specified {@link JsonRpcError}.
+     * The {@code id} field is automatically filled if this response is sent via {@link JsonRpcService}.
+     */
+    static JsonRpcResponse ofFailure(JsonRpcError error) {
+        requireNonNull(error);
+        return new DefaultJsonRpcResponse(null, null, error);
+    }
+
+    /**
+     * Parses the specified {@link JsonNode} into a {@link JsonRpcResponse}.
+     */
+    @JsonCreator
+    static JsonRpcResponse fromJson(JsonNode node) {
+        requireNonNull(node, "node");
+        try {
+            return objectMapper.treeToValue(node, DefaultJsonRpcResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new JsonRpcParseException(e);
+        }
+    }
+
+    /**
+     * Parses the specified JSON string into a {@link JsonRpcResponse}.
+     */
+    static JsonRpcResponse fromJson(String json) {
+        requireNonNull(json, "json");
+        try {
+            return objectMapper.readValue(json, DefaultJsonRpcResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new JsonRpcParseException(e);
+        }
+    }
+
+    /**
+     * Returns a new {@link JsonRpcStreamableResponse} to stream multiple JSON-RPC messages.
+     */
+    static JsonRpcStreamableResponse streaming() {
+        return new DefaultJsonRpcStreamableResponse();
+    }
+
+    /**
+     * Returns the JSON-RPC id.
+     */
+    @Nullable
+    @JsonProperty
+    Object id();
+
+    /**
+     * Returns a new {@link JsonRpcResponse} with the specified {@code id}.
+     * If the current {@link #id()} is equal to the specified {@code id}, this instance is returned.
+     */
+    JsonRpcResponse withId(Object id);
+
+    /**
+     * Returns the JSON-RPC result.
+     */
+    @Nullable
+    @JsonProperty
+    Object result();
+
+    /**
+     * Returns {@code true} if this response has a result.
+     */
+    @JsonIgnore
+    default boolean hasResult() {
+        return result() != null;
+    }
+
+    /**
+     * Returns {@code true} if this response indicates a successful response.
+     */
+    @JsonIgnore
+    default boolean isSuccess() {
+        return hasResult();
+    }
+
+    /**
+     * Returns the JSON-RPC error.
+     */
+    @Nullable
+    @JsonProperty
+    JsonRpcError error();
+
+    /**
+     * Returns {@code true} if this response has an error.
+     */
+    @JsonIgnore
+    default boolean hasError() {
+        return error() != null;
+    }
+}
---
+++ jsonrpc/src/test/java/com/linecorp/armeria/server/jsonrpc/JsonRpcHandlerTest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2025 LY Corporation
+ *
+ * LY Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.linecorp.armeria.server.jsonrpc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import com.google.common.collect.ImmutableList;
+
+import com.linecorp.armeria.client.BlockingWebClient;
+import com.linecorp.armeria.common.AggregatedHttpResponse;
+import com.linecorp.armeria.common.HttpStatus;
+import com.linecorp.armeria.common.ResponseEntity;
+import com.linecorp.armeria.common.jsonrpc.JsonRpcError;
+import com.linecorp.armeria.common.jsonrpc.JsonRpcMessage;
+import com.linecorp.armeria.common.jsonrpc.JsonRpcNotification;
+import com.linecorp.armeria.common.jsonrpc.JsonRpcRequest;
+import com.linecorp.armeria.common.jsonrpc.JsonRpcResponse;
+import com.linecorp.armeria.common.util.UnmodifiableFuture;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.testing.junit5.server.ServerExtension;
+
+class JsonRpcHandlerTest {
+
+    @RegisterExtension
+    static ServerExtension server = new ServerExtension() {
+        @Override
+        protected void configure(ServerBuilder sb) {
+            final JsonRpcService jsonRpcService =
+                    JsonRpcService.builder()
+                            .methodHandler("echo", (ctx, req) ->
+                                    UnmodifiableFuture.completedFuture(
+                                            JsonRpcResponse.ofSuccess(req.id(), req.params())))
+                                  .build();
+            sb.service("/json-rpc", jsonRpcService);
+        }
+    };
+
+    private BlockingWebClient client;
+
+    @BeforeEach
+    void setUp() {
+        client = server.blockingWebClient();
+    }
+
+    @Test
+    void testEcho() {
+        final JsonRpcRequest req = JsonRpcRequest.of(1, "echo", ImmutableList.of(1, 2));
+
+        final ResponseEntity<JsonRpcResponse> response = execute(req);
+        assertThat(response.status()).isEqualTo(HttpStatus.OK);
+        final JsonRpcResponse rpcResponse = response.content();
+        assertThat(rpcResponse.id()).isEqualTo(1);
+        assertThat(rpcResponse.isSuccess()).isTrue();
+        assertThat((List<Integer>) rpcResponse.result()).containsExactly(1, 2);
+    }
+
+    @Test
+    void handleUnknownMethod() {
+        final JsonRpcRequest req = JsonRpcRequest.of(123, "Unknown");
+
+        final ResponseEntity<JsonRpcResponse> response = execute(req);
+        assertThat(response.status()).isEqualTo(HttpStatus.BAD_REQUEST);
+        final JsonRpcResponse rpcResponse = response.content();
+        assertThat(rpcResponse.id()).isEqualTo(123);
+        assertThat(rpcResponse.error().code()).isEqualTo(JsonRpcError.METHOD_NOT_FOUND.code());
+    }
+
+    @Test
+    void handleNotification() {
+        final JsonRpcNotification req = JsonRpcNotification.of("Unknown");
+        final AggregatedHttpResponse response = client.prepare()
+                                                      .post("/json-rpc")
+                                                      .contentJson(req)
+                                                      .execute();
+        assertThat(response.status()).isEqualTo(HttpStatus.ACCEPTED);
+        assertThat(response.content().isEmpty()).isTrue();
+    }
+
+    @Test
+    void handleRpcResponseInput() {
+        // MCP allows a response as an input, and it should be accepted as a notification.
+        final JsonRpcResponse req = JsonRpcResponse.ofSuccess("Input");
+        final AggregatedHttpResponse response = client.prepare()
+                                                      .post("/json-rpc")
+                                                      .contentJson(req)
+                                                      .execute();
+        assertThat(response.status()).isEqualTo(HttpStatus.ACCEPTED);
+        assertThat(response.content().isEmpty()).isTrue();
+    }
+
+    private ResponseEntity<JsonRpcResponse> execute(JsonRpcMessage request) {
+        return client.prepare()
+                     .post("/json-rpc")
+                     .contentJson(request)
+                     .asJson(JsonRpcResponse.class, status -> true)
+                     .execute();
+    }
+}
