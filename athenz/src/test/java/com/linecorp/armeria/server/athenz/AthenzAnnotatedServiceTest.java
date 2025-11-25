6ae06e4dce2f2ee205abcba82bb7d974467ade70
Ikhun Um <ih.pert@gmail.com>
2025-11-25 17:30:39 +0900

    revert stale code

--- athenz/src/test/java/com/linecorp/armeria/server/athenz/AthenzAnnotatedServiceTest.java
+++ athenz/src/test/java/com/linecorp/armeria/server/athenz/AthenzAnnotatedServiceTest.java
@@ -23,7 +23,6 @@ import static com.linecorp.armeria.server.athenz.AthenzDocker.USER_ROLE;
 import static net.javacrumbs.jsonunit.fluent.JsonFluentAssert.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.awaitility.Awaitility.await;
 
 import java.util.List;
 
@@ -43,22 +42,14 @@ import com.linecorp.armeria.common.DependencyInjector;
 import com.linecorp.armeria.common.HttpStatus;
 import com.linecorp.armeria.common.athenz.AccessDeniedException;
 import com.linecorp.armeria.common.athenz.TokenType;
-import com.linecorp.armeria.common.metric.MeterIdPrefix;
-import com.linecorp.armeria.common.metric.MeterIdPrefixFunction;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.ServerListener;
 import com.linecorp.armeria.server.annotation.Get;
 import com.linecorp.armeria.server.annotation.ProducesJson;
 import com.linecorp.armeria.testing.junit5.server.ServerExtension;
 
-import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.Timer;
-import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-
 @EnabledIfDockerAvailable
 class AthenzAnnotatedServiceTest {
-
-    private static final MeterRegistry meterRegistry = new SimpleMeterRegistry();
 
     @Order(1)
     @RegisterExtension
@@ -73,13 +64,11 @@ class AthenzAnnotatedServiceTest {
             final AthenzServiceDecoratorFactory decoratorFactory =
                     AthenzServiceDecoratorFactory.builder(baseClient)
                                                  .policyConfig(new AthenzPolicyConfig(TEST_DOMAIN_NAME))
-                                                 .meterIdPrefix(new MeterIdPrefix("athenz.service.test"))
                                                  .build();
             sb.annotatedService(new AthenzAnnotatedService());
             final DependencyInjector di = DependencyInjector.ofSingletons(decoratorFactory)
                                                             .orElse(DependencyInjector.ofReflective());
             sb.dependencyInjector(di, true);
-            sb.meterRegistry(meterRegistry);
             sb.serverListener(ServerListener.builder()
                                             .whenStopped(server -> baseClient.close())
                                             .build());
@@ -138,84 +127,6 @@ class AthenzAnnotatedServiceTest {
                         .hasMessage("Failed to obtain an Athenz %s token. " +
                                     "(domain: testing, roles: test_role_admin)",
                                     tokenType.isRoleToken() ? "role" : "access");
-            }
-        }
-    }
-
-    @CsvSource({
-            "foo-service, YAHOO_ROLE_TOKEN, false",
-            "foo-service, ATHENZ_ROLE_TOKEN, false",
-            "foo-service, ACCESS_TOKEN, false",
-            "test-service, YAHOO_ROLE_TOKEN, true",
-            "test-service, ATHENZ_ROLE_TOKEN, true",
-            "test-service, ACCESS_TOKEN, true"
-    })
-    @ParameterizedTest
-    void testMetrics(String serviceName, TokenType tokenType, boolean shouldSucceed) {
-        final Timer serviceAllowedMeter = meterRegistry.get("athenz.service.test.token.authorization")
-                                                       .tag("result", "allowed")
-                                                       .tag("resource", "secrets")
-                                                       .tag("action", "obtain")
-                                                       .timer();
-
-        final Timer serviceDeniedMeter = meterRegistry.get("athenz.service.test.token.authorization")
-                                                      .tag("result", "denied")
-                                                      .tag("resources", "secrets")
-                                                      .tag("action", "obtain")
-                                                      .timer();
-        final int numAllowed = (int) serviceAllowedMeter.count();
-        final int numDenied = (int) serviceDeniedMeter.count();
-
-        try (ZtsBaseClient ztsBaseClient = athenzExtension.newZtsBaseClient(serviceName, cb -> {
-            cb.enableMetrics(meterRegistry, MeterIdPrefixFunction.ofDefault("athenz.zts.client.test"));
-        })) {
-            final BlockingWebClient client =
-                    WebClient.builder(server.httpUri())
-                             .decorator(AthenzClient.builder(ztsBaseClient)
-                                                    .domainName(TEST_DOMAIN_NAME)
-                                                    .roleNames(ADMIN_ROLE)
-                                                    .tokenType(tokenType)
-                                                    .meterIdPrefix(new MeterIdPrefix("athenz.client.test"))
-                                                    .newDecorator())
-                             .build()
-                             .blocking();
-
-            if (shouldSucceed) {
-                final AggregatedHttpResponse response = client.get("/secrets");
-                assertThat(response.status()).isEqualTo(HttpStatus.OK);
-                assertThatJson(response.contentUtf8()).isEqualTo(ImmutableList.of("Armeria", "Athenz"));
-                await().untilAsserted(() -> {
-                    assertThat(meterRegistry.get("athenz.client.test.token.fetch")
-                                            .tag("result", "success")
-                                            .tag("domain", TEST_DOMAIN_NAME)
-                                            .tag("roles", ADMIN_ROLE)
-                                            .tag("type", tokenType.name())
-                                            .timer()
-                                            .count())
-                            .isEqualTo(1);
-                    assertThat(meterRegistry.get("athenz.client.test.token.fetch")
-                                            .tag("result", "failure")
-                                            .tag("domain", TEST_DOMAIN_NAME)
-                                            .tag("roles", ADMIN_ROLE)
-                                            .tag("type", tokenType.name())
-                                            .timer()
-                                            .count())
-                            .isZero();
-
-                    assertThat(serviceAllowedMeter.count() - numAllowed)
-                            .isEqualTo(1);
-                    assertThat(serviceDeniedMeter.count() - numDenied)
-                            .isZero();
-                });
-            } else {
-                assertThatThrownBy(() -> client.get("/secrets"))
-                        .isInstanceOf(AccessDeniedException.class)
-                        .hasMessage("Failed to obtain an Athenz %s token. " +
-                                    "(domain: testing, roles: test_role_admin)",
-                                    tokenType.isRoleToken() ? "role" : "access");
-
-                await().untilAsserted(() -> {
-                });
             }
         }
     }
