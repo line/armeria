7ebb413dcda3e0eb0d57d74e6f020fdb18b8e383
Park Hanee[ 박하늬 ] <haneepark@linecorp.com>
2025-12-02 18:31:59 +0900

    Apply a custom 404 page

---
+++ site-new/src/theme/NotFound/Content/index.tsx
@@ -0,0 +1,26 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import type {Props} from '@theme/NotFound/Content';
+import Logo from '@site/src/components/logo';
+
+import styles from './index.module.css';
+
+export default function NotFoundContent({className}: Props): ReactNode {
+  return (
+    <main className={clsx('container', 'margin-vert--xl', styles.wrapper, className)}>
+      <div className={styles.main}>
+        4
+        <Logo
+          className={styles.zero}
+          width="auto"
+          notext
+          primaryColor="var(--ifm-font-color-base)"
+          secondaryColor="var(--ifm-color-emphasis-500)"
+          tertiaryColor="transparent"
+          label="0"
+        />
+        4
+      </div>
+    </main>
+  );
+}
