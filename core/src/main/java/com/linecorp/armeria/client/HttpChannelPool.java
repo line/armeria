207063366264ee6529199f5e4f3c306a5f499035
jrhee17 <guins_j@guins.org>
2025-12-26 16:45:19 +0900

    fix a bad merge

--- core/src/main/java/com/linecorp/armeria/client/HttpChannelPool.java
+++ core/src/main/java/com/linecorp/armeria/client/HttpChannelPool.java
@@ -58,10 +58,8 @@ import com.linecorp.armeria.common.util.AsyncCloseableSupport;
 import com.linecorp.armeria.common.util.DomainSocketAddress;
 import com.linecorp.armeria.internal.client.HttpSession;
 import com.linecorp.armeria.internal.client.PooledChannel;
-import com.linecorp.armeria.internal.common.SslContextFactory;
-import com.linecorp.armeria.internal.common.util.ChannelUtil;
-import com.linecorp.armeria.internal.common.ClientSslContextFactory;
 import com.linecorp.armeria.internal.common.ConnectionEventListener;
+import com.linecorp.armeria.internal.common.SslContextFactory;
 import com.linecorp.armeria.internal.common.util.TemporaryThreadLocals;
 
 import io.netty.bootstrap.Bootstrap;
